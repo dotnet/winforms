251bff0c6010745a02373c4da122ef9e26801e55
Zachary Danz <zsd4yr@virginia.edu>
2018-11-15 10:56:29 -0800

    Revert "Revert "Merge branch 'dev/shgu/winforms_feature' into dev/zadanz/more_unit_tests""
    This reverts commit 3e2209d571765beb1ba29ef218f353015d96026b.

---
+++ src/System.Windows.Forms/tests/unittests/ErrorProvider.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+using Moq;
+using Xunit;
+
+namespace System.Windows.Forms.Tests
+{
+    public class ErrorProviderTests
+    {
+        [Fact]
+        public void Constructor()
+        {
+            // act
+            var ep = new ErrorProvider();
+
+            // assert
+            Assert.NotNull(ep);
+            Assert.NotNull(ep.Icon);
+        }
+        
+        [Fact]
+        public void ConstructorContainerControl()
+        {
+            // arrange
+            var parent = new ContainerControl();
+
+            // act
+            var ep = new ErrorProvider(parent);
+
+            // assert
+            Assert.NotNull(ep);
+            Assert.NotNull(ep.Icon);
+        }
+        
+        [Fact]
+        public void ConstructorIContainer()
+        {
+            // arrange
+            IContainer nullContainer = null;
+            var mockContainer = new Mock<IContainer>(MockBehavior.Strict);
+            mockContainer.Setup(x => x.Add(It.IsAny<ErrorProvider>())).Verifiable();
+
+            // act & assert
+            var ex = Assert.Throws<ArgumentNullException>(() => new ErrorProvider(nullContainer));
+            Assert.Equal("container", ex.ParamName);
+
+            var ep = new ErrorProvider(mockContainer.Object);
+            Assert.NotNull(ep);
+            mockContainer.Verify(x => x.Add(ep));
+        }
+    }
+}
---
+++ src/System.Windows.Forms/tests/unittests/NotifyIcon.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Moq;
+using System.ComponentModel;
+using Xunit;
+
+namespace System.Windows.Forms.Tests
+{
+    public class NotifyIconTests
+    {
+        [Fact]
+        public void Constructor()
+        {
+            // act
+            var icon = new NotifyIcon();
+
+            // assert
+            Assert.NotNull(icon);
+        }
+
+        [Fact]
+        public void ConstructorIContainer()
+        {
+            // arrange
+            IContainer nullContainer = null;
+            var mockContainer = new Mock<IContainer>(MockBehavior.Strict);
+            mockContainer.Setup(x => x.Add(It.IsAny<NotifyIcon>())).Verifiable();
+
+            // act & assert
+            var ex = Assert.Throws<ArgumentNullException>(() => new NotifyIcon(nullContainer));
+            Assert.Equal("container", ex.ParamName);
+
+            var icon = new NotifyIcon(mockContainer.Object);
+            Assert.NotNull(icon);
+            mockContainer.Verify(x => x.Add(icon));
+        }
+    }
+}
