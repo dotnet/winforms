ea3ddb50a31642801e46749e9eabaa5f6bab21c2
Zachary Danz <zsd4yr@users.noreply.github.com>
2018-12-03 15:29:52 -0800

    Update src/System.Windows.Forms/tests/UnitTests/ApplicationTests.cs
    Co-Authored-By: Shyam-Gupta <shgu@microsoft.com>

--- src/System.Windows.Forms/tests/UnitTests/ApplicationTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/ApplicationTests.cs
@@ -5,7 +5,7 @@ namespace System.Windows.Forms.Tests
     public class ApplicationTests
     {
         [Fact]
-        public void EnsureVisualStylesAreEnabled()
+        public void Application_EnsureVisualStylesAreEnabled()
         {
             Application.EnableVisualStyles();
             Assert.True(Application.UseVisualStyles, "New Visual Styles will not be applied on Winforms app. This is a high priority bug and must be looked into");            
