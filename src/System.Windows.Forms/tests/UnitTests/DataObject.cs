fe2bd39141f271bc30e77995029ceafc3aafd915
raffaeler <raffaeler@vevy.com>
2018-12-05 18:07:44 +0100

    Added tests for DataObject.SetData

--- src/System.Windows.Forms/tests/UnitTests/DataObject.cs
+++ src/System.Windows.Forms/tests/UnitTests/DataObject.cs
@@ -66,5 +66,70 @@ namespace System.Windows.Forms.Tests
             Assert.True(dataObject.GetDataPresent(format, autoConvert: false));
             Assert.Equal(input, dataObject.GetData(format, autoConvert: false));
         }
+
+        public static TheoryData<Memory<byte>> AudioData => TestHelper.GetMemoryBytes();
+
+        [Theory]
+        [MemberData(nameof(AudioData))]
+        public void DataObject_SetAudio(Memory<byte> audioBytes)
+        {
+            var inner = new TestDataObject();
+            inner.AutoConvert = true;
+
+            var outer = new DataObject((IDataObject)inner);
+            outer.SetAudio(audioBytes.Span);
+
+            Assert.True(audioBytes.Span.SequenceEqual(inner.GetDataAsStream()));
+            Assert.Equal(DataFormats.WaveAudio, inner.Format);
+            Assert.False(inner.AutoConvert);
+        }
+
+        [Fact]
+        public void DataObject_SetAudioEmpty()
+        {
+            var outer = new DataObject();
+            Assert.Throws<ArgumentException>(() => outer.SetAudio(Array.Empty<byte>().AsSpan()));
+        }
+
+        private class TestDataObject : DataObject
+        {
+            public object Data { get; set; }
+            public string Format { get; set; }
+            public bool AutoConvert { get; set; }
+            public Type FormatType { get; set; }
+
+            public byte[] GetDataAsStream()
+            {
+                if (!(Data is System.IO.Stream stream))
+                    throw new Exception($"The content of {nameof(Data)} was expected to be a Stream");
+
+                var blob = new byte[stream.Length];
+                var read = stream.Read(blob);
+                if (blob.Length != read)
+                    throw new Exception($"The stream length {stream.Length} does not match the content length {read}");
+                return blob;
+            }
+
+            public override void SetData(object data) => Data = data;
+
+            public override void SetData(string format, bool autoConvert, object data)
+            {
+                Format = format;
+                AutoConvert = autoConvert;
+                Data = data;
+            }
+
+            public override void SetData(string format, object data)
+            {
+                Format = format;
+                Data = data;
+            }
+
+            public override void SetData(Type format, object data)
+            {
+                FormatType = format;
+                Data = data;
+            }
+        }
     }
 }
--- src/System.Windows.Forms/tests/UnitTests/TestHelper.cs
+++ src/System.Windows.Forms/tests/UnitTests/TestHelper.cs
@@ -4,11 +4,118 @@
 
 using Xunit;
 using System.Drawing;
+using System.Linq;
 
 namespace System.Windows.Forms.Tests
 {
     public static class TestHelper
     {
+        // helper method to generate theory data from all values of an enum type
+        public static TheoryData<T> GetEnumTheoryData<T>() where T : Enum
+        {
+            var data = new TheoryData<T>();
+            foreach (T item in Enum.GetValues(typeof(T)))
+                data.Add(item);
+            return data;
+        }
+
+        // helper method to generate invalid theory data for an enum type
+        // This method assumes that int.MinValue and int.MaxValue are not in the enum
+        public static TheoryData<T> GetEnumTheoryDataInvalid<T>() where T : Enum
+        {
+            var data = new TheoryData<T>();
+            // This boxing is necessary because you can't cast an int to a generic,
+            // even if the generic is guaranteed to be an enum
+            data.Add((T)(object)int.MinValue);
+            data.Add((T)(object)int.MaxValue);
+            return data;
+        }
+
+        #region Primitives
+
+        // helper method to generate theory data for all values of a boolean
+        public static TheoryData<bool> GetBoolTheoryData()
+        {
+            var data = new TheoryData<bool>();
+            data.Add(true);
+            data.Add(false);
+            return data;
+        }
+
+        // helper method to generate theory data for some values of a int
+        public static TheoryData<int> GetIntTheoryData()
+        {
+            var data = new TheoryData<int>();
+            data.Add(int.MinValue);
+            data.Add(int.MaxValue);
+            data.Add(0);
+            data.Add(1);
+            data.Add(-1);
+            data.Add(int.MaxValue / 2);
+            return data;
+        }
+
+        // helper method to generate theory data for some values of a int
+        public static TheoryData<uint> GetUIntTheoryData()
+        {
+            var data = new TheoryData<uint>();
+            data.Add(int.MaxValue);
+            data.Add(0);
+            data.Add(1);
+            data.Add(int.MaxValue / 2);
+            return data;
+        }
+
+        // helper method to generate theory data for some values of a int
+        public static TheoryData<int> GetNIntTheoryData()
+        {
+            var data = new TheoryData<int>();
+            data.Add(int.MinValue);
+            data.Add(-1);
+            data.Add(int.MinValue / 2);
+            return data;
+        }
+
+        // helper method to generate theory data for some values of a int
+        public static TheoryData<float> GetFloatTheoryData()
+        {
+            var data = new TheoryData<float>();
+            data.Add(float.MaxValue);
+            data.Add(float.MinValue);
+            data.Add(Single.Epsilon);
+            data.Add(Single.Epsilon * -1);
+            data.Add(Single.NegativeInfinity); // not sure about these two
+            data.Add(Single.PositiveInfinity); // 2
+            data.Add(0);
+            data.Add(-1);
+            data.Add(1);
+            data.Add(float.MaxValue / 2);
+            return data;
+        }
+
+        // helper method to generate theory data for some values of a int
+        public static TheoryData<float> GetUFloatTheoryData()
+        {
+            var data = new TheoryData<float>();
+            data.Add(float.MaxValue);
+            data.Add(Single.Epsilon);
+            data.Add(Single.PositiveInfinity); // not sure about this one
+            data.Add(0);
+            data.Add(1);
+            data.Add(float.MaxValue / 2);
+            return data;
+        }
+
+        // helper method to generate theory data for a span of string values
+        public static TheoryData<string> GetStringTheoryData()
+        {
+            var data = new TheoryData<string>();
+            data.Add(string.Empty);
+            data.Add("reasonable");
+            return data;
+        }
+
+        #endregion
 
         #region System.Windows.Forms
 
@@ -38,12 +145,22 @@ namespace System.Windows.Forms.Tests
             var data = new TheoryData<Cursor>();
             foreach (System.Reflection.MethodInfo info in typeof(Cursors).GetMethods())
             {
-                if(info.ReturnType == typeof(Cursor))
+                if (info.ReturnType == typeof(Cursor))
                 {
                     data.Add(info.Invoke(null, null) as Cursor);
                 }
             }
             return data;
+        }
+
+        // get some fake audio data to be used in DataObject tests
+        public static TheoryData<Memory<byte>> GetMemoryBytes()
+        {
+            var audioData = new TheoryData<Memory<byte>>();
+            audioData.Add(new byte[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.AsMemory());
+            audioData.Add(new byte[] { 10, 55, 99, 255 }.AsMemory());
+            audioData.Add(Enumerable.Range(0, 255).Select(a => (byte)a).ToArray());
+            return audioData;
         }
 
         #endregion
@@ -70,7 +187,7 @@ namespace System.Windows.Forms.Tests
             data.Add(Color.Black);
             return data;
         }
-        
+
         // helper method to generate invalid theory data Color value(s)
         public static TheoryData<Color> GetColorTheoryDataInvalid()
         {
@@ -85,7 +202,7 @@ namespace System.Windows.Forms.Tests
         {
             var data = new TheoryData<Size>();
             data.Add(new Size());
-            data.Add(new Size(new Point(1,1)));
+            data.Add(new Size(new Point(1, 1)));
             data.Add(new Size(1, 2));
             data.Add(new Size(int.MaxValue, int.MinValue));
             return data;
