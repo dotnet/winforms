caabe14111a3b44f23e8ae13a4e5fe84bfbed88f
Zachary Danz <zsd4yr@users.noreply.github.com>
2018-12-06 09:02:09 -0800

    fixes for DpiHelper to use TryFindDpiAwarenessContextsEqual, fixes to CommonUnsafeNativeMethods to check for unique isntance of enum before performing work with extern (#70)

---
+++ src/System.Windows.Forms/tests/UnitTests/DpiHelper.cs
@@ -0,0 +1,44 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+using System.Drawing;
+
+namespace System.Windows.Forms.Tests
+{
+    public class DpiHelperTests
+    {
+
+        /// <summary>
+        /// Data for the LogicalToDeviceUnits test
+        /// </summary>
+        public static TheoryData<int> LogicalToDeviceUnitsData =>
+            TestHelper.GetIntTheoryData();
+
+        [Theory]
+        [MemberData(nameof(LogicalToDeviceUnitsData))]
+        public void DpiHelper_LogicalToDeviceUnits(int value)
+        {
+            var expected = Math.Round(value * (DpiHelper.DeviceDpi / DpiHelper.LogicalDpi));
+
+            Assert.Equal(expected, DpiHelper.LogicalToDeviceUnits(value));
+        }
+
+        /// <summary>
+        /// Data for the LogicalToDeviceUnitsSize test
+        /// </summary>
+        public static TheoryData<Size> LogicalToDeviceUnitsSizeData =>
+            TestHelper.GetSizeTheoryData();
+
+        [Theory]
+        [MemberData(nameof(LogicalToDeviceUnitsSizeData))]
+        public void DpiHelper_LogicalToDeviceUnitsSize(Size value)
+        {
+            var expected = new Size ((int)Math.Round(value.Width * (DpiHelper.DeviceDpi / DpiHelper.LogicalDpi)),
+                                     (int)Math.Round(value.Height * (DpiHelper.DeviceDpi / DpiHelper.LogicalDpi)));
+
+            Assert.Equal(expected, DpiHelper.LogicalToDeviceUnits(value));
+        }
+    }
+}
