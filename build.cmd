5467be247bf50823f38126f45206dee12b2187bd
Zachary Danz <zsd4yr@users.noreply.github.com>
2018-12-11 14:43:26 -0800

    I'm your host phillip jakobs and I like my personal space

--- build.cmd
+++ build.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -bl%*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -bl %*"
 exit /b %ErrorLevel%
