1ff38786c33224ab7c5f081bc129a81176459463
Steven Liekens <steven.liekens@gmail.com>
2018-12-18 14:23:03 +0100

    Make WebBrowserUriTypeConverter secure by default
    Previously, a string like `"www.microsoft.com"` would be converted to `"http://www.microsoft.com"`. This PR changes that behavior to use HTTPS by default.

--- src/System.Windows.Forms/src/System/Windows/Forms/WebBrowserUriTypeConverter.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/WebBrowserUriTypeConverter.cs
@@ -12,17 +12,17 @@ namespace System.Windows.Forms
         public override object ConvertFrom(ITypeDescriptorContext context, System.Globalization.CultureInfo culture, object value)
         {
             //The UriTypeConverter gives back a relative Uri for things like "www.microsoft.com".  If 
-            //the Uri is relative, we'll try sticking "http://" on the front to see whether that fixes it up.
+            //the Uri is relative, we'll try sticking "https://" on the front to see whether that fixes it up.
             Uri uri = base.ConvertFrom(context, culture, value) as Uri;
             if (uri != null && !string.IsNullOrEmpty(uri.OriginalString) && !uri.IsAbsoluteUri)
             {
                 try 
                 {
-                    uri = new Uri("http://" + uri.OriginalString.Trim());
+                    uri = new Uri("https://" + uri.OriginalString.Trim());
                 }
                 catch (UriFormatException) 
                 {
-                    //We can't throw "http://" on the front: just return the original (relative) Uri,
+                    //We can't throw "https://" on the front: just return the original (relative) Uri,
                     //which will throw an exception with reasonable text later.
                 }
             }
