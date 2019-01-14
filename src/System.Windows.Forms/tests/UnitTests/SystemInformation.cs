210df125dccac5774edbfef6265a29352f0d423f
tanyaso <tanyaso@microsoft.com>
2019-01-14 12:00:33 -0800

    adding an initial test for SystemInformation class

---
+++ src/System.Windows.Forms/tests/UnitTests/SystemInformation.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+using System.Windows.Forms;
+
+namespace System.Windows.Forms.Tests
+{
+    public class SystemInformationTest
+    {
+        private const int LogicalDpi = 96;
+
+        [Fact]
+        public void SystemInformation_CompareDeviceToLogicalValues()
+        {
+            Assert.Equal(SystemInformation.VerticalScrollBarArrowHeightForDpi(LogicalDpi), SystemInformation.VerticalScrollBarArrowHeight);
+            Assert.Equal(SystemInformation.GetHorizontalScrollBarArrowWidthForDpi(LogicalDpi), SystemInformation.HorizontalScrollBarArrowWidth);
+        }
+    }
+}
