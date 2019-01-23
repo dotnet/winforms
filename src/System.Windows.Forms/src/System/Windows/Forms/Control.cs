9e60eb936b830d073f1207af27450e6c7c58e669
tanyaso <tanyaso@microsoft.com>
2019-01-22 19:09:51 -0800

    removed a duplicate FreeLibrary declaration

--- src/System.Windows.Forms/src/System/Windows/Forms/Control.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Control.cs
@@ -19211,7 +19211,7 @@ example usage
                         }
 
                         if (freeLib) {
-                            UnsafeNativeMethods.FreeLibrary(new HandleRef(null, oleAccAvailable));
+                            CommonUnsafeNativeMethods.FreeLibrary(new HandleRef(null, oleAccAvailable));
                         }
 
                     }
