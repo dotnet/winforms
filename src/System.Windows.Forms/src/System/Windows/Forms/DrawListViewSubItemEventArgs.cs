e516365b58904ac0b27969d33f14608afbb4f505
Hugh Bellamy <hughbellars@gmail.com>
2019-01-23 12:24:56 +0000

    Cleanup ListView event args

--- src/System.Windows.Forms/src/System/Windows/Forms/DrawListViewSubItemEventArgs.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/DrawListViewSubItemEventArgs.cs
@@ -2,197 +2,109 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms 
+using System.Drawing;
+using System.Diagnostics.CodeAnalysis;
+
+namespace System.Windows.Forms
 {
-
-    using System.Diagnostics;
-    using System.Diagnostics.CodeAnalysis;
-    using System;
-    using System.ComponentModel;
-    using System.Drawing;
-    using System.Windows.Forms.Internal;
-    using Microsoft.Win32;
-    using System.Windows.Forms.VisualStyles;
-
-    /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs"]/*' />
     /// <devdoc>
-    ///     This class contains the information a user needs to paint ListView sub-items (Details view only).
+    /// This class contains the information a user needs to paint ListView sub-items (Details view only).
     /// </devdoc>
-    public class DrawListViewSubItemEventArgs : EventArgs 
+    public class DrawListViewSubItemEventArgs : EventArgs
     {
-
-        private readonly Graphics graphics;
-        private readonly Rectangle bounds;
-        private readonly ListViewItem item;
-        private readonly ListViewItem.ListViewSubItem subItem;
-        private readonly int itemIndex;
-        private readonly int columnIndex;
-        private readonly ColumnHeader header;
-        private readonly ListViewItemStates itemState;
-        private bool     drawDefault;
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.DrawListViewSubItemEventArgs"]/*' />
         /// <devdoc>
-        ///     Creates a new DrawListViewSubItemEventArgs with the given parameters.
+        /// Creates a new DrawListViewSubItemEventArgs with the given parameters.
         /// </devdoc>
-        public DrawListViewSubItemEventArgs(Graphics graphics, Rectangle bounds, ListViewItem item, 
-                        ListViewItem.ListViewSubItem subItem, int itemIndex, int columnIndex, 
-                        ColumnHeader header, ListViewItemStates itemState) 
+        public DrawListViewSubItemEventArgs(Graphics graphics, Rectangle bounds, ListViewItem item,
+                        ListViewItem.ListViewSubItem subItem, int itemIndex, int columnIndex,
+                        ColumnHeader header, ListViewItemStates itemState)
         {
-            this.graphics = graphics;
-            this.bounds = bounds;
-            this.item = item;
-            this.subItem = subItem;
-            this.itemIndex = itemIndex;
-            this.columnIndex = columnIndex;
-            this.header = header; 
-            this.itemState = itemState;
+            Graphics = graphics;
+            Bounds = bounds;
+            Item = item;
+            SubItem = subItem;
+            ItemIndex = itemIndex;
+            ColumnIndex = columnIndex;
+            Header = header;
+            ItemState = itemState;
         }
 
-        
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.DrawDefault"]/*' />
         /// <devdoc>
-        ///     Causes the item do be drawn by the system instead of owner drawn.
-        /// </devdoc>        
-        public bool DrawDefault {
-            get  {
-                return drawDefault;
-            }
-            set {
-                drawDefault = value;
-            }
-        }
-        
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.Graphics"]/*' />
-        /// <devdoc>
-        ///     Graphics object with which painting should be done.
+        /// Graphics object with which painting should be done.
         /// </devdoc>
-        public Graphics Graphics 
+        public Graphics Graphics { get; }
+
+        /// <devdoc>
+        /// The rectangle outlining the area in which the painting should be done.
+        /// </devdoc>
+        public Rectangle Bounds { get; }
+
+        /// <devdoc>
+        /// The parent item.
+        /// </devdoc>
+        public ListViewItem Item { get; }
+
+        /// <devdoc>
+        /// The parent item.
+        /// </devdoc>
+        public ListViewItem.ListViewSubItem SubItem { get; }
+
+        /// <devdoc>
+        /// The index in the ListView of the parent item.
+        /// </devdoc>
+        public int ItemIndex { get; }
+
+        /// <devdoc>
+        /// The column index of this sub-item.
+        /// </devdoc>
+        public int ColumnIndex { get; }
+
+        /// <devdoc>
+        /// The header of this sub-item's column
+        /// </devdoc>
+        public ColumnHeader Header { get; }
+
+        /// <devdoc>
+        /// Miscellaneous state information pertaining to the parent item.
+        /// </devdoc>
+        public ListViewItemStates ItemState { get; }
+
+        /// <devdoc>
+        /// Causes the item do be drawn by the system instead of owner drawn.
+        /// </devdoc>
+        public bool DrawDefault { get; set; }
+
+        /// <devdoc>
+        /// Draws the sub-item's background.
+        /// </devdoc>
+        public void DrawBackground()
         {
-            get 
+            Color backColor = (ItemIndex == -1) ? Item.BackColor : SubItem.BackColor;
+            using (var backBrush = new SolidBrush(backColor))
             {
-                return graphics;
+                Graphics.FillRectangle(backBrush, Bounds);
             }
         }
 
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.Bounds"]/*' />
         /// <devdoc>
-        ///     The rectangle outlining the area in which the painting should be done.
+        /// Draws a focus rectangle in the given bounds, if the item has focus.
         /// </devdoc>
-        public Rectangle Bounds 
+        public void DrawFocusRectangle(Rectangle bounds)
         {
-            get 
+            if ((ItemState & ListViewItemStates.Focused) == ListViewItemStates.Focused)
             {
-                return bounds;
+                ControlPaint.DrawFocusRectangle(Graphics, Rectangle.Inflate(bounds, -1, -1), Item.ForeColor, Item.BackColor);
             }
         }
 
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.Item"]/*' />
         /// <devdoc>
-        ///     The parent item. 
+        /// Draws the sub-item's text (overloaded)
         /// </devdoc>
-        public ListViewItem Item 
-        {
-            get 
-            {
-                return item;
-            }
-        }	
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.SubItem"]/*' />
-        /// <devdoc>
-        ///     The parent item. 
-        /// </devdoc>
-        public ListViewItem.ListViewSubItem SubItem 
-        {
-            get 
-            {
-                return subItem;
-            }
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.ItemIndex"]/*' />
-        /// <devdoc>
-        ///     The index in the ListView of the parent item.
-        /// </devdoc>
-        public int ItemIndex 
-        {
-            get 
-            {
-                return itemIndex;
-            }
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.ColumnIndex"]/*' />
-        /// <devdoc>
-        ///     The column index of this sub-item. 
-        /// </devdoc>
-        public int ColumnIndex 
-        {
-            get 
-            {
-                return columnIndex;
-            }
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.Header"]/*' />
-        /// <devdoc>
-        ///    The header of this sub-item's column 
-        /// </devdoc>
-        public ColumnHeader Header 
-        {
-            get 
-            {
-                return header;
-            }
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.ItemState"]/*' />
-        /// <devdoc>
-        ///     Miscellaneous state information pertaining to the parent item.
-        /// </devdoc>
-        public ListViewItemStates ItemState 
-        {
-            get 
-            {
-                return itemState;
-            }
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.DrawBackground"]/*' />
-        /// <devdoc>
-        ///     Draws the sub-item's background.
-        /// </devdoc>
-        public void DrawBackground() 
-        {
-            Color backColor = (itemIndex == -1) ? item.BackColor : subItem.BackColor;
-            using (Brush backBrush = new SolidBrush(backColor)) {
-                Graphics.FillRectangle(backBrush, bounds);
-            }
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.DrawFocusRectangle"]/*' />
-        /// <devdoc>
-        ///     Draws a focus rectangle in the given bounds, if the item has focus.
-        /// </devdoc>
-        public void DrawFocusRectangle(Rectangle bounds) 
-        {
-            if((itemState & ListViewItemStates.Focused) == ListViewItemStates.Focused) 
-            {
-                ControlPaint.DrawFocusRectangle(graphics, Rectangle.Inflate(bounds, -1, -1), item.ForeColor, item.BackColor);
-            }                
-        }
-
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.DrawText"]/*' />
-        /// <devdoc>
-        ///     Draws the sub-item's text (overloaded) 
-        /// </devdoc>
-        public void DrawText() 
+        public void DrawText()
         {
             // Map the ColumnHeader::TextAlign to the TextFormatFlags.
-            HorizontalAlignment hAlign = header.TextAlign;
-            TextFormatFlags flags = (hAlign == HorizontalAlignment.Left) ? TextFormatFlags.Left : 
+            HorizontalAlignment hAlign = Header.TextAlign;
+            TextFormatFlags flags = (hAlign == HorizontalAlignment.Left) ? TextFormatFlags.Left :
                                                    ((hAlign == HorizontalAlignment.Center) ? TextFormatFlags.HorizontalCenter :
                                                    TextFormatFlags.Right);
             flags |= TextFormatFlags.WordEllipsis;
@@ -200,23 +112,19 @@ namespace System.Windows.Forms
             DrawText(flags);
         }
 
-        /// <include file='doc\DrawListViewSubItemEventArgs.uex' path='docs/doc[@for="DrawListViewSubItemEventArgs.DrawText1"]/*' />
         /// <devdoc>
-        ///     Draws the sub-item's text (overloaded) - takes a TextFormatFlags argument.
+        /// Draws the sub-item's text (overloaded) - takes a TextFormatFlags argument.
         /// </devdoc>
-        [
-            SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters") // We want to measure the size of blank space.
-                                                                                                        // So we don't have to localize it.
-        ]
-        public void DrawText(TextFormatFlags flags) 
+        [SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters")] // We want to measure the size of blank spaces o we don't have to localize it.
+        public void DrawText(TextFormatFlags flags)
         {
-            string text  = (itemIndex == -1) ? item.Text      : subItem.Text;
-            Font   font  = (itemIndex == -1) ? item.Font      : subItem.Font;
-            Color  color = (itemIndex == -1) ? item.ForeColor : subItem.ForeColor;
+            string text  = (ItemIndex == -1) ? Item.Text      : SubItem.Text;
+            Font   font  = (ItemIndex == -1) ? Item.Font      : SubItem.Font;
+            Color  color = (ItemIndex == -1) ? Item.ForeColor : SubItem.ForeColor;
             int padding = TextRenderer.MeasureText(" ", font).Width;
-            Rectangle newBounds = Rectangle.Inflate(bounds, -padding, 0);
+            Rectangle newBounds = Rectangle.Inflate(Bounds, -padding, 0);
 
-            TextRenderer.DrawText(graphics, text, font, newBounds, color, flags);
+            TextRenderer.DrawText(Graphics, text, font, newBounds, color, flags);
         }
     }
 }
--- src/System.Windows.Forms/src/System/Windows/Forms/SearchForVirtualItemEventArgs.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/SearchForVirtualItemEventArgs.cs
@@ -2,90 +2,37 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.Drawing;
 
-namespace System.Windows.Forms {
-    /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs"]/*' />
-    public class SearchForVirtualItemEventArgs : EventArgs {
-        private bool isTextSearch;
-        private bool isPrefixSearch;
-        private bool includeSubItemsInSearch;
-        private string text;
-        private Point startingPoint;
-        private SearchDirectionHint direction;
-        private int startIndex;
-        private int index = -1;
-
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.SearchForVirtualItemEventArgs"]/*' />
-        public SearchForVirtualItemEventArgs(bool isTextSearch, bool isPrefixSearch, bool includeSubItemsInSearch, string text, Point startingPoint, SearchDirectionHint direction, int startIndex) {
-            this.isTextSearch = isTextSearch;
-            this.isPrefixSearch = isPrefixSearch;
-            this.includeSubItemsInSearch = includeSubItemsInSearch;
-            this.text = text;
-            this.startingPoint = startingPoint;
-            this.direction = direction;
-            this.startIndex = startIndex;
+namespace System.Windows.Forms
+{
+    public class SearchForVirtualItemEventArgs : EventArgs
+    {
+        public SearchForVirtualItemEventArgs(bool isTextSearch, bool isPrefixSearch, bool includeSubItemsInSearch, string text, Point startingPoint, SearchDirectionHint direction, int startIndex)
+        {
+            IsTextSearch = isTextSearch;
+            IsPrefixSearch = isPrefixSearch;
+            IncludeSubItemsInSearch = includeSubItemsInSearch;
+            Text = text;
+            StartingPoint = startingPoint;
+            Direction = direction;
+            StartIndex = startIndex;
         }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.IsTextSearch"]/*' />
-        public bool IsTextSearch {
-            get  {
-                return isTextSearch;
-            }
-        }
+        public bool IsTextSearch { get; }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.IncludeSubItemsInSearch"]/*' />
-        public bool IncludeSubItemsInSearch {
-            get {
-                return includeSubItemsInSearch;
-            }
-        }
+        public bool IsPrefixSearch { get; }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.Index"]/*' />
-        public int Index {
-            get  {
-                return this.index;
-            }
-            set
-            {
-                this.index = value;
-            }
-        }
+        public bool IncludeSubItemsInSearch { get; }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.IsPrefixSearch"]/*' />
-        public bool IsPrefixSearch {
-            get  {
-                return isPrefixSearch;
-            }
-        }
+        public int Index { get; set; } = -1;
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.Text"]/*' />
-        public string Text{
-            get  {
-                return text;
-            }
-        }
+        public string Text{ get; }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.StartingPoint"]/*' />
-        public Point StartingPoint {
-            get  {
-                return startingPoint;
-            }
-        }
+        public Point StartingPoint { get; }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.Direction"]/*' />
-        public SearchDirectionHint Direction {
-            get  {
-                return direction;
-            }
-        }
+        public SearchDirectionHint Direction { get; }
 
-        /// <include file='doc\SearchForVirtualItemEventArgs.uex' path='docs/doc[@for="SearchForVirtualItemEventArgs.StartIndex"]/*' />
-        public int StartIndex {
-            get  {
-                return startIndex;
-            }
-        }
+        public int StartIndex { get; }
     }
 }
