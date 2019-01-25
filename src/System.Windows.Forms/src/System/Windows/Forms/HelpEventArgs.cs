1c257a4639d6f2d5aca218ce36e29f1eef4ade40
Hugh Bellamy <hughbellars@gmail.com>
2019-01-25 18:29:31 +0000

    Cleanup Form event args (#353)
    * Cleanup Form event args
    
    * Fix build

--- src/System.Windows.Forms/src/System/Windows/Forms/HelpEventArgs.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/HelpEventArgs.cs
@@ -2,65 +2,33 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
+using System.Runtime.InteropServices;
 
-
-namespace System.Windows.Forms {
-    using System.Runtime.Remoting;
-    using System.ComponentModel;
-    using System.Diagnostics;
-    using System;
-    using System.Drawing;    
-    using Microsoft.Win32;
-
-
-    /// <include file='doc\HelpEvent.uex' path='docs/doc[@for="HelpEventArgs"]/*' />
+namespace System.Windows.Forms
+{
     /// <devdoc>
-    ///    <para>
-    ///       Provides data for the Control.HelpRequest event.
-    ///    </para>
+    /// Provides data for the Control.HelpRequest event.
     /// </devdoc>
-    [System.Runtime.InteropServices.ComVisible(true)]
-    public class HelpEventArgs : EventArgs {
-        private readonly Point mousePos;
-        private bool           handled = false;
-
-        /// <include file='doc\HelpEvent.uex' path='docs/doc[@for="HelpEventArgs.HelpEventArgs"]/*' />
+    [ComVisible(true)]
+    public class HelpEventArgs : EventArgs
+    {
         /// <devdoc>
-        ///    <para>
-        ///       Initializes a new instance of the <see cref='System.Windows.Forms.HelpEventArgs'/>class.
-        ///    </para>
+        /// Initializes a new instance of the <see cref='System.Windows.Forms.HelpEventArgs'/>class.
         /// </devdoc>
-        public HelpEventArgs(Point mousePos) {
-            this.mousePos = mousePos;
+        public HelpEventArgs(Point mousePos)
+        {
+            MousePos = mousePos;
         }
 
-        /// <include file='doc\HelpEvent.uex' path='docs/doc[@for="HelpEventArgs.MousePos"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       Gets the screen coordinates of the mouse pointer.
-        ///    </para>
+        /// Gets the screen coordinates of the mouse pointer.
         /// </devdoc>
-        public Point MousePos {
-            get {
-                return mousePos;
-            }
-        }
+        public Point MousePos { get; }
 
-        /// <include file='doc\HelpEvent.uex' path='docs/doc[@for="HelpEventArgs.Handled"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       Gets
-        ///       or sets a value indicating
-        ///       whether the Help event was handled.
-        ///    </para>
+        /// Gets or sets a value indicating whether the Help event was handled.
         /// </devdoc>
-        public bool Handled {
-            get {
-                return handled;
-            }
-            set {
-                handled = value;
-            }
-        }
+        public bool Handled { get; set; }
     }
 }
