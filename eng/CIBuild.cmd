11238728ad491f70a09d12ddfe8db4affdc05f25
Zachary Danz <zsd4yr@virginia.edu>
2019-02-04 13:18:01 -0800

    change to use bl flag, move to local

--- eng/CIBuild.cmd
+++ eng/CIBuild.cmd
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0\common\Build.ps1""" -restore -ci %*"
+powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0\Build.ps1""" -restore -ci %*"
