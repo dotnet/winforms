c16e8632084c31894a04e8e1d5e1b39ce44d3d15
Zac White <zacpwhite@users.noreply.github.com>
2019-02-09 08:50:22 -0500

    Removed unnecessary lines
    Remove unnecessary lines leftover from removal of obsolete Resource attributes

--- src/System.Windows.Forms.Design.Editors/src/Misc/UnsafeNativeMethods.cs
+++ src/System.Windows.Forms.Design.Editors/src/Misc/UnsafeNativeMethods.cs
@@ -14,87 +14,67 @@ namespace System.Windows.Forms.Design
     internal class UnsafeNativeMethods
     {
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern int ClientToScreen(HandleRef hWnd, [In] [Out] NativeMethods.POINT pt);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
         public static extern int ScreenToClient(HandleRef hWnd, [In, Out] NativeMethods.POINT pt);
 
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
 
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto)]
         [SuppressMessage("Microsoft.Portability", "CA1901:PInvokeDeclarationsShouldBePortable")]
-        
         public static extern IntPtr SendMessage(IntPtr hwnd, int msg, bool wparam, int lparam);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr GetActiveWindow();
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern int GetMessageTime();
 
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr SetActiveWindow(HandleRef hWnd);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern void NotifyWinEvent(int winEvent, HandleRef hwnd, int objType, int objID);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr SetFocus(HandleRef hWnd);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr GetFocus();
 
         //Can't use IsMdiChild: IsChild's not being used for MDI-specific or Form-specific stuff.
         [SuppressMessage("Microsoft.Usage", "CA2205:UseManagedEquivalentsOfWin32Api")]
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern bool IsChild(HandleRef hWndParent, HandleRef hwnd);
 
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto)]
-        
         public static extern int GetWindowText(HandleRef hWnd, StringBuilder lpString, int nMaxCount);
-
-        [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
         
+        [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
         public static extern int MsgWaitForMultipleObjectsEx(int nCount, IntPtr pHandles, int dwMilliseconds,
             int dwWakeMask, int dwFlags);
 
         [DllImport(ExternDll.Ole32)]
-        
         public static extern int ReadClassStg(HandleRef pStg, [In] [Out] ref Guid pclsid);
 
         // Investigate removing this if the duplicate code in OleDragDropHandler.cs is removed
         [DllImport(ExternDll.Gdi32, SetLastError = true, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr GetStockObject(int nIndex);
 
         [DllImport(ExternDll.Oleacc, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr LresultFromObject(ref Guid refiid, IntPtr wParam, IntPtr pAcc);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, EntryPoint = "BeginPaint", CharSet = CharSet.Auto)]
-        
         public static extern IntPtr BeginPaint(IntPtr hWnd, [In] [Out] ref PAINTSTRUCT lpPaint);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, EntryPoint = "EndPaint", CharSet = CharSet.Auto)]
-        
         public static extern bool EndPaint(IntPtr hWnd, ref PAINTSTRUCT lpPaint);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, EntryPoint = "GetDC", CharSet = CharSet.Auto)]
-        
         private static extern IntPtr IntGetDC(HandleRef hWnd);
-
-        
         
         public static IntPtr GetDC(HandleRef hWnd)
         {
@@ -102,7 +82,6 @@ namespace System.Windows.Forms.Design
         }
 
         [DllImport(ExternDll.User32, ExactSpelling = true, EntryPoint = "ReleaseDC", CharSet = CharSet.Auto)]
-        
         private static extern int IntReleaseDC(HandleRef hWnd, HandleRef hDC);
 
         public static int ReleaseDC(HandleRef hWnd, HandleRef hDC)
@@ -112,19 +91,15 @@ namespace System.Windows.Forms.Design
         }
 
         [DllImport(ExternDll.Gdi32, SetLastError = true, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern int GetDeviceCaps(HandleRef hDC, int nIndex);
 
         [DllImport(ExternDll.Shell32)]
-        
         public static extern IntPtr ExtractIcon(HandleRef hMod, string exeName, int index);
 
         [DllImport(ExternDll.User32)]
-        
         public static extern bool DestroyIcon(HandleRef hIcon);
 
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr SetWindowsHookEx(int hookid, HookProc pfnhook, HandleRef hinst, int threadid);
 
         //GetWindowLong won't work correctly for 64-bit: we should use GetWindowLongPtr instead.  On
@@ -139,12 +114,10 @@ namespace System.Windows.Forms.Design
 
         [SuppressMessage("Microsoft.Portability", "CA1901:PInvokeDeclarationsShouldBePortable")]
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto, EntryPoint = "GetWindowLong")]
-        
         public static extern IntPtr GetWindowLong32(HandleRef hWnd, int nIndex);
 
         [SuppressMessage("Microsoft.Interoperability", "CA1400:PInvokeEntryPointsShouldExist")]
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto, EntryPoint = "GetWindowLongPtr")]
-        
         public static extern IntPtr GetWindowLongPtr64(HandleRef hWnd, int nIndex);
 
         //This is called, and needs to be owned (unmanaged SetWindowLong) vs. parented (managed 
@@ -164,32 +137,25 @@ namespace System.Windows.Forms.Design
 
         [SuppressMessage("Microsoft.Portability", "CA1901:PInvokeDeclarationsShouldBePortable")]
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto, EntryPoint = "SetWindowLong")]
-        
         public static extern IntPtr SetWindowLongPtr32(HandleRef hWnd, int nIndex, HandleRef dwNewLong);
 
         [SuppressMessage("Microsoft.Interoperability", "CA1400:PInvokeEntryPointsShouldExist")]
         [DllImport(ExternDll.User32, CharSet = CharSet.Auto, EntryPoint = "SetWindowLongPtr")]
-        
         public static extern IntPtr SetWindowLongPtr64(HandleRef hWnd, int nIndex, HandleRef dwNewLong);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern bool UnhookWindowsHookEx(HandleRef hhook);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern int GetWindowThreadProcessId(HandleRef hWnd, out int lpdwProcessId);
 
         [DllImport(ExternDll.User32, ExactSpelling = true, CharSet = CharSet.Auto)]
-        
         public static extern IntPtr CallNextHookEx(HandleRef hhook, int code, IntPtr wparam, IntPtr lparam);
 
         [DllImport(ExternDll.Ole32, PreserveSig = false)]
-        
         public static extern ILockBytes CreateILockBytesOnHGlobal(HandleRef hGlobal, bool fDeleteOnRelease);
 
         [DllImport(ExternDll.Ole32, PreserveSig = false)]
-        
         public static extern IStorage StgCreateDocfileOnILockBytes(ILockBytes iLockBytes, int grfMode, int reserved);
 
         [Flags]
@@ -246,22 +212,18 @@ namespace System.Windows.Forms.Design
         public class Shell32
         {
             [DllImport(ExternDll.Shell32)]
-            
             public static extern int SHGetSpecialFolderLocation(IntPtr hwnd, int csidl, ref IntPtr ppidl);
             //SHSTDAPI SHGetSpecialFolderLocation(HWND hwnd, int csidl, LPITEMIDLIST *ppidl);
 
             [DllImport(ExternDll.Shell32, CharSet = CharSet.Auto)]
-            
             public static extern bool SHGetPathFromIDList(IntPtr pidl, IntPtr pszPath);
             //SHSTDAPI_(BOOL) SHGetPathFromIDListW(LPCITEMIDLIST pidl, LPWSTR pszPath);
 
             [DllImport(ExternDll.Shell32, CharSet = CharSet.Auto)]
-            
             public static extern IntPtr SHBrowseForFolder([In] BROWSEINFO lpbi);
             //SHSTDAPI_(LPITEMIDLIST) SHBrowseForFolderW(LPBROWSEINFOW lpbi);
 
             [DllImport(ExternDll.Shell32)]
-            
             public static extern int SHGetMalloc([Out] [MarshalAs(UnmanagedType.LPArray)]
                 IMalloc[] ppMalloc);
 
