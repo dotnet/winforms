d0154d798cb3dfc6b7e0edf9f43a6c76af63a838
Mikhail Lipin <v-milipi@microsoft.com>
2019-02-12 13:40:06 -0800

    Adding basic tests for ComboBoxAccessibleObject, DataGridViewAccessibleObject, PropertyGridAccessibleObject and ToolStripAccessibleObject.

---
+++ src/System.Windows.Forms/tests/UnitTests/PropertyGridAccessibleObject.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Windows.Forms.Tests
+{
+    public class PropertyGridAccessibleObjectTests
+    {
+        [Fact]
+        public void PropertyGridAccessibleObject_Ctor_Default()
+        {
+            const string propertyGridAccessibleName = "Properties";
+
+            PropertyGrid propertyGrid = new PropertyGrid();
+            propertyGrid.AccessibleName = propertyGridAccessibleName;
+
+            var accessibleObject = new PropertyGridAccessibleObject(propertyGrid);
+            Assert.NotNull(accessibleObject.Owner);
+            Assert.Equal(propertyGridAccessibleName, accessibleObject.Name);
+        }
+    }
+}
