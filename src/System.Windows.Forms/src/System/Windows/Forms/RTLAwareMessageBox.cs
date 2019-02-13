29b434cef21f684ac23e0c01198654c14f264004
Judit Varsanyi Rozsa <juvarsan@microsoft.com>
2019-02-13 10:18:57 -0800

    adding collection editor and object selector editor

--- src/System.Windows.Forms/src/System/Windows/Forms/RTLAwareMessageBox.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/RTLAwareMessageBox.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -15,7 +15,7 @@ namespace System.Windows.Forms {
     ///       for this dll have been localized to a RTL language.
     ///    </para>
     /// </devdoc>
-    internal sealed class RTLAwareMessageBox {
+    public sealed class RTLAwareMessageBox {
 
         /// <include file='doc\MessageBox.uex' path='docs/doc[@for="MessageBox.Show6"]/*' />
         /// <devdoc>
--- src/System.Windows.Forms/src/misc/ClientUtils.cs
+++ src/System.Windows.Forms/src/misc/ClientUtils.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -23,7 +23,7 @@
     using System.Diagnostics;
 
     // Miscellaneous utilities
-    static internal class ClientUtils {
+    static public class ClientUtils {
 
 // ExecutionEngineException is obsolete and shouldn't be used (to catch, throw or reference) anymore.
 // Pragma added to prevent converting the "type is obsolete" warning into build error.
