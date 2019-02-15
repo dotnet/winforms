a10780151a3ac49d640f9ff0ed6c02afc76a0379
Judit Varsanyi Rozsa <juvarsan@microsoft.com>
2019-02-14 18:12:10 -0800

    added new classes to ref

--- src/System.Windows.Forms.Design.Editors/ref/System.Windows.Forms.Design.Editors.cs
+++ src/System.Windows.Forms.Design.Editors/ref/System.Windows.Forms.Design.Editors.cs
@@ -1,10 +1,14 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 // ------------------------------------------------------------------------------
 // Changes to this file must follow the http://aka.ms/api-review process.
 // ------------------------------------------------------------------------------
+
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing.Design;
 
 namespace System.Drawing.Design
 {
@@ -145,5 +149,84 @@ namespace System.Windows.Forms.Design
         public ShortcutKeysEditor() { }
         public override object EditValue(System.ComponentModel.ITypeDescriptorContext context, System.IServiceProvider provider, object value) { throw null; }
         public override System.Drawing.Design.UITypeEditorEditStyle GetEditStyle(System.ComponentModel.ITypeDescriptorContext context) { throw null; }
+    }
+}
+namespace System.Windows.Forms.Design.Editors
+{
+    [System.CLSCompliantAttribute(false)]
+    public partial class CollectionEditor : System.Drawing.Design.UITypeEditor
+    {
+        public CollectionEditor() { }
+        public CollectionEditor(Type type) { }
+        protected Type CollectionItemType { get { throw null; } }
+        protected Type CollectionType { get { throw null; } }
+        protected ITypeDescriptorContext Context { get { throw null; } }
+        protected Type[] NewItemTypes { get { throw null; } }
+        protected virtual string HelpTopic { get { throw null; } }
+        protected virtual bool CanRemoveInstance(object value) { throw null; }
+        protected virtual bool CanSelectMultipleInstances() { throw null; }
+        protected virtual CollectionForm CreateCollectionForm() { throw null; }
+        protected virtual object CreateInstance(Type itemType) { throw null; }
+        protected virtual IList GetObjectsFromInstance(object instance) { throw null; }
+        protected virtual string GetDisplayText(object value) { throw null; }
+        protected virtual Type CreateCollectionItemType() { throw null; }
+        protected virtual Type[] CreateNewItemTypes() { throw null; }
+        protected virtual void DestroyInstance(object instance) { throw null; }
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value) { throw null; }
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) { throw null; }
+        protected virtual object[] GetItems(object editValue) { throw null; }
+        protected object GetService(Type serviceType) { throw null; }
+        protected virtual object SetItems(object editValue, object[] value) { throw null; }
+        protected virtual void ShowHelp() { }
+        protected abstract class CollectionForm : Form
+        {
+            public CollectionForm(CollectionEditor editor) { }
+            protected Type CollectionItemType { get { throw null; } }
+            protected Type CollectionType { get { throw null; } }
+            protected ITypeDescriptorContext Context { get { throw null; } }
+            public object EditValue { get { throw null; } set { } }
+            protected object[] Items { get { throw null; } set { } }
+            protected Type[] NewItemTypes { get { throw null; } }
+            protected bool CanRemoveInstance(object value) { throw null; }
+            protected virtual bool CanSelectMultipleInstances() { throw null; }
+            protected object CreateInstance(Type itemType) { throw null; }
+            protected void DestroyInstance(object instance) { }
+            protected virtual void DisplayError(Exception e) { }
+            protected override object GetService(Type serviceType) { throw null; }
+            protected internal virtual DialogResult ShowEditorDialog(IWindowsFormsEditorService edSvc) { throw null; }
+            protected abstract void OnEditValueChanged();
+        }
+    }
+
+    [System.CLSCompliantAttribute(false)]
+    public partial class ObjectSelectorEditor : System.Drawing.Design.UITypeEditor
+    {
+        public ObjectSelectorEditor() { }
+        public ObjectSelectorEditor(bool subObjectSelector) { }
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value) { throw null; }
+        public static void ApplyTreeViewThemeStyles(TreeView treeView) { }
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context) { throw null; }
+        public bool EqualsToValue(object value) { throw null; }
+        protected virtual void FillTreeWithData(Selector selector, ITypeDescriptorContext context, IServiceProvider provider) { }
+        public virtual void SetValue(object value) { }
+        public class Selector : TreeView
+        {
+            public Selector(ObjectSelectorEditor editor) { }
+            public SelectorNode AddNode(string label, object value, SelectorNode parent) { throw null; }
+            public void Clear() { }
+            protected void OnAfterSelect(object sender, TreeViewEventArgs e) { }
+            protected override void OnKeyDown(KeyEventArgs e) { }
+            protected override void OnKeyPress(KeyPressEventArgs e) { }
+            protected override void OnNodeMouseClick(TreeNodeMouseClickEventArgs e) { }
+            public bool SetSelection(object value, TreeNodeCollection nodes) { throw null; }
+            public void Start(IWindowsFormsEditorService edSvc, object value) { }
+            public void Stop() { }
+            protected override void WndProc(ref Message m) { }
+        }
+        public class SelectorNode : TreeNode
+        {
+            public SelectorNode(string label, object value) : base(label) { }
+
+        }
     }
 }
