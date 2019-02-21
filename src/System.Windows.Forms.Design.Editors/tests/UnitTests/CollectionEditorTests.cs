9b141e455d81a2036ac470d3d5db175ad696c593
Judit Varsanyi Rozsa <juvarsan@microsoft.com>
2019-02-21 11:02:57 -0800

    adding collection editor unit tests

---
+++ src/System.Windows.Forms.Design.Editors/tests/UnitTests/CollectionEditorTests.cs
@@ -0,0 +1,64 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using Xunit;
+
+namespace System.Windows.Forms.Design.Editors.System.ComponentModel.Design.Tests
+{
+    public class CollectionEditorTests
+    {
+        [Fact]
+        public void CollectionEditor_Constructor()
+        {
+            var underTest = GetNewEditor();
+            Assert.NotNull(underTest);
+        }
+
+        [Fact]
+        public void CollectionEditor_Getters()
+        {
+            var underTest = GetNewEditor();
+            Assert.Equal(typeof(string), underTest.GetCollectionType());
+            Assert.True(underTest.CanSelectMultiple());
+            Assert.True(underTest.CanRemove("some string"));
+            Assert.True(underTest.CanRemove(1234));
+            Assert.Equal("net.ComponentModel.CollectionEditor", underTest.GetHelpTopic());
+        }
+
+        private TestCollectionEditor GetNewEditor()
+        {
+            return new TestCollectionEditor(typeof(string));
+        }
+
+        private class TestCollectionEditor : CollectionEditor
+        {
+            public TestCollectionEditor(Type type) : base(type)
+            {
+            }
+
+            public Type GetCollectionType()
+            {
+                return base.CollectionType;
+            }
+
+            public bool CanSelectMultiple()
+            {
+                return base.CanSelectMultipleInstances();
+            }
+
+            public bool CanRemove(object value)
+            {
+                return base.CanRemoveInstance(value);
+            }
+
+            public string GetHelpTopic()
+            {
+                return base.HelpTopic;
+            }
+        }
+    }
+}
