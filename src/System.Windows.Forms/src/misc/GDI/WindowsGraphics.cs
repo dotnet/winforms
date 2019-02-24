9596f68aded4c7a1aac627f014fc0c3e999f7ebb
RussKie <russkie@gmail.com>
2019-02-24 15:49:41 +1100

    Fix a stray apostrophe
    Convert the file from UTF-8 to UTF-8-BOM
    
    Fixes #198

--- src/System.Windows.Forms/src/misc/GDI/WindowsGraphics.cs
+++ src/System.Windows.Forms/src/misc/GDI/WindowsGraphics.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -244,7 +244,7 @@ namespace System.Experimental.Gdi
 
         /// <include file='doc\WindowsGraphics.uex' path='docs/doc[@for="WindowsGraphics.Dispose"]/*' />
         // Okay to suppress.
-        //"WindowsGraphics object does not own the Graphics object.  For instance in a control�s Paint event we pass the 
+        //"WindowsGraphics object does not own the Graphics object.  For instance in a control's Paint event we pass the 
         //GraphicsContainer object to TextRenderer, which uses WindowsGraphics; 
         //if the Graphics object is disposed then further painting will be broken."
         [SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed")]
