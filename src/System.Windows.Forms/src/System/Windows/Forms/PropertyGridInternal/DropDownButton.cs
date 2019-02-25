128aa078378c2ad7956c2ddd23cfb5783da23e83
Dmitry Bryukov (Akvelon INC) <25530829+bryukov@users.noreply.github.com>
2019-02-25 09:49:49 -0800

    The focused PropertyGrid button with 3 dots is not following HC standards on HC Black/White theme
    The appropriate color is now used for the icon's dots when the HC Black or HC White theme is enabled

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/DropDownButton.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/DropDownButton.cs
@@ -209,7 +209,7 @@ namespace System.Windows.Forms.PropertyGridInternal {
         }
 
         internal override void DrawImageCore(Graphics graphics, Image image, Rectangle imageBounds, Point imageStart, ButtonBaseAdapter.LayoutData layout) {
-             ControlPaint.DrawImageReplaceColor(graphics, image, imageBounds, Color.Black, Control.ForeColor);
+             ControlPaint.DrawImageReplaceColor(graphics, image, imageBounds, Color.Black, SystemInformation.HighContrast && (this.Control.MouseIsOver || this.Control.Focused) ? SystemColors.HighlightText : Control.ForeColor);
              //ControlPaint.DrawImageColorized(graphics, image, imageBounds , Control.ForeColor);
         } 
     }
