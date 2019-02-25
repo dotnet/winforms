6b963fe9071e7e092e698fc05799b22aa6fedb4c
Sam Harwell <sam@tunnelvisionlabs.com>
2019-02-25 15:13:52 -0600

    Save files as utf-8-bom (#487)
    * Save files as UTF-8 (all files that did not change)
    
    * Save files as UTF-8 (files with changes)

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripPanel.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripPanel.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -834,7 +834,7 @@ namespace System.Windows.Forms {
                     Region rgn = new Region(bounds);  // create region 
                     rgn.Exclude(regionRect);          // exclude the center part
 
-                    // set it into the toolstripdropdown�s region
+                    // set it into the toolstripdropdown’s region
                     this.Region = rgn;                
                 }
                 
