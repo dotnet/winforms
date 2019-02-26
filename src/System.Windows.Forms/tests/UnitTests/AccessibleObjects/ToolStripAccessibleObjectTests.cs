3c05d762625af52385f72cee772cdaa24abfe922
RussKie <russkie@gmail.com>
2019-02-26 19:09:44 +1100

    Fix selected namespaces (tests/System.Windows.Forms)

--- src/System.Windows.Forms/tests/UnitTests/AccessibleObjects/ToolStripAccessibleObjectTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/AccessibleObjects/ToolStripAccessibleObjectTests.cs
@@ -5,7 +5,7 @@
 using System.Collections.Generic;
 using Xunit;
 
-namespace System.Windows.Forms.Tests
+namespace System.Windows.Forms.Tests.AccessibleObjects
 {
     public class ToolStripAccessibleObjectTests
     {
