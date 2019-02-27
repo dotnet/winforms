ea3a2cec28af1e0248460a93c14942078090a2b8
Hugh Bellamy <hughbellars@gmail.com>
2019-02-27 21:54:34 +0000

    Add ListBindingHelper tests (#395)
    * Add ListBindingHelper tests
    
    * More examples

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripSplitButton.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripSplitButton.cs
@@ -562,12 +562,12 @@ namespace System.Windows.Forms {
                     }
                 }
                 if (shouldFireDoubleClick) {
-                    OnButtonDoubleClick(new System.EventArgs());
+                    OnButtonDoubleClick(EventArgs.Empty);
                     // If we actually fired DoubleClick - reset the lastClickTime.
                     lastClickTime = 0;
                 } 
                 else {
-                    OnButtonClick(new System.EventArgs());
+                    OnButtonClick(EventArgs.Empty);
                 }           
             }
 
