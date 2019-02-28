eec953980c44611a26c5ebf414b787db907a0d30
tanyaso <tanyaso@microsoft.com>
2019-02-27 17:24:28 -0800

    FIxed interactive test for CollectionEditor

--- src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/PropertyGrid.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/PropertyGrid.cs
@@ -6,6 +6,10 @@ using System.ComponentModel;
 using System.Drawing.Design;
 using System.Windows.Forms;
 using System.ComponentModel.Design;
+using System.Collections.Generic;
+using System;
+using System.Globalization;
+using System.Text;
 
 namespace WinformsControlsTest
 {
@@ -18,7 +22,7 @@ namespace WinformsControlsTest
         }
     }
 
-    public partial class UserControlWithObjectCollectionEditor : UserControl
+    internal class UserControlWithObjectCollectionEditor : UserControl
     {
         public UserControlWithObjectCollectionEditor()
         {
@@ -28,10 +32,32 @@ namespace WinformsControlsTest
         [Editor(typeof(CollectionEditor), typeof(UITypeEditor))]
         [Browsable(true)]
         [EditorBrowsable(EditorBrowsableState.Always)]
-        public int[] SomeCollection
+        [TypeConverter(typeof(SomeCollectionTypeConverter))]
+        public IList<int> SomeCollection
         {
-            get { return new int[] { 1, 2, 3 }; }
+            get { return new List<int>(new int[] { 1, 2, 3 }); }
             set { }
+        }
+    }
+
+    internal class SomeCollectionTypeConverter : TypeConverter
+    {
+        public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+        {
+            if (destinationType != null && destinationType.IsAssignableFrom(typeof(string)) && value != null && value is IList<int> list)
+            {
+                var result = new StringBuilder("");
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (i != 0)
+                    {
+                        result.Append(", ");
+                    }
+                    result.Append(list[i]);
+                }
+                return result.ToString();
+            }
+            return base.ConvertTo(context, culture, value, destinationType);
         }
     }
 
