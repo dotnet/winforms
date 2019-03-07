060e3caf2b8af2b31910ae0ab32747ac0f1cec7a
Judit Varsanyi Rozsa <juvarsan@microsoft.com>
2019-03-07 13:41:26 -0800

    organized imports order

--- src/System.Windows.Forms.Design/src/System/Drawing/Design/ToolboxItem.cs
+++ src/System.Windows.Forms.Design/src/System/Drawing/Design/ToolboxItem.cs
@@ -2,15 +2,15 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Runtime.Serialization;
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Design;
-using System.Reflection;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Windows.Forms;
 
 namespace System.Drawing.Design
