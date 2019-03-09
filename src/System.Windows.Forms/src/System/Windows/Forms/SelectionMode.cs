d409500081763852a6081fdcfea12f7a6feab7f8
Hugh Bellamy <hughbellars@gmail.com>
2019-03-09 10:25:02 +0000

    Cleanup enums [R-S]

--- src/System.Windows.Forms/src/System/Windows/Forms/SelectionMode.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/SelectionMode.cs
@@ -2,39 +2,33 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms {
+using System.Runtime.InteropServices;
 
-    /// <include file='doc\SelectionMode.uex' path='docs/doc[@for="SelectionMode"]/*' />
-    /// <devdoc>
-    /// </devdoc>
-    [System.Runtime.InteropServices.ComVisible(true)]
-    public enum SelectionMode {
-
-        /// <include file='doc\SelectionMode.uex' path='docs/doc[@for="SelectionMode.None"]/*' />
+namespace System.Windows.Forms
+{
+    [ComVisible(true)]
+    public enum SelectionMode
+    {
         /// <devdoc>
-        ///     indicates that no items can be selected.
+        /// Indicates that no items can be selected.
         /// </devdoc>
-        None             = 0,
+        None = 0,
 
-        /// <include file='doc\SelectionMode.uex' path='docs/doc[@for="SelectionMode.One"]/*' />
         /// <devdoc>
-        ///     indicates that only one item at a time can be selected.
+        /// Indicates that only one item at a time can be selected.
         /// </devdoc>
-        One              = 1,
+        One = 1,
 
-        /// <include file='doc\SelectionMode.uex' path='docs/doc[@for="SelectionMode.MultiSimple"]/*' />
         /// <devdoc>
-        ///     indicates that more than one item at a time can be selected.
+        /// Indicates that more than one item at a time can be selected.
         /// </devdoc>
-        MultiSimple     = 2,
+        MultiSimple = 2,
 
-        /// <include file='doc\SelectionMode.uex' path='docs/doc[@for="SelectionMode.MultiExtended"]/*' />
         /// <devdoc>
-        ///     Indicates that more than one item at a time can be selected, and
-        ///     keyboard combinations, such as SHIFT and CTRL can be used to help
-        ///     in selection.
+        /// Indicates that more than one item at a time can be selected, and
+        /// keyboard combinations, such as SHIFT and CTRL can be used to help
+        /// in selection.
         /// </devdoc>
-        MultiExtended   = 3,
-
+        MultiExtended = 3,
     }
 }
--- src/System.Windows.Forms/src/System/Windows/Forms/SizeType.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/SizeType.cs
@@ -1,13 +1,13 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 namespace System.Windows.Forms
 {
-    public enum StructFormat
+    public enum SizeType
     {
-        Ansi = 1,
-        Unicode = 2,
-        Auto = 3,
+        AutoSize,
+        Absolute,
+        Percent
     }
 }
