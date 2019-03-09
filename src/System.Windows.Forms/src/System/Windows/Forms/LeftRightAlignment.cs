43e79540b62eaf3549b2c0ad81735e2842087117
Hugh Bellamy <hughbellars@gmail.com>
2019-03-09 10:21:44 +0000

    Cleanup enums [L-P]

--- src/System.Windows.Forms/src/System/Windows/Forms/LeftRightAlignment.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/LeftRightAlignment.cs
@@ -2,45 +2,26 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Runtime.InteropServices;
 
-namespace System.Windows.Forms {
-
-    using System.Diagnostics;
-
-    using System;
-    using System.ComponentModel;
-    using System.Drawing;
-    using Microsoft.Win32;
-
-
-    /// <include file='doc\LeftRightAlignment.uex' path='docs/doc[@for="LeftRightAlignment"]/*' />
+namespace System.Windows.Forms
+{
     /// <devdoc>
-    ///    <para>
-    ///       Specifies whether an object or text is aligned to
-    ///       the left or
-    ///       right of a reference point.
-    ///    </para>
+    /// Specifies whether an object or text is aligned to
+    /// the left or
+    /// right of a reference point.
     /// </devdoc>
-    [System.Runtime.InteropServices.ComVisible(true)]
-    public enum LeftRightAlignment {
-
-        /// <include file='doc\LeftRightAlignment.uex' path='docs/doc[@for="LeftRightAlignment.Left"]/*' />
+    [ComVisible(true)]
+    public enum LeftRightAlignment
+    {
         /// <devdoc>
-        ///    <para>
-        ///       The object or
-        ///       text is aligned to the left of the reference
-        ///       point.
-        ///    </para>
+        /// The object or text is aligned to the left of the reference point.
         /// </devdoc>
         Left = 0,
 
-        /// <include file='doc\LeftRightAlignment.uex' path='docs/doc[@for="LeftRightAlignment.Right"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       The object or text is aligned to the right of the reference point.
-        ///    </para>
+        /// The object or text is aligned to the right of the reference point.
         /// </devdoc>
         Right = 1,
-
     }
 }
