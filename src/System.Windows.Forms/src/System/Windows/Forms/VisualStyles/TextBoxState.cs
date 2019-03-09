02354dc88a1a50c415b910b0151cada10416f544
Hugh Bellamy <hughbellars@gmail.com>
2019-03-09 10:28:01 +0000

    Cleanup VisualStyles enums

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/VisualStyles/TextBoxState.cs
@@ -0,0 +1,23 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.MSInternal", "CA905:SystemAndMicrosoftNamespacesRequireApproval", Scope = "namespace", Target = "System.Windows.Forms.VisualStyles")]
+
+namespace System.Windows.Forms.VisualStyles
+{
+    [SuppressMessage("Microsoft.Design", "CA1008:EnumsShouldHaveZeroValue")]
+    [SuppressMessage("Microsoft.Design", "CA1027:MarkEnumsWithFlags")]
+    public enum TextBoxState
+    {
+        Normal = 1,
+        Hot = 2,
+        Selected = 3,
+        Disabled = 4,
+        //Focused = 5, 
+        Readonly = 6,
+        Assist = 7
+    }
+}
