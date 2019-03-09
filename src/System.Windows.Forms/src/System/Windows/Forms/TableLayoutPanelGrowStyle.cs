b61605bb620c2875a2781c45b06d0d548031e0f2
Hugh Bellamy <hughbellars@gmail.com>
2019-03-09 10:27:28 +0000

    Cleanup enums [T-W]

--- src/System.Windows.Forms/src/System/Windows/Forms/TableLayoutPanelGrowStyle.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/TableLayoutPanelGrowStyle.cs
@@ -2,49 +2,30 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms {
-
-    using System.Diagnostics;
-
-    using System;
-    using System.ComponentModel;
-    using System.Drawing;
-    using Microsoft.Win32;
-
-
-    /// <include file='doc\TableLayoutPanelGrowStyle.uex' path='docs/doc[@for="TableLayoutPanelGrowStyle"]/*' />
+namespace System.Windows.Forms
+{
     /// <devdoc>
-    ///    <para>
-    ///       Specifies if a TableLayoutPanel will gain additional rows or columns once its existing cells
-    ///       become full.  If the value is 'None' then the TableLayoutPanel will throw an exception
-    ///       when the TableLayoutPanel is over-filled.
-    ///    </para>
+    /// Specifies if a TableLayoutPanel will gain additional rows or columns once
+    /// its existing cells become full.  If the value is 'None' then the
+    /// TableLayoutPanel will throw an exception when the TableLayoutPanel is
+    /// over-filled.
     /// </devdoc>
-    public enum TableLayoutPanelGrowStyle {
-
-        /// <include file='doc\TableLayoutGrowStyle.uex' path='docs/doc[@for="TableLayoutPanelGrowStyle.FixedSize"]/*' />
+    public enum TableLayoutPanelGrowStyle
+    {
         /// <devdoc>
-        ///    <para>
-        ///       The TableLayoutPanel will not allow additional rows or columns once it is full.
-        ///    </para>
+        /// The TableLayoutPanel will not allow additional rows or columns once
+        /// it is full.
         /// </devdoc>
-        FixedSize       = 0,
+        FixedSize = 0,
 
-        /// <include file='doc\TableLayoutPanelGrowStyle.uex' path='docs/doc[@for="TableLayoutPanelGrowStyle.AddRows"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       The TableLayoutPanel will gain additional rows once it becomes full.
-        ///    </para>
+        /// The TableLayoutPanel will gain additional rows once it becomes full.
         /// </devdoc>
-        AddRows         = 1,
+        AddRows = 1,
 
-        /// <include file='doc\TableLayoutPanelGrowStyle.uex' path='docs/doc[@for="TableLayoutPanelGrowStyle.AddColumns"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       The TableLayoutPanel will gain additional columns once it becomes full.
-        ///    </para>
+        /// The TableLayoutPanel will gain additional columns once it becomes full.
         /// </devdoc>
-        AddColumns      = 2
-
+        AddColumns = 2
     }
 }
