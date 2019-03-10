255f78328932cb802050052cc846b6c957154e48
Hugh Bellamy <hughbellars@gmail.com>
2019-03-10 11:36:27 +0000

    Add GridItem[Collection] tests and cleanup code

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/GridEntryCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/GridEntryCollection.cs
@@ -2,84 +2,71 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+namespace System.Windows.Forms.PropertyGridInternal
+{
+    internal sealed class GridEntryCollection : GridItemCollection
+    {
+        private GridEntry _owner;
 
-namespace System.Windows.Forms.PropertyGridInternal {
-    using System.Runtime.InteropServices;
-
-    using System.Diagnostics;
-
-    using System;
-    using System.Collections;
-    using System.Reflection;
-
-    using System.Drawing.Design;
-    using System.ComponentModel;
-    using System.ComponentModel.Design;
-    using System.Windows.Forms;
-    using System.Windows.Forms.Design;
-    using System.Drawing;
-    using Microsoft.Win32;
-
-    internal class GridEntryCollection : GridItemCollection {
-
-        private GridEntry owner;
-        
-        public GridEntryCollection(GridEntry owner, GridEntry[] entries) : base(entries) {
-            this.owner = owner;
+        public GridEntryCollection(GridEntry owner, GridEntry[] entries) : base(entries)
+        {
+            _owner = owner;
         }
-        
-        public void AddRange(GridEntry[] value) {
-            if (value == null) {
+
+        public void AddRange(GridEntry[] value)
+        {
+            if (value == null)
+            {
                 throw new ArgumentNullException(nameof(value));
             }
-            if (entries != null) {
-                GridEntry[] newArray = new GridEntry[entries.Length + value.Length];
-                entries.CopyTo(newArray, 0);
-                value.CopyTo(newArray, entries.Length);
-                entries = newArray;
+
+            if (_entries != null)
+            {
+                var newArray = new GridEntry[_entries.Length + value.Length];
+                _entries.CopyTo(newArray, 0);
+                value.CopyTo(newArray, _entries.Length);
+                _entries = newArray;
             }
-            else {
-                entries = (GridEntry[])value.Clone();
+            else
+            {
+                _entries = (GridEntry[])value.Clone();
             }
-        }                                       
-                                       
-        public void Clear() {
-            entries = new GridEntry[0];            
-        }
-        
-        public void CopyTo(Array dest, int index) {
-            entries.CopyTo(dest, index);
-        }
-        
-        internal GridEntry GetEntry(int index) {
-            return (GridEntry)entries[index];
         }
 
-         internal int GetEntry(GridEntry child) {
-            return Array.IndexOf(entries, child);   
-        }
+        public void Clear() => _entries = Array.Empty<GridEntry>();
 
-        public void Dispose() {
+        public void CopyTo(Array dest, int index) => _entries.CopyTo(dest, index);
+
+        internal GridEntry GetEntry(int index) => (GridEntry)_entries[index];
+
+        internal int GetEntry(GridEntry child) => Array.IndexOf(_entries, child);
+
+        public void Dispose()
+        {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        protected virtual void Dispose(bool disposing) {
-            if (disposing) {
-                if (owner != null && entries != null) {
-                    for (int i = 0; i < entries.Length; i++) {
-                        if (entries[i] != null) {
-                            ((GridEntry)entries[i]).Dispose();
-                            entries[i] = null;
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_owner != null && _entries != null)
+                {
+                    for (int i = 0; i < _entries.Length; i++)
+                    {
+                        if (_entries[i] != null)
+                        {
+                            ((GridEntry)_entries[i]).Dispose();
+                            _entries[i] = null;
                         }
                     }
-                    entries = new GridEntry[0];
+
+                    _entries = Array.Empty<GridEntry>();
                 }
             }
         }
 
-        ~GridEntryCollection() {
-            Dispose(false);
-        }
+        ~GridEntryCollection() => Dispose(false);
     }
 }
