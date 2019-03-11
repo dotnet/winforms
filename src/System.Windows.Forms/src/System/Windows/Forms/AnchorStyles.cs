64bc53679eb70cff4011204a6c1929a9d0466a99
Hugh Bellamy <hughbellars@gmail.com>
2019-03-11 17:31:35 +0000

    Cleanup enums [A] (#555)
    * Cleanup enums [A]
    
    * PR feedback

--- src/System.Windows.Forms/src/System/Windows/Forms/AnchorStyles.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AnchorStyles.cs
@@ -2,67 +2,41 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms {
+using System.ComponentModel;
+using System.Drawing.Design;
 
-    using System.Diagnostics;
-
-    using System;
-    using System.ComponentModel;
-    using System.Drawing;
-    using System.Drawing.Design;
-    using System.Windows.Forms.Design;
-    using Microsoft.Win32;
-
-
-    /// <include file='doc\AnchorStyles.uex' path='docs/doc[@for="AnchorStyles"]/*' />
+namespace System.Windows.Forms
+{
     /// <devdoc>
-    ///    <para>
-    ///       Specifies how
-    ///       a
-    ///       control anchors to the edges of its container.
-    ///    </para>
+    /// Specifies how a control anchors to the edges of its container.
     /// </devdoc>
-    [
-        Editor("System.Windows.Forms.Design.AnchorEditor, " + AssemblyRef.SystemDesign, typeof(UITypeEditor)),
-    ]
+    [Editor("System.Windows.Forms.Design.AnchorEditor, " + AssemblyRef.SystemDesign, typeof(UITypeEditor))]
     [Flags]
-    public enum AnchorStyles {
+    public enum AnchorStyles
+    {
+        /// <devdoc>
+        /// The control is anchored to the top edge of its container.
+        /// </devdoc>
+        Top = 0x01,
 
-        /// <include file='doc\AnchorStyles.uex' path='docs/doc[@for="AnchorStyles.Top"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       The control is anchored to the top edge of its container.
-        ///    </para>
+        /// The control is anchored to the bottom edge of its container.
         /// </devdoc>
-        Top         = 0x01,
-        /// <include file='doc\AnchorStyles.uex' path='docs/doc[@for="AnchorStyles.Bottom"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       The control is anchored to the bottom edge of its container.
-        ///    </para>
-        /// </devdoc>
-        Bottom      = 0x02,
-        /// <include file='doc\AnchorStyles.uex' path='docs/doc[@for="AnchorStyles.Left"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       The control is anchored to the left edge of its container.
-        ///    </para>
-        /// </devdoc>
-        Left        = 0x04,
-        /// <include file='doc\AnchorStyles.uex' path='docs/doc[@for="AnchorStyles.Right"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       The control is anchored to the right edge of its container.
-        ///    </para>
-        /// </devdoc>
-        Right       = 0x08,
+        Bottom = 0x02,
 
-        /// <include file='doc\AnchorStyles.uex' path='docs/doc[@for="AnchorStyles.None"]/*' />
         /// <devdoc>
-        ///    <para>
-        ///       The control is not anchored to any edges of its container.
-        ///    </para>
+        /// The control is anchored to the left edge of its container.
         /// </devdoc>
-        None            = 0,
+        Left = 0x04,
+
+        /// <devdoc>
+        /// The control is anchored to the right edge of its container.
+        /// </devdoc>
+        Right = 0x08,
+
+        /// <devdoc>
+        /// The control is not anchored to any edges of its container.
+        /// </devdoc>
+        None = 0,
     }
 }
