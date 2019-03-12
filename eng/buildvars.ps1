c0c633181529db783d12a50e24485957f0390829
Zachary Danz <zsd4yr@virginia.edu>
2019-03-12 14:14:36 -0700

    actually path to json

--- eng/buildvars.ps1
+++ eng/buildvars.ps1
@@ -1,5 +1,6 @@
 # Open global.json
-$jsondata = Get-Content -Raw -Path $env:BUILD_SOURCESDIRECTORY | ConvertFrom-Json
+$globaljsonpath = Join-Path $env:BUILD_SOURCESDIRECTORY 'global.json'
+$jsondata = Get-Content -Raw -Path $globaljsonpath | ConvertFrom-Json
 
 # Set DotNetCliVersion to global.json.tools.dotnet
 $dotnetcliver = $jsondata.tools.dotnet
