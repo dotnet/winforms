6c11f3a063d9cde3d34b62561d402825c4ab1806
Hugh Bellamy <hughbellars@gmail.com>
2019-03-12 11:33:25 +0000

    PR feedback

--- src/System.Windows.Forms/src/System/Windows/Forms/Automation/IAutomationLiveRegion.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Automation/IAutomationLiveRegion.cs
@@ -4,11 +4,6 @@
 
 namespace System.Windows.Forms.Automation
 {
-    /// <summary>
-    /// Provides support for UIA live regions.
-    /// The feature is available in applications that are recompiled to target .NET
-    /// Framework 4.7.3 or opt in into this functionality using compatibility switches.
-    /// </summary>
     public interface IAutomationLiveRegion
     {
         /// <summary>
