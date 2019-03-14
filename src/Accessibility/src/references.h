a221608f55b3c8abb2273a8a65db9e5011d9954f
Eric St. John <ericstj@microsoft.com>
2019-03-13 21:30:51 -0700

    Add Accessibility.dll to WinForms
    This is a primary interop assembly for OLEACC.dll.
    
    It's needed because both Winforms and WPF expose public types that depend on the
    COM interop types in OLEACC so they cannot embed the types.

---
+++ src/Accessibility/src/references.h
@@ -0,0 +1,15 @@
+#define CORE_ASSEMBLY "System.Runtime"
+#define INTEROP_ASSEMBLY "System.Runtime.InteropServices"
+#define netcoreapp
+
+// Metadata version: v4.0.30319
+.assembly extern CORE_ASSEMBLY
+{
+  .publickeytoken = (B0 3F 5F 7F 11 D5 0A 3A )                         // .?_....:
+  .ver 4:0:0:0
+}
+.assembly extern INTEROP_ASSEMBLY
+{
+  .publickeytoken = (B0 3F 5F 7F 11 D5 0A 3A )                         // .?_....:
+  .ver 4:0:0:0
+}
