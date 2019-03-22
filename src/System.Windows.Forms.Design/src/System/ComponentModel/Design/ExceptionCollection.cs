e70fe16639bf582db55ef8f2f0061f6bb26b3ad0
Judit Varsanyi Rozsa <jvrozsa@gmail.com>
2019-03-21 20:22:16 -0700

    Port System.ComponentModel.Design.* for #469 (#606)
    * port DesignerActionPropertyItem
    
    * port DesignerActionMethodItem
    
    * port UndoEngine
    
    * adding inheritance service constructor test
    
    * port DesignerSurface
    
    * PR comments
    
    * PR comments
    
    * adding line ending to SR.resx
    
    * fixed xml line endings

---
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/ExceptionCollection.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.Runtime.Serialization;
+using System.Security;
+
+namespace System.ComponentModel.Design
+{
+    [Serializable]
+    public sealed class ExceptionCollection : Exception
+    {
+        readonly ArrayList _exceptions;
+
+        public ExceptionCollection(ArrayList exceptions)
+        {
+            _exceptions = exceptions;
+        }
+
+        /// <summary>
+        /// Need this constructor since Exception implements ISerializable. 
+        /// </summary>
+        private ExceptionCollection(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+            _exceptions = (ArrayList)info.GetValue("exceptions", typeof(ArrayList));
+        }
+
+        public ArrayList Exceptions
+        {
+            get
+            {
+                if (_exceptions != null)
+                {
+                    return (ArrayList)_exceptions.Clone();
+                }
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Need this since Exception implements ISerializable and we have fields to save out.
+        /// </summary>
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (info == null)
+            {
+                throw new ArgumentNullException(nameof(info));
+            }
+
+            info.AddValue("exceptions", _exceptions);
+            base.GetObjectData(info, context);
+        }
+    }
+}
