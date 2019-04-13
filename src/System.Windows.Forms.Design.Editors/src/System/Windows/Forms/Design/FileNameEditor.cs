7c4cccb70f0b957418d177e3792489ef6e644208
Hugh Bellamy <hughbellars@gmail.com>
2019-04-13 10:49:19 +0100

    Cleanup FileNameEditor

--- src/System.Windows.Forms.Design.Editors/src/System/Windows/Forms/Design/FileNameEditor.cs
+++ src/System.Windows.Forms.Design.Editors/src/System/Windows/Forms/Design/FileNameEditor.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
@@ -11,40 +10,41 @@ using System.Drawing.Design;
 namespace System.Windows.Forms.Design
 {
     /// <summary>>
-    ///         Provides an
-    ///         editor for filenames.
+    /// Provides an editor for filenames.
     /// </summary>
     [CLSCompliant(false)]
     public class FileNameEditor : UITypeEditor
     {
-        private OpenFileDialog openFileDialog;
+        private OpenFileDialog _openFileDialog;
 
         /// <summary>
-        ///     Edits the given object value using the editor style provided by
-        ///     GetEditorStyle.  A service provider is provided so that any
-        ///     required editing services can be obtained.
+        /// Edits the given object value using the editor style provided by GetEditorStyle.
+        /// A service provider is provided so that any required editing services can be obtained.
         /// </summary>
-        [SuppressMessage("Microsoft.Security", "CA2123:OverrideLinkDemandsShouldBeIdenticalToBase")]
         public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
         {
             Debug.Assert(provider != null, "No service provider; we cannot edit the value");
             if (provider != null)
             {
-                IWindowsFormsEditorService edSvc =
-                    (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
-
+                IWindowsFormsEditorService edSvc = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
                 Debug.Assert(edSvc != null, "No editor service; we cannot edit the value");
                 if (edSvc != null)
                 {
-                    if (openFileDialog == null)
+                    if (_openFileDialog == null)
                     {
-                        openFileDialog = new OpenFileDialog();
-                        InitializeDialog(openFileDialog);
+                        _openFileDialog = new OpenFileDialog();
+                        InitializeDialog(_openFileDialog);
                     }
 
-                    if (value is string) openFileDialog.FileName = (string)value;
+                    if (value is string stringValue)
+                    {
+                        _openFileDialog.FileName = stringValue;
+                    }
 
-                    if (openFileDialog.ShowDialog() == DialogResult.OK) value = openFileDialog.FileName;
+                    if (_openFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        return _openFileDialog.FileName;
+                    }
                 }
             }
 
@@ -52,20 +52,17 @@ namespace System.Windows.Forms.Design
         }
 
         /// <summary>>
-        ///         Gets the editing style of the Edit method. If the method
-        ///         is not supported, this will return None.
+        /// Gets the editing style of the Edit method.
         /// </summary>
-        [SuppressMessage("Microsoft.Security", "CA2123:OverrideLinkDemandsShouldBeIdenticalToBase")]
-        // everything in this assembly is full trust.
         public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
         {
             return UITypeEditorEditStyle.Modal;
         }
 
         /// <summary>
-        ///     Initializes the open file dialog when it is created.  This gives you
-        ///     an opportunity to configure the dialog as you please.  The default
-        ///     implementation provides a generic file filter and title.
+        /// Initializes the open file dialog when it is created. This gives you an opportunity to
+        /// configure the dialog as you please. The default implementation provides a generic file
+        /// filter and title.
         /// </summary>
         [SuppressMessage("Microsoft.Design", "CA1011:ConsiderPassingBaseTypesAsParameters")]
         protected virtual void InitializeDialog(OpenFileDialog openFileDialog)
