dc70adf88734b723a54f92529f2f60cf461c32ac
Hugh Bellamy <hughbellars@gmail.com>
2019-04-16 17:14:32 +0100

    Cleanup ScrollBar/ScrollProperties (#700)
    * Cleanup ScrollBar/ScrollProperties
    
    * Rename fields to match conventions
    
    * More cleanup

--- src/System.Windows.Forms/src/System/Windows/Forms/HScrollBar.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/HScrollBar.cs
@@ -2,56 +2,33 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms {
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
-    using System.Diagnostics;
-
-    using System;
-    using System.Windows.Forms;
-    using System.Drawing;
-    using Microsoft.Win32;
-    using System.Runtime.InteropServices;
-
-    /// <include file='doc\HScrollBar.uex' path='docs/doc[@for="HScrollBar"]/*' />
-    /// <devdoc>
-    ///    <para>
-    ///       Represents
-    ///       a standard Windows horizontal scroll bar.
-    ///    </para>
-    /// </devdoc>
-    [ComVisible(true),
-     ClassInterface(ClassInterfaceType.AutoDispatch),
-     SRDescription(nameof(SR.DescriptionHScrollBar))
-    ]
-    public class HScrollBar : ScrollBar {
-        
-        /// <include file='doc\HScrollBar.uex' path='docs/doc[@for="HScrollBar.CreateParams"]/*' />
-        /// <internalonly/>
-        /// <devdoc>
-        ///    <para>
-        ///       Returns the parameters needed to create the handle. Inheriting classes
-        ///       can override this to provide extra functionality. They should not,
-        ///       however, forget to call base.getCreateParams() first to get the struct
-        ///       filled up with the basic info.
-        ///    </para>
-        /// </devdoc>
-        protected override CreateParams CreateParams {
-            get {
+namespace System.Windows.Forms
+{
+    /// <summary>
+    /// Represents a standard Windows horizontal scroll bar.
+    /// </summary>
+    [ComVisible(true)]
+    [ClassInterface(ClassInterfaceType.AutoDispatch)]
+    [SRDescription(nameof(SR.DescriptionHScrollBar))]
+    public class HScrollBar : ScrollBar
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
                 CreateParams cp = base.CreateParams;
                 cp.Style |= NativeMethods.SBS_HORZ;
                 return cp;
             }
         }
-        
-        /// <include file='doc\HScrollBar.uex' path='docs/doc[@for="HScrollBar.DefaultSize"]/*' />
-        /// <devdoc>
-        ///     Deriving classes can override this to configure a default size for their control.
-        ///     This is more efficient than setting the size in the control's constructor.
-        /// </devdoc>
-        protected override Size DefaultSize {
-            get {
-                return new Size(80, SystemInformation.HorizontalScrollBarHeight);
-            }
+
+        protected override Size DefaultSize
+        {
+            get => new Size(80, SystemInformation.HorizontalScrollBarHeight);
         }
     }
 }
