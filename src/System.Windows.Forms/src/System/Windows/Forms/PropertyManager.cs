a3e046fa0caa9608e9a94efe54db49d618d41206
Hugh Bellamy <hughbellars@gmail.com>
2019-04-22 19:42:51 +0100

    Cleanup BindingManagerBase (#821)
    * Rename BindingManagerBase
    
    * Cleanup BindingManagerBase
    
    * Rename accessors
    
    * PR feedback
    
    * PR feedback

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyManager.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyManager.cs
@@ -23,7 +23,7 @@ namespace System.Windows.Forms
             OnCurrentChanged(EventArgs.Empty);
         }
 
-        internal override void SetDataSource(object dataSource)
+        private protected override void SetDataSource(object dataSource)
         {
             if (_dataSource != null && !string.IsNullOrEmpty(_propName))
             {
@@ -145,14 +145,14 @@ namespace System.Windows.Forms
             PushData();
 
             onCurrentChangedHandler?.Invoke(this, ea);
-            onCurrentItemChangedHandler?.Invoke(this, ea);
+            _onCurrentItemChangedHandler?.Invoke(this, ea);
         }
 
         internal protected override void OnCurrentItemChanged(EventArgs ea)
         {
             PushData();
 
-            onCurrentItemChangedHandler?.Invoke(this, ea);
+            _onCurrentItemChangedHandler?.Invoke(this, ea);
         }
 
         internal override object DataSource => _dataSource;
