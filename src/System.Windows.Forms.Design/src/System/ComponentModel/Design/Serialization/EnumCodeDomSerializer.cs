7cd5bb6a139c7933376ccc8f96d9c5e31d60ea3e
Zachary Danz <zsd4yr@virginia.edu>
2019-04-23 16:48:45 -0700

    update license headers

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/EnumCodeDomSerializer.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/EnumCodeDomSerializer.cs
@@ -1,12 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-//------------------------------------------------------------------------------
-// <copyright file="EnumCodeDomSerializer.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>                                                                
-//------------------------------------------------------------------------------
-
-/*
- */
 namespace System.ComponentModel.Design.Serialization {
 
     using System;
--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/EventMemberCodeDomSerializer.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/EventMemberCodeDomSerializer.cs
@@ -1,8 +1,7 @@
-//------------------------------------------------------------------------------
-// <copyright file="EventMemberCodeDomSerializer.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>                                                                
-//------------------------------------------------------------------------------
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace System.ComponentModel.Design.Serialization {
 
     using System;
