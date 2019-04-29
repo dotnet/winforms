bac1a1f2a9ea80a2dcb55884878fe8b31c303ca7
Hugh Bellamy <hughbellars@gmail.com>
2019-04-29 20:44:55 +0100

    Cleanup ToolStripItemDesignerAvailabilityAttribute

--- src/System.Windows.Forms/src/System/Windows/Forms/Design/ToolStripItemDesignerAvailabilityAttribute.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Design/ToolStripItemDesignerAvailabilityAttribute.cs
@@ -2,54 +2,43 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms.Design {
 using System.Diagnostics.CodeAnalysis;
 
-       [AttributeUsage(AttributeTargets.Class)]
-       public sealed class ToolStripItemDesignerAvailabilityAttribute : Attribute {
-           private ToolStripItemDesignerAvailability  visibility;
-            [
-                SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes")   // ToolStripDesignAvailabilityAttribute is
-                                                                                                            // actually immutable.
-            ]
-           public static readonly ToolStripItemDesignerAvailabilityAttribute Default = new ToolStripItemDesignerAvailabilityAttribute();
-                  
-           // <devdoc> 
-           // Specifies which ToolStrip types the Item can appear in - ToolStrip,MenuStrip,StatusStrip,ContextMenuStrip
-           // Adding this attribute over a class lets you add to the list of custom items in the ToolStrip design time.
-           // </devdoc>
-           public ToolStripItemDesignerAvailabilityAttribute() {
-                this.visibility = ToolStripItemDesignerAvailability.None;
-           }
-   
-           public ToolStripItemDesignerAvailabilityAttribute(ToolStripItemDesignerAvailability visibility) {
-              this.visibility = visibility;
-           }
-   
-           public ToolStripItemDesignerAvailability ItemAdditionVisibility {
-               get {
-                   return visibility;
-               }
-           }
-           public override bool Equals(object obj) {
-               if (obj == this) {
-                   return true;
-               }
-    
-               ToolStripItemDesignerAvailabilityAttribute other = obj as ToolStripItemDesignerAvailabilityAttribute;
-               return (other != null) && other.ItemAdditionVisibility == this.visibility;
-           }
-    
-           public override int GetHashCode() {
-               return visibility.GetHashCode();
-           }
-    
-           public override bool IsDefaultAttribute() {
-               return (this.Equals(Default));
-           }
-       }
+namespace System.Windows.Forms.Design
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class ToolStripItemDesignerAvailabilityAttribute : Attribute
+    {
+        [SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes", Justification = "This class is immutable")]
+        public static readonly ToolStripItemDesignerAvailabilityAttribute Default = new ToolStripItemDesignerAvailabilityAttribute();
 
+        /// <summary>
+        /// Specifies which ToolStrip types the Item can appear in - ToolStrip,MenuStrip,StatusStrip,ContextMenuStrip
+        /// Adding this attribute over a class lets you add to the list of custom items in the ToolStrip design time.
+        /// </summary>
+        public ToolStripItemDesignerAvailabilityAttribute()
+        {
+        }
 
+        public ToolStripItemDesignerAvailabilityAttribute(ToolStripItemDesignerAvailability visibility)
+        {
+            ItemAdditionVisibility = visibility;
+        }
+
+        public ToolStripItemDesignerAvailability ItemAdditionVisibility { get; } = ToolStripItemDesignerAvailability.None;
+
+        public override bool Equals(object obj)
+        {
+            if (obj == this)
+            {
+                return true;
+            }
+
+            return obj is ToolStripItemDesignerAvailabilityAttribute other && other.ItemAdditionVisibility == ItemAdditionVisibility;
+        }
+
+        public override int GetHashCode() => ItemAdditionVisibility.GetHashCode();
+
+        public override bool IsDefaultAttribute() => Equals(Default);
+    }
 }
-
-
