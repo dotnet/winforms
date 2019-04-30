a65d34380dd46668aaf3ecfe5b7634a95942f2f9
Hugh Bellamy <hughbellars@gmail.com>
2019-04-30 19:48:57 +0100

    Cleanup and add WindowsFormsComponentEditor tests (#901)
    * Rename files
    
    * Cleanup WindowsFormsComponentEditor
    
    * Add WindowsFormsComponentEditor tests
    
    * PR feedback

--- src/System.Windows.Forms/src/System/Windows/Forms/Design/IUIService.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Design/IUIService.cs
@@ -7,77 +7,77 @@ using System.Runtime.InteropServices;
 
 namespace System.Windows.Forms.Design
 {
-    /// <devdoc>
+    /// <summary>
     /// Provides support for interaction with the user interface of the development
     /// environment object that is hosting the designer
-    /// </devdoc>
+    /// </summary>
     [Guid("06A9C74B-5E32-4561-BE73-381B37869F4F")]
     public interface IUIService
     {
-        /// <devdoc>
+        /// <summary>
         /// Gets or sets the collections of styles that are specific to the host's environment
-        /// </devdoc>
+        /// </summary>
         IDictionary Styles { get; }
 
-        /// <devdoc>
+        /// <summary>
         /// Indicates whether the component can display a ComponentDesigner
-        /// </devdoc>
+        /// </summary>
         bool CanShowComponentEditor(object component);
 
-        /// <devdoc>
+        /// <summary>
         /// Gets the window that should be used for dialog parenting
-        /// </devdoc>
+        /// </summary>
         IWin32Window GetDialogOwnerWindow();
 
-        /// <devdoc>
+        /// <summary>
         /// Sets a flag indicating the UI is dirty
-        /// </devdoc>
+        /// </summary>
         void SetUIDirty();
 
-        /// <devdoc>
+        /// <summary>
         /// Attempts to display a ComponentEditor for a component
-        /// </devdoc>
+        /// </summary>
         bool ShowComponentEditor(object component, IWin32Window parent);
 
-        /// <devdoc>
+        /// <summary>
         /// Attempts to display the specified form in a dialog box
-        /// </devdoc>
+        /// </summary>
         DialogResult ShowDialog(Form form);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified error message in a message box
-        /// </devdoc>
+        /// </summary>
         void ShowError(string message);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified exception and its information in a message box
-        /// </devdoc>
+        /// </summary>
         void ShowError(Exception ex);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified exception and its information in a message box
-        /// </devdoc>
+        /// </summary>
         void ShowError(Exception ex, string message);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified message in a message box
-        /// </devdoc>
+        /// </summary>
         void ShowMessage(string message);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified message in a message box with the specified caption
-        /// </devdoc>
+        /// </summary>
         void ShowMessage(string message, string caption);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified message in a message box with the specified caption and
         /// buttons to place on the dialog box
-        /// </devdoc>
+        /// </summary>
         DialogResult ShowMessage(string message, string caption, MessageBoxButtons buttons);
 
-        /// <devdoc>
+        /// <summary>
         /// Displays the specified tool window
-        /// </devdoc>
+        /// </summary>
         bool ShowToolWindow(Guid toolWindow);
     }
 }
