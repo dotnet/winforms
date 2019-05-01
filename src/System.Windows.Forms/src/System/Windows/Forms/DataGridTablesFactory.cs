d40d35223224f897696bd84ca8ae2ea02487fc18
Hugh Bellamy <hughbellars@gmail.com>
2019-05-01 19:26:21 +0100

    Cleanup GridTablesFactory

--- src/System.Windows.Forms/src/System/Windows/Forms/DataGridTablesFactory.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/DataGridTablesFactory.cs
@@ -2,37 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Forms {
-    using System.Text;
-    using System.Runtime.Remoting;
-
-    using System.Diagnostics;
-    using System;
-    using System.Collections;
-    using System.ComponentModel;
-
-    using System.Windows.Forms;
-    using Microsoft.Win32;
-
-    /// <include file='doc\DataGridTablesFactory.uex' path='docs/doc[@for="GridTablesFactory"]/*' />
-    public sealed class GridTablesFactory {
-        // private static DataTableComparer dtComparer = new DataTableComparer();
-
-        // not creatable...
-        //
-        private GridTablesFactory() {
-        }
-
-
-        /// <include file='doc\DataGridTablesFactory.uex' path='docs/doc[@for="GridTablesFactory.CreateGridTables"]/*' />
-        /// <devdoc>
-        ///      Takes a DataView and creates an intelligent mapping of
-        ///      DataView storage types into available DataColumn types.
-        /// </devdoc>
-        public static DataGridTableStyle[]
-            CreateGridTables(DataGridTableStyle gridTable, object dataSource, string dataMember, BindingContext bindingManager)
+namespace System.Windows.Forms
+{
+    public static class GridTablesFactory
+    {
+        /// <summary>
+        /// Takes a DataView and creates an intelligent mapping of DataView storage
+        /// types into available DataColumn types.
+        /// </summary>
+        public static DataGridTableStyle[] CreateGridTables(DataGridTableStyle gridTable, object dataSource, string dataMember, BindingContext bindingManager)
         {
-            return new DataGridTableStyle[] {gridTable};
+            return new DataGridTableStyle[] { gridTable };
         }
     }
 }
