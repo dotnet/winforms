fcd276663138bf5955cc5b551a0f473cefe42bab
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-06-20 17:31:00 +1000

    Add docs and a bit code maintenance

--- build-local.ps1
+++ build-local.ps1
@@ -4,45 +4,32 @@ Param(
   [Parameter(ValueFromRemainingArguments=$true)][String[]]$properties
 )
 
-function Get-SystemDotnetPath {
-    return [string][System.IO.Path]::Combine($env:ProgramFiles, 'dotnet');
-}
-
-
-function Start-ElevatedModeIfRequired {
-    Param(
-      [Parameter(Mandatory=$true, Position=0)]
-      [bool] $IsFullCleanRequested,
-      [Parameter(Mandatory=$true, Position=1)]
-      [string] $LocalSdkLocation,
-      [Parameter(Mandatory=$true, Position=2)]
-      [string] $SystemSdkLocation,
-      [Parameter(Mandatory=$true, Position=2)]
-      [string] $LocalNETCoreAppLocation,
-      [Parameter(Mandatory=$true, Position=3)]
-      [string] $SystemNETCoreAppLocation
-    )
-
-    $isSdkPresent = Check-SdkExists -LocalSdkLocation $LocalSdkLocation -SystemSdkLocation $SystemSdkLocation;
-    $isNETCoreAppPresent = Check-SdkExists -LocalSdkLocation $LocalNETCoreAppLocation -SystemSdkLocation $SystemNETCoreAppLocation;
-
-    # if the SDK is found and not doing a full clean - we're done here
-    if ($isSdkPresent -eq $true -and `
-        $isNETCoreAppPresent -eq $true -and `
-        $IsFullCleanRequested -ne $true) {
-        return;
-    }
-
-
-    # To perform full cleanup we must run the script in an elevated mode
-    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { 
-        $args = if ($IsFullCleanRequested -eq $true) { "-FullClean" } else { '' }
-        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $args" -Verb RunAs; 
-        exit 0
-    }
-}
 
 function Check-SdkExists {
+<#
+.SYNOPSIS
+
+Checks whether a system-wise NETCore SDK and a matching local NETCore SDK installations exist
+
+.DESCRIPTION
+
+Checks whether a system-wise NETCore SDK and a matching local NETCore SDK installations exist
+
+.PARAMETER LocalSdkLocation
+Specifies the expected location of local NETCore SDK (e.g. D:\winforms\.dotnet\sdk\3.0.100-preview5-011568).
+
+.PARAMETER SystemSdkLocation
+Specifies the expected location of local NETCore SDK (e.g. C:\Program Files\dotnet\sdk\3.0.100-preview5-011568).
+
+.INPUTS
+
+None.
+
+.OUTPUTS
+
+None.
+
+#>
     Param(
       [Parameter(Mandatory=$true, Position=0)]
       [string] $LocalSdkLocation,
@@ -68,6 +55,30 @@ function Check-SdkExists {
 }
 
 function Create-SymLink {
+<#
+.SYNOPSIS
+
+Creates a symbolic link from a system-wise NETCore SDK to a local NETCore SDK installation
+
+.DESCRIPTION
+
+Creates a symbolic link from a system-wise NETCore SDK to a local NETCore SDK installation
+
+.PARAMETER LocalSdkLocation
+Specifies the expected location of local NETCore SDK (e.g. D:\winforms\.dotnet\sdk\3.0.100-preview5-011568).
+
+.PARAMETER SystemSdkLocation
+Specifies the expected location of local NETCore SDK (e.g. C:\Program Files\dotnet\sdk\3.0.100-preview5-011568).
+
+.INPUTS
+
+None.
+
+.OUTPUTS
+
+None.
+
+#>
     param (
       [Parameter(Mandatory=$true, Position=0)]
       [string] $LocalSdkLocation,
@@ -94,6 +105,10 @@ function Create-SymLink {
         }
     }
 
+}
+
+function Get-SystemDotnetPath {
+    return [string][System.IO.Path]::Combine($env:ProgramFiles, 'dotnet');
 }
 
 function Invoke-FullCleanup {
@@ -130,6 +145,79 @@ function Invoke-FullCleanup {
     git clean -xfd
     Write-Host "√ Local folder cleaned" -ForegroundColor Green;
 }
+
+function Start-ElevatedModeIfRequired {
+<#
+.SYNOPSIS
+
+Checks whether the script needs to be restarted in the elevated mode
+
+.DESCRIPTION
+
+The elevated mode is required in the following cases:
+- FullClean is requested, or
+- LocalSdkLocation or SystemSdkLocation does not exist, or
+- LocalNETCoreAppLocation or SystemNETCoreAppLocation does not exist.
+
+
+.PARAMETER IsFullCleanRequested
+Specifies whether a full clean up is required.
+
+.PARAMETER LocalSdkLocation
+Specifies the expected location of local NETCore SDK (e.g. D:\winforms\.dotnet\sdk\3.0.100-preview5-011568).
+
+.PARAMETER SystemSdkLocation
+Specifies the expected location of local NETCore SDK (e.g. C:\Program Files\dotnet\sdk\3.0.100-preview5-011568).
+
+.PARAMETER LocalNETCoreAppLocation
+Specifies the expected location of local NETCore SDK (e.g. D:\winforms\.dotnet\shared\Microsoft.NETCore.App\3.0.0-preview7-27819-07).
+
+.PARAMETER SystemNETCoreAppLocation
+Specifies the expected location of local NETCore SDK (e.g. C:\Program Files\dotnet\shared\Microsoft.NETCore.App\3.0.0-preview7-27819-07).
+
+.INPUTS
+
+None.
+
+.OUTPUTS
+
+None.
+
+#>
+
+    Param(
+      [Parameter(Mandatory=$true, Position=0)]
+      [bool] $IsFullCleanRequested,
+      [Parameter(Mandatory=$true, Position=1)]
+      [string] $LocalSdkLocation,
+      [Parameter(Mandatory=$true, Position=2)]
+      [string] $SystemSdkLocation,
+      [Parameter(Mandatory=$true, Position=2)]
+      [string] $LocalNETCoreAppLocation,
+      [Parameter(Mandatory=$true, Position=3)]
+      [string] $SystemNETCoreAppLocation
+    )
+
+    $isSdkPresent = Check-SdkExists -LocalSdkLocation $LocalSdkLocation -SystemSdkLocation $SystemSdkLocation;
+    $isNETCoreAppPresent = Check-SdkExists -LocalSdkLocation $LocalNETCoreAppLocation -SystemSdkLocation $SystemNETCoreAppLocation;
+
+    # if the SDK is found and not doing a full clean - we're done here
+    if ($isSdkPresent -eq $true -and `
+        $isNETCoreAppPresent -eq $true -and `
+        $IsFullCleanRequested -ne $true) {
+        return;
+    }
+
+
+    # To perform full cleanup we must run the script in an elevated mode
+    if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) { 
+        $args = if ($IsFullCleanRequested -eq $true) { "-FullClean" } else { '' }
+        Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $args" -Verb RunAs; 
+        exit 0
+    }
+}
+
+
 
 
 # break on errors
