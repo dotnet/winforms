2b3a4a45ddbefec62ac73e273c12bc50455975af
Jeremy Kuhne <jkuhne@microsoft.com>
2019-07-02 10:44:24 -0700

    Fix COM interop ELEMDESC (#1254)
    ELEMDESC doesn't contain a pointer. This fixes a regression introduced in #818.
    
    Fixes #896

--- src/Common/src/NativeMethods.cs
+++ src/Common/src/NativeMethods.cs
@@ -5396,7 +5396,7 @@ namespace System.Windows.Forms
         [StructLayout(LayoutKind.Sequential)]
         public unsafe struct tagELEMDESC
         {
-            public NativeMethods.tagTYPEDESC* tdesc;
+            public NativeMethods.tagTYPEDESC tdesc;
             public NativeMethods.tagPARAMDESC paramdesc;
         }
 
