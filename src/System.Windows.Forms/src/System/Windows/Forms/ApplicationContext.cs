9927573ae0a0358f0f8cd324ace04fbfc17db624
Hugh Bellamy <hughbellars@gmail.com>
2019-07-02 18:59:42 +0100

    Cleanup ApplicationContext

--- src/System.Windows.Forms/src/System/Windows/Forms/ApplicationContext.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ApplicationContext.cs
@@ -2,105 +2,84 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.ComponentModel;
+
 namespace System.Windows.Forms
 {
-    using System;
-    using System.Diagnostics;
-    using System.ComponentModel;
-
     /// <summary>
-    ///    ApplicationContext provides contextual information about an application
-    ///    thread. Specifically this allows an application author to redifine what
-    ///    circurmstances cause a message loop to exit. By default the application
-    ///    context listens to the close event on the mainForm, then exits the
-    ///    thread's message loop.
+    /// ApplicationContext provides contextual information about an application
+    /// thread. Specifically this allows an application author to redifine what
+    /// circurmstances cause a message loop to exit. By default the application
+    /// context listens to the close event on the mainForm, then exits the
+    /// thread's message loop.
     /// </summary>
     public class ApplicationContext : IDisposable
     {
-        Form mainForm;
-        object userData;
+        private Form _mainForm;
 
         /// <summary>
-        ///     Creates a new ApplicationContext with no mainForm.
+        /// Creates a new ApplicationContext with no mainForm.
         /// </summary>
         public ApplicationContext() : this(null)
         {
         }
 
         /// <summary>
-        ///     Creates a new ApplicationContext with the specified mainForm.
-        ///     If OnMainFormClosed is not overriden, the thread's message
-        ///     loop will be terminated when mainForm is closed.
+        /// Creates a new ApplicationContext with the specified mainForm.
+        /// If OnMainFormClosed is not overriden, the thread's message
+        /// loop will be terminated when mainForm is closed.
         /// </summary>
         public ApplicationContext(Form mainForm)
         {
             MainForm = mainForm;
         }
 
-        ~ApplicationContext()
-        {
-            Dispose(false);
-        }
+        ~ApplicationContext() => Dispose(false);
 
         /// <summary>
-        ///     Determines the mainForm for this context. This may be changed
-        ///     at anytime.
-        ///     If OnMainFormClosed is not overriden, the thread's message
-        ///     loop will be terminated when mainForm is closed.
+        /// Determines the mainForm for this context. This may be changed
+        /// at anytime.
+        /// If OnMainFormClosed is not overriden, the thread's message
+        /// loop will be terminated when mainForm is closed.
         /// </summary>
         public Form MainForm
         {
-            get
-            {
-                return mainForm;
-            }
+            get => _mainForm;
             set
             {
-                EventHandler onClose = new EventHandler(OnMainFormDestroy);
-                if (mainForm != null)
+                EventHandler onClose = OnMainFormDestroy;
+                if (_mainForm != null)
                 {
-                    mainForm.HandleDestroyed -= onClose;
+                    _mainForm.HandleDestroyed -= onClose;
                 }
 
-                mainForm = value;
+                _mainForm = value;
 
-                if (mainForm != null)
+                if (_mainForm != null)
                 {
-                    mainForm.HandleDestroyed += onClose;
+                    _mainForm.HandleDestroyed += onClose;
                 }
             }
         }
 
-        [
-        SRCategory(nameof(SR.CatData)),
-        Localizable(false),
-        Bindable(true),
-        SRDescription(nameof(SR.ControlTagDescr)),
-        DefaultValue(null),
-        TypeConverter(typeof(StringConverter)),
-        ]
-        public object Tag
-        {
-            get
-            {
-                return userData;
-            }
-            set
-            {
-                userData = value;
-            }
-        }
+        [SRCategory(nameof(SR.CatData))]
+        [Localizable(false)]
+        [Bindable(true)]
+        [SRDescription(nameof(SR.ControlTagDescr))]
+        [DefaultValue(null)]
+        [TypeConverter(typeof(StringConverter))]
+        public object Tag { get; set; }
 
         /// <summary>
-        ///     Is raised when the thread's message loop should be terminated.
-        ///     This is raised by calling ExitThread.
+        /// Is raised when the thread's message loop should be terminated.
+        /// This is raised by calling ExitThread.
         /// </summary>
         public event EventHandler ThreadExit;
 
         /// <summary>
-        ///     Disposes the context. This should dispose the mainForm. This is
-        ///     called immediately after the thread's message loop is terminated.
-        ///     Application will dispose all forms on this thread by default.
+        /// Disposes the context. This should dispose the mainForm. This is
+        /// called immediately after the thread's message loop is terminated.
+        /// Application will dispose all forms on this thread by default.
         /// </summary>
         public void Dispose()
         {
@@ -112,46 +91,37 @@ namespace System.Windows.Forms
         {
             if (disposing)
             {
-                if (mainForm != null)
+                if (_mainForm != null)
                 {
-                    if (!mainForm.IsDisposed)
+                    if (!_mainForm.IsDisposed)
                     {
-                        mainForm.Dispose();
+                        _mainForm.Dispose();
                     }
-                    mainForm = null;
+
+                    _mainForm = null;
                 }
             }
         }
 
         /// <summary>
-        ///     Causes the thread's message loop to be terminated. This
-        ///     will call ExitThreadCore.
+        /// Causes the thread's message loop to be terminated. This will call ExitThreadCore.
         /// </summary>
-        public void ExitThread()
-        {
-            ExitThreadCore();
-        }
+        public void ExitThread() => ExitThreadCore();
 
         /// <summary>
-        ///     Causes the thread's message loop to be terminated.
+        /// Causes the thread's message loop to be terminated.
         /// </summary>
-        protected virtual void ExitThreadCore()
-        {
-            ThreadExit?.Invoke(this, EventArgs.Empty);
-        }
+        protected virtual void ExitThreadCore() => ThreadExit?.Invoke(this, EventArgs.Empty);
 
         /// <summary>
-        ///     Called when the mainForm is closed. The default implementation
-        ///     of this will call ExitThreadCore.
+        /// Called when the mainForm is closed. The default implementation
+        /// of this will call ExitThreadCore.
         /// </summary>
-        protected virtual void OnMainFormClosed(object sender, EventArgs e)
-        {
-            ExitThreadCore();
-        }
+        protected virtual void OnMainFormClosed(object sender, EventArgs e) => ExitThreadCore();
 
         /// <summary>
-        ///     Called when the mainForm is closed. The default implementation
-        ///     of this will call ExitThreadCore.
+        /// Called when the mainForm is closed. The default implementation
+        /// of this will call ExitThreadCore.
         /// </summary>
         private void OnMainFormDestroy(object sender, EventArgs e)
         {
