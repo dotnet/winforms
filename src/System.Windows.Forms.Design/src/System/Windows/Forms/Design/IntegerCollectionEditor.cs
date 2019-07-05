437d823241037c99856d6566dbd0252cb1465ee1
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-07-05 15:11:14 +1000

    chore: General cleanup
    - Fix xml-docs
    - Fix names
    - Remove unused usings
    - Remove `this.`
    - Add missing braces
    - Use arrow functions, where appropriate

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/IntegerCollectionEditor.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/IntegerCollectionEditor.cs
@@ -1,205 +1,174 @@
-﻿//------------------------------------------------------------------------------
-// <copyright file="IntegerCollectionEditor.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>                                                                
-//------------------------------------------------------------------------------
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-#define NEEDHELPBUTTON    
+#define NEEDHELPBUTTON
 
-/*
- */
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Scope = "member", Target = "System.Windows.Forms.Design.IntegerCollectionEditor..ctor(System.Type)")]
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Globalization;
 
 namespace System.Windows.Forms.Design
 {
-    using System;
-    using System.ComponentModel.Design;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Drawing;
-    using System.Globalization;
-    using System.Windows.Forms;
-
-    /// <include file='doc\IntegerCollectionEditor.uex' path='docs/doc[@for="IntegerCollectionEditor"]/*' />
-    /// <devdoc>
-    ///      The IntegerCollectionEditor is a collection editor that is specifically
-    ///      designed to edit collections containing Integers.  
-    /// </devdoc>
+    /// <summary>
+    ///  The <see cref="IntegerCollectionEditor"/> is a collection editor that is specifically
+    ///  designed to edit collections containing Integers.  
+    /// </summary>
     internal class IntegerCollectionEditor : CollectionEditor
     {
-
-        public IntegerCollectionEditor(Type type) : base(type)
+        public IntegerCollectionEditor(Type type)
+            : base(type)
         {
         }
 
-        /// <include file='doc\IntegerCollectionEditor.uex' path='docs/doc[@for="IntegerCollectionEditor.CreateCollectionForm"]/*' />
-        /// <devdoc>
-        ///      Creates a new form to show the current collection.  You may inherit
-        ///      from CollectionForm to provide your own form.
-        /// </devdoc>
-        protected override CollectionForm CreateCollectionForm()
-        {
-            return new IntegerCollectionForm(this);
-        }
+        /// <summary>
+        ///  Creates a new form to show the current collection. 
+        ///  You may inherit from CollectionForm to provide your own form.
+        /// </summary>
+        protected override CollectionForm CreateCollectionForm() => new IntegerCollectionForm(this);
 
-        /// <include file='doc\IntegerCollectionEditor.uex' path='docs/doc[@for="IntegerCollectionEditor.HelpTopic"]/*' />
-        /// <devdoc>
-        ///    <para>Gets the help topic to display for the dialog help button or pressing F1. Override to
-        ///          display a different help topic.</para>
-        /// </devdoc>
-        protected override string HelpTopic
-        {
-            get
-            {
-                return "net.ComponentModel.IntegerCollectionEditor";
-            }
-        }
+        /// <summary>
+        ///<para>Gets the help topic to display for the dialog help button or pressing F1. 
+        ///  Override to display a different help topic.</para>
+        /// </summary>
+        protected override string HelpTopic => "net.ComponentModel.IntegerCollectionEditor";
 
-        /// <include file='doc\IntegerCollectionEditor.uex' path='docs/doc[@for="IntegerCollectionEditor.IntegerCollectionEditor"]/*' />
-        /// <devdoc>
-        ///     IntegerCollectionEditor allows visible editing of a integer array. Each line in
-        ///     the edit box is an array entry.
-        /// </devdoc>
+        /// <summary>
+        /// IntegerCollectionEditor allows visible editing of a integer array.
+        /// Each line in the edit box is an array entry.
+        /// </summary>
         private class IntegerCollectionForm : CollectionForm
         {
-
-            private Label instruction = new Label();
-            private TextBox textEntry = new TextBox();
-            private Button okButton = new Button();
-            private Button cancelButton = new Button();
+            private readonly IntegerCollectionEditor _editor;
+            private readonly Label _instruction = new Label();
+            private readonly TextBox _textEntry = new TextBox();
+            private readonly Button _okButton = new Button();
+            private readonly Button _cancelButton = new Button();
 #if NEEDHELPBUTTON
-            private Button helpButton = new Button();
+            private readonly Button _helpButton = new Button();
 #endif
 
-            private IntegerCollectionEditor editor = null;
-
-            /// <include file='doc\IntegerCollectionEditor.uex' path='docs/doc[@for="IntegerCollectionEditor.StringCollectionForm.IntegerCollectionForm"]/*' />
-            /// <devdoc>
-            ///     Constructs a StringCollectionForm.
-            /// </devdoc>
-            public IntegerCollectionForm(CollectionEditor editor) : base(editor)
+            /// <summary>
+            /// Constructs a StringCollectionForm.
+            /// </summary>
+            public IntegerCollectionForm(CollectionEditor editor)
+                : base(editor)
             {
-                this.editor = (IntegerCollectionEditor)editor;
+                _editor = (IntegerCollectionEditor)editor;
                 InitializeComponent();
             }
 
             private void Edit1_keyDown(object sender, KeyEventArgs e)
             {
-                if (e.KeyCode == Keys.Escape)
+                if (e.KeyCode != Keys.Escape)
                 {
-                    cancelButton.PerformClick();
-                    e.Handled = true;
+                    return;
                 }
+
+                _cancelButton.PerformClick();
+                e.Handled = true;
             }
 
-            private void HelpButton_click(object sender, EventArgs e)
-            {
-                editor.ShowHelp();
-            }
+            private void HelpButton_click(object sender, EventArgs e) => _editor.ShowHelp();
 
-            private void Form_HelpRequested(object sender, HelpEventArgs e)
-            {
-                editor.ShowHelp();
-            }
+            private void Form_HelpRequested(object sender, HelpEventArgs e) => _editor.ShowHelp();
 
-            /// <include file='doc\IntegerCollectionForm.uex' path='docs/doc[@for="IntegerCollectionForm.IntegerCollectionForm.InitializeComponent"]/*' />
-            /// <devdoc>
-            ///     NOTE: The following code is required by the form
-            ///     designer.  It can be modified using the form editor.  Do not
-            ///     modify it using the code editor.
-            /// </devdoc>
-            [SuppressMessage("Microsoft.Globalization", "CA1303:DoNotPassLiteralsAsLocalizedParameters")]
+            /// <summary>
+            ///  NOTE: The following code is required by the form designer. It can be modified using the form editor.  
+            ///  Do not modify it using the code editor.
+            /// </summary>
             private void InitializeComponent()
             {
-                instruction.Location = new Point(4, 7);
-                instruction.Size = new Size(422, 14);
-                instruction.TabIndex = 0;
-                instruction.TabStop = false;
-                instruction.Text = SR.IntegerCollectionEditorInstruction;
+                _instruction.Location = new Point(4, 7);
+                _instruction.Size = new Size(422, 14);
+                _instruction.TabIndex = 0;
+                _instruction.TabStop = false;
+                _instruction.Text = SR.IntegerCollectionEditorInstruction;
 
-                textEntry.Location = new Point(4, 22);
-                textEntry.Size = new Size(422, 244);
-                textEntry.TabIndex = 0;
-                textEntry.Text = "";
-                textEntry.AcceptsTab = false;
-                textEntry.AcceptsReturn = true;
-                textEntry.AutoSize = false;
-                textEntry.Multiline = true;
-                textEntry.ScrollBars = ScrollBars.Both;
-                textEntry.WordWrap = false;
-                textEntry.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right;
-                textEntry.KeyDown += new KeyEventHandler(this.Edit1_keyDown);
+                _textEntry.Location = new Point(4, 22);
+                _textEntry.Size = new Size(422, 244);
+                _textEntry.TabIndex = 0;
+                _textEntry.Text = "";
+                _textEntry.AcceptsTab = false;
+                _textEntry.AcceptsReturn = true;
+                _textEntry.AutoSize = false;
+                _textEntry.Multiline = true;
+                _textEntry.ScrollBars = ScrollBars.Both;
+                _textEntry.WordWrap = false;
+                _textEntry.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Bottom | AnchorStyles.Right;
+                _textEntry.KeyDown += new KeyEventHandler(Edit1_keyDown);
 
 #if NEEDHELPBUTTON
-                okButton.Location = new Point(185, 274);
+                _okButton.Location = new Point(185, 274);
 #else
-                okButton.Location = new Point(264, 274);
+                _okButton.Location = new Point(264, 274);
 #endif
-                okButton.Size = new Size(75, 23);
-                okButton.TabIndex = 1;
-                okButton.Text = SR.IntegerCollectionEditorOKCaption;
-                okButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-                okButton.DialogResult = DialogResult.OK;
-                okButton.Click += new EventHandler(this.OKButton_click);
+                _okButton.Size = new Size(75, 23);
+                _okButton.TabIndex = 1;
+                _okButton.Text = SR.IntegerCollectionEditorOKCaption;
+                _okButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+                _okButton.DialogResult = DialogResult.OK;
+                _okButton.Click += new EventHandler(OKButton_click);
 
 #if NEEDHELPBUTTON
-                cancelButton.Location = new Point(264, 274);
+                _cancelButton.Location = new Point(264, 274);
 #else
-                cancelButton.Location = new Point(343, 274);
+                _cancelButton.Location = new Point(343, 274);
 #endif
-                cancelButton.Size = new Size(75, 23);
-                cancelButton.TabIndex = 2;
-                cancelButton.Text = SR.IntegerCollectionEditorCancelCaption;
-                cancelButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-                cancelButton.DialogResult = DialogResult.Cancel;
+                _cancelButton.Size = new Size(75, 23);
+                _cancelButton.TabIndex = 2;
+                _cancelButton.Text = SR.IntegerCollectionEditorCancelCaption;
+                _cancelButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+                _cancelButton.DialogResult = DialogResult.Cancel;
 
 #if NEEDHELPBUTTON
-                helpButton.Location = new Point(343, 274);
-                helpButton.Size = new Size(75, 23);
-                helpButton.TabIndex = 3;
-                helpButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-                helpButton.Text = SR.IntegerCollectionEditorHelpCaption;
+                _helpButton.Location = new Point(343, 274);
+                _helpButton.Size = new Size(75, 23);
+                _helpButton.TabIndex = 3;
+                _helpButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+                _helpButton.Text = SR.IntegerCollectionEditorHelpCaption;
+
+                _helpButton.Click += new EventHandler(HelpButton_click);
+                HelpRequested += new HelpEventHandler(Form_HelpRequested);
 #endif
 
-                this.Location = new Point(7, 7);
-                this.Text = SR.IntegerCollectionEditorTitle;
-                this.AcceptButton = okButton;
-                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-                this.AutoScaleDimensions = new SizeF(6, 13);
-                this.CancelButton = cancelButton;
-                this.ClientSize = new Size(429, 307);
-                this.MaximizeBox = false;
-                this.MinimizeBox = false;
-                this.ControlBox = false;
-                this.ShowInTaskbar = false;
+                Location = new Point(7, 7);
+                Text = SR.IntegerCollectionEditorTitle;
+                AcceptButton = _okButton;
+                AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+                AutoScaleDimensions = new SizeF(6, 13);
+                CancelButton = _cancelButton;
+                ClientSize = new Size(429, 307);
+                MaximizeBox = false;
+                MinimizeBox = false;
+                ControlBox = false;
+                ShowInTaskbar = false;
                 // VSWhidbey 94233: Force the dialog to open using FormStartPosition.CenterScreen.
-                this.StartPosition = FormStartPosition.CenterScreen;
-                this.MinimumSize = new Size(300, 200);
+                StartPosition = FormStartPosition.CenterScreen;
+                MinimumSize = new Size(300, 200);
 
-                helpButton.Click += new EventHandler(this.HelpButton_click);
-                this.HelpRequested += new HelpEventHandler(this.Form_HelpRequested);
-
-                this.Controls.Clear();
-                this.Controls.AddRange(new Control[] {
-                                        instruction,
-                                        textEntry,
-                                        okButton,
-                                        cancelButton,
+                Controls.Clear();
+                Controls.AddRange(new Control[] {
+                                        _instruction,
+                                        _textEntry,
+                                        _okButton,
+                                        _cancelButton,
     #if NEEDHELPBUTTON
-                                        helpButton
+                                        _helpButton
     #endif
                                         });
             }
 
             /// <include file='doc\IntegerCollectionEditor.uex' path='docs/doc[@for="IntegerCollectionEditor.IntegerCollectionForm.OKButton_click"]/*' />
-            /// <devdoc>
-            ///      Commits the changes to the editor.
-            /// </devdoc>
+            /// <summary>
+            ///  Commits the changes to the editor.
+            /// </summary>
             private void OKButton_click(object sender, EventArgs e)
             {
                 char[] delims = new char[] { '\n' };
                 char[] trims = new char[] { '\r' };
 
-                string[] strings = textEntry.Text.Split(delims);
+                string[] strings = _textEntry.Text.Split(delims);
                 object[] curItems = Items;
 
                 int nItems = strings.Length;
@@ -243,7 +212,9 @@ namespace System.Windows.Forms.Design
                     }
 
                     if (i == nItems)
+                    {
                         dirty = false;
+                    }
                 }
 
                 if (!dirty)
@@ -262,8 +233,8 @@ namespace System.Windows.Forms.Design
             }
 
             // <summary>
-            //      This is called when the value property in the CollectionForm has changed.
-            //      In it you should update your user interface to reflect the current value.
+            //  This is called when the value property in the CollectionForm has changed.
+            //  In it you should update your user interface to reflect the current value.
             // </summary>
             // </doc>
             protected override void OnEditValueChanged()
@@ -283,7 +254,7 @@ namespace System.Windows.Forms.Design
                     }
                 }
 
-                textEntry.Text = text;
+                _textEntry.Text = text;
             }
         }
     }
