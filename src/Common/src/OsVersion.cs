2b185075c936e19c909239c1ae50047654f0a141
Jeremy Kuhne <jkuhne@microsoft.com>
2019-07-23 11:17:18 -0700

    Address feedback

--- src/Common/src/OsVersion.cs
+++ src/Common/src/OsVersion.cs
@@ -6,34 +6,35 @@ namespace System.Windows.Forms
 {
     internal static class OsVersion
     {
-        private static Interop.NtDll.RTL_OSVERSIONINFOEX _versionInfo = InitVersion;
+        private static Interop.NtDll.RTL_OSVERSIONINFOEX s_versionInfo = InitVersion();
 
-        private static Interop.NtDll.RTL_OSVERSIONINFOEX InitVersion
+        private static Interop.NtDll.RTL_OSVERSIONINFOEX InitVersion()
         {
-            get
-            {
-                Interop.NtDll.RtlGetVersion(out Interop.NtDll.RTL_OSVERSIONINFOEX info);
-                return info;
-            }
+            // We use RtlGetVersion as it isn't subject to version lie. GetVersion
+            // won't tell you the real version unless the launching exe is manifested
+            // with the latest OS version.
+
+            Interop.NtDll.RtlGetVersion(out Interop.NtDll.RTL_OSVERSIONINFOEX info);
+            return info;
         }
 
         /// <summary>
         /// Is Windows 10 Anniversary Update or later. (Redstone 1, build 14393, version 1607)
         /// </summary>
         public static bool IsWindows10_1607OrGreater
-            => _versionInfo.dwMajorVersion >= 10 && _versionInfo.dwBuildNumber >= 14393;
+            => s_versionInfo.dwMajorVersion >= 10 && s_versionInfo.dwBuildNumber >= 14393;
 
         /// <summary>
         /// Is Windows 10 Creators Update or later. (Redstone 2, build 15063, version 1703)
         /// </summary>
         public static bool IsWindows10_1703OrGreater
-            => _versionInfo.dwMajorVersion >= 10 && _versionInfo.dwBuildNumber >= 15063;
+            => s_versionInfo.dwMajorVersion >= 10 && s_versionInfo.dwBuildNumber >= 15063;
 
         /// <summary>
         /// Is Windows 8.1 or later.
         /// </summary>
         public static bool IsWindows8_1OrGreater
-            => _versionInfo.dwMajorVersion >= 10
-                || _versionInfo.dwMajorVersion == 6 && _versionInfo.dwMinorVersion == 3;
+            => s_versionInfo.dwMajorVersion >= 10
+                || s_versionInfo.dwMajorVersion == 6 && s_versionInfo.dwMinorVersion == 3;
     }
 }
