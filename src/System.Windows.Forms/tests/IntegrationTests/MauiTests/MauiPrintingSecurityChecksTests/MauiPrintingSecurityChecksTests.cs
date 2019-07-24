cd9620e15de654cb95d23278157346aa13287103
Amy Li <v-jiaol@microsoft.com>
2019-07-24 03:18:32 -0700

    add cases

---
+++ src/System.Windows.Forms/tests/IntegrationTests/MauiTests/MauiPrintingSecurityChecksTests/MauiPrintingSecurityChecksTests.cs
@@ -0,0 +1,171 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using ReflectTools;
+using ReflectTools.AutoPME;
+using WFCTestLib.Util;
+using WFCTestLib.Log;
+using System.Diagnostics;
+using System.Windows.Forms.IntegrationTests.Common;
+using System.Security.Authentication.ExtendedProtection;
+
+namespace System.Windows.Forms.IntegrationTests.MauiTests
+{
+    public class MauiPrintingSecurityChecksTests : ReflectBase
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.SetCulture("en-US");
+            Application.Run(new MauiPrintingSecurityChecksTests(args));
+        }
+
+        public MauiPrintingSecurityChecksTests(String[] args) : base(args)
+        {
+            this.BringToForeground();
+        }
+
+        public System.Windows.Forms.Timer tTimer = new System.Windows.Forms.Timer();
+        private PrintDocument GetNonDefaultPrinter(TParams p)
+        {
+            // changeddate: 06-27-2006, changedby: v-hzhang
+            // if only have one printer, it will be the default printer always             
+            if (SafeMethods.GetInstalledPrinters().Count <= 1)
+            {
+                return null;
+            }
+
+            PrintDocument doc = new PrintDocument();
+
+            foreach (string printerName in SafeMethods.GetInstalledPrinters())
+            {
+                SafeMethods.SetPrinterName(doc.PrinterSettings, printerName);
+                if (!doc.PrinterSettings.IsDefaultPrinter) break;
+
+            }
+
+            if (doc.PrinterSettings.IsDefaultPrinter && SafeMethods.GetInstalledPrinters().Count > 1)
+                return null;
+
+
+            p.log.WriteLine("Got non-default printer: " + SafeMethods.GetPrinterName(doc.PrinterSettings));
+
+            return doc;
+        }
+
+        // AllPrinting lets us do anything, so we'll check it with a non-default printer and no dialog
+        [Scenario(true)]
+        public ScenarioResult CheckAllPrinting(TParams p)
+        {
+            PrintDocument doc = GetNonDefaultPrinter(p);
+            if (doc == null) return new ScenarioResult(false, "could not get non-default printer");
+
+            ScenarioResult sr = new ScenarioResult();
+
+            //SafeMethods.Print(doc);
+
+            //[v-torend 8/31/2007]
+            //When Microsoft Office Docuemnt Writer is set as default printer the save file dialog comes up
+            //and no printing happens.
+            if (SafeMethods.GetPrinterName(doc.PrinterSettings).Contains("Writer"))
+            {
+                sr.IncCounters(true);
+            }
+            else
+            {
+                SecurityCheck(sr,
+                    delegate { doc.Print(); },
+                    typeof(PrintDocument).GetMethod("Print"),
+                    LibSecurity.AllPrinting);
+            }
+            return sr;
+        }
+
+        // DefaultPrinting allows printing to the default printer settings without showing a dialog
+        [Scenario(true)]
+        public ScenarioResult CheckDefaultPrinting(TParams p)
+        {
+            PrintDocument doc = new PrintDocument();
+            ScenarioResult sr = new ScenarioResult();
+
+            //When Microsoft Office Docuemnt Writer is set as default printer the save file dialog comes up
+            //and no printing happens.
+            if (SafeMethods.GetPrinterName(doc.PrinterSettings).Contains("Writer"))
+            {
+                sr.IncCounters(true);
+            }
+            else
+            {
+                SecurityCheck(sr,
+                    delegate { doc.Print(); },
+                    typeof(PrintDocument).GetMethod("Print"),
+                    LibSecurity.DefaultPrinting);
+            }
+
+            return sr;
+        }
+
+        // SafePrinting requires that you show a dialog before printing
+        [Scenario(true)]
+        public ScenarioResult CheckSafePrinting(TParams p)
+        {
+            PrintDocument doc = new PrintDocument();
+            PrintDialog dialog = new PrintDialog();
+            ScenarioResult sr = new ScenarioResult();
+            dialog.PrinterSettings = doc.PrinterSettings;
+            p.log.WriteLine("Starting second thread");
+            EndFaxAndSendEnterByTimer();
+            p.log.WriteLine("calling ShowDialog()");
+            dialog.ShowDialog();
+            tTimer.Enabled = false;
+            //When Microsoft Office Docuemnt Writer is set as default printer the save file dialog comes up
+            if (SafeMethods.GetPrinterName(doc.PrinterSettings).Contains("Writer"))
+            {
+                sr.IncCounters(true);
+            }
+            else
+            {
+                SecurityCheck(sr,
+                    delegate { doc.Print(); },
+                    typeof(PrintDocument).GetMethod("Print"),
+                    LibSecurity.SafePrinting);
+            }
+
+            return sr;
+        }
+
+        public void EndFaxAndSendEnterByTimer()
+        {
+            tTimer.Interval = 500;
+            tTimer.Tick += new System.EventHandler(this.tTimer_Tick);
+            tTimer.Enabled = true;
+        }
+
+        public void tTimer_Tick(object sender, EventArgs e)
+        {
+            //1.Kill Process "WFS" related to Fax Setup window by which print dialog is covered.
+            //2.Using a timer instead of a thread to send enter key can make sure that print dialog can get enter key even if OS is slow.
+            foreach (Process prc in Process.GetProcesses())
+            {
+                if (prc.ProcessName.ToUpper() == "WFS")
+                {
+                    prc.Kill();
+                    break;
+                }
+            }
+
+            SendKeys.Send("{Enter}");
+        }
+    }
+}
---
+++ src/System.Windows.Forms/tests/IntegrationTests/MauiTests/MauiResetItemTests/MauiResetItemTests.cs
@@ -0,0 +1,390 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Windows.Forms;
+using System.Windows.Forms.IntegrationTests.Common;
+using WFCTestLib.Util;
+using WFCTestLib.Log;
+using ReflectTools;
+using System.Collections;
+using System.Data;
+using System.ComponentModel;
+using System.Threading;
+
+namespace System.Windows.Forms.IntegrationTests.MauiTests
+{
+    public class MauiResetItemTests : ReflectBase
+    {
+
+        DataSet ds;
+        DataTable dt;
+        ArrayList strs;
+        BindingSource dc;
+        const int DefaultRowCount = 10;
+        ScenarioResult sr = null;
+        DataGridView dgv = null;
+        int initialize = 0;
+        ListChangedEventArgs args = null;
+        int eventCount = 0;
+
+        #region Testcase setup
+        public MauiResetItemTests(string[] args) : base(args)
+        {
+            this.BringToForeground();
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.SetCulture("en-US");
+            Application.Run(new MauiResetItemTests(args));
+        }
+
+        protected override void InitTest(TParams p)
+        {
+            base.InitTest(p);
+        }
+
+        protected override bool BeforeScenario(TParams p, System.Reflection.MethodInfo scenario)
+        {
+            dc = new BindingSource();
+            int i = p.ru.GetRange(1, 5);
+            initialize = i;
+            switch (i)
+            {
+                case 1:
+                    InitIList(DefaultRowCount);
+                    break;
+                case 2:
+                    InitIListAsIListSource(DefaultRowCount);
+                    break;
+                case 3:
+                    InitIListSource(DefaultRowCount, p);
+                    break;
+                case 4:
+                    InitIBindingList(DefaultRowCount, p, p.ru.GetBoolean());
+                    break;
+                case 5:
+                    InitIBindingListAsIListSource(DefaultRowCount, p, p.ru.GetBoolean());
+                    break;
+                default:
+                    break;
+            }
+            sr = new ScenarioResult();
+            dc.ListChanged += new System.ComponentModel.ListChangedEventHandler(dc_ListChanged);
+            this.Controls.Clear();
+            dgv = new DataGridView();
+            dgv.DataSource = dc;
+            this.Controls.Add(dgv);
+            Application.DoEvents();
+            return base.BeforeScenario(p, scenario);
+        }
+
+        void dc_ListChanged(object sender, System.ComponentModel.ListChangedEventArgs e)
+        {
+            eventCount++;
+            args = e;
+        }
+
+        // IList
+        void InitIList(int rows)
+        {
+            strs = new ArrayList();
+            for (int i = 0; i < rows; i++)
+                strs.Add(new Customer(scenarioParams.ru.GetString(20), i));
+
+            scenarioParams.log.WriteLine("Data Source: IList");
+            dc.DataSource = strs;
+        }
+
+        // IList as IListSource
+        void InitIListAsIListSource(int rows)
+        {
+            strs = new ArrayList();
+            for (int i = 0; i < rows; i++)
+                strs.Add(new Customer(scenarioParams.ru.GetString(20), i));
+
+            scenarioParams.log.WriteLine("Data Source: IList as an IListSource");
+            dc.DataSource = new MyIListSource(strs);
+        }
+
+        // IListSource
+        void InitIListSource(int rows, TParams scenarioParams)
+        {
+            dt = new DataTable("tbl");
+            for (int i = 0; i < 5; i++)
+                dt.Columns.Add("col" + i);
+
+            for (int i = 0; i < rows; i++)
+                dt.Rows.Add(scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50));
+
+            ds = new DataSet();
+            ds.Tables.Add(dt);
+            scenarioParams.log.WriteLine("Data Source: IListSource");
+            // DataTable implements IListSource
+            dc.DataSource = dt;
+        }
+
+        // IBindingList
+        void InitIBindingList(int rows, TParams scenarioParams, bool allowNew)
+        {
+            dt = new DataTable("tbl");
+            for (int i = 0; i < 5; i++)
+                dt.Columns.Add("col" + i);
+
+            for (int i = 0; i < rows; i++)
+                dt.Rows.Add(scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50));
+
+            DataView dv = dt.DefaultView;
+            dv.AllowNew = allowNew;
+            scenarioParams.log.WriteLine("Data Source: IBindingList");
+            // DataView implements IBindingList
+            dc.DataSource = dv;
+        }
+
+        // IBindingList as IListSource
+        void InitIBindingListAsIListSource(int rows, TParams scenarioParams, bool allowNew)
+        {
+            dt = new DataTable("tbl");
+            for (int i = 0; i < 5; i++)
+                dt.Columns.Add("col" + i);
+
+            for (int i = 0; i < rows; i++)
+                dt.Rows.Add(scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50), scenarioParams.ru.GetString(50));
+
+            DataView dv = dt.DefaultView;
+            dv.AllowNew = allowNew;
+            scenarioParams.log.WriteLine("Data Source: IBindingList as an IListSource w/ Add New False");
+            dc.DataSource = new MyIListSource(dv);
+        }
+
+        private class MyIListSource : System.ComponentModel.IListSource
+        {
+            IList ilist;
+
+            public MyIListSource(IList s)
+            {
+                ilist = s;
+            }
+
+            #region IListSource Members
+
+            bool System.ComponentModel.IListSource.ContainsListCollection
+            {
+                get
+                {
+                    return true;
+                }
+            }
+
+            IList System.ComponentModel.IListSource.GetList()
+            {
+                return ilist;
+            }
+
+            #endregion
+        }
+
+
+        #endregion
+
+        //==========================================
+        // Scenarios
+        //==========================================
+        #region Scenarios
+
+        //[Scenario("Dont change value. Call ResetItem.")]
+        [Scenario(true)]
+        public ScenarioResult Scenario1(TParams p)
+        {
+            string oldValue;
+            eventCount = 0;
+            args = null;
+            int index = p.ru.GetRange(0, dc.List.Count - 1);
+            if (initialize == 1 || initialize == 2)
+            {
+                oldValue = (strs[index] as Customer).Name;
+                dc.ResetItem(index);
+                sr.IncCounters(oldValue, dgv.Rows[index].Cells[0].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            else
+            {
+                int col = p.ru.GetRange(0, dt.Columns.Count - 1);
+                oldValue = (dc[index] as DataRowView)[col].ToString();
+                dc.ResetItem(index);
+                sr.IncCounters(oldValue, dgv.Rows[index].Cells[col].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            sr.IncCounters(1, eventCount, "Wrong count for eventCount", p.log);
+            sr.IncCounters(ListChangedType.ItemChanged, args.ListChangedType, "Wrong type", p.log);
+            return sr;
+        }
+
+        //[Scenario("Change value. Call ResetItem. ")]
+        [Scenario(true)]
+        public ScenarioResult Scenario2(TParams p)
+        {
+            string oldValue;
+            string newValue = p.ru.GetString(50);
+            eventCount = 0;
+            args = null;
+            int index = p.ru.GetRange(0, dc.List.Count - 1);
+            if (initialize == 1 || initialize == 2)
+            {
+                oldValue = (strs[index] as Customer).Name;
+                (strs[index] as Customer).Name = newValue;
+                dc.ResetItem(index);
+                sr.IncCounters(newValue, dgv.Rows[index].Cells[0].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            else
+            {
+                int col = p.ru.GetRange(0, dt.Columns.Count - 1);
+                oldValue = (dc[index] as DataRowView)[col].ToString();
+                (dc[index] as DataRowView)[col] = newValue;
+                eventCount = 0;
+                dc.ResetItem(index);
+                sr.IncCounters(newValue, dgv.Rows[index].Cells[col].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            sr.IncCounters(1, eventCount, "Wrong count for eventCount", p.log);
+            sr.IncCounters(ListChangedType.ItemChanged, args.ListChangedType, "Wrong type", p.log);
+            return sr;
+        }
+
+        //[Scenario("Edit to same value. ")]
+        [Scenario(true)]
+        public ScenarioResult Scenario3(TParams p)
+        {
+            string oldValue;
+            string newValue;
+            eventCount = 0;
+            args = null;
+            int index = p.ru.GetRange(0, dc.List.Count - 1);
+            if (initialize == 1 || initialize == 2)
+            {
+                oldValue = (strs[index] as Customer).Name;
+                newValue = oldValue;
+                (strs[index] as Customer).Name = newValue;
+                dc.ResetItem(index);
+                sr.IncCounters(newValue, dgv.Rows[index].Cells[0].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            else
+            {
+                int col = p.ru.GetRange(0, dt.Columns.Count - 1);
+                oldValue = (dc[index] as DataRowView)[col].ToString();
+                newValue = oldValue;
+                (dc[index] as DataRowView)[col] = newValue;
+                eventCount = 0;
+                dc.ResetItem(index);
+                sr.IncCounters(newValue, dgv.Rows[index].Cells[col].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            sr.IncCounters(1, eventCount, "Wrong count for eventCount", p.log);
+            sr.IncCounters(ListChangedType.ItemChanged, args.ListChangedType, "Wrong type", p.log);
+            return sr;
+        }
+
+        //[Scenario("Edit from null value to another value. ")]
+        [Scenario(true)]
+        public ScenarioResult Scenario4(TParams p)
+        {
+            string oldValue;
+            string newValue;
+            int index = p.ru.GetRange(0, dc.List.Count - 1);
+            if (initialize == 1 || initialize == 2)
+            {
+                (strs[index] as Customer).Name = null;
+                eventCount = 0;
+                args = null;
+                oldValue = (strs[index] as Customer).Name;
+                newValue = p.ru.GetString(50);
+                (strs[index] as Customer).Name = newValue;
+                dc.ResetItem(index);
+                sr.IncCounters(newValue, dgv.Rows[index].Cells[0].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            else
+            {
+                int col = p.ru.GetRange(0, dt.Columns.Count - 1);
+                (dc[index] as DataRowView)[col] = null;
+                oldValue = (dc[index] as DataRowView)[col].ToString();
+                newValue = p.ru.GetString(50);
+                (dc[index] as DataRowView)[col] = newValue;
+                eventCount = 0;
+                args = null;
+                dc.ResetItem(index);
+                sr.IncCounters(newValue, dgv.Rows[index].Cells[col].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            sr.IncCounters(1, eventCount, "Wrong count for eventCount", p.log);
+            sr.IncCounters(ListChangedType.ItemChanged, args.ListChangedType, "Wrong type", p.log);
+            return sr;
+        }
+
+        //[Scenario("Edit to null value")]
+        [Scenario(true)]
+        public ScenarioResult Scenario5(TParams p)
+        {
+            string oldValue;
+            string newValue = null;
+            eventCount = 0;
+            args = null;
+            int index = p.ru.GetRange(0, dc.List.Count - 1);
+            if (initialize == 1 || initialize == 2)
+            {
+                oldValue = (strs[index] as Customer).Name;
+                (strs[index] as Customer).Name = newValue;
+                dc.ResetItem(index);
+                sr.IncCounters(null, dgv.Rows[index].Cells[0].Value, "Wrong value after reset", p.log);
+            }
+            else
+            {
+                int col = p.ru.GetRange(0, dt.Columns.Count - 1);
+                oldValue = (dc[index] as DataRowView)[col].ToString();
+                (dc[index] as DataRowView)[col] = newValue;
+                eventCount = 0;
+                dc.ResetItem(index);
+                sr.IncCounters(String.Empty, dgv.Rows[index].Cells[col].Value.ToString(), "Wrong value after reset", p.log);
+            }
+            sr.IncCounters(1, eventCount, "Wrong count for eventCount", p.log);
+            sr.IncCounters(ListChangedType.ItemChanged, args.ListChangedType, "Wrong type", p.log);
+            return sr;
+        }
+
+        #endregion
+
+        public class Customer
+        {
+            private string name;
+            private int id;
+
+            public Customer(string name, int id)
+            {
+                this.name = name;
+                this.id = id;
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+
+            public int ID
+            {
+                get
+                {
+                    return id;
+                }
+                set
+                {
+                    id = value;
+                }
+            }
+        }
+    }
+}
+
