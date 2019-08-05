619a97d2beefb68cb9d2072dc024f3fe52c69a5c
Hugh Bellamy <hughbellars@gmail.com>
2019-08-05 11:04:09 +0100

    Fix weirdness in TextFormatFlags

--- src/System.Windows.Forms/src/System/Windows/Forms/GDI/TextFormatFlags.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/GDI/TextFormatFlags.cs
@@ -2,16 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Windows.Forms.Internal;
-
 namespace System.Windows.Forms
 {
     /// <summary>
-    /// Note: This is a public enum wrapping the internal Interop.User32.TextFormatFlags
-    /// defined in the System.Windows.Forms.Internal namespace, we need to do this to
-    /// be able to compile the internal one into different assemblies w/o
-    /// creating any conflict/dependency on public namespaces.
-    /// Additionally, TextFormatFlags has some extra values.
+    ///  Note: This is a public enum wrapping the internal Interop.User32.TextFormatFlags.
     /// </summary>
     [Flags]
     public enum TextFormatFlags
@@ -24,7 +18,11 @@ namespace System.Windows.Forms
         HidePrefix = Interop.User32.TextFormatFlags.DT_HIDEPREFIX,
         HorizontalCenter = Interop.User32.TextFormatFlags.DT_CENTER,
         Internal = Interop.User32.TextFormatFlags.DT_INTERNAL,
-        Left = Interop.User32.TextFormatFlags.DT_LEFT,  // default
+
+        /// <remarks>
+        ///  This is the default.
+        /// </remarks>
+        Left = Interop.User32.TextFormatFlags.DT_LEFT,
         ModifyString = Interop.User32.TextFormatFlags.DT_MODIFYSTRING,
         NoClipping = Interop.User32.TextFormatFlags.DT_NOCLIP,
         NoPrefix = Interop.User32.TextFormatFlags.DT_NOPREFIX,
@@ -34,29 +32,34 @@ namespace System.Windows.Forms
         Right = Interop.User32.TextFormatFlags.DT_RIGHT,
         RightToLeft = Interop.User32.TextFormatFlags.DT_RTLREADING,
         SingleLine = Interop.User32.TextFormatFlags.DT_SINGLELINE,
-        // NOTE: TextRenderer does not expose a way to set the tab stops. Observe that ExapandTabs is available.
-        // TabStop                     = Interop.User32.TextFormatFlags.TabStop,
         TextBoxControl = Interop.User32.TextFormatFlags.DT_EDITCONTROL,
-        Top = Interop.User32.TextFormatFlags.DT_TOP, // default
-        VerticalCenter = Interop.User32.TextFormatFlags.DT_VCENTER,
 
+        /// <remarks>
+        ///  This is the default.
+        /// </remarks>
+        Top = Interop.User32.TextFormatFlags.DT_TOP,
+
+        VerticalCenter = Interop.User32.TextFormatFlags.DT_VCENTER,
         WordBreak = Interop.User32.TextFormatFlags.DT_WORDBREAK,
         WordEllipsis = Interop.User32.TextFormatFlags.DT_WORD_ELLIPSIS,
 
         /// <summary>
-        /// The following flags are exclusive of TextRenderer (no Windows native flags)
-        /// and apply to methods receiving a Graphics as the IDeviceContext object, and
-        /// specify whether to reapply clipping and coordintate transformations to the hdc
-        /// obtained from the Graphics object, which returns a clean hdc.
+        ///  The following flags are exclusive of TextRenderer (no Windows native flags)
+        ///  and apply to methods receiving a Graphics as the IDeviceContext object, and
+        ///  specify whether to reapply clipping and coordintate transformations to the hdc
+        ///  obtained from the Graphics object, which returns a clean hdc.
         /// </summary>
         PreserveGraphicsClipping = 0x01000000,
         PreserveGraphicsTranslateTransform = 0x02000000,
 
         /// <summary>
-        /// Adds padding related to the drawing binding box, computed according to the font size.
-        /// Match the System.Internal.GDI.TextPaddingOptions.
+        ///  Adds padding related to the drawing binding box, computed according to the font size.
+        ///  Match the System.Internal.GDI.TextPaddingOptions.
         /// </summary>
-        GlyphOverhangPadding = 0x00000000, // default.
+        /// <remarks>
+        ///  This is the default.
+        /// </remarks>
+        GlyphOverhangPadding = 0x00000000,
         NoPadding = 0x10000000,
         LeftAndRightPadding = 0x20000000
     }
