5666e2e2f6d91e8062026806bb3e1221dfec3dca
Zachary Danz <zsd4yr@users.noreply.github.com>
2019-08-06 11:30:17 -0700

    add back unused usings to ease expected first edits

--- pkg/Microsoft.Dotnet.WinForms.ProjectTemplates/content/WinFormsApplication-CSharp/Program.cs
+++ pkg/Microsoft.Dotnet.WinForms.ProjectTemplates/content/WinFormsApplication-CSharp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Company.WinFormsApplication1
