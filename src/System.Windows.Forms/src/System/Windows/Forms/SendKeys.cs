b3d5e682e82d254b0fe07314954538db24351d80
Hugh Bellamy <hughbellars@gmail.com>
2019-10-04 17:03:51 +0100

    Structify EVENTMSG

--- src/System.Windows.Forms/src/System/Windows/Forms/SendKeys.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/SendKeys.cs
@@ -158,21 +158,21 @@ namespace System.Windows.Forms
             {
                 if (haveKeys[HAVESHIFT] == 0 && (vk & SHIFTKEYSCAN) != 0)
                 {
-                    AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.ShiftKey, fStartNewChar, hwnd));
+                    AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.ShiftKey, fStartNewChar, hwnd));
                     fStartNewChar = false;
                     haveKeys[HAVESHIFT] = UNKNOWN_GROUPING;
                 }
 
                 if (haveKeys[HAVECTRL] == 0 && (vk & CTRLKEYSCAN) != 0)
                 {
-                    AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.ControlKey, fStartNewChar, hwnd));
+                    AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.ControlKey, fStartNewChar, hwnd));
                     fStartNewChar = false;
                     haveKeys[HAVECTRL] = UNKNOWN_GROUPING;
                 }
 
                 if (haveKeys[HAVEALT] == 0 && (vk & ALTKEYSCAN) != 0)
                 {
-                    AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.Menu, fStartNewChar, hwnd));
+                    AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.Menu, fStartNewChar, hwnd));
                     fStartNewChar = false;
                     haveKeys[HAVEALT] = UNKNOWN_GROUPING;
                 }
@@ -185,7 +185,7 @@ namespace System.Windows.Forms
                 uint oemVal = User32.OemKeyScan((ushort)(0xFF & (int)character));
                 for (int i = 0; i < repeat; i++)
                 {
-                    AddEvent(new SKEvent(WindowMessages.WM_CHAR, character, (int)(oemVal & 0xFFFF), hwnd));
+                    AddEvent(new SKEvent(User32.WindowMessage.WM_CHAR, character, (oemVal & 0xFFFF), hwnd));
                 }
             }
 
@@ -204,9 +204,9 @@ namespace System.Windows.Forms
         {
             for (int i = 0; i < repeat; i++)
             {
-                AddEvent(new SKEvent(altnoctrldown ? WindowMessages.WM_SYSKEYDOWN : WindowMessages.WM_KEYDOWN, vk, fStartNewChar, hwnd));
+                AddEvent(new SKEvent(altnoctrldown ? User32.WindowMessage.WM_SYSKEYDOWN : User32.WindowMessage.WM_KEYDOWN, (uint)vk, fStartNewChar, hwnd));
                 // fStartNewChar = false;
-                AddEvent(new SKEvent(altnoctrldown ? WindowMessages.WM_SYSKEYUP : WindowMessages.WM_KEYUP, vk, fStartNewChar, hwnd));
+                AddEvent(new SKEvent(altnoctrldown ? User32.WindowMessage.WM_SYSKEYUP : User32.WindowMessage.WM_KEYUP, (uint)vk, fStartNewChar, hwnd));
             }
         }
 
@@ -218,17 +218,17 @@ namespace System.Windows.Forms
         {
             if (haveKeys[HAVESHIFT] == level)
             {
-                AddEvent(new SKEvent(WindowMessages.WM_KEYUP, (int)Keys.ShiftKey, false, hwnd));
+                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYUP, (int)Keys.ShiftKey, false, hwnd));
                 haveKeys[HAVESHIFT] = 0;
             }
             if (haveKeys[HAVECTRL] == level)
             {
-                AddEvent(new SKEvent(WindowMessages.WM_KEYUP, (int)Keys.ControlKey, false, hwnd));
+                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYUP, (int)Keys.ControlKey, false, hwnd));
                 haveKeys[HAVECTRL] = 0;
             }
             if (haveKeys[HAVEALT] == level)
             {
-                AddEvent(new SKEvent(WindowMessages.WM_SYSKEYUP, (int)Keys.Menu, false, hwnd));
+                AddEvent(new SKEvent(User32.WindowMessage.WM_SYSKEYUP, (int)Keys.Menu, false, hwnd));
                 haveKeys[HAVEALT] = 0;
             }
         }
@@ -507,21 +507,21 @@ namespace System.Windows.Forms
                             // Unlike AddSimpleKey, the bit mask uses Keys, rather than scan keys
                             if (haveKeys[HAVESHIFT] == 0 && (vk & (int)Keys.Shift) != 0)
                             {
-                                AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.ShiftKey, fStartNewChar, hwnd));
+                                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.ShiftKey, fStartNewChar, hwnd));
                                 fStartNewChar = false;
                                 haveKeys[HAVESHIFT] = UNKNOWN_GROUPING;
                             }
 
                             if (haveKeys[HAVECTRL] == 0 && (vk & (int)Keys.Control) != 0)
                             {
-                                AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.ControlKey, fStartNewChar, hwnd));
+                                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.ControlKey, fStartNewChar, hwnd));
                                 fStartNewChar = false;
                                 haveKeys[HAVECTRL] = UNKNOWN_GROUPING;
                             }
 
                             if (haveKeys[HAVEALT] == 0 && (vk & (int)Keys.Alt) != 0)
                             {
-                                AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.Menu, fStartNewChar, hwnd));
+                                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.Menu, fStartNewChar, hwnd));
                                 fStartNewChar = false;
                                 haveKeys[HAVEALT] = UNKNOWN_GROUPING;
                             }
@@ -547,7 +547,7 @@ namespace System.Windows.Forms
                             throw new ArgumentException(string.Format(SR.InvalidSendKeysString, keys));
                         }
 
-                        AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.ShiftKey, fStartNewChar, hwnd));
+                        AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.ShiftKey, fStartNewChar, hwnd));
                         fStartNewChar = false;
                         haveKeys[HAVESHIFT] = UNKNOWN_GROUPING;
                         break;
@@ -558,7 +558,7 @@ namespace System.Windows.Forms
                             throw new ArgumentException(string.Format(SR.InvalidSendKeysString, keys));
                         }
 
-                        AddEvent(new SKEvent(WindowMessages.WM_KEYDOWN, (int)Keys.ControlKey, fStartNewChar, hwnd));
+                        AddEvent(new SKEvent(User32.WindowMessage.WM_KEYDOWN, (uint)Keys.ControlKey, fStartNewChar, hwnd));
                         fStartNewChar = false;
                         haveKeys[HAVECTRL] = UNKNOWN_GROUPING;
                         break;
@@ -569,7 +569,7 @@ namespace System.Windows.Forms
                             throw new ArgumentException(string.Format(SR.InvalidSendKeysString, keys));
                         }
 
-                        AddEvent(new SKEvent((haveKeys[HAVECTRL] != 0) ? WindowMessages.WM_KEYDOWN : WindowMessages.WM_SYSKEYDOWN,
+                        AddEvent(new SKEvent((haveKeys[HAVECTRL] != 0) ? User32.WindowMessage.WM_KEYDOWN : User32.WindowMessage.WM_SYSKEYDOWN,
                                              (int)Keys.Menu, fStartNewChar, hwnd));
                         fStartNewChar = false;
                         haveKeys[HAVEALT] = UNKNOWN_GROUPING;
@@ -695,7 +695,7 @@ namespace System.Windows.Forms
 
                         currentInput[0].inputUnion.ki.dwFlags = 0;
 
-                        if (skEvent.wm == WindowMessages.WM_CHAR)
+                        if (skEvent.wm == User32.WindowMessage.WM_CHAR)
                         {
                             // for WM_CHAR, send a KEYEVENTF_UNICODE instead of a Keyboard event
                             // to support extended ascii characters with no keyboard equivalent.
@@ -714,7 +714,7 @@ namespace System.Windows.Forms
                             currentInput[0].inputUnion.ki.wScan = 0;
 
                             // add KeyUp flag if we have a KeyUp
-                            if (skEvent.wm == WindowMessages.WM_KEYUP || skEvent.wm == WindowMessages.WM_SYSKEYUP)
+                            if (skEvent.wm == User32.WindowMessage.WM_KEYUP || skEvent.wm == User32.WindowMessage.WM_SYSKEYUP)
                             {
                                 currentInput[0].inputUnion.ki.dwFlags |= User32.KEYEVENTF.KEYUP;
                             }
@@ -784,13 +784,13 @@ namespace System.Windows.Forms
                 SKEvent skEvent = (SKEvent)previousEvents.Dequeue();
 
                 bool isOn;
-                if ((skEvent.wm == WindowMessages.WM_KEYUP) ||
-                    (skEvent.wm == WindowMessages.WM_SYSKEYUP))
+                if ((skEvent.wm == User32.WindowMessage.WM_KEYUP) ||
+                    (skEvent.wm == User32.WindowMessage.WM_SYSKEYUP))
                 {
                     isOn = false;
                 }
-                else if ((skEvent.wm == WindowMessages.WM_KEYDOWN) ||
-                         (skEvent.wm == WindowMessages.WM_SYSKEYDOWN))
+                else if ((skEvent.wm == User32.WindowMessage.WM_KEYDOWN) ||
+                         (skEvent.wm == User32.WindowMessage.WM_SYSKEYDOWN))
                 {
                     isOn = true;
                 }
@@ -815,15 +815,15 @@ namespace System.Windows.Forms
 
             if (shift)
             {
-                AddEvent(new SKEvent(WindowMessages.WM_KEYUP, (int)Keys.ShiftKey, false, IntPtr.Zero));
+                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYUP, (int)Keys.ShiftKey, false, IntPtr.Zero));
             }
             else if (ctrl)
             {
-                AddEvent(new SKEvent(WindowMessages.WM_KEYUP, (int)Keys.ControlKey, false, IntPtr.Zero));
+                AddEvent(new SKEvent(User32.WindowMessage.WM_KEYUP, (int)Keys.ControlKey, false, IntPtr.Zero));
             }
             else if (alt)
             {
-                AddEvent(new SKEvent(WindowMessages.WM_SYSKEYUP, (int)Keys.Menu, false, IntPtr.Zero));
+                AddEvent(new SKEvent(User32.WindowMessage.WM_SYSKEYUP, (int)Keys.Menu, false, IntPtr.Zero));
             }
         }
 
@@ -851,7 +851,7 @@ namespace System.Windows.Forms
 
         private static void CheckGlobalKeys(SKEvent skEvent)
         {
-            if (skEvent.wm == WindowMessages.WM_KEYDOWN)
+            if (skEvent.wm == User32.WindowMessage.WM_KEYDOWN)
             {
                 switch (skEvent.paramL)
                 {
@@ -1090,24 +1090,24 @@ namespace System.Windows.Forms
         /// </summary>
         private class SKEvent
         {
-            internal int wm;
-            internal int paramL;
-            internal int paramH;
+            internal User32.WindowMessage wm;
+            internal uint paramL;
+            internal uint paramH;
             internal IntPtr hwnd;
 
-            public SKEvent(int a, int b, bool c, IntPtr hwnd)
+            public SKEvent(User32.WindowMessage wm, uint paramL, bool paramH, IntPtr hwnd)
             {
-                wm = a;
-                paramL = b;
-                paramH = c ? 1 : 0;
+                this.wm = wm;
+                this.paramL = paramL;
+                this.paramH = paramH ? 1u : 0;
                 this.hwnd = hwnd;
             }
 
-            public SKEvent(int a, int b, int c, IntPtr hwnd)
+            public SKEvent(User32.WindowMessage wm, uint paramL, uint paramH, IntPtr hwnd)
             {
-                wm = a;
-                paramL = b;
-                paramH = c;
+                this.wm = wm;
+                this.paramL = paramL;
+                this.paramH = paramH;
                 this.hwnd = hwnd;
             }
         }
@@ -1140,9 +1140,9 @@ namespace System.Windows.Forms
             //
             private bool gotNextEvent = false;
 
-            public virtual IntPtr Callback(User32.HC nCode, IntPtr wparam, IntPtr lparam)
+            public unsafe virtual IntPtr Callback(User32.HC nCode, IntPtr wparam, IntPtr lparam)
             {
-                NativeMethods.EVENTMSG eventmsg = Marshal.PtrToStructure<NativeMethods.EVENTMSG>(lparam);
+                User32.EVENTMSG* eventmsg = (User32.EVENTMSG*)lparam;
 
                 if (User32.GetAsyncKeyState((int)Keys.Pause) != 0)
                 {
@@ -1174,12 +1174,11 @@ namespace System.Windows.Forms
 #endif
 
                         SKEvent evt = (SKEvent)SendKeys.events.Peek();
-                        eventmsg.message = evt.wm;
-                        eventmsg.paramL = evt.paramL;
-                        eventmsg.paramH = evt.paramH;
-                        eventmsg.hwnd = evt.hwnd;
-                        eventmsg.time = (int)Kernel32.GetTickCount();
-                        Marshal.StructureToPtr(eventmsg, lparam, true);
+                        eventmsg->message = evt.wm;
+                        eventmsg->paramL = evt.paramL;
+                        eventmsg->paramH = evt.paramH;
+                        eventmsg->hwnd = evt.hwnd;
+                        eventmsg->time = Kernel32.GetTickCount();
                         break;
 
                     default:
