791b95d5d5627d8df94a10dda8491f3f4d0b5db6
Vladimir Krestov <v-vlkres@microsoft.com>
2019-10-14 15:44:46 +0300

    Port ListControlStringCollectionEditor
    Closes #1271

--- src/System.Design/src/System.Design.Forwards.cs
+++ src/System.Design/src/System.Design.Forwards.cs
@@ -1,5 +1,4 @@
 // Licensed to the .NET Foundation under one or more agreements.
-﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,6 +6,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageCollectionEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageIndexEditor))]
+[assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ListControlStringCollectionEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.StringArrayEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.StringCollectionEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.TabPageCollectionEditor))]
---
+++ src/System.Windows.Forms.Design.Editors/src/System/Windows/Forms/Design/ListControlStringCollectionEditor.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+
+namespace System.Windows.Forms.Design
+{
+    /// <summary>
+    ///  The ListControlStringCollectionEditor override StringCollectionEditor
+    ///  to prevent the string collection from being edited if a DataSource
+    ///  has been set on the control.
+    /// </summary>
+    internal class ListControlStringCollectionEditor : StringCollectionEditor
+    {
+        public ListControlStringCollectionEditor(Type type) : base(type)
+        {
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            // If we're trying to edit the items in an object that has a DataSource set, throw an exception
+            ListControl control = context.Instance as ListControl;
+            if (control?.DataSource != null)
+            {
+                throw new ArgumentException(SR.DataSourceLocksItems);
+            }
+
+            return base.EditValue(context, provider, value);
+        }
+    }
+}
