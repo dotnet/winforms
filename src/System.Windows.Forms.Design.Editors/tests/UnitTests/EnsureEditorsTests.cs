d19292fbc1b995a50952846f814a9dd6527ab3d1
Zachary Danz <zsd4yr@virginia.edu>
2019-10-17 11:59:25 -0700

    add editor type to editor test and create basic test for properties

--- src/System.Windows.Forms.Design.Editors/tests/UnitTests/EnsureEditorsTests.cs
+++ src/System.Windows.Forms.Design.Editors/tests/UnitTests/EnsureEditorsTests.cs
@@ -4,6 +4,7 @@
 
 using System.Collections;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Drawing;
 using System.Drawing.Design;
 using System.Drawing.Imaging;
@@ -14,27 +15,46 @@ namespace System.Windows.Forms.Design.Editors.Tests
     public class EnsureEditorsTests
     {
         [Theory]
-        [InlineData(typeof(Array))]
-        [InlineData(typeof(IList))]
-        [InlineData(typeof(ICollection))]
-        [InlineData(typeof(byte[]))]
-        [InlineData(typeof(string[]))]
-        [InlineData(typeof(Bitmap))]
-        [InlineData(typeof(Color))]
-        [InlineData(typeof(Font))]
-        [InlineData(typeof(Image))]
-        [InlineData(typeof(Metafile))]
+        [InlineData(typeof(Array), typeof(ArrayEditor))]
+        [InlineData(typeof(IList), typeof(CollectionEditor))]
+        [InlineData(typeof(ICollection), typeof(CollectionEditor))]
+        [InlineData(typeof(byte[]), typeof(ArrayEditor))]
+        [InlineData(typeof(string[]), typeof(StringArrayEditor))]
+        [InlineData(typeof(Bitmap), typeof(BitmapEditor))]
+        [InlineData(typeof(Color), typeof(ColorEditor))]
+        [InlineData(typeof(Font), typeof(FontEditor))]
+        [InlineData(typeof(Image), typeof(ImageEditor))]
+        [InlineData(typeof(Metafile), typeof(MetafileEditor))]
 
-        [InlineData(typeof(AnchorStyles))]
-        [InlineData(typeof(ToolStripStatusLabelBorderSides))]
-        [InlineData(typeof(DockStyle))]
-        [InlineData(typeof(ImageList.ImageCollection))]
-        [InlineData(typeof(ImageListImage))]
-        [InlineData(typeof(Keys))]
-        public void EnsureUITypeEditorForType(Type type)
+        [InlineData(typeof(AnchorStyles), typeof(AnchorEditor))]
+        [InlineData(typeof(ToolStripStatusLabelBorderSides), typeof(BorderSidesEditor))]
+        [InlineData(typeof(DockStyle), typeof(DockEditor))]
+        [InlineData(typeof(ImageList.ImageCollection), typeof(ImageCollectionEditor))]
+        [InlineData(typeof(ImageListImage), typeof(ImageListImageEditor))]
+        [InlineData(typeof(Keys), typeof(ShortcutKeysEditor))]
+        public void EnsureUITypeEditorForType(Type type, Type expectedEditorType)
         {
             var editor = TypeDescriptor.GetEditor(type, typeof(UITypeEditor));
             Assert.NotNull(editor);
+
+            Assert.Equal(expectedEditorType, editor.GetType());
+        }
+
+        [Theory]
+        [InlineData(typeof(ButtonBase), "Text", typeof(MultilineStringEditor))]
+        public void EnsureUITypeEditorForProperty(Type type, string propertyName, Type expectedEditorType)
+        {
+            var properties = TypeDescriptor.GetProperties(type);
+            Assert.NotNull(properties);
+            Assert.NotEmpty(properties);
+
+            var propertyDescriptor = properties.Find(propertyName, true);
+            Assert.NotNull(propertyDescriptor);
+
+            var editor = propertyDescriptor.GetEditor(typeof(UITypeEditor));
+            Assert.NotNull(editor);
+
+            Assert.Equal(expectedEditorType, editor.GetType());
         }
     }
 }
