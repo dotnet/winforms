6ffadcf9c7114d724522164f354dc88fe4425125
Zachary Danz <zsd4yr@virginia.edu>
2019-10-17 15:42:33 -0700

    Port ImageIndexEditor
    Closes #2116

--- src/System.Design/src/System.Design.Forwards.cs
+++ src/System.Design/src/System.Design.Forwards.cs
@@ -1,9 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 using System.Runtime.CompilerServices;
 
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageCollectionEditor))]
+[assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageIndexEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.StringArrayEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.StringCollectionEditor))]
