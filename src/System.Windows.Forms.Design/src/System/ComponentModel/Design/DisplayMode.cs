0c8c86f541c774a2755dc842872165a328f29011
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-10-18 16:30:21 +1100

    chore: Code cleanup

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/DisplayMode.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/DisplayMode.cs
@@ -1,53 +1,33 @@
-//------------------------------------------------------------------------------
-// <copyright file="DisplayMode.cs" company="Microsoft">
-//     Copyright (c) Microsoft Corporation.  All rights reserved.
-// </copyright>                                                                
-//------------------------------------------------------------------------------
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-namespace System.ComponentModel.Design {
-
-    using System.Diagnostics;
-
-    using System;
-    using System.ComponentModel;
-    using Microsoft.Win32;
-
-    /// <include file='doc\DisplayMode.uex' path='docs/doc[@for="DisplayMode"]/*' />
-    /// <devdoc>
-    ///    <para>Specifies identifiers to indicate the display modes used 
-    ///       by <see cref='System.ComponentModel.Design.ByteViewer'/>.</para>
-    /// </devdoc>
-    public enum DisplayMode {
-
-        /// <include file='doc\DisplayMode.uex' path='docs/doc[@for="DisplayMode.Hexdump"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       Indicates using
-        ///       Hexadecimal format.
-        ///    </para>
-        /// </devdoc>
+namespace System.ComponentModel.Design
+{
+    /// <summary>
+    ///  Specifies identifiers to indicate the display modes used by <see cref='ByteViewer'/>.
+    /// </summary>
+    public enum DisplayMode
+    {
+        /// <summary>
+        ///  Indicates using Hexadecimal format.
+        /// </summary>
         Hexdump = 1,
-        /// <include file='doc\DisplayMode.uex' path='docs/doc[@for="DisplayMode.Ansi"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       Indicates using ANSI format.
-        ///    </para>
-        /// </devdoc>
-        Ansi    = 2,
-        /// <include file='doc\DisplayMode.uex' path='docs/doc[@for="DisplayMode.Unicode"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       Indicates using Unicode format.
-        ///    </para>
-        /// </devdoc>
+
+        /// <summary>
+        ///  Indicates using ANSI format.
+        /// </summary>
+        Ansi = 2,
+
+        /// <summary>
+        ///  Indicates using Unicode format.
+        /// </summary>
         Unicode = 3,
-        /// <include file='doc\DisplayMode.uex' path='docs/doc[@for="DisplayMode.Auto"]/*' />
-        /// <devdoc>
-        ///    <para>
-        ///       Indicates using automatic format selection.
-        ///    </para>
-        /// </devdoc>
-        Auto    = 4,
+
+        /// <summary>
+        ///  Indicates using automatic format selection.
+        /// </summary>
+        Auto = 4,
 
     }
 }
