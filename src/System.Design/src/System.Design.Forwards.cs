ace2e812c3f8508896d0808e1601c8cd66b4df08
Igor Velikorossov <igor.velikorossov@microsoft.com>
2019-10-23 12:12:06 +1100

    Partially revert "Porting DataMemberFieldConverter"  f477b2a
    Whilst we do not support nor cant be expected to support customers who
    reference our internals, in some scenarios like `UITypeEditor`s and
    `TypeConverter`s may still be used by consumers to enhance their runtime
    experience.
    E.g. https://github.com/dotnet/winforms/pull/2078#discussion_r337777751
    
    Type forward `DataMemberFieldConverter` to avoid breaking existing customers.
    
    Relates to #1545

--- src/System.Design/src/System.Design.Forwards.cs
+++ src/System.Design/src/System.Design.Forwards.cs
@@ -1,10 +1,11 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 using System.Runtime.CompilerServices;
 
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.DataGridColumnCollectionEditor))]
+[assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.DataMemberFieldConverter))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageCollectionEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageIndexEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ListControlStringCollectionEditor))]
