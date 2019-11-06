197c38109da08fbcc4f39e454b2d30f010fd364e
Mikhail Lipin <v-milipi@microsoft.com>
2019-11-06 16:39:59 +0300

    Port MaskedTextBoxTextEditor and MaskPropertyEditor
    Closes #2285
    Closes #2286

--- src/System.Design/src/System.Design.Forwards.cs
+++ src/System.Design/src/System.Design.Forwards.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -18,3 +18,5 @@ using System.Runtime.CompilerServices;
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.StringArrayEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.StringCollectionEditor))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.TabPageCollectionEditor))]
+[assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.MaskPropertyEditor))]
+[assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.MaskedTextBoxTextEditor))]
