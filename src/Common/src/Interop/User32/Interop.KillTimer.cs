6aa850905e2d9912d5933bf1bbd55fa4694c21fc
Hugh Bellamy <hughbellars@gmail.com>
2019-11-09 16:37:59 +0000

    Cleanup and fix Timer interop

---
+++ src/Common/src/Interop/User32/Interop.KillTimer.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop
+{
+    internal static partial class User32
+    {
+        [DllImport(Libraries.User32, ExactSpelling = true)]
+        public static extern BOOL KillTimer(IntPtr hWnd, IntPtr uIDEvent);
+
+        public static BOOL KillTimer(HandleRef hWnd, IntPtr uIDEvent)
+        {
+            BOOL result = KillTimer(hWnd.Handle, uIDEvent);
+            GC.KeepAlive(hWnd.Wrapper);
+            return result;
+        }
+    }
+}
