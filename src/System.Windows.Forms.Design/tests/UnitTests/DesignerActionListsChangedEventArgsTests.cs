147ac4625e746d0aa5b12e43e2bfc6877ab82e63
Bruno Alexander <brualexander@gmail.com>
2019-11-14 12:14:53 -0500

    Created DesignerActionListsChangedEventArgsTests

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/DesignerActionListsChangedEventArgsTests.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel.Design;
+using Xunit;
+
+namespace System.Windows.Forms.Design.Tests
+{
+    public class DesignerActionListsChangedEventArgsTests
+    {
+        [Fact]
+        public void DesignerActionListsChangedEventArgs_Constructor()
+        {
+            var button = new Button();
+            var designerActionListsChangedEventArgs = new DesignerActionListsChangedEventArgs(button, 
+                DesignerActionListsChangedType.ActionListsAdded, new DesignerActionListCollection());
+
+            Assert.NotNull(designerActionListsChangedEventArgs);
+
+            //Test properties
+            Assert.Equal(designerActionListsChangedEventArgs.RelatedObject, button);
+            Assert.NotNull(designerActionListsChangedEventArgs.ActionLists);
+        }
+    }
+}
