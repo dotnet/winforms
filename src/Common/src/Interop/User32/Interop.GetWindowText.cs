3625a46c6ab2eae50631ed8b76b35d7dfddc358d
kpreisser <kpreisser@users.noreply.github.com>
2019-12-03 19:12:28 +0100

    Fix issues in Interop.User32.GetWindowText: - Ensure the buffer passed to GetWindowText() has at least the specified size, to avoid an out-of-bounds write. - Fix the condition to detect if the text length has increased, to ensure the text is not truncated. - Avoid an infinite loop (if the control text changes back and forth between a small and long text) by keeping the actualTextLenght value as minimum text length. - Avoid unnecessary loops by specifying the full buffer length as maximum number of characters, rather than the text length value.
    Fixes #2424

--- src/Common/src/Interop/User32/Interop.GetWindowText.cs
+++ src/Common/src/Interop/User32/Interop.GetWindowText.cs
@@ -25,21 +25,30 @@ internal static partial class Interop
 
         public static unsafe string GetWindowText(IntPtr hWnd)
         {
+            int textLength = 0;
+
             while (true)
             {
                 // GetWindowTextLengthW returns the length of the text not
                 // including the null terminator.
-                int textLengthWithNullTerminator = GetWindowTextLengthW(hWnd) + 1;
-                char[] windowTitleBuffer = ArrayPool<char>.Shared.Rent(textLengthWithNullTerminator);
+                textLength = Math.Max(textLength, GetWindowTextLengthW(hWnd));
+                
+                // Use a buffer that has room for at least two additional chars
+                // (one for the null terminator, and one to detect if the text length
+                // has increased).
+                char[] windowTitleBuffer = ArrayPool<char>.Shared.Rent(textLength + 2);
                 string windowTitle;
                 fixed (char* pWindowTitle = windowTitleBuffer)
                 {
-                    int actualTextLength = GetWindowTextW(hWnd, pWindowTitle, textLengthWithNullTerminator + 1);
+                    int actualTextLength = GetWindowTextW(hWnd, pWindowTitle, windowTitleBuffer.Length);
 
                     // The window text may have changed between calls.
                     // Keep looping until we get a buffer that can fit.
-                    if (actualTextLength > textLengthWithNullTerminator)
+                    if (actualTextLength > windowTitleBuffer.Length - 2)
                     {
+                        // We know the text is at last actualTextLength characters
+                        // long, so use this as minimum value for the next iteration.
+                        textLength = actualTextLength;
                         ArrayPool<char>.Shared.Return(windowTitleBuffer);
                         continue;
                     }
