cc5f2e2f4ab9bb511e76cc008294bf9d26c994f5
kpreisser <kpreisser@users.noreply.github.com>
2019-12-17 15:47:26 +0100

    Explicitely specify values of the enum members.

--- src/System.Windows.Forms/src/System/Windows/Forms/TaskDialogProgressBarState.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/TaskDialogProgressBarState.cs
@@ -7,46 +7,50 @@
 namespace System.Windows.Forms
 {
     /// <summary>
-    /// Contains constants that specify the state of a task dialog progress bar.
+    ///   Contains constants that specify the state of a task dialog progress bar.
     /// </summary>
     public enum TaskDialogProgressBarState : int
     {
         /// <summary>
-        /// Shows a regular progress bar.
+        ///   Shows a regular progress bar.
         /// </summary>
         Normal = 0,
 
         /// <summary>
-        /// Shows a paused (yellow) progress bar.
+        ///   Shows a paused (yellow) progress bar.
         /// </summary>
-        Paused,
+        Paused = 1,
 
         /// <summary>
-        /// Shows an error (red) progress bar.
+        ///   Shows an error (red) progress bar.
         /// </summary>
-        Error,
+        Error = 2,
 
         /// <summary>
-        /// Shows a marquee progress bar.
+        ///   Shows a marquee progress bar.
         /// </summary>
-        Marquee,
+        Marquee = 3,
 
         /// <summary>
-        /// Shows a marquee progress bar where the marquee animation is paused.
+        ///   Shows a marquee progress bar where the marquee animation is paused.
         /// </summary>
         /// <remarks>
-        /// If you switch from <see cref="Marquee"/> to <see cref="MarqueePaused"/> while the
-        /// dialog is shown, the  marquee animation will stop.
+        /// <para>
+        ///   If you switch from <see cref="Marquee"/> to <see cref="MarqueePaused"/> while the
+        ///   dialog is shown, the  marquee animation will stop.
+        /// </para>
         /// </remarks>
-        MarqueePaused,
+        MarqueePaused = 4,
 
         /// <summary>
-        /// The progress bar will not be displayed.
+        ///   The progress bar will not be displayed.
         /// </summary>
         /// <remarks>
-        /// Note that while the dialog is showing, you cannot switch from
-        /// <see cref="None"/> to any other state, and vice versa.
+        /// <para>
+        ///   Note that while the dialog is showing, you cannot switch from
+        ///   <see cref="None"/> to any other state, and vice versa.
+        /// </para>
         /// </remarks>
-        None
+        None = 5
     }
 }
