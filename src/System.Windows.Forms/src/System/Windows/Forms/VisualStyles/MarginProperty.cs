966db68e4a685ec135619501f4252fc9b76351ae
gpetrou <4172445+gpetrou@users.noreply.github.com>
2019-12-29 11:07:21 +0000

    Add more TMT enum values (#2541)

--- src/System.Windows.Forms/src/System/Windows/Forms/VisualStyles/MarginProperty.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/VisualStyles/MarginProperty.cs
@@ -2,15 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using static Interop.UxTheme;
+
 namespace System.Windows.Forms.VisualStyles
 {
     public enum MarginProperty
     {
-        SizingMargins = 3601,
-        ContentMargins = 3602,
-        CaptionMargins = 3603
-        //		TM_PROP(3601, TMT, SIZINGMARGINS,     MARGINS)    // margins used for 9-grid sizing
-        //		TM_PROP(3602, TMT, CONTENTMARGINS,    MARGINS)    // margins that define where content can be placed
-        //		TM_PROP(3603, TMT, CAPTIONMARGINS,    MARGINS)    // margins that define where caption text can be placed
+        SizingMargins = TMT.SIZINGMARGINS,
+        ContentMargins = TMT.CONTENTMARGINS,
+        CaptionMargins = TMT.CAPTIONMARGINS
     }
 }
--- src/System.Windows.Forms/src/System/Windows/Forms/VisualStyles/StringProperty.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/VisualStyles/StringProperty.cs
@@ -2,11 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using static Interop.UxTheme;
+
 namespace System.Windows.Forms.VisualStyles
 {
     public enum StringProperty
     {
-        Text = 3201
-        //TM_PROP(3201, TMT, TEXT,              STRING)
+        Text = TMT.TEXT
     }
 }
