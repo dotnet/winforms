792e3f27ec95ddd4061dccd7ef34bd190c18c9fb
Hugh Bellamy <hughbellars@gmail.com>
2020-01-03 01:06:50 +0000

    Cleanup MessageBox interop (#2605)

--- src/System.Windows.Forms/src/System/Windows/Forms/MessageBoxDefaultButton.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/MessageBoxDefaultButton.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using static Interop.User32;
+
 namespace System.Windows.Forms
 {
     public enum MessageBoxDefaultButton
@@ -10,18 +12,18 @@ namespace System.Windows.Forms
         ///  Specifies that the first button on the message box should be the
         ///  default button.
         /// </summary>
-        Button1 = 0x00000000,
+        Button1 = (int)MB.DEFBUTTON1,
 
         /// <summary>
         ///  Specifies that the second button on the message box should be the
         ///  default button.
         /// </summary>
-        Button2 = 0x00000100,
+        Button2 = (int)MB.DEFBUTTON2,
 
         /// <summary>
         ///  Specifies that the third button on the message box should be the
         ///  default button.
         /// </summary>
-        Button3 = 0x00000200,
+        Button3 = (int)MB.DEFBUTTON3,
     }
 }
