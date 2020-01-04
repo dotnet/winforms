2c142509623e2be593e37d577e9a1f422a934401
Hugh Bellamy <hughbellars@gmail.com>
2020-01-04 22:59:35 +0000

    Cleanup TCN constants

---
+++ src/System.Windows.Forms.Primitives/src/Interop/ComCtl32/Interop.TCN.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+internal static partial class Interop
+{
+    internal static partial class ComCtl32
+    {
+        public enum TCN : int
+        {
+            FIRST = 0 - 550,
+            KEYDOWN = FIRST - 0,
+            SELCHANGE = FIRST - 1,
+            SELCHANGING = FIRST - 2,
+            GETOBJECT = FIRST - 3,
+            FOCUSCHANGE = FIRST - 4,
+        }
+    }
+}
