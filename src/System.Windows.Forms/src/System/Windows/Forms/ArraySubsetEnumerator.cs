6013bc57cf88ee476536326bf56d321c5b7b0542
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-01-23 14:54:22 +1100

    Revert incorrect access modifier
    Relates to #2671

--- src/System.Windows.Forms/src/System/Windows/Forms/ArraySubsetEnumerator.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ArraySubsetEnumerator.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -7,7 +7,7 @@ using System.Diagnostics;
 
 namespace System.Windows.Forms
 {
-    public class ArraySubsetEnumerator : IEnumerator
+    internal class ArraySubsetEnumerator : IEnumerator
     {
         private readonly object[] _array;
         private readonly int _total;
