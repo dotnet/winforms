2e2d6815c4a4c19a56e96f24d21c79402d4fc0b1
Hugh Bellamy <hughbellars@gmail.com>
2020-02-16 16:53:53 +0000

    Extract UpDownBase nested classes

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/UpDownBase.ButtonID.cs
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Windows.Forms
+{
+    public abstract partial class UpDownBase
+    {
+        internal enum ButtonID
+        {
+            None = 0,
+            Up = 1,
+            Down = 2,
+        }
+    }
+}
