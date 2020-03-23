adbcfe7d1070116c854ff094f2550a074f492bcd
Mikhail Lipin <v-milipi@microsoft.com>
2020-03-23 16:18:49 +0300

    Fixing the issue with Property Grid crash in case GridViewListBox selected item is null.

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/PropertyGridView.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/PropertyGridView.cs
@@ -7285,7 +7285,7 @@ namespace System.Windows.Forms.PropertyGridInternal
             internal void SetListBoxItemFocus()
             {
                 var selectedItem = _owningGridViewListBox.SelectedItem;
-                if (_itemAccessibleObjects[selectedItem] is AccessibleObject itemAccessibleObject)
+                if (selectedItem != null && _itemAccessibleObjects[selectedItem] is AccessibleObject itemAccessibleObject)
                 {
                     itemAccessibleObject.SetFocus();
                 }
---
+++ src/System.Windows.Forms/tests/UnitTests/PropertyGridInternal/PropertyGridViewTests.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Windows.Forms.Tests.PropertyGridInternal.Tests
+{
+    public class PropertyGridViewTests
+    {
+        [WinFormsFact]
+        public void PropertyGridView_Constructor()
+        {
+            var propertyGrid = new PropertyGrid();
+            var entity = new Entity();
+            propertyGrid.SelectedObject = entity;
+
+            var item = propertyGrid.SelectedGridItem;
+
+            Assert.NotNull(item);
+            Assert.NotNull(item.Value);
+        }
+    }
+
+    public class Entity
+    {
+        string[] Values { get; set; }
+    }
+}
