8138bd0e8bd045589aa735ee1501acec40923eb6
Mikhail Lipin <v-milipi@microsoft.com>
2020-03-31 03:32:36 +0300

    PropertyGridView crashes on editing ListBoxProperty with null v… (#3008)
    Fixing the issue with Property Grid crash in case GridViewListBox selected item is null.

---
+++ src/System.Windows.Forms/tests/UnitTests/PropertyGridInternal/PropertyGridViewTests.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+
+namespace System.Windows.Forms.Tests.PropertyGridInternal.Tests
+{
+    public class PropertyGridViewTests
+    {
+        [WinFormsFact]
+        public void PropertyGridView_Constructor()
+        {
+            using var propertyGrid = new PropertyGrid();
+            var entity = new Entity();
+            propertyGrid.SelectedObject = entity;
+
+            var item = propertyGrid.SelectedGridItem;
+
+            Assert.NotNull(item);
+            Assert.Same(entity, item.Value);
+        }
+
+        private class Entity
+        {
+            string[] Values { get; set; }
+        }
+    }
+}
