bf609cc473b9b2aaa22fd52b4d135f9a7c8445a1
kpreisser <kpreisser@users.noreply.github.com>
2020-04-02 19:15:18 +0200

    Remove dialog box command ID constants in favor of the existing ID enum.

--- src/System.Windows.Forms/src/System/Windows/Forms/TaskDialogResult.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/TaskDialogResult.cs
@@ -12,63 +12,63 @@ namespace System.Windows.Forms
     internal enum TaskDialogResult : int
     {
         /// <summary>
-        ///  A custom button was selected.
+        ///  No button was selected.
         /// </summary>
         None = 0,
 
         /// <summary>
         ///  The <c>OK</c> button was selected.
         /// </summary>
-        OK = User32.IDOK,
+        OK = User32.ID.OK,
 
         /// <summary>
         ///  The <c>Cancel</c> button was selected.
         /// </summary>
-        Cancel = User32.IDCANCEL,
+        Cancel = User32.ID.CANCEL,
 
         /// <summary>
         ///  The <c>Abort</c> button was selected.
         /// </summary>
-        Abort = User32.IDABORT,
+        Abort = User32.ID.ABORT,
 
         /// <summary>
         ///  The <c>Retry</c> button was selected.
         /// </summary>
-        Retry = User32.IDRETRY,
+        Retry = User32.ID.RETRY,
 
         /// <summary>
         ///  The <c>Ignore</c> button was selected.
         /// </summary>
-        Ignore = User32.IDIGNORE,
+        Ignore = User32.ID.IGNORE,
 
         /// <summary>
         ///  The <c>Yes</c> button was selected.
         /// </summary>
-        Yes = User32.IDYES,
+        Yes = User32.ID.YES,
 
         /// <summary>
         ///  The <c>No</c> button was selected.
         /// </summary>
-        No = User32.IDNO,
+        No = User32.ID.NO,
 
         /// <summary>
         ///  The <c>Close</c> button was selected.
         /// </summary>
-        Close = User32.IDCLOSE,
+        Close = User32.ID.CLOSE,
 
         /// <summary>
         ///  The <c>Help</c> button was selected.
         /// </summary>
-        Help = User32.IDHELP,
+        Help = User32.ID.HELP,
 
         /// <summary>
         ///  The <c>Try Again</c> button was selected.
         /// </summary>
-        TryAgain = User32.IDTRYAGAIN,
+        TryAgain = User32.ID.TRYAGAIN,
 
         /// <summary>
         ///  The <c>Continue</c> button was selected.
         /// </summary>
-        Continue = User32.IDCONTINUE
+        Continue = User32.ID.CONTINUE
     }
 }
