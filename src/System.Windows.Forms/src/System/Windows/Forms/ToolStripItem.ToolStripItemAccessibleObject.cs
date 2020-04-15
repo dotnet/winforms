4ff12f66bfd74157a9eb0ad3baa741afa4cbee17
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-04-15 15:08:00 +1000

    Part of #3062

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripItem.ToolStripItemAccessibleObject.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripItem.ToolStripItemAccessibleObject.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -476,7 +476,7 @@ namespace System.Windows.Forms
             internal void RaiseFocusChanged()
             {
                 ToolStrip root = _ownerItem.RootToolStrip;
-                if (root != null && root.SupportsUiaProviders)
+                if (root != null && root.IsHandleCreated && root.SupportsUiaProviders)
                 {
                     RaiseAutomationEvent(UiaCore.UIA.AutomationFocusChangedEventId);
                 }
