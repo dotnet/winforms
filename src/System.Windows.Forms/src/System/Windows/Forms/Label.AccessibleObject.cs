c37020da37a671a5d4c2e2676ac8feac4d870963
Mikhail Lipin <v-milipi@microsoft.com>
2020-05-04 22:33:09 +0300

    Moving LabelAccessibleObject to a separate file. Adding unit tests.

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/Label.AccessibleObject.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Runtime.InteropServices;
+using static Interop;
+
+namespace System.Windows.Forms
+{
+    public partial class Label
+    {
+        [ComVisible(true)]
+        internal class LabelAccessibleObject : ControlAccessibleObject
+        {
+            public LabelAccessibleObject(Label owner) : base(owner)
+            {
+            }
+
+            public override AccessibleRole Role
+            {
+                get
+                {
+                    AccessibleRole role = OwningLabel.AccessibleRole;
+                    if (role != AccessibleRole.Default)
+                    {
+                        return role;
+                    }
+
+                    return AccessibleRole.StaticText;
+                }
+            }
+
+            private Label OwningLabel => Owner as Label;
+
+            internal override object GetPropertyValue(UiaCore.UIA propertyID)
+                => propertyID switch
+                {
+                    UiaCore.UIA.NamePropertyId => Name,
+                    UiaCore.UIA.AutomationIdPropertyId
+                        => OwningLabel.IsHandleCreated ? OwningLabel.Name : string.Empty,
+                    UiaCore.UIA.ControlTypePropertyId => UiaCore.UIA.TextControlTypeId,
+                    _ => base.GetPropertyValue(propertyID)
+                };
+        }
+    }
+}
