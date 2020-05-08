aee0d5fc9f9d392f612e74ab97c87a9e4a3882eb
Mikhail Lipin <v-milipi@microsoft.com>
2020-05-08 21:36:27 +0300

    Adding UIA provider support to RadioButtonAccessibleObject. Moving RadioButtonAccessibleObject to separate file.

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/RadioButton.RadioButtonAccessibleObject.cs
@@ -0,0 +1,101 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using static Interop;
+
+namespace System.Windows.Forms
+{
+    public partial class RadioButton
+    {
+        public class RadioButtonAccessibleObject : ControlAccessibleObject
+        {
+            public RadioButtonAccessibleObject(RadioButton owner) : base(owner)
+            {
+                OwningRadioButton = owner;
+            }
+
+            private RadioButton OwningRadioButton { get; set; }
+
+            internal override bool IsPatternSupported(UiaCore.UIA patternId)
+            {
+                if (patternId == UiaCore.UIA.SelectionItemPatternId)
+                {
+                    return true;
+                }
+
+                return base.IsPatternSupported(patternId);
+            }
+
+            internal override object GetPropertyValue(UiaCore.UIA propertyID)
+                => propertyID switch
+                {
+                    UiaCore.UIA.NamePropertyId
+                        => Name,
+                    UiaCore.UIA.AutomationIdPropertyId
+                        => OwningRadioButton.IsHandleCreated ? OwningRadioButton.Name : string.Empty,
+                    UiaCore.UIA.IsSelectionItemPatternAvailablePropertyId
+                        => IsPatternSupported(UiaCore.UIA.SelectionItemPatternId),
+                    UiaCore.UIA.ControlTypePropertyId
+                        => UiaCore.UIA.RadioButtonControlTypeId,
+                    UiaCore.UIA.IsKeyboardFocusablePropertyId
+                        // This is necessary for compatibility with MSAA proxy:
+                        // IsKeyboardFocusable = true regardless the control is enabled/disabled.
+                        => true,
+                    _ => base.GetPropertyValue(propertyID)
+                };
+
+            public override string DefaultAction
+            {
+                get
+                {
+                    string defaultAction = Owner.AccessibleDefaultActionDescription;
+                    if (defaultAction != null)
+                    {
+                        return defaultAction;
+                    }
+
+                    return SR.AccessibleActionCheck;
+                }
+            }
+
+            internal override bool IsItemSelected
+                => OwningRadioButton.Checked;
+
+            public override void DoDefaultAction()
+            {
+                if (OwningRadioButton.IsHandleCreated)
+                {
+                    OwningRadioButton.PerformClick();
+                }
+            }
+
+            public override AccessibleRole Role
+            {
+                get
+                {
+                    AccessibleRole role = Owner.AccessibleRole;
+                    if (role != AccessibleRole.Default)
+                    {
+                        return role;
+                    }
+
+                    return AccessibleRole.RadioButton;
+                }
+            }
+
+            public override AccessibleStates State
+            {
+                get
+                {
+                    if (OwningRadioButton.Checked)
+                    {
+                        return AccessibleStates.Checked | base.State;
+                    }
+
+                    return base.State;
+                }
+            }
+        }
+    }
+}
--- src/System.Windows.Forms/src/System/Windows/Forms/RadioButton.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/RadioButton.cs
@@ -24,7 +24,7 @@ namespace System.Windows.Forms
     [ToolboxItem("System.Windows.Forms.Design.AutoSizeToolboxItem," + AssemblyRef.SystemDesign)]
     [Designer("System.Windows.Forms.Design.RadioButtonDesigner, " + AssemblyRef.SystemDesign)]
     [SRDescription(nameof(SR.DescriptionRadioButton))]
-    public class RadioButton : ButtonBase
+    public partial class RadioButton : ButtonBase
     {
         private static readonly object EVENT_CHECKEDCHANGED = new object();
         private static readonly ContentAlignment anyRight = ContentAlignment.TopRight | ContentAlignment.MiddleRight | ContentAlignment.BottomRight;
@@ -344,6 +344,8 @@ namespace System.Windows.Forms
             }
         }
 
+        internal override bool SupportsUiaProviders => true;
+
         [DefaultValue(false)]
         new public bool TabStop
         {
@@ -599,57 +601,6 @@ namespace System.Windows.Forms
         {
             string s = base.ToString();
             return s + ", Checked: " + Checked.ToString();
-        }
-
-        public class RadioButtonAccessibleObject : ButtonBaseAccessibleObject
-        {
-            public RadioButtonAccessibleObject(RadioButton owner) : base(owner)
-            {
-            }
-
-            public override string DefaultAction
-            {
-                get
-                {
-                    string defaultAction = Owner.AccessibleDefaultActionDescription;
-                    if (defaultAction != null)
-                    {
-                        return defaultAction;
-                    }
-
-                    return SR.AccessibleActionCheck;
-                }
-            }
-
-            public override AccessibleRole Role
-            {
-                get
-                {
-                    AccessibleRole role = Owner.AccessibleRole;
-                    if (role != AccessibleRole.Default)
-                    {
-                        return role;
-                    }
-                    return AccessibleRole.RadioButton;
-                }
-            }
-
-            public override AccessibleStates State
-            {
-                get
-                {
-                    if (((RadioButton)Owner).Checked)
-                    {
-                        return AccessibleStates.Checked | base.State;
-                    }
-                    return base.State;
-                }
-            }
-
-            public override void DoDefaultAction()
-            {
-                ((RadioButton)Owner).PerformClick();
-            }
         }
     }
 }
