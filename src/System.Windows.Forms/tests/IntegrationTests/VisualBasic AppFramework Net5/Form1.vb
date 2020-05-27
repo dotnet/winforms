2216a5d5b31a66473a242de4f8ddca7d3799e516
KlausLoeffelmann <klaus.loeffelmann@microsoft.com>
2020-05-26 17:05:08 -0700

    Implement minimal Visual Basic AppFramework WinForms app.

---
+++ src/System.Windows.Forms/tests/IntegrationTests/VisualBasic AppFramework Net5/Form1.vb
@@ -0,0 +1,5 @@
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hello VB!", "VB WinForms NET5.0")
+    End Sub
+End Class
