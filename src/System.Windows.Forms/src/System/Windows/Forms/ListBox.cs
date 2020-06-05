6d05c2597a92b24e1fe9878f7837dfb9b886f590
Hugh Bellamy <hughbellars@gmail.com>
2020-06-05 22:31:03 +0100

    Fix trailing null in NativeGetItemText (#3282)
    * Fix trailing null in NativeGetItemText
    
    * PR feedback

--- src/System.Windows.Forms/src/System/Windows/Forms/ListBox.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ListBox.cs
@@ -4,6 +4,7 @@
 
 #nullable disable
 
+using System.Buffers;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -1618,13 +1619,30 @@ namespace System.Windows.Forms
         /// <summary>
         ///  Get the text stored by the native control for the specified list item.
         /// </summary>
-        internal string NativeGetItemText(int index)
+        internal unsafe string NativeGetItemText(int index)
         {
-            int len = unchecked((int)(long)SendMessageW(this, (WM)LB.GETTEXTLEN, (IntPtr)index));
-            return string.Create(len + 1, this, (span, listBox) =>
+            int maxLength = PARAM.ToInt(SendMessageW(this, (WM)LB.GETTEXTLEN, (IntPtr)index));
+            if (maxLength == LB_ERR)
             {
-                SendMessageW(listBox, (WM)LB.GETTEXT, (IntPtr)index, ref span[0]);
-            });
+                return string.Empty;
+            }
+
+            char[] text = ArrayPool<char>.Shared.Rent(maxLength + 1);
+            string result;
+            fixed (char* pText = text)
+            {
+                int actualLength = PARAM.ToInt(SendMessageW(this, (WM)LB.GETTEXT, (IntPtr)index, (IntPtr)pText));
+                Debug.Assert(actualLength != LB_ERR, "Should have validated the index above");
+                if (actualLength == LB_ERR)
+                {
+                    return string.Empty;
+                }
+
+                result = new string(pText, 0, Math.Min(maxLength, actualLength));
+            }
+
+            ArrayPool<char>.Shared.Return(text);
+            return result;
         }
 
         /// <summary>
