312a32d8e436f1e62762a50bd7e63ecf311d2b16
Jeremy Kuhne <jkuhne@microsoft.com>
2020-06-11 14:18:40 -0700

    Update start-vs.cmd to use current VS
    Use the developer command prompt VS if available, fall back on devenv
    in the path, then finally to file association for .sln.
    
    Normalize casing of the commands.

--- start-vs.cmd
+++ start-vs.cmd
@@ -1,21 +1,31 @@
-@ECHO OFF
-SETLOCAL
+@echo off
+setlocal
 
 :: This command launches a Visual Studio solution with environment variables required to use a local version of the .NET Core SDK.
 
 :: This tells .NET Core to use the same dotnet.exe that build scripts use
-SET DOTNET_ROOT=%~dp0.dotnet
-SET DOTNET_ROOT(x86)=%~dp0.dotnet\x86
+set DOTNET_ROOT=%~dp0.dotnet
+set DOTNET_ROOT(x86)=%~dp0.dotnet\x86
 
 :: This tells .NET Core not to go looking for .NET Core in other places
-SET DOTNET_MULTILEVEL_LOOKUP=0
+set DOTNET_MULTILEVEL_LOOKUP=0
 
 :: Put our local dotnet.exe on PATH first so Visual Studio knows which one to use
-SET PATH=%DOTNET_ROOT%;%PATH%
+set PATH=%DOTNET_ROOT%;%PATH%
 
-IF NOT EXIST "%DOTNET_ROOT%\dotnet.exe" (
+if not exist "%DOTNET_ROOT%\dotnet.exe" (
     echo [ERROR] .NET Core has not yet been installed. Run `%~dp0restore.cmd` to install tools
     exit /b 1
 )
 
-start "" "Winforms.sln"
+:: Prefer the VS in the developer command prompt if we're in one, followed by whatever shows up in the current search path.
+set DEVENV="%DevEnvDir%devenv.exe"
+if not exist %DEVENV% (
+    set DEVENV="devenv.exe"
+    if not exist %DEVENV% (
+        :: Can't find devenv.exe, let file associations take care of it
+        set DEVENV=""
+    )
+)
+
+start %DEVENV% "Winforms.sln"
