c76e7b0d3d029fd8a529c314d6e8f339e77b292a
Paul M Cohen <paul1956@users.noreply.github.com>
2020-06-11 18:21:49 -0700

    Respond to PR feedback add additional tests

--- src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/ApplicationServices/SingleInstanceTests.cs
+++ src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/ApplicationServices/SingleInstanceTests.cs
@@ -8,8 +8,6 @@ using System.Collections.Immutable;
 using System.IO.Pipes;
 using System.Linq;
 using System.Reflection;
-using System.Reflection.Emit;
-using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading;
@@ -385,26 +383,6 @@ namespace Microsoft.VisualBasic.ApplicationServices.Tests
                 return null;
             }
             return pipeClient;
-        }
-
-        [Fact]
-        public void GetApplicationInstanceID_GuidAttribute()
-        {
-            var guid = Guid.NewGuid().ToString();
-            var attributeBuilder = new CustomAttributeBuilder(
-                typeof(GuidAttribute).GetConstructor(new[] { typeof(string) }), new[] { guid });
-            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
-                new AssemblyName(Guid.NewGuid().ToString()),
-                AssemblyBuilderAccess.RunAndCollect,
-                new[] { attributeBuilder });
-            assemblyBuilder.DefineDynamicModule(Guid.NewGuid().ToString());
-
-            var type = typeof(ApplicationServices.WindowsFormsApplicationBase);
-            var method = type.GetMethod("GetApplicationInstanceID",
-                BindingFlags.NonPublic | BindingFlags.Static);
-            Assert.Equal(
-                $"{guid}0.0",
-                (string)method.Invoke(null, new object[] { assemblyBuilder }));
         }
     }
 }
--- src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/ApplicationServices/WindowsFormsApplicationBaseTests.cs
+++ src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/ApplicationServices/WindowsFormsApplicationBaseTests.cs
@@ -2,7 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 using Xunit;
 
 namespace Microsoft.VisualBasic.ApplicationServices.Tests
@@ -22,6 +25,63 @@ namespace Microsoft.VisualBasic.ApplicationServices.Tests
             var assembly = typeof(WindowsFormsApplicationBaseTests).Assembly;
             var expectedId = assembly.ManifestModule.ModuleVersionId.ToString();
             Assert.Equal(expectedId, GetAppID(assembly));
+        }
+
+        [Fact]
+        public void GetApplicationInstanceID_GuidAttribute()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var attributeBuilder = new CustomAttributeBuilder(
+                typeof(GuidAttribute).GetConstructor(new[] { typeof(string) }), new[] { guid });
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
+                new AssemblyName(Guid.NewGuid().ToString()) { Version = new Version(1, 2, 3, 4) },
+                AssemblyBuilderAccess.RunAndCollect,
+                new[] { attributeBuilder });
+            assemblyBuilder.DefineDynamicModule(Guid.NewGuid().ToString());
+            var type = typeof(WindowsFormsApplicationBase);
+            var method = type.GetMethod("GetApplicationInstanceID",
+                BindingFlags.NonPublic | BindingFlags.Static);
+            Assert.Equal(
+                $"{guid}1.2",
+                (string)method.Invoke(null, new object[] { assemblyBuilder }));
+        }
+
+        [Fact]
+        public void GetApplicationInstanceID_GuidAttributeNewVersion()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var attributeBuilder = new CustomAttributeBuilder(
+                typeof(GuidAttribute).GetConstructor(new[] { typeof(string) }), new[] { guid });
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
+                new AssemblyName(Guid.NewGuid().ToString()) { Version = new Version() },
+                AssemblyBuilderAccess.RunAndCollect,
+                new[] { attributeBuilder });
+            assemblyBuilder.DefineDynamicModule(Guid.NewGuid().ToString());
+            var type = typeof(WindowsFormsApplicationBase);
+            var method = type.GetMethod("GetApplicationInstanceID",
+                BindingFlags.NonPublic | BindingFlags.Static);
+            Assert.Equal(
+                $"{guid}0.0",
+                (string)method.Invoke(null, new object[] { assemblyBuilder }));
+        }
+
+        [Fact]
+        public void GetApplicationInstanceID_GuidAttributeNullVersion()
+        {
+            var guid = Guid.NewGuid().ToString();
+            var attributeBuilder = new CustomAttributeBuilder(
+                typeof(GuidAttribute).GetConstructor(new[] { typeof(string) }), new[] { guid });
+            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(
+                new AssemblyName(Guid.NewGuid().ToString()) { Version = null },
+                AssemblyBuilderAccess.RunAndCollect,
+                new[] { attributeBuilder });
+            assemblyBuilder.DefineDynamicModule(Guid.NewGuid().ToString());
+            var type = typeof(WindowsFormsApplicationBase);
+            var method = type.GetMethod("GetApplicationInstanceID",
+                BindingFlags.NonPublic | BindingFlags.Static);
+            Assert.Equal(
+                $"{guid}0.0",
+                (string)method.Invoke(null, new object[] { assemblyBuilder }));
         }
     }
 }
