d7a88d8be41aec9bbd60626cb701fb3c8681b284
Charles Stoner <chucks@microsoft.com>
2020-06-12 16:44:37 -0700

    Test WindowsFormsApplicationBase.GetApplicationInstanceID (#3433)
    * Test WindowsFormsApplicationBase.GetApplicationInstanceID
    
    * Reset CA1825
    
    * Use TestAccessor

---
+++ src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/ApplicationServices/WindowsFormsApplicationBaseTests.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Microsoft.VisualBasic.ApplicationServices.Tests
+{
+    public class WindowsFormsApplicationBaseTests
+    {
+        private static string GetAppID(Assembly assembly)
+        {
+            var testAccessor = typeof(WindowsFormsApplicationBase).TestAccessor();
+            return testAccessor.Dynamic.GetApplicationInstanceID(assembly);
+        }
+
+        [Fact]
+        public void GetApplicationInstanceID()
+        {
+            var assembly = typeof(WindowsFormsApplicationBaseTests).Assembly;
+            var expectedId = assembly.ManifestModule.ModuleVersionId.ToString();
+            Assert.Equal(expectedId, GetAppID(assembly));
+        }
+    }
+}
