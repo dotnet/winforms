32eaa68bf5cde8a8619b61d079f48ce96f31cfd5
Hugh Bellamy <hughbellars@gmail.com>
2020-06-15 07:48:59 +0100

    Avoid initializing native tools twice (#3335)

--- eng/configure-toolset.ps1
+++ eng/configure-toolset.ps1
@@ -1,4 +1,2 @@
-$DoNotAbortNativeToolsInstallationOnFailure = $true
-$DoNotDisplayNativeToolsInstallationWarnings = $true
-
-. $PsScriptRoot\common\init-tools-native.ps1 -InstallDirectory $PSScriptRoot\..\.tools\native -GlobalJsonFile $PSScriptRoot\..\global.json
+$script:DoNotAbortNativeToolsInstallationOnFailure = $true
+$script:DoNotDisplayNativeToolsInstallationWarnings = $true
