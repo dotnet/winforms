5710d64f13cce019f12ce90b05b5ee69e6a9eb14
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-06-17 11:31:23 +1000

    fixup! Review public APIs for nullability

--- src/System.Windows.Forms.Primitives/src/System/Windows/Forms/FileDialogCustomPlacesCollection.cs
+++ src/System.Windows.Forms.Primitives/src/System/Windows/Forms/FileDialogCustomPlacesCollection.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -20,7 +20,7 @@ namespace System.Windows.Forms
                 try
                 {
                     IShellItem? shellItem = customPlace.GetNativePath();
-                    if (shellItem is object)
+                    if (shellItem != null)
                     {
                         dialog.AddPlace(shellItem, 0);
                     }
