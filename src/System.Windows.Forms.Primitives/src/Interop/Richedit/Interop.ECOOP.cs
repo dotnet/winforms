977acfe61364181cc7732f2f5098b2445d4db1e2
gpetrou <4172445+gpetrou@users.noreply.github.com>
2020-06-22 10:58:42 +0100

    Add ECO and ECOOP enums in Interop Richedit (#3481)

---
+++ src/System.Windows.Forms.Primitives/src/Interop/Richedit/Interop.ECOOP.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+internal partial class Interop
+{
+    internal static partial class Richedit
+    {
+        public enum ECOOP
+        {
+            SET = 0x0001,
+            OR = 0x0002,
+            AND = 0x0003,
+            XOR = 0x0004
+        }
+    }
+}
--- src/System.Windows.Forms/src/System/Windows/Forms/RichTextBoxConstants.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/RichTextBoxConstants.cs
@@ -13,30 +13,5 @@ namespace System.Windows.Forms
         internal const int RTB_HORIZ = 0x0001;
         internal const int RTB_VERT = 0x0002;
         internal const int RTB_FORCE = 0x0010;
-
-        /* RichTextBox messages */
-
-        /* TextBox control options */
-        internal const int ECO_AUTOWORDSELECTION = 0x00000001;
-        internal const int ECO_AUTOVSCROLL = 0x00000040;
-        internal const int ECO_AUTOHSCROLL = 0x00000080;
-        internal const int ECO_NOHIDESEL = 0x00000100;
-        internal const int ECO_READONLY = 0x00000800;
-        internal const int ECO_WANTRETURN = 0x00001000;
-        internal const int ECO_SAVESEL = 0x00008000;
-        internal const int ECO_SELECTIONBAR = 0x01000000; // guessing this is selection margin
-        internal const int ECO_VERTICAL = 0x00400000;   /* FE specific */
-
-        /* ECO operations */
-        internal const int ECOOP_SET = 0x0001;
-        internal const int ECOOP_OR = 0x0002;
-        internal const int ECOOP_AND = 0x0003;
-        internal const int ECOOP_XOR = 0x0004;
-
-        /* UNICODE embedding character */
-        // disable csharp compiler warning #0414: field assigned unused value
-#pragma warning disable 0414
-        internal static readonly char WCH_EMBEDDING = (char)0xFFFC;
-#pragma warning restore 0414
     }
 }
