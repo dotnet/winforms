6f85a5d9c78295e47bc1d0740d0b4b22cd551c34
Mikhail Lipin <v-milipi@microsoft.com>
2020-07-06 12:28:22 +0300

    Fixing issue 3206: Label control accessibility reworking (#3208)
    * Moving LabelAccessibleObject to a separate file. Adding unit tests.
    * Ensuring Label supports UIA providers.

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/Label.LabelAccessibleObject.cs
@@ -0,0 +1,46 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.InteropServices;
+using static Interop;
+
+namespace System.Windows.Forms
+{
+    public partial class Label
+    {
+        internal class LabelAccessibleObject : ControlAccessibleObject
+        {
+            private readonly Label _owningLabel;
+
+            public LabelAccessibleObject(Label owner) : base(owner)
+            {
+                _owningLabel = owner;
+            }
+
+            public override AccessibleRole Role
+            {
+                get
+                {
+                    AccessibleRole role = _owningLabel.AccessibleRole;
+
+                    if (role != AccessibleRole.Default)
+                    {
+                        return role;
+                    }
+
+                    return AccessibleRole.StaticText;
+                }
+            }
+
+            internal override object? GetPropertyValue(UiaCore.UIA propertyID)
+                => propertyID switch
+                {
+                    UiaCore.UIA.NamePropertyId => Name,
+                    UiaCore.UIA.AutomationIdPropertyId => _owningLabel.Name,
+                    UiaCore.UIA.ControlTypePropertyId => UiaCore.UIA.TextControlTypeId,
+                    _ => base.GetPropertyValue(propertyID)
+                };
+        }
+    }
+}
