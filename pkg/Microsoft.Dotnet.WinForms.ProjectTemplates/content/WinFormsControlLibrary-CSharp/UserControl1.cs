f04b45db75b52e67c6e208797a97946714e6ef74
Igor Velikorossov <RussKie@users.noreply.github.com>
2020-07-28 14:41:09 +1000

    Add template for WinForms Control Library (#3631)
    Resolves #3608

---
+++ pkg/Microsoft.Dotnet.WinForms.ProjectTemplates/content/WinFormsControlLibrary-CSharp/UserControl1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Company.ControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
