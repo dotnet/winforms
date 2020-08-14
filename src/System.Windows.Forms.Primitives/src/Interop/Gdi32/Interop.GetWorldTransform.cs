7aae3e4ec288d1e19a611878cd94de5900dd5301
Jeremy Kuhne <jkuhne@microsoft.com>
2020-08-14 11:42:23 -0700

    Simplify HDC caching
    We're getting intermittent errors in tests when trying to create a Graphics object from a cached DC. For 5.0 rolling this cache back to be per thread and no longer use the background thread to create HDCs. Also add additional diagonstics so we can better follow up on errors when they occur.

--- src/System.Windows.Forms.Primitives/src/Interop/Gdi32/Interop.GetWorldTransform.cs
+++ src/System.Windows.Forms.Primitives/src/Interop/Gdi32/Interop.GetWorldTransform.cs
@@ -3,21 +3,14 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Numerics;
 using System.Runtime.InteropServices;
 
 internal static partial class Interop
 {
     internal static partial class Gdi32
     {
-        // This does set last error but *only* in one error case that doesn't add any real value.
         [DllImport(Libraries.Gdi32, ExactSpelling = true)]
-        public static extern OBJ GetObjectType(HGDIOBJ h);
-
-        public static OBJ GetObjectType(HandleRef h)
-        {
-            OBJ result = GetObjectType((HGDIOBJ)h.Handle);
-            GC.KeepAlive(h.Wrapper);
-            return result;
-        }
+        public static extern BOOL GetWorldTransform(HDC hdc, ref Matrix3x2 matrix);
     }
 }
