41e2b1389654a184769d1eafdc980e55030b4dfd
Igor Velikorossov <RussKie@users.noreply.github.com>
2020-09-17 11:33:43 +1000

    Correct regression introduced in #659 (#3934)
    Resolves #3932
    
    (cherry picked from commit 6b20c21512a696a383a673e1411793846610000c)

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataGridViewRowConverterTests.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using Xunit;
+
+namespace System.Windows.Forms.Tests
+{
+    // NB: doesn't require thread affinity
+    public class DataGridViewRowConverterTests
+    {
+        [Fact]
+        public void CanConvertTo_returns_expected()
+        {
+            using DataGridViewRow row = new DataGridViewRow();
+            TypeConverter converter = TypeDescriptor.GetConverter(row);
+
+            Assert.True(converter.CanConvertTo(typeof(InstanceDescriptor)));
+        }
+
+        [Fact]
+        public void ConvertTo_returns_InstanceDescriptor()
+        {
+            using DataGridViewRow row = new DataGridViewRow();
+
+            TypeConverter converter = TypeDescriptor.GetConverter(row);
+            var descriptor = converter.ConvertTo(row, typeof(InstanceDescriptor));
+
+            Assert.NotNull(descriptor);
+            Assert.IsType<InstanceDescriptor>(descriptor);
+        }
+    }
+}
