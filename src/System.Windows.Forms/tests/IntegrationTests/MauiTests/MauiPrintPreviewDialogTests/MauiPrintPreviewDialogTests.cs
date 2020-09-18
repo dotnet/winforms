29b9d88f1bbb578997b6b50b8b110db91dc68c82
SergeySmirnov-Akvelon <sergey.smirnov@akvelon.com>
2020-09-18 09:50:26 +0300

    Revert changes. Fixed ui tests

--- src/System.Windows.Forms/tests/IntegrationTests/MauiTests/MauiPrintPreviewDialogTests/MauiPrintPreviewDialogTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/MauiTests/MauiPrintPreviewDialogTests/MauiPrintPreviewDialogTests.cs
@@ -11,17 +11,12 @@ namespace System.Windows.Forms.IntegrationTests.MauiTests
 {
     public class MauiPrintPreviewDialogTests : ReflectBase
     {
-        private readonly PrintPreviewDialog _printPreviewDialog;
+        private readonly PrintPreviewDialog printPreviewDialog;
 
         public MauiPrintPreviewDialogTests(string[] args) : base(args)
         {
             this.BringToForeground();
-            _printPreviewDialog = new PrintPreviewDialog {
-                Document = new Drawing.Printing.PrintDocument()
-            };
-
-            _printPreviewDialog.ShowDialog();
-            Application.DoEvents();
+            printPreviewDialog = new PrintPreviewDialog();
         }
 
         public static void Main(string[] args)
@@ -33,56 +28,46 @@ namespace System.Windows.Forms.IntegrationTests.MauiTests
         [Scenario(true)]
         public ScenarioResult Hotkey_Ctrl_1(TParams p)
         {
-            Application.DoEvents();
+            printPreviewDialog.Show();
             p.log.WriteLine("Press Ctrl+1");
             SendKeys.SendWait("^1");
-
-            Application.DoEvents();
-            return new ScenarioResult(_printPreviewDialog.PrintPreviewControl.Rows == 1 && _printPreviewDialog.PrintPreviewControl.Columns == 1);
+            return new ScenarioResult(printPreviewDialog.PrintPreviewControl.Rows == 1 && printPreviewDialog.PrintPreviewControl.Columns == 1);
         }
 
         [Scenario(true)]
         public ScenarioResult Hotkey_Ctrl_2(TParams p)
         {
-            Application.DoEvents();
+            printPreviewDialog.Show();
             p.log.WriteLine("Press Ctrl+2");
             SendKeys.SendWait("^2");
-
-            Application.DoEvents();
-            return new ScenarioResult(_printPreviewDialog.PrintPreviewControl.Rows == 1 && _printPreviewDialog.PrintPreviewControl.Columns == 2);
+            return new ScenarioResult(printPreviewDialog.PrintPreviewControl.Rows == 1 && printPreviewDialog.PrintPreviewControl.Columns == 2);
         }
 
         [Scenario(true)]
         public ScenarioResult Hotkey_Ctrl_3(TParams p)
         {
-            Application.DoEvents();
+            printPreviewDialog.Show();
             p.log.WriteLine("Press Ctrl+3");
             SendKeys.SendWait("^3");
-
-            Application.DoEvents();
-            return new ScenarioResult(_printPreviewDialog.PrintPreviewControl.Rows == 1 && _printPreviewDialog.PrintPreviewControl.Columns == 3);
+            return new ScenarioResult(printPreviewDialog.PrintPreviewControl.Rows == 1 && printPreviewDialog.PrintPreviewControl.Columns == 3);
         }
 
         [Scenario(true)]
         public ScenarioResult Hotkey_Ctrl_4(TParams p)
         {
-            Application.DoEvents();
+            printPreviewDialog.Show();
             p.log.WriteLine("Press Ctrl+4");
             SendKeys.SendWait("^4");
-
-            Application.DoEvents();
-            return new ScenarioResult(_printPreviewDialog.PrintPreviewControl.Rows == 2 && _printPreviewDialog.PrintPreviewControl.Columns == 2);
+            return new ScenarioResult(printPreviewDialog.PrintPreviewControl.Rows == 2 && printPreviewDialog.PrintPreviewControl.Columns == 2);
         }
 
         [Scenario(true)]
         public ScenarioResult Hotkey_Ctrl_5(TParams p)
         {
-            Application.DoEvents();
-            p.log.WriteLine("Press Ctrl+5 ");
+            printPreviewDialog.Show();
+            p.log.WriteLine("Press Ctrl+5");
             SendKeys.SendWait("^5");
-
-            Application.DoEvents();
-            return new ScenarioResult(_printPreviewDialog.PrintPreviewControl.Rows == 2 && _printPreviewDialog.PrintPreviewControl.Columns == 3);
+            return new ScenarioResult(printPreviewDialog.PrintPreviewControl.Rows == 2 && printPreviewDialog.PrintPreviewControl.Columns == 3);
         }
     }
 }
