d0974fc45e7be195721091b4a8d09caf7394397b
Sergey Smirnov <sergey.smirnov@akvelon.com>
2020-10-08 01:16:08 +0300

    Moving Combobox subclasses to their own files (#4064)

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/ComboBox.ComboBoxItemAccessibleObject.cs
@@ -0,0 +1,302 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Drawing;
+using System.Runtime.InteropServices;
+using Accessibility;
+using static Interop;
+using static Interop.User32;
+
+namespace System.Windows.Forms
+{
+    public partial class ComboBox
+    {
+        /// <summary>
+        ///  Represents the ComboBox item accessible object.
+        /// </summary>
+        internal class ComboBoxItemAccessibleObject : AccessibleObject
+        {
+            private readonly ComboBox _owningComboBox;
+            private readonly object _owningItem;
+            private IAccessible _systemIAccessible;
+
+            /// <summary>
+            ///  Initializes new instance of ComboBox item accessible object.
+            /// </summary>
+            /// <param name="owningComboBox">The owning ComboBox.</param>
+            /// <param name="owningItem">The owning ComboBox item.</param>
+            public ComboBoxItemAccessibleObject(ComboBox owningComboBox, object owningItem)
+            {
+                _owningComboBox = owningComboBox;
+                _owningItem = owningItem;
+
+                _systemIAccessible = _owningComboBox.ChildListAccessibleObject.GetSystemIAccessibleInternal();
+            }
+
+            /// <summary>
+            ///  Gets the ComboBox Item bounds.
+            /// </summary>
+            public override Rectangle Bounds
+            {
+                get
+                {
+                    ChildAccessibleObject listAccessibleObject = _owningComboBox.ChildListAccessibleObject;
+                    int currentIndex = GetCurrentIndex();
+
+                    Rectangle parentRect = listAccessibleObject.BoundingRectangle;
+                    int left = parentRect.Left;
+                    int top = parentRect.Top + _owningComboBox.ItemHeight * currentIndex;
+                    int width = parentRect.Width;
+                    int height = _owningComboBox.ItemHeight;
+
+                    return new Rectangle(left, top, width, height);
+                }
+            }
+
+            /// <summary>
+            ///  Gets the ComboBox item default action.
+            /// </summary>
+            public override string DefaultAction => _systemIAccessible.accDefaultAction[GetChildId()];
+
+            internal override UiaCore.IRawElementProviderFragment FragmentNavigate(UiaCore.NavigateDirection direction)
+            {
+                switch (direction)
+                {
+                    case UiaCore.NavigateDirection.Parent:
+                        return _owningComboBox.ChildListAccessibleObject;
+                    case UiaCore.NavigateDirection.NextSibling:
+                        int currentIndex = GetCurrentIndex();
+                        if (_owningComboBox.ChildListAccessibleObject is ComboBoxChildListUiaProvider comboBoxChildListUiaProvider)
+                        {
+                            int itemsCount = comboBoxChildListUiaProvider.GetChildFragmentCount();
+                            int nextItemIndex = currentIndex + 1;
+                            if (itemsCount > nextItemIndex)
+                            {
+                                return comboBoxChildListUiaProvider.GetChildFragment(nextItemIndex);
+                            }
+                        }
+                        break;
+                    case UiaCore.NavigateDirection.PreviousSibling:
+                        currentIndex = GetCurrentIndex();
+                        comboBoxChildListUiaProvider = _owningComboBox.ChildListAccessibleObject as ComboBoxChildListUiaProvider;
+                        if (comboBoxChildListUiaProvider != null)
+                        {
+                            var itemsCount = comboBoxChildListUiaProvider.GetChildFragmentCount();
+                            int previousItemIndex = currentIndex - 1;
+                            if (previousItemIndex >= 0)
+                            {
+                                return comboBoxChildListUiaProvider.GetChildFragment(previousItemIndex);
+                            }
+                        }
+
+                        break;
+                }
+
+                return base.FragmentNavigate(direction);
+            }
+
+            internal override UiaCore.IRawElementProviderFragmentRoot FragmentRoot
+            {
+                get
+                {
+                    return _owningComboBox.AccessibilityObject;
+                }
+            }
+
+            private int GetCurrentIndex()
+            {
+                return _owningComboBox.Items.IndexOf(_owningItem);
+            }
+
+            internal override int GetChildId()
+            {
+                return GetCurrentIndex() + 1; // Index is zero-based, Child ID is 1-based.
+            }
+
+            internal override object GetPropertyValue(UiaCore.UIA propertyID)
+            {
+                switch (propertyID)
+                {
+                    case UiaCore.UIA.RuntimeIdPropertyId:
+                        return RuntimeId;
+                    case UiaCore.UIA.BoundingRectanglePropertyId:
+                        return BoundingRectangle;
+                    case UiaCore.UIA.ControlTypePropertyId:
+                        return UiaCore.UIA.ListItemControlTypeId;
+                    case UiaCore.UIA.NamePropertyId:
+                        return Name;
+                    case UiaCore.UIA.AccessKeyPropertyId:
+                        return KeyboardShortcut ?? string.Empty;
+                    case UiaCore.UIA.HasKeyboardFocusPropertyId:
+                        return _owningComboBox.Focused && _owningComboBox.SelectedIndex == GetCurrentIndex();
+                    case UiaCore.UIA.IsKeyboardFocusablePropertyId:
+                        return (State & AccessibleStates.Focusable) == AccessibleStates.Focusable;
+                    case UiaCore.UIA.IsEnabledPropertyId:
+                        return _owningComboBox.Enabled;
+                    case UiaCore.UIA.HelpTextPropertyId:
+                        return Help ?? string.Empty;
+                    case UiaCore.UIA.IsControlElementPropertyId:
+                        return true;
+                    case UiaCore.UIA.IsContentElementPropertyId:
+                        return true;
+                    case UiaCore.UIA.IsPasswordPropertyId:
+                        return false;
+                    case UiaCore.UIA.IsOffscreenPropertyId:
+                        return (State & AccessibleStates.Offscreen) == AccessibleStates.Offscreen;
+                    case UiaCore.UIA.IsSelectionItemPatternAvailablePropertyId:
+                        return true;
+                    case UiaCore.UIA.SelectionItemIsSelectedPropertyId:
+                        return (State & AccessibleStates.Selected) != 0;
+                    case UiaCore.UIA.SelectionItemSelectionContainerPropertyId:
+                        return _owningComboBox.ChildListAccessibleObject;
+
+                    default:
+                        return base.GetPropertyValue(propertyID);
+                }
+            }
+
+            /// <summary>
+            ///  Gets the help text.
+            /// </summary>
+            public override string Help => _systemIAccessible.accHelp[GetChildId()];
+
+            /// <summary>
+            ///  Indicates whether specified pattern is supported.
+            /// </summary>
+            /// <param name="patternId">The pattern ID.</param>
+            /// <returns>True if specified </returns>
+            internal override bool IsPatternSupported(UiaCore.UIA patternId)
+            {
+                if (patternId == UiaCore.UIA.LegacyIAccessiblePatternId ||
+                    patternId == UiaCore.UIA.InvokePatternId ||
+                    patternId == UiaCore.UIA.SelectionItemPatternId)
+                {
+                    return true;
+                }
+
+                return base.IsPatternSupported(patternId);
+            }
+
+            /// <summary>
+            ///  Gets or sets the accessible name.
+            /// </summary>
+            public override string Name
+            {
+                get
+                {
+                    if (_owningComboBox != null)
+                    {
+                        return _owningComboBox.GetItemText(_owningItem);
+                    }
+
+                    return base.Name;
+                }
+                set => base.Name = value;
+            }
+
+            /// <summary>
+            ///  Gets the accessible role.
+            /// </summary>
+            public override AccessibleRole Role
+            {
+                get
+                {
+                    var accRole = _systemIAccessible.get_accRole(GetChildId());
+                    return accRole != null
+                        ? (AccessibleRole)accRole
+                        : AccessibleRole.None;
+                }
+            }
+
+            /// <summary>
+            ///  Gets the runtime ID.
+            /// </summary>
+            internal override int[] RuntimeId
+            {
+                get
+                {
+                    if (!_owningComboBox.IsHandleCreated)
+                    {
+                        return base.RuntimeId;
+                    }
+
+                    var runtimeId = new int[4];
+                    runtimeId[0] = RuntimeIDFirstItem;
+                    runtimeId[1] = (int)(long)_owningComboBox.InternalHandle;
+                    runtimeId[2] = _owningComboBox.GetListNativeWindowRuntimeIdPart();
+
+                    var comboBoxAccessibleObject = _owningComboBox.AccessibilityObject as ComboBoxAccessibleObject;
+                    runtimeId[3] = comboBoxAccessibleObject.ItemAccessibleObjects.GetId(_owningItem);
+
+                    return runtimeId;
+                }
+            }
+
+            /// <summary>
+            ///  Gets the accessible state.
+            /// </summary>
+            public override AccessibleStates State
+            {
+                get
+                {
+                    var accState = _systemIAccessible.get_accState(GetChildId());
+                    return accState != null
+                        ? (AccessibleStates)accState
+                        : AccessibleStates.None;
+                }
+            }
+
+            internal override void SetFocus()
+            {
+                RaiseAutomationEvent(UiaCore.UIA.AutomationFocusChangedEventId);
+
+                base.SetFocus();
+            }
+
+            internal unsafe override void SelectItem()
+            {
+                if (!_owningComboBox.IsHandleCreated)
+                {
+                    return;
+                }
+
+                _owningComboBox.SelectedIndex = GetCurrentIndex();
+                InvalidateRect(new HandleRef(this, _owningComboBox.GetListHandle()), null, BOOL.FALSE);
+            }
+
+            internal override void AddToSelection()
+            {
+                if (!_owningComboBox.IsHandleCreated)
+                {
+                    return;
+                }
+
+                SelectItem();
+            }
+
+            internal override void RemoveFromSelection()
+            {
+                // Do nothing, C++ implementation returns UIA_E_INVALIDOPERATION 0x80131509
+            }
+
+            internal override bool IsItemSelected
+            {
+                get
+                {
+                    return (State & AccessibleStates.Selected) != 0;
+                }
+            }
+
+            internal override UiaCore.IRawElementProviderSimple ItemSelectionContainer
+            {
+                get
+                {
+                    return _owningComboBox.ChildListAccessibleObject;
+                }
+            }
+        }
+    }
+}
---
+++ src/System.Windows.Forms/src/System/Windows/Forms/ComboBox.ObjectCollection.cs
@@ -0,0 +1,435 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace System.Windows.Forms
+{
+    public partial class ComboBox
+    {
+        [ListBindable(false)]
+        public class ObjectCollection : IList
+        {
+            private readonly ComboBox _owner;
+            private ArrayList _innerList;
+            private IComparer _comparer;
+
+            public ObjectCollection(ComboBox owner)
+            {
+                _owner = owner;
+            }
+
+            private IComparer Comparer
+            {
+                get
+                {
+                    if (_comparer is null)
+                    {
+                        _comparer = new ItemComparer(_owner);
+                    }
+                    return _comparer;
+                }
+            }
+
+            private ArrayList InnerList
+            {
+                get
+                {
+                    if (_innerList is null)
+                    {
+                        _innerList = new ArrayList();
+                    }
+                    return _innerList;
+                }
+            }
+
+            /// <summary>
+            ///  Retrieves the number of items.
+            /// </summary>
+            public int Count
+            {
+                get
+                {
+                    return InnerList.Count;
+                }
+            }
+
+            object ICollection.SyncRoot
+            {
+                get
+                {
+                    return this;
+                }
+            }
+
+            bool ICollection.IsSynchronized
+            {
+                get
+                {
+                    return false;
+                }
+            }
+
+            bool IList.IsFixedSize
+            {
+                get
+                {
+                    return false;
+                }
+            }
+
+            public bool IsReadOnly
+            {
+                get
+                {
+                    return false;
+                }
+            }
+
+            /// <summary>
+            ///  Adds an item to the combo box. For an unsorted combo box, the item is
+            ///  added to the end of the existing list of items. For a sorted combo box,
+            ///  the item is inserted into the list according to its sorted position.
+            ///  The item's toString() method is called to obtain the string that is
+            ///  displayed in the combo box.
+            ///  A SystemException occurs if there is insufficient space available to
+            ///  store the new item.
+            /// </summary>
+            public int Add(object item)
+            {
+                _owner.CheckNoDataSource();
+                int index = AddInternal(item);
+                if (_owner.UpdateNeeded() && _owner.AutoCompleteSource == AutoCompleteSource.ListItems)
+                {
+                    _owner.SetAutoComplete(false, false);
+                }
+                return index;
+            }
+
+            private int AddInternal(object item)
+            {
+                if (item is null)
+                {
+                    throw new ArgumentNullException(nameof(item));
+                }
+                int index = -1;
+                if (!_owner._sorted)
+                {
+                    InnerList.Add(item);
+                }
+                else
+                {
+                    index = InnerList.BinarySearch(item, Comparer);
+                    if (index < 0)
+                    {
+                        index = ~index; // getting the index of the first element that is larger than the search value
+                    }
+
+                    Debug.Assert(index >= 0 && index <= InnerList.Count, "Wrong index for insert");
+                    InnerList.Insert(index, item);
+                }
+                bool successful = false;
+
+                try
+                {
+                    if (_owner._sorted)
+                    {
+                        if (_owner.IsHandleCreated)
+                        {
+                            _owner.NativeInsert(index, item);
+                        }
+                    }
+                    else
+                    {
+                        index = InnerList.Count - 1;
+                        if (_owner.IsHandleCreated)
+                        {
+                            _owner.NativeAdd(item);
+                        }
+                    }
+                    successful = true;
+                }
+                finally
+                {
+                    if (!successful)
+                    {
+                        InnerList.Remove(item);
+                    }
+                }
+
+                return index;
+            }
+
+            int IList.Add(object item)
+            {
+                return Add(item);
+            }
+
+            public void AddRange(object[] items)
+            {
+                _owner.CheckNoDataSource();
+                _owner.BeginUpdate();
+                try
+                {
+                    AddRangeInternal(items);
+                }
+                finally
+                {
+                    _owner.EndUpdate();
+                }
+            }
+
+            internal void AddRangeInternal(IList items)
+            {
+                if (items is null)
+                {
+                    throw new ArgumentNullException(nameof(items));
+                }
+                foreach (object item in items)
+                {
+                    // adding items one-by-one for performance (especially for sorted combobox)
+                    // we can not rely on ArrayList.Sort since its worst case complexity is n*n
+                    // AddInternal is based on BinarySearch and ensures n*log(n) complexity
+                    AddInternal(item);
+                }
+                if (_owner.AutoCompleteSource == AutoCompleteSource.ListItems)
+                {
+                    _owner.SetAutoComplete(false, false);
+                }
+            }
+
+            /// <summary>
+            ///  Retrieves the item with the specified index.
+            /// </summary>
+            [Browsable(false)]
+            [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+            public virtual object this[int index]
+            {
+                get
+                {
+                    if (index < 0 || index >= InnerList.Count)
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(index), index, string.Format(SR.InvalidArgument, nameof(index), index));
+                    }
+
+                    return InnerList[index];
+                }
+                set
+                {
+                    _owner.CheckNoDataSource();
+                    SetItemInternal(index, value);
+                }
+            }
+
+            /// <summary>
+            ///  Removes all items from the ComboBox.
+            /// </summary>
+            public void Clear()
+            {
+                _owner.CheckNoDataSource();
+                ClearInternal();
+            }
+
+            internal void ClearInternal()
+            {
+                if (_owner.IsHandleCreated)
+                {
+                    _owner.NativeClear();
+                }
+
+                InnerList.Clear();
+                _owner._selectedIndex = -1;
+                if (_owner.AutoCompleteSource == AutoCompleteSource.ListItems)
+                {
+                    _owner.SetAutoComplete(false, true /*recreateHandle*/);
+                }
+            }
+
+            public bool Contains(object value)
+            {
+                return IndexOf(value) != -1;
+            }
+
+            /// <summary>
+            ///  Copies the ComboBox Items collection to a destination array.
+            /// </summary>
+            public void CopyTo(object[] destination, int arrayIndex)
+            {
+                InnerList.CopyTo(destination, arrayIndex);
+            }
+
+            void ICollection.CopyTo(Array destination, int index)
+            {
+                InnerList.CopyTo(destination, index);
+            }
+
+            /// <summary>
+            ///  Returns an enumerator for the ComboBox Items collection.
+            /// </summary>
+            public IEnumerator GetEnumerator()
+            {
+                return InnerList.GetEnumerator();
+            }
+
+            public int IndexOf(object value)
+            {
+                if (value is null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+
+                return InnerList.IndexOf(value);
+            }
+
+            /// <summary>
+            ///  Adds an item to the combo box. For an unsorted combo box, the item is
+            ///  added to the end of the existing list of items. For a sorted combo box,
+            ///  the item is inserted into the list according to its sorted position.
+            ///  The item's toString() method is called to obtain the string that is
+            ///  displayed in the combo box.
+            ///  A SystemException occurs if there is insufficient space available to
+            ///  store the new item.
+            /// </summary>
+            public void Insert(int index, object item)
+            {
+                _owner.CheckNoDataSource();
+
+                if (item is null)
+                {
+                    throw new ArgumentNullException(nameof(item));
+                }
+
+                if (index < 0 || index > InnerList.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), index, string.Format(SR.InvalidArgument, nameof(index), index));
+                }
+
+                // If the combo box is sorted, then nust treat this like an add
+                // because we are going to twiddle the index anyway.
+                //
+                if (_owner._sorted)
+                {
+                    Add(item);
+                }
+                else
+                {
+                    InnerList.Insert(index, item);
+                    if (_owner.IsHandleCreated)
+                    {
+                        bool successful = false;
+
+                        try
+                        {
+                            _owner.NativeInsert(index, item);
+                            successful = true;
+                        }
+                        finally
+                        {
+                            if (successful)
+                            {
+                                if (_owner.AutoCompleteSource == AutoCompleteSource.ListItems)
+                                {
+                                    _owner.SetAutoComplete(false, false);
+                                }
+                            }
+                            else
+                            {
+                                InnerList.RemoveAt(index);
+                            }
+                        }
+                    }
+                }
+            }
+
+            /// <summary>
+            ///  Removes an item from the ComboBox at the given index.
+            /// </summary>
+            public void RemoveAt(int index)
+            {
+                _owner.CheckNoDataSource();
+
+                if (index < 0 || index >= InnerList.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), index, string.Format(SR.InvalidArgument, nameof(index), index));
+                }
+
+                if (_owner.IsHandleCreated)
+                {
+                    _owner.NativeRemoveAt(index);
+                }
+
+                InnerList.RemoveAt(index);
+                if (!_owner.IsHandleCreated && index < _owner._selectedIndex)
+                {
+                    _owner._selectedIndex--;
+                }
+                if (_owner.AutoCompleteSource == AutoCompleteSource.ListItems)
+                {
+                    _owner.SetAutoComplete(false, false);
+                }
+            }
+
+            /// <summary>
+            ///  Removes the given item from the ComboBox, provided that it is
+            ///  actually in the list.
+            /// </summary>
+            public void Remove(object value)
+            {
+                int index = InnerList.IndexOf(value);
+
+                if (index != -1)
+                {
+                    RemoveAt(index);
+                }
+            }
+
+            internal void SetItemInternal(int index, object value)
+            {
+                if (index < 0 || index >= InnerList.Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), index, string.Format(SR.InvalidArgument, nameof(index), index));
+                }
+
+                InnerList[index] = value ?? throw new ArgumentNullException(nameof(value));
+
+                // If the native control has been created, and the display text of the new list item object
+                // is different to the current text in the native list item, recreate the native list item...
+                if (_owner.IsHandleCreated)
+                {
+                    bool selected = (index == _owner.SelectedIndex);
+
+                    if (string.Compare(_owner.GetItemText(value), _owner.NativeGetItemText(index), true, CultureInfo.CurrentCulture) != 0)
+                    {
+                        _owner.NativeRemoveAt(index);
+                        _owner.NativeInsert(index, value);
+                        if (selected)
+                        {
+                            _owner.SelectedIndex = index;
+                            _owner.UpdateText();
+                        }
+                        if (_owner.AutoCompleteSource == AutoCompleteSource.ListItems)
+                        {
+                            _owner.SetAutoComplete(false, false);
+                        }
+                    }
+                    else
+                    {
+                        // NEW - FOR COMPATIBILITY REASONS
+                        // Minimum compatibility fix
+                        if (selected)
+                        {
+                            _owner.OnSelectedItemChanged(EventArgs.Empty);   //we do this because set_SelectedIndex does this. (for consistency)
+                            _owner.OnSelectedIndexChanged(EventArgs.Empty);
+                        }
+                    }
+                }
+            }
+        } // end ObjectCollection
+    }
+}
