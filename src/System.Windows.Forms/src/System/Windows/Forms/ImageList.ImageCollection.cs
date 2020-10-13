40bb4daa9b789a4c271577b2d3b91ffbd2a24e99
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-10-13 12:27:33 +1100

    Correct and simplify ImageCollection_Item_Get32bppColorDepth_Success
    The original implementation incorrectly referenced elements in the collection.
    It also double disposed elements in the collection that under stress
    could lead to instabilities.
    And it was too verbose that made it harder to maintain the test.
    
    Closes #3898

--- src/System.Windows.Forms/src/System/Windows/Forms/ImageList.ImageCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ImageList.ImageCollection.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -150,7 +150,6 @@ namespace System.Windows.Forms
                     {
                         // Since there's no ImageList_ReplaceMasked, we need to generate
                         // a transparent bitmap
-                        Bitmap source = bitmap;
                         bitmap = (Bitmap)bitmap.Clone();
                         bitmap.MakeTransparent(_owner.TransparentColor);
                         ownsImage = true;
