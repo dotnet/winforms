ea4fbf253682b18070171499e207cfcc18415a68
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-10-15 12:23:06 +1100

    Fix initial directory and custom places in file dialogs
    `FileDialog.InitialDirectory` and `FileDialog.CustomPlaces` do not work as paths we're incorrectly marshalled as ANSI.
    
    Wrote an interop regression test and manually validated the two scenarios.
    
    (cherry picked from commit 197420f37596230252e283a2debc2f938bfb0e70)

---
+++ src/System.Windows.Forms.Primitives/tests/UnitTests/Interop/Shell32/ShellItemTests.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.IO;
+using System.Runtime.InteropServices;
+using Xunit;
+using static Interop;
+
+namespace System.Windows.Forms.Tests.Interop.Shell32Tests
+{
+    public class ShellItemTests
+    {
+        [Fact]
+        public void SHILCreateFromPath_ValidPath()
+        {
+            string path = Path.GetTempPath();
+            uint rgflnOut = default;
+            HRESULT result = Shell32.SHILCreateFromPath(path, out IntPtr ppidl, ref rgflnOut);
+            try
+            {
+                Assert.Equal(HRESULT.S_OK, result);
+                Assert.NotEqual(IntPtr.Zero, ppidl);
+            }
+            finally
+            {
+                if (ppidl != IntPtr.Zero)
+                {
+                    Marshal.FreeCoTaskMem(ppidl);
+                }
+            }
+        }
+    }
+}
