cb043bbff43e59014c4c4f71dc3b2d36158516eb
v-jiaol <62929087+Amy-Li03@users.noreply.github.com>
2020-10-21 23:33:45 -0700

    Add some codes in Accessibility sample app to work around bug 1155463 (#4132)

--- src/System.Windows.Forms/tests/AccessibilityTests/DataControls.cs
+++ src/System.Windows.Forms/tests/AccessibilityTests/DataControls.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Data;
 using System.Windows.Forms;
+using System.Windows.Forms.Automation;
 
 namespace AccessibilityTests
 {
@@ -41,6 +42,12 @@ namespace AccessibilityTests
             DataGridViewComboBoxEditingControl cbox = dataGridView1.EditingControl as DataGridViewComboBoxEditingControl;
             if (cbox != null)
                 cbox.DroppedDown = true;
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            dataGridView2.Focus();
+            bindingNavigator1.AccessibilityObject.RaiseAutomationNotification(AutomationNotificationKind.Other, AutomationNotificationProcessing.CurrentThenMostRecent, "Please enter first name now");
         }
     }
 }
