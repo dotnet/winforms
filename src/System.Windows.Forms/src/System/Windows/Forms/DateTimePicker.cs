7a8c10579e7fcb7472c82ee171e292907488914b
Thomas Bolon <tbolon@users.noreply.github.com>
2020-12-18 11:14:22 +0100

    rephrasing
    Co-authored-by: Igor Velikorossov <RussKie@users.noreply.github.com>

--- src/System.Windows.Forms/src/System/Windows/Forms/DateTimePicker.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/DateTimePicker.cs
@@ -1203,7 +1203,7 @@ namespace System.Windows.Forms
             if (IsHandleCreated)
             {
                 // fix #3533
-                // There is a bug in the underlying COM control where the datetime picker does not redraw its border correctly when switching between enabled and disabled state.
+                // There is a bug in the underlying Win32 control which prevents it from correctly redrawing its borders when switching between enabled and disabled state.
                 // The border color is never updated when switching state, so the control will always paint using the color set when the handle was created (either enabled or disabled).
                 // The only way to force the control to update its border color is to force a handle creation.
                 RecreateHandle();
