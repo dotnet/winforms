99335075d75b190e7136221635be972e764ba131
Igor Velikorossov <igor.velikorossov@microsoft.com>
2020-12-22 15:04:04 +1100

    Add test harness
    (cherry picked from commit ccdfaedd14e3d2325ca7d9d8611c92ad7f39edae)

---
+++ src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/MdiParent.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WinformsControlsTest
+{
+    public partial class MdiParent : Form
+    {
+        private readonly MenuStrip _menuStrip;
+
+        public MdiParent()
+        {
+            InitializeComponent();
+
+            Text = RuntimeInformation.FrameworkDescription;
+
+            _menuStrip = new MenuStrip();
+            _menuStrip.Items.Add(new ToolStripMenuItem { Text = "Parent" });
+        }
+
+        public MenuStrip MainMenu => _menuStrip;
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            MdiChild frm = new MdiChild();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+    }
+}
