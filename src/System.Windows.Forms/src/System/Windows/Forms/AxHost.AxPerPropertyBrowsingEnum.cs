ade2dc0e9dfeeb103fd6d53320eb0c8adff7bc81
Jeremy Kuhne <jkuhne@microsoft.com>
2021-02-10 16:49:05 -0800

    Some style cleanup

--- src/System.Windows.Forms/src/System/Windows/Forms/AxHost.AxPerPropertyBrowsingEnum.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AxHost.AxPerPropertyBrowsingEnum.cs
@@ -1,11 +1,10 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
 #nullable disable
 
 using System.Diagnostics;
-using System.Globalization;
 using System.Windows.Forms.ComponentModel.Com2Interop;
 using static Interop;
 
@@ -14,28 +13,33 @@ namespace System.Windows.Forms
     public abstract partial class AxHost
     {
         /// <summary>
-        ///  This exists for perf reasons. We delay doing this until we are actually asked for the
+        ///  This exists for perf reasons. We delay marshalling the data until we are actually asked for the
         ///  array of values.
         /// </summary>
         private class AxPerPropertyBrowsingEnum : Com2Enum
         {
-            private readonly AxPropertyDescriptor target;
-            private readonly AxHost owner;
-            private OleStrCAMarshaler nameMarshaller;
-            private Int32CAMarshaler valueMarshaller;
-            private bool arraysFetched;
+            private readonly AxPropertyDescriptor _target;
+            private readonly AxHost _owner;
+            private OleStrCAMarshaler _nameMarshaller;
+            private Int32CAMarshaler _valueMarshaller;
+            private bool _arraysFetched;
 
-            public AxPerPropertyBrowsingEnum(AxPropertyDescriptor targetObject, AxHost owner, OleStrCAMarshaler names, Int32CAMarshaler values, bool allowUnknowns) : base(Array.Empty<string>(), Array.Empty<object>(), allowUnknowns)
+            public AxPerPropertyBrowsingEnum(
+                AxPropertyDescriptor targetObject,
+                AxHost owner,
+                OleStrCAMarshaler names,
+                Int32CAMarshaler values,
+                bool allowUnknowns) : base(Array.Empty<string>(), Array.Empty<object>(), allowUnknowns)
             {
-                target = targetObject;
-                nameMarshaller = names;
-                valueMarshaller = values;
-                this.owner = owner;
-                arraysFetched = false;
+                _target = targetObject;
+                _nameMarshaller = names;
+                _valueMarshaller = values;
+                _owner = owner;
+                _arraysFetched = false;
             }
 
             /// <summary>
-            ///  Retrieve a copy of the value array
+            ///  Retrieve a copy of the value array.
             /// </summary>
             public override object[] Values
             {
@@ -59,24 +63,23 @@ namespace System.Windows.Forms
             }
 
             /// <summary>
-            ///  Ensure that we have processed the caStructs into arrays
-            ///  of values and strings
+            ///  Ensure that we have processed the caStructs into arrays of values and strings.
             /// </summary>
             private unsafe void EnsureArrays()
             {
-                if (arraysFetched)
+                if (_arraysFetched)
                 {
                     return;
                 }
 
-                arraysFetched = true;
+                _arraysFetched = true;
 
                 try
                 {
-                    // marshal the items.
-                    object[] nameItems = nameMarshaller.Items;
-                    object[] cookieItems = valueMarshaller.Items;
-                    Oleaut32.IPerPropertyBrowsing ppb = (Oleaut32.IPerPropertyBrowsing)owner.GetPerPropertyBrowsing();
+                    // Marshal the items.
+                    object[] nameItems = _nameMarshaller.Items;
+                    object[] cookieItems = _valueMarshaller.Items;
+                    Oleaut32.IPerPropertyBrowsing ppb = _owner.GetPerPropertyBrowsing();
                     int itemCount = 0;
 
                     Debug.Assert(cookieItems is not null && nameItems is not null, "An item array is null");
@@ -88,17 +91,18 @@ namespace System.Windows.Forms
 
                         Debug.Assert(cookieItems.Length == nameItems.Length, "Got uneven names and cookies");
 
-                        // for each name item, we ask the object for it's corresponding value.
+                        // For each name item, we ask the object for it's corresponding value.
                         for (int i = 0; i < nameItems.Length; i++)
                         {
                             cookie = (int)cookieItems[i];
                             if (nameItems[i] is null || !(nameItems[i] is string))
                             {
-                                Debug.Fail("Bad IPerPropertyBrowsing item [" + i.ToString(CultureInfo.InvariantCulture) + "], name=" + (nameItems is null ? "(unknown)" : nameItems[i].ToString()));
+                                Debug.Fail($"Bad IPerPropertyBrowsing item [{i}], name={(nameItems is null ? "(unknown)" : nameItems[i].ToString())}");
                                 continue;
                             }
+
                             using var var = new Oleaut32.VARIANT();
-                            HRESULT hr = ppb.GetPredefinedValue(target.Dispid, (uint)cookie, &var);
+                            HRESULT hr = ppb.GetPredefinedValue(_target.Dispid, (uint)cookie, &var);
                             if (hr == HRESULT.S_OK && var.vt != Ole32.VARENUM.EMPTY)
                             {
                                 valueItems[i] = var.ToObject();
@@ -106,7 +110,7 @@ namespace System.Windows.Forms
                             itemCount++;
                         }
 
-                        // pass this data down to the base Com2Enum object...
+                        // Pass the data to the base Com2Enum object.
                         if (itemCount > 0)
                         {
                             string[] strings = new string[itemCount];
@@ -117,20 +121,20 @@ namespace System.Windows.Forms
                 }
                 catch (Exception ex)
                 {
-                    Debug.Fail("Failed to build IPerPropertyBrowsing editor. " + ex.GetType().Name + ", " + ex.Message);
+                    Debug.Fail($"Failed to build IPerPropertyBrowsing editor. {ex.GetType().Name}, {ex.Message}");
                 }
             }
 
             internal void RefreshArrays(OleStrCAMarshaler names, Int32CAMarshaler values)
             {
-                nameMarshaller = names;
-                valueMarshaller = values;
-                arraysFetched = false;
+                _nameMarshaller = names;
+                _valueMarshaller = values;
+                _arraysFetched = false;
             }
 
             protected override void PopulateArrays(string[] names, object[] values)
             {
-                // we call base.PopulateArrays directly when we actually want to do this.
+                // We call base.PopulateArrays directly when we actually want to do this.
             }
 
             public override object FromString(string s)
