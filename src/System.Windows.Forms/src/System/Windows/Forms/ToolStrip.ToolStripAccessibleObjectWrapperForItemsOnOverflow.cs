871ef502b78c8785434dee9d8e5b10b28da80820
Sergey Smirnov <sergey.smirnov@akvelon.com>
2021-02-18 09:44:50 +0300

    Move sub-classes of "ToolStrip", "ToolStripContainer" and "ToolStripDropDown" classes to their own files (#4577)

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStrip.ToolStripAccessibleObjectWrapperForItemsOnOverflow.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms.Layout;
+using Microsoft.Win32;
+using static Interop;
+
+namespace System.Windows.Forms
+{
+    public partial class ToolStrip
+    {
+        private class ToolStripAccessibleObjectWrapperForItemsOnOverflow : ToolStripItem.ToolStripItemAccessibleObject
+        {
+            public ToolStripAccessibleObjectWrapperForItemsOnOverflow(ToolStripItem item)
+                : base(item)
+            {
+            }
+            public override AccessibleStates State
+            {
+                get
+                {
+                    AccessibleStates state = base.State;
+                    state |= AccessibleStates.Offscreen;
+                    state |= AccessibleStates.Invisible;
+                    return state;
+                }
+            }
+        }
+    }
+}
--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripContainer.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripContainer.cs
@@ -6,13 +6,12 @@
 
 using System.ComponentModel;
 using System.Drawing;
-using System.Globalization;
 
 namespace System.Windows.Forms
 {
     [Designer("System.Windows.Forms.Design.ToolStripContainerDesigner, " + AssemblyRef.SystemDesign)]
     [SRDescription(nameof(SR.ToolStripContainerDesc))]
-    public class ToolStripContainer : ContainerControl
+    public partial class ToolStripContainer : ContainerControl
     {
         private readonly ToolStripPanel topPanel;
         private readonly ToolStripPanel bottomPanel;
@@ -399,72 +398,6 @@ namespace System.Windows.Forms
         internal override bool AllowsKeyboardToolTip()
         {
             return false;
-        }
-
-        internal class ToolStripContainerTypedControlCollection : ReadOnlyControlCollection
-        {
-            readonly ToolStripContainer owner;
-            readonly Type contentPanelType = typeof(ToolStripContentPanel);
-            readonly Type panelType = typeof(ToolStripPanel);
-
-            public ToolStripContainerTypedControlCollection(Control c, bool isReadOnly)
-                : base(c, isReadOnly)
-            {
-                owner = c as ToolStripContainer;
-            }
-
-            public override void Add(Control value)
-            {
-                if (value is null)
-                {
-                    throw new ArgumentNullException(nameof(value));
-                }
-                if (IsReadOnly)
-                {
-                    throw new NotSupportedException(SR.ToolStripContainerUseContentPanel);
-                }
-
-                Type controlType = value.GetType();
-                if (!contentPanelType.IsAssignableFrom(controlType) && !panelType.IsAssignableFrom(controlType))
-                {
-                    throw new ArgumentException(string.Format(CultureInfo.CurrentCulture, string.Format(SR.TypedControlCollectionShouldBeOfTypes, contentPanelType.Name, panelType.Name)), value.GetType().Name);
-                }
-                base.Add(value);
-            }
-            public override void Remove(Control value)
-            {
-                if (value is ToolStripPanel || value is ToolStripContentPanel)
-                {
-                    if (!owner.DesignMode)
-                    {
-                        if (IsReadOnly)
-                        {
-                            throw new NotSupportedException(SR.ReadonlyControlsCollection);
-                        }
-                    }
-                }
-                base.Remove(value);
-            }
-
-            internal override void SetChildIndexInternal(Control child, int newIndex)
-            {
-                if (child is ToolStripPanel || child is ToolStripContentPanel)
-                {
-                    if (!owner.DesignMode)
-                    {
-                        if (IsReadOnly)
-                        {
-                            throw new NotSupportedException(SR.ReadonlyControlsCollection);
-                        }
-                    }
-                    else
-                    {
-                        // just no-op it at DT.
-                        return;
-                    }
-                }
-                base.SetChildIndexInternal(child, newIndex);
-            }
         }
     }
 }
