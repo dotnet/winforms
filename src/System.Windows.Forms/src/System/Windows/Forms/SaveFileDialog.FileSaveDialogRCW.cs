9a58ad90cdc82095fd69880daca1bcf1ec6ba707
SergeySmirnov-Akvelon <sergey.smirnov@akvelon.com>
2021-02-25 17:31:44 +0300

    Moved sub-classes of "FileDialog", "OpenFileDialog" and "SaveFileDialog" classes to their own files

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/SaveFileDialog.FileSaveDialogRCW.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Runtime.InteropServices;
+
+namespace System.Windows.Forms
+{
+    public sealed partial class SaveFileDialog
+    {
+        [ComImport]
+        [ClassInterface(ClassInterfaceType.None)]
+        [TypeLibType(TypeLibTypeFlags.FCanCreate)]
+        [Guid("C0B4E2F3-BA21-4773-8DBA-335EC946EB8B")]
+        private class FileSaveDialogRCW
+        {
+        }
+    }
+}
