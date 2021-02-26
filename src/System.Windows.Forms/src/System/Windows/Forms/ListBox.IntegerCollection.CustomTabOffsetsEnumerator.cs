c58f57d4619fad3b4c16dc30e28f14a9fca61416
SergeySmirnov-Akvelon <sergey.smirnov@akvelon.com>
2021-02-26 16:41:30 +0300

    Moved sub-classes of "DataGridViewRowCollection", "ListBox.IntegerCollection" and "ListBox.SelectedIndexCollection" classes to their own files

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/ListBox.IntegerCollection.CustomTabOffsetsEnumerator.cs
@@ -0,0 +1,77 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable disable
+
+using System.Collections;
+using System.ComponentModel;
+
+namespace System.Windows.Forms
+{
+    public partial class ListBox
+    {
+        public partial class IntegerCollection : IList
+        {
+            /// <summary>
+            ///  EntryEnumerator is an enumerator that will enumerate over
+            ///  a given state mask.
+            /// </summary>
+            private class CustomTabOffsetsEnumerator : IEnumerator
+            {
+                private readonly IntegerCollection items;
+                private int current;
+
+                /// <summary>
+                ///  Creates a new enumerator that will enumerate over the given state.
+                /// </summary>
+                public CustomTabOffsetsEnumerator(IntegerCollection items)
+                {
+                    this.items = items;
+                    current = -1;
+                }
+
+                /// <summary>
+                ///  Moves to the next element, or returns false if at the end.
+                /// </summary>
+                bool IEnumerator.MoveNext()
+                {
+                    if (current < items.Count - 1)
+                    {
+                        current++;
+                        return true;
+                    }
+                    else
+                    {
+                        current = items.Count;
+                        return false;
+                    }
+                }
+
+                /// <summary>
+                ///  Resets the enumeration back to the beginning.
+                /// </summary>
+                void IEnumerator.Reset()
+                {
+                    current = -1;
+                }
+
+                /// <summary>
+                ///  Retrieves the current value in the enumerator.
+                /// </summary>
+                object IEnumerator.Current
+                {
+                    get
+                    {
+                        if (current == -1 || current == items.Count)
+                        {
+                            throw new InvalidOperationException(SR.ListEnumCurrentOutOfRange);
+                        }
+
+                        return items[current];
+                    }
+                }
+            }
+        }
+    }
+}
--- src/System.Windows.Forms/src/System/Windows/Forms/ListBox.IntegerCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ListBox.IntegerCollection.cs
@@ -11,7 +11,7 @@ namespace System.Windows.Forms
 {
     public partial class ListBox
     {
-        public class IntegerCollection : IList
+        public partial class IntegerCollection : IList
         {
             private readonly ListBox owner;
             private int[] innerArray;
@@ -340,66 +340,6 @@ namespace System.Windows.Forms
             IEnumerator IEnumerable.GetEnumerator()
             {
                 return new CustomTabOffsetsEnumerator(this);
-            }
-
-            /// <summary>
-            ///  EntryEnumerator is an enumerator that will enumerate over
-            ///  a given state mask.
-            /// </summary>
-            private class CustomTabOffsetsEnumerator : IEnumerator
-            {
-                private readonly IntegerCollection items;
-                private int current;
-
-                /// <summary>
-                ///  Creates a new enumerator that will enumerate over the given state.
-                /// </summary>
-                public CustomTabOffsetsEnumerator(IntegerCollection items)
-                {
-                    this.items = items;
-                    current = -1;
-                }
-
-                /// <summary>
-                ///  Moves to the next element, or returns false if at the end.
-                /// </summary>
-                bool IEnumerator.MoveNext()
-                {
-                    if (current < items.Count - 1)
-                    {
-                        current++;
-                        return true;
-                    }
-                    else
-                    {
-                        current = items.Count;
-                        return false;
-                    }
-                }
-
-                /// <summary>
-                ///  Resets the enumeration back to the beginning.
-                /// </summary>
-                void IEnumerator.Reset()
-                {
-                    current = -1;
-                }
-
-                /// <summary>
-                ///  Retrieves the current value in the enumerator.
-                /// </summary>
-                object IEnumerator.Current
-                {
-                    get
-                    {
-                        if (current == -1 || current == items.Count)
-                        {
-                            throw new InvalidOperationException(SR.ListEnumCurrentOutOfRange);
-                        }
-
-                        return items[current];
-                    }
-                }
             }
         }
     }
