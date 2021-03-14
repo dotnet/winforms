ed114d886d58a6e2d165b64441753cb5f00d0628
Tobias Käs <tobias.kaes@gmail.com>
2021-03-15 00:19:55 +0100

    Show ClientGuid in PropertyGrid for interactive test

--- src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/Dialogs.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/Dialogs.cs
@@ -5,17 +5,64 @@
 #nullable enable
 
 using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Windows.Forms;
 
 namespace WinformsControlsTest
 {
     public partial class Dialogs : Form
     {
+        private sealed class ClientGuidConverter : GuidConverter
+        {
+            private StandardValuesCollection? _values;
+
+            public override bool GetStandardValuesSupported(ITypeDescriptorContext context) => true;
+            public override bool GetStandardValuesExclusive(ITypeDescriptorContext context) => true;
+
+            public override object? ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+            {
+                // base class is for plain Guid, but ClientGuid is nullable Guid, which we need to implement separately
+                if ((value is null) || (value is string str && str.Length == 0))
+                    return null;
+
+                return base.ConvertFrom(context, culture, value);
+            }
+
+            public override StandardValuesCollection GetStandardValues(ITypeDescriptorContext context)
+            {
+                if (_values is null)
+                {
+                    // chose from two GUIDs we pregenerated for testing
+                    _values = new StandardValuesCollection(new Guid?[]
+                    {
+                        null,
+                        new Guid("38EA9AE9-13BE-4992-9482-DAD370894BBD"),
+                        new Guid("46DFEE70-A89E-4D9A-8842-6D46DBC1F195"),
+                    });
+                }
+
+                return _values;
+            }
+        }
+
+        private sealed class ExposedClientGuidMetadata
+        {
+            [Browsable(true)]
+            [TypeConverter(typeof(ClientGuidConverter))]
+            public Guid? ClientGuid { get; set; }
+        }
+
         private readonly ToolStripButton _btnOpen;
 
         public Dialogs()
         {
             InitializeComponent();
+
+            TypeDescriptor.AddProvider(new AssociatedMetadataTypeTypeDescriptionProvider(openFileDialog1.GetType(), typeof(ExposedClientGuidMetadata)), openFileDialog1);
+            TypeDescriptor.AddProvider(new AssociatedMetadataTypeTypeDescriptionProvider(saveFileDialog1.GetType(), typeof(ExposedClientGuidMetadata)), saveFileDialog1);
+            TypeDescriptor.AddProvider(new AssociatedMetadataTypeTypeDescriptionProvider(folderBrowserDialog1.GetType(), typeof(ExposedClientGuidMetadata)), folderBrowserDialog1);
 
             _btnOpen = new("Open dialog")
             {
