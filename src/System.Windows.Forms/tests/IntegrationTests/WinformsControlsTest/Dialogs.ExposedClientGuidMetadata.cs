caf6e05dfee0719a413cbeb6d1f9ef4845da3d4c
Tobias Käs <tobias.kaes@gmail.com>
2021-03-15 23:05:05 +0100

    Move nested classes into separate files (PR feedback)

---
+++ src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/Dialogs.ExposedClientGuidMetadata.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System;
+using System.ComponentModel;
+
+namespace WinformsControlsTest
+{
+    public partial class Dialogs
+    {
+        private sealed class ExposedClientGuidMetadata
+        {
+            [Browsable(true)]
+            [TypeConverter(typeof(ClientGuidConverter))]
+            public Guid? ClientGuid { get; set; }
+        }
+    }
+}
