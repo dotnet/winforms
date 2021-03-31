f8db94c054423daa8c6439d3cc8eaf8cf4339fed
Vladimir Krestov <v-vlkres@microsoft.com>
2021-03-31 08:00:40 +0300

    Added an extended UI test case for MonthCalendar control

--- src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/Calendar.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/Calendar.cs
@@ -2,7 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using System.Windows.Forms;
+using static System.Windows.Forms.DateTimePicker;
 
 namespace WinformsControlsTest
 {
@@ -11,6 +13,56 @@ namespace WinformsControlsTest
         public Calendar()
         {
             InitializeComponent();
+            daysOfWeekComboBox.SelectedIndex = (int)monthCalendar1.FirstDayOfWeek;
+            showWeekNumbrsCheckBox.Checked = monthCalendar1.ShowWeekNumbers;
+            showTodayCheckBox.Checked = monthCalendar1.ShowToday;
+        }
+
+        private void setMinDateButton_Click(object sender, EventArgs e)
+        {
+            if (minDateDateTimePicker.Value > monthCalendar1.MaxDate)
+            {
+                MessageBox.Show("MinDate should be less then MaxDate", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            monthCalendar1.MinDate = minDateDateTimePicker.Value;
+        }
+
+        private void setMaxDateButton_Click(object sender, EventArgs e)
+        {
+            if (maxDateDateTimePicker.Value < monthCalendar1.MinDate)
+            {
+                MessageBox.Show("MaxDate should be grater then MinDate", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            monthCalendar1.MaxDate = maxDateDateTimePicker.Value;
+        }
+
+        private void daysOfWeekComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            monthCalendar1.FirstDayOfWeek = (Day)daysOfWeekComboBox.SelectedIndex;
+        }
+
+        private void resetMinDateButton_Click(object sender, EventArgs e)
+        {
+            monthCalendar1.MinDate = MinimumDateTime;
+        }
+
+        private void resetMaxDateButton_Click(object sender, EventArgs e)
+        {
+            monthCalendar1.MaxDate = MaximumDateTime;
+        }
+
+        private void showWeekNumbrsCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            monthCalendar1.ShowWeekNumbers = showWeekNumbrsCheckBox.Checked;
+        }
+
+        private void showTodayCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            monthCalendar1.ShowToday = showTodayCheckBox.Checked;
         }
     }
 }
