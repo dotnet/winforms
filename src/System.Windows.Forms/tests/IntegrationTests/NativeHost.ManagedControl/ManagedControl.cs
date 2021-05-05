a49857ac5d0b601ad7a0f0796b503a4cd7a0f54b
Tobias Käs <tobias.kaes@gmail.com>
2021-05-06 00:18:37 +0200

    add interactive test with native host for managed control

---
+++ src/System.Windows.Forms/tests/IntegrationTests/NativeHost.ManagedControl/ManagedControl.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NativeHost
+{
+    [ComVisible(true)]
+    [Guid("54479E5D-EABC-448C-A767-EAFF17BC28C9")]
+    [ComDefaultInterface(typeof(IManagedControl))]
+    public partial class ManagedControl : UserControl, IManagedControl
+    {
+        public ManagedControl()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world from .NET");
+        }
+    }
+
+    [ComVisible(true)]
+    [Guid("3223D73E-286A-462C-AF8D-392D472673BF")]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface IManagedControl { }
+}
