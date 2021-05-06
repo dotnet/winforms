b0d535228991220c193f592fa6c3cd2e7254e14e
Pranav K <prkrishn@hotmail.com>
2021-05-06 15:01:41 -0700

    React to MetadataUpdateHandler renames

--- src/System.Windows.Forms/src/System/Windows/Forms/Internal/WinFormsMetadataUpdateHandler.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Internal/WinFormsMetadataUpdateHandler.cs
@@ -14,7 +14,7 @@ namespace System.Windows.Forms
     {
         /// <summary>Invoked after a metadata update is applied.</summary>
         /// <param name="types">The list of types known to be updated, or null if it couldn't be determined.</param>
-        internal static void AfterUpdate(Type[]? types)
+        internal static void UpdateApplication(Type[]? types)
         {
             // Repaint all open forms.
             foreach (Form openForm in Application.OpenForms)
