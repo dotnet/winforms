e119304e766923f887533a9680f086f7dbe316b4
Josh Soref <jsoref@users.noreply.github.com>
2021-05-26 19:59:04 -0400

    spelling: collection
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CollectionCodeDomSerializer.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CollectionCodeDomSerializer.cs
@@ -165,7 +165,7 @@ namespace System.ComponentModel.Design.Serialization
                 // We serialize collections as follows:
                 //      If the collection is an array, we write out the array.
                 //      If the collection has a method called AddRange, we will call that, providing an array.
-                //      If the colleciton has an Add method, we will call it repeatedly.
+                //      If the collection has an Add method, we will call it repeatedly.
                 //      If the collection is an IList, we will cast to IList and add to it.
                 //      If the collection has no add method, but is marked with PersistContents, we will enumerate the collection and serialize each element.
                 // Check to see if there is a CodePropertyReferenceExpression on the stack.  If there is, we can use it as a guide for serialization.
