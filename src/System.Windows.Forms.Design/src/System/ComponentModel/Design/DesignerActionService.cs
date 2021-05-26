d594f49af40aac4e19e225e6ea9d20c5d65bd387
Josh Soref <jsoref@users.noreply.github.com>
2021-05-26 19:55:33 -0400

    spelling: accessed
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/DesignerActionService.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/DesignerActionService.cs
@@ -17,7 +17,7 @@ namespace System.ComponentModel.Design
         private readonly IServiceProvider _serviceProvider; // standard service provider
         private readonly ISelectionService _selSvc; // selection service
         private readonly Hashtable _componentToVerbsEventHookedUp; //table component true/false
-        // Guard against ReEntrant Code. The Infragistics TabControlDesigner, Sets the Commands Status when the Verbs property is accesssed. This property is used in the OnVerbStatusChanged code here and hence causes recursion leading to Stack Overflow Exception.
+        // Guard against ReEntrant Code. The Infragistics TabControlDesigner, Sets the Commands Status when the Verbs property is accessed. This property is used in the OnVerbStatusChanged code here and hence causes recursion leading to Stack Overflow Exception.
         private bool _reEntrantCode;
 
         /// <summary>
