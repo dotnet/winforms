e3893d2dbe9c362359858f4ecbfdfc9caa0dca99
Josh Soref <jsoref@users.noreply.github.com>
2021-05-26 20:10:04 -0400

    spelling: expandable
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/GridEntry.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/GridEntry.cs
@@ -533,7 +533,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                 {
                     _flags |= FL_EXPANDABLE;
 
-                    // If we're exapndable, but we don't support editing,
+                    // If we're expandable, but we don't support editing,
                     // make us read only editable so we don't paint grey.
                     //
                     if (!forceReadOnly && (Flags & FLAG_TEXT_EDITABLE) == 0 && !isImmutableReadOnly)
