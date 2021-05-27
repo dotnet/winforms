ca57838b5531d40bf12caa4488c6992ee54dec68
Josh Soref <jsoref@users.noreply.github.com>
2021-05-26 21:31:52 -0400

    spelling: redefine
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms/src/System/Windows/Forms/ApplicationContext.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ApplicationContext.cs
@@ -9,7 +9,7 @@ namespace System.Windows.Forms
 {
     /// <summary>
     ///  ApplicationContext provides contextual information about an application
-    ///  thread. Specifically this allows an application author to redifine what
+    ///  thread. Specifically this allows an application author to redefine what
     ///  circumstances cause a message loop to exit. By default the application
     ///  context listens to the close event on the mainForm, then exits the
     ///  thread's message loop.
