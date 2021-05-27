f7e59307af905406bd2d4d5cca3637f7d5c99a3d
Josh Soref <jsoref@users.noreply.github.com>
2021-05-26 21:36:29 -0400

    spelling: suppress
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms/src/System/Windows/Forms/ComponentModel/COM2Interop/COM2PropertyDescriptor.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ComponentModel/COM2Interop/COM2PropertyDescriptor.cs
@@ -1383,7 +1383,7 @@ namespace System.Windows.Forms.ComponentModel.Com2Interop
 
             private const int CheckSubprops = 0;
             private const int AllowSubprops = 1;
-            private const int SupressSubprops = 2;
+            private const int SuppressSubprops = 2;
 
             private int subprops = CheckSubprops;
 
@@ -1443,7 +1443,7 @@ namespace System.Windows.Forms.ComponentModel.Com2Interop
                 {
                     if (!base.GetPropertiesSupported(context))
                     {
-                        subprops = SupressSubprops;
+                        subprops = SuppressSubprops;
                     }
                     else
                     {
