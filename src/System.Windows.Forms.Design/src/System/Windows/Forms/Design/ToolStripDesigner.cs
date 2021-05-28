61bcc00c6404baa8cff146e837b12adf072e8494
Josh Soref <jsoref@users.noreply.github.com>
2021-05-27 23:22:28 -0400

    spelling: editmanager
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/ToolStripDesigner.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/ToolStripDesigner.cs
@@ -1182,7 +1182,7 @@ namespace System.Windows.Forms.Design
                 }
 
                 EnableDragDrop(false);
-                //Dispose of the EitManager
+                //Dispose of the EditManager
                 if (_editManager != null)
                 {
                     _editManager.CloseManager();
