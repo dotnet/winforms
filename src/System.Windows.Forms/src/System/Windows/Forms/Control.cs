30da3220860e3de8250bd2d4cf026f4c8495869a
Josh Soref <jsoref@users.noreply.github.com>
2021-05-28 00:17:27 -0400

    spelling: in order
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/System.Windows.Forms/src/System/Windows/Forms/Control.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Control.cs
@@ -12189,7 +12189,7 @@ namespace System.Windows.Forms
                 _deviceDpi = (int)User32.GetDpiForWindow(this);
 
                 // Controls are by default font scaled.
-                // Dpi change requires font to be recalculated inorder to get controls scaled with right dpi.
+                // Dpi change requires font to be recalculated in order to get controls scaled with right dpi.
                 if (_oldDeviceDpi != _deviceDpi)
                 {
                     var factor = (float)_deviceDpi / _oldDeviceDpi;
