9d4a57c887f60835ac4f20043b16d93744179dc5
Andrii Kurdiumov <kant2002@gmail.com>
2021-06-01 12:42:48 +0600

    Update src/System.Windows.Forms.Primitives/src/Interop/Interop.CLSID.cs
    Co-authored-by: Igor Velikorossov <RussKie@users.noreply.github.com>

--- src/System.Windows.Forms.Primitives/src/Interop/Interop.CLSID.cs
+++ src/System.Windows.Forms.Primitives/src/Interop/Interop.CLSID.cs
@@ -11,6 +11,7 @@ internal partial class Interop
         // C0B4E2F3-BA21-4773-8DBA-335EC946EB8B
         internal static Guid FileSaveDialog = new Guid(0xC0B4E2F3, 0xBA21, 0x4773, 0x8D, 0xBA, 0x33, 0x5E, 0xC9, 0x46, 0xEB, 0x8B);
 
-        internal static Guid FileOpenDialog = new Guid("DC1C5A9C-E88A-4dde-A5A1-60F82A20AEF7");
+        // DC1C5A9C-E88A-4DDE-A5A1-60F82A20AEF7
+        internal static Guid FileOpenDialog = new Guid(0xDC1C5A9C, 0xE88A, 0x4DDE, 0xA5, 0xA1, 0x60, 0xF8, 0x2A, 0x20, 0xAE, 0xF7);
     }
 }
