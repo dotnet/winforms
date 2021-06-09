check-spelling/dotnet-winforms

spelling: milliseconds
Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/PropertyGridView.cs
@@ -3053,7 +3053,7 @@ namespace System.Windows.Forms.PropertyGridInternal
             // this allows the edits to be selected with two clicks instead of 3 (select row, double click).
             //
             long timeStamp = DateTime.Now.Ticks;
-            int delta = (int)((timeStamp - rowSelectTime) / 10000); // make it milleseconds
+            int delta = (int)((timeStamp - rowSelectTime) / 10000); // make it milliseconds
 
             if (delta < SystemInformation.DoubleClickTime)
             {

dotnet/winforms

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/PropertyGridView.cs
@@ -3047,7 +3047,7 @@ namespace System.Windows.Forms.PropertyGridInternal
             // this allows the edits to be selected with two clicks instead of 3 (select row, double click).
             //
             long timeStamp = DateTime.Now.Ticks;
-            int delta = (int)((timeStamp - _rowSelectTime) / 10000); // make it milleseconds
+            int delta = (int)((timeStamp - _rowSelectTime) / 10000); // make it milliseconds
 
             if (delta < SystemInformation.DoubleClickTime)
             {