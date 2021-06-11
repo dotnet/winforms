d174e035e180aae7cc3d0736b1664b09b69300fd
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2021-06-10 20:00:47 -0700

    Address feedback.

--- src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/ApplicationServices/WindowsFormsApplicationBase.vb
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/ApplicationServices/WindowsFormsApplicationBase.vb
@@ -802,15 +802,11 @@ Namespace Microsoft.VisualBasic.ApplicationServices
             Dim invoked = False
 
             Try
-                If _appSyncronizationContext.IsWaitNotificationRequired() Then
-                    _appSyncronizationContext.Send(
-                        Sub()
-                            invoked = True
-                            OnStartupNextInstance(New StartupNextInstanceEventArgs(New ReadOnlyCollection(Of String)(args), bringToForegroundFlag:=True))
-                        End Sub, Nothing)
-                Else
-                    OnStartupNextInstance(New StartupNextInstanceEventArgs(New ReadOnlyCollection(Of String)(args), bringToForegroundFlag:=True))
-                End If
+                _appSyncronizationContext.Send(
+                    Sub()
+                        invoked = True
+                        OnStartupNextInstance(New StartupNextInstanceEventArgs(New ReadOnlyCollection(Of String)(args), bringToForegroundFlag:=True))
+                    End Sub, Nothing)
 
             Catch ex As Exception When Not invoked
                 ' Only catch exceptions thrown when the UI thread is not available, before
