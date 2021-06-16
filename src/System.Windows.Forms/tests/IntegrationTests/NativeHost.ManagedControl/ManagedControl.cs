c55ee5485a5d66946e7afc0a981c0c3065a793a6
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-06-16 18:02:38 +1000

    Fix usings

--- src/System.Windows.Forms/tests/IntegrationTests/NativeHost.ManagedControl/ManagedControl.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/NativeHost.ManagedControl/ManagedControl.cs
@@ -3,14 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NativeHost
