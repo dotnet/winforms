dotnet/winforms

commit c5e6890d27861fabd53da802c4da5c7da24ef883

Bring PropertyGrid members up to guidelines (#5058)
* Bring PropertyGrid members up to guidelines

A time boxed pass to fix more PropertyGrid member naming and accessiblity.

- Fix locking in CategoryGridEntry
- Remove TriState in MergePropertyDescriptor

* Fix test issues

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/MultiSelectRootGridEntry.cs
@@ -39,7 +39,7 @@ namespace System.Windows.Forms.PropertyGridInternal
 
                     if (anyRO)
                     {
-                        flags |= FLAG_FORCE_READONLY;
+                        Flags |= FLAG_FORCE_READONLY;
                     }
 
                     forceReadOnlyChecked = true;

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/SingleSelectRootGridEntry.cs
@@ -169,7 +169,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                     ReadOnlyAttribute readOnlyAttr = (ReadOnlyAttribute)TypeDescriptor.GetAttributes(objValue)[typeof(ReadOnlyAttribute)];
                     if ((readOnlyAttr is not null && !readOnlyAttr.IsDefaultAttribute()) || TypeDescriptor.GetAttributes(objValue).Contains(InheritanceAttribute.InheritedReadOnly))
                     {
-                        flags |= FLAG_FORCE_READONLY;
+                        Flags |= FLAG_FORCE_READONLY;
                     }
 
                     forceReadOnlyChecked = true;
@@ -257,7 +257,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                 object old = objValue;
                 objValue = value;
                 objValueClassName = TypeDescriptor.GetClassName(objValue);
-                ownerGrid.ReplaceSelectedObject(old, value);
+                OwnerGrid.ReplaceSelectedObject(old, value);
             }
         }
 
@@ -385,7 +385,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                                 bin.CopyTo(rgpes, 0);
                                 try
                                 {
-                                    propList.Add(new CategoryGridEntry(ownerGrid, this, category, rgpes));
+                                    propList.Add(new CategoryGridEntry(OwnerGrid, this, category, rgpes));
                                 }
                                 catch
                                 {

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGrid.SnappableControl.cs
@@ -12,24 +12,21 @@ namespace System.Windows.Forms
     {
         internal abstract class SnappableControl : Control
         {
-            protected PropertyGrid ownerPropertyGrid;
-            internal bool userSized;
+            protected PropertyGrid OwnerPropertyGrid { get; }
+            internal bool UserSized { get; set; }
 
             public abstract int GetOptimalHeight(int width);
             public abstract int SnapHeightRequest(int request);
 
             public SnappableControl(PropertyGrid ownerPropertyGrid)
             {
-                this.ownerPropertyGrid = ownerPropertyGrid;
+                OwnerPropertyGrid = ownerPropertyGrid;
                 SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
             }
 
             public override Cursor Cursor
             {
-                get
-                {
-                    return Cursors.Default;
-                }
+                get => Cursors.Default;
                 set => base.Cursor = value;
             }
 

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGrid.cs
@@ -22,7 +22,7 @@ using static Interop;
 
 namespace System.Windows.Forms
 {
-    [Designer("System.Windows.Forms.Design.PropertyGridDesigner, " + AssemblyRef.SystemDesign)]
+    [Designer($"System.Windows.Forms.Design.PropertyGridDesigner, {AssemblyRef.SystemDesign}")]
     [SRDescription(nameof(SR.DescriptionPropertyGrid))]
     public partial class PropertyGrid : ContainerControl, IComPropertyBrowser, Ole32.IPropertyNotifySink
     {
@@ -3108,7 +3108,7 @@ namespace System.Windows.Forms
                     if (_doccomment.Visible)
                     {
                         dcOptHeight = _doccomment.GetOptimalHeight(Size.Width - s_cyDivider);
-                        if (_doccomment.userSized)
+                        if (_doccomment.UserSized)
                         {
                             dcRequestedHeight = _doccomment.Size.Height;
                         }
@@ -3125,7 +3125,7 @@ namespace System.Windows.Forms
                     if (_hotcommands.Visible)
                     {
                         hcOptHeight = _hotcommands.GetOptimalHeight(Size.Width - s_cyDivider);
-                        if (_hotcommands.userSized)
+                        if (_hotcommands.UserSized)
                         {
                             hcRequestedHeight = _hotcommands.Size.Height;
                         }
@@ -3168,9 +3168,9 @@ namespace System.Windows.Forms
                     // if we've modified the height to less than the optimal, clear the userSized item
                     if (height <= dcOptHeight && height < dcRequestedHeight)
                     {
-                        _doccomment.userSized = false;
+                        _doccomment.UserSized = false;
                     }
-                    else if (_dcSizeRatio != -1 || _doccomment.userSized)
+                    else if (_dcSizeRatio != -1 || _doccomment.UserSized)
                     {
                         _dcSizeRatio = (_doccomment.Height * 100) / Height;
                     }
@@ -3201,9 +3201,9 @@ namespace System.Windows.Forms
                     // if we've modified the height, clear the userSized item
                     if (height <= hcOptHeight && height < hcRequestedHeight)
                     {
-                        _hotcommands.userSized = false;
+                        _hotcommands.UserSized = false;
                     }
-                    else if (_hcSizeRatio != -1 || _hotcommands.userSized)
+                    else if (_hcSizeRatio != -1 || _hotcommands.UserSized)
                     {
                         _hcSizeRatio = (_hotcommands.Height * 100) / Height;
                     }
@@ -3282,7 +3282,7 @@ namespace System.Windows.Forms
                 Size size = _targetMove.Size;
                 size.Height = Math.Max(0, yNew);
                 _targetMove.Size = size;
-                _targetMove.userSized = true;
+                _targetMove.UserSized = true;
                 OnLayoutInternal(true);
                 // invalidate the divider area so we cleanup anything
                 // left by the xor

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/ArrayElementGridEntry.cs
@@ -44,13 +44,7 @@ namespace System.Windows.Forms.PropertyGridInternal
             }
         }
 
-        public override Type PropertyType
-        {
-            get
-            {
-                return parentPE.PropertyType.GetElementType();
-            }
-        }
+        public override Type PropertyType => ParentGridEntry.PropertyType.GetElementType();
 
         public override object PropertyValue
         {

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/CategoryGridEntry.cs
@@ -14,14 +14,15 @@ namespace System.Windows.Forms.PropertyGridInternal
 {
     internal partial class CategoryGridEntry : GridEntry
     {
-        internal string name;
-        private Brush backBrush;
-        private static Hashtable categoryStates;
+        private readonly string _name;
+        private Brush _backBrush;
+        private static Hashtable s_categoryStates;
+        private readonly static object s_lock = new();
 
         public CategoryGridEntry(PropertyGrid ownerGrid, GridEntry peParent, string name, GridEntry[] childGridEntries)
-        : base(ownerGrid, peParent)
+            : base(ownerGrid, peParent)
         {
-            this.name = name;
+            _name = name;
 
 #if DEBUG
             for (int n = 0; n < childGridEntries.Length; n++)
@@ -29,16 +30,14 @@ namespace System.Windows.Forms.PropertyGridInternal
                 Debug.Assert(childGridEntries[n] is not null, "Null item in category subproperty list");
             }
 #endif
-            if (categoryStates is null)
-            {
-                categoryStates = new Hashtable();
-            }
 
-            lock (categoryStates)
+            lock (s_lock)
             {
-                if (!categoryStates.ContainsKey(name))
+                s_categoryStates ??= new Hashtable();
+
+                if (!s_categoryStates.ContainsKey(name))
                 {
-                    categoryStates.Add(name, true);
+                    s_categoryStates.Add(name, true);
                 }
             }
 
@@ -51,12 +50,12 @@ namespace System.Windows.Forms.PropertyGridInternal
 
             ChildCollection = new GridEntryCollection(this, childGridEntries);
 
-            lock (categoryStates)
+            lock (s_lock)
             {
-                InternalExpanded = (bool)categoryStates[name];
+                InternalExpanded = (bool)s_categoryStates[name];
             }
 
-            SetFlag(GridEntry.FLAG_LABEL_BOLD, true);
+            SetFlag(FLAG_LABEL_BOLD, true);
         }
 
         /// <summary>
@@ -74,10 +73,10 @@ namespace System.Windows.Forms.PropertyGridInternal
         {
             if (disposing)
             {
-                if (backBrush is not null)
+                if (_backBrush is not null)
                 {
-                    backBrush.Dispose();
-                    backBrush = null;
+                    _backBrush.Dispose();
+                    _backBrush = null;
                 }
 
                 if (ChildCollection is not null)
@@ -108,7 +107,7 @@ namespace System.Windows.Forms.PropertyGridInternal
 
         protected override Color GetBackgroundColor() => GridEntryHost.GetLineColor();
 
-        protected override Color LabelTextColor => ownerGrid.CategoryForeColor;
+        protected override Color LabelTextColor => OwnerGrid.CategoryForeColor;
 
         public override bool Expandable
         {
@@ -123,9 +122,9 @@ namespace System.Windows.Forms.PropertyGridInternal
             set
             {
                 base.InternalExpanded = value;
-                lock (categoryStates)
+                lock (s_lock)
                 {
-                    categoryStates[name] = value;
+                    s_categoryStates[_name] = value;
                 }
             }
         }
@@ -150,7 +149,7 @@ namespace System.Windows.Forms.PropertyGridInternal
         {
             get
             {
-                return name;
+                return _name;
             }
         }
 
@@ -167,10 +166,7 @@ namespace System.Windows.Forms.PropertyGridInternal
             }
         }
 
-        public override string GetPropertyTextValue(object o)
-        {
-            return "";
-        }
+        public override string GetPropertyTextValue(object o) => string.Empty;
 
         public override Type PropertyType
         {
@@ -207,9 +203,9 @@ namespace System.Windows.Forms.PropertyGridInternal
             base.PaintLabel(g, rect, clipRect, false, true);
 
             // now draw the focus rect
-            if (selected && hasFocus)
+            if (selected && HasFocus)
             {
-                bool bold = ((Flags & GridEntry.FLAG_LABEL_BOLD) != 0);
+                bool bold = ((Flags & FLAG_LABEL_BOLD) != 0);
                 Font font = GetFont(bold);
                 int labelWidth = GetLabelTextWidth(PropertyLabel, g, font);
 
@@ -226,10 +222,10 @@ namespace System.Windows.Forms.PropertyGridInternal
                 }
             }
 
-            // draw the line along the top
-            if (parentPE.GetChildIndex(this) > 0)
+            // Draw the line along the top
+            if (ParentGridEntry.GetChildIndex(this) > 0)
             {
-                using var topLinePen = ownerGrid.CategorySplitterColor.GetCachedPenScope();
+                using var topLinePen = OwnerGrid.CategorySplitterColor.GetCachedPenScope();
                 g.DrawLine(topLinePen, rect.X - 1, rect.Y - 1, rect.Width + 2, rect.Y - 1);
             }
         }
@@ -238,17 +234,17 @@ namespace System.Windows.Forms.PropertyGridInternal
         {
             base.PaintValue(val, g, rect, clipRect, paintFlags & ~PaintValueFlags.DrawSelected);
 
-            // draw the line along the top
-            if (parentPE.GetChildIndex(this) > 0)
+            // Draw the line along the top
+            if (ParentGridEntry.GetChildIndex(this) > 0)
             {
-                using var topLinePen = ownerGrid.CategorySplitterColor.GetCachedPenScope();
+                using var topLinePen = OwnerGrid.CategorySplitterColor.GetCachedPenScope();
                 g.DrawLine(topLinePen, rect.X - 2, rect.Y - 1, rect.Width + 1, rect.Y - 1);
             }
         }
 
         internal override bool NotifyChildValue(GridEntry pe, int type)
         {
-            return parentPE.NotifyChildValue(pe, type);
+            return ParentGridEntry.NotifyChildValue(pe, type);
         }
     }
 }

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/DetailsButton.cs
@@ -8,18 +8,18 @@ namespace System.Windows.Forms.PropertyGridInternal
 {
     internal partial class DetailsButton : Button
     {
-        private readonly GridErrorDlg parent;
+        private readonly GridErrorDlg _parent;
 
         public DetailsButton(GridErrorDlg form)
         {
-            parent = form;
+            _parent = form;
         }
 
         public bool Expanded
         {
             get
             {
-                return parent.DetailsButtonExpanded;
+                return _parent.DetailsButtonExpanded;
             }
         }
 

commit 452b9a6d337bf66010116f5a77da8de86b86e125

Reworked `Flags` to `_flags` in GridEntry to return previous logic that was broken in c5e6890. Fixes #5143 The main reason of the regression is that `Flags`'s getter calls TypeConverter, that tries to get non-initialized `_propertyInfo` field.

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/GridEntry.cs
@@ -132,7 +132,7 @@ namespace System.Windows.Forms.PropertyGridInternal
 
                 if (peParent.ForceReadOnly)
                 {
-                    Flags |= FLAG_FORCE_READONLY;
+                    _flags |= FLAG_FORCE_READONLY;
                 }
             }
             else
@@ -434,7 +434,7 @@ namespace System.Windows.Forms.PropertyGridInternal
         {
             get
             {
-                return (Flags & FLAG_FORCE_READONLY) != 0;
+                return (_flags & FLAG_FORCE_READONLY) != 0;
             }
         }
 
@@ -538,7 +538,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                     // If we're expandable, but we don't support editing,
                     // make us read only editable so we don't paint grey.
                     //
-                    if (!forceReadOnly && (Flags & FLAG_TEXT_EDITABLE) == 0 && !isImmutableReadOnly)
+                    if (!forceReadOnly && (_flags & FLAG_TEXT_EDITABLE) == 0 && !isImmutableReadOnly)
                     {
                         _flags |= FLAG_READONLY_EDITABLE;
                     }
@@ -754,25 +754,25 @@ namespace System.Windows.Forms.PropertyGridInternal
             get
             {
                 // prevent full flag population if possible.
-                if ((Flags & FL_CHECKED) == 0)
+                if ((_flags & FL_CHECKED) == 0)
                 {
                     UITypeEditor typeEd = UITypeEditor;
                     if (typeEd is not null)
                     {
-                        if ((Flags & FLAG_CUSTOM_PAINT) != 0 ||
-                            (Flags & FL_NO_CUSTOM_PAINT) != 0)
+                        if ((_flags & FLAG_CUSTOM_PAINT) != 0 ||
+                            (_flags & FL_NO_CUSTOM_PAINT) != 0)
                         {
-                            return (Flags & FLAG_CUSTOM_PAINT) != 0;
+                            return (_flags & FLAG_CUSTOM_PAINT) != 0;
                         }
 
                         if (typeEd.GetPaintValueSupported(this))
                         {
-                            Flags |= FLAG_CUSTOM_PAINT;
+                            _flags |= FLAG_CUSTOM_PAINT;
                             return true;
                         }
                         else
                         {
-                            Flags |= FL_NO_CUSTOM_PAINT;
+                            _flags |= FL_NO_CUSTOM_PAINT;
                             return false;
                         }
                     }
@@ -1826,7 +1826,7 @@ namespace System.Windows.Forms.PropertyGridInternal
 
                             if (forceReadOnly)
                             {
-                                newEntry.Flags |= FLAG_FORCE_READONLY;
+                                newEntry._flags |= FLAG_FORCE_READONLY;
                             }
 
                             // check to see if we've come across the default item.

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/MultiSelectRootGridEntry.cs
@@ -39,7 +39,7 @@ namespace System.Windows.Forms.PropertyGridInternal
 
                     if (anyRO)
                     {
-                        Flags |= FLAG_FORCE_READONLY;
+                        _flags |= FLAG_FORCE_READONLY;
                     }
 
                     forceReadOnlyChecked = true;

src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/SingleSelectRootGridEntry.cs
@@ -169,7 +169,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                     ReadOnlyAttribute readOnlyAttr = (ReadOnlyAttribute)TypeDescriptor.GetAttributes(objValue)[typeof(ReadOnlyAttribute)];
                     if ((readOnlyAttr is not null && !readOnlyAttr.IsDefaultAttribute()) || TypeDescriptor.GetAttributes(objValue).Contains(InheritanceAttribute.InheritedReadOnly))
                     {
-                        Flags |= FLAG_FORCE_READONLY;
+                        _flags |= FLAG_FORCE_READONLY;
                     }
 
                     forceReadOnlyChecked = true;

src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/PropertyGridTests.cs
@@ -3788,6 +3788,22 @@ namespace System.Windows.Forms.Tests
 #endif
         }
 
+        [WinFormsTheory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void PropertyGrid_SetEnableFalse_DoesntBreakEntries_AndFlagsReturnCorrectValue(bool enable)
+        {
+            using PropertyGrid propertyGrid = new();
+            using Button button = new();
+            propertyGrid.SelectedObject = button;
+            propertyGrid.Enabled = enable;
+            var entry = (GridEntry)propertyGrid.SelectedGridItem;
+
+            Assert.True(entry.Flags != 0);
+            Assert.False(propertyGrid.IsHandleCreated);
+            Assert.False(button.IsHandleCreated);
+        }
+
         private class SubToolStripRenderer : ToolStripRenderer
         {
         }