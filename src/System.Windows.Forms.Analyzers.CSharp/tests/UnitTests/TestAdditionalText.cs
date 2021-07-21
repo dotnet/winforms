31dcabdfd71a11ac72a7ff088170c81d7b0b93b3
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-07-21 20:34:05 +1000

    Add ApplicationConfiguration.Initialize() source generator
    Add a C# source generator that emits application bootstrap as described
    in https://github.com/dotnet/designs/blob/main/accepted/2021/winforms/streamline-application-bootstrap.md
    for the following properties:
    * ApplicationVisualStyles
    * ApplicationDefaultFont
    * ApplicationHighDpiMode
    * ApplicationUseCompatibleTextRendering
    
    For top level statements the bootstrap also emits code to set STA mode.
    
    A build will fail if any of the properties contain invalid or otherwise
    incorrect values.
    
    FontConverter is not available in netstandard 2.0, to bridge the gap
    FontConverter logic has been partially copied from the corresponding .NET
    runtime implementation to facilitate the comparable behaviour.
    The only missing gap is parsing of FontFamily (which requires GDI+ calls),
    which means that if a supplied font descriptor contains an invalid font
    name it will cause an app to crash. This however is a desired behaviour.

---
+++ src/System.Windows.Forms.Analyzers.CSharp/tests/UnitTests/TestAdditionalText.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace System.Windows.Forms.Analyzers.Tests
+{
+    // Borrowed from https://github.com/dotnet/roslyn/blob/main/src/Compilers/Test/Core/Mocks/TestAdditionalText.cs
+
+    [ExcludeFromCodeCoverage]
+    public sealed class TestAdditionalText : AdditionalText
+    {
+        private readonly SourceText _text;
+
+        public TestAdditionalText(string path, SourceText text)
+        {
+            Path = path;
+            _text = text;
+        }
+
+        public TestAdditionalText(string text = "", Encoding? encoding = null, string path = "dummy")
+            : this(path, SourceText.From(text, encoding))
+        {
+        }
+
+        public override string Path { get; }
+
+        public override SourceText GetText(CancellationToken cancellationToken = default) => _text;
+    }
+}
+
