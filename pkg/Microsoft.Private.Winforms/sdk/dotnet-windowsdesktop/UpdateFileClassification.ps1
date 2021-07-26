88480017bc85ec085f2b19acfc18f9ece48e1a50
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-07-26 16:47:24 +1000

    Package analyzers

--- pkg/Microsoft.Private.Winforms/sdk/dotnet-windowsdesktop/UpdateFileClassification.ps1
+++ pkg/Microsoft.Private.Winforms/sdk/dotnet-windowsdesktop/UpdateFileClassification.ps1
@@ -9,24 +9,24 @@ Param(
   [Parameter(ValueFromRemainingArguments=$true)][String[]] $properties
 )
 
-$assemblies = @( );
-# this isn't explicitly present in the list
-$assemblies += 'System.Drawing.Common.dll';
 
 [xml] $xmlDoc = Get-Content -Path $NuspecFile -Force;
-$xmlDoc.package.files.file | `
+
+$assemblies = $xmlDoc.package.files.file | `
     Where-Object { 
             # take only assemblies placed in \lib\netcoreappX.Y, and that are not resources
             # also exclude Accessibility.dll as it is explicitly added to WindowsDesktop bundle
-            $_.target.StartsWith('lib\') `
+            ($_.target.StartsWith('lib\') -or $_.target.StartsWith('ref\') -or $_.target.StartsWith('sdk\analyzers\'))`
                 -and $_.target.EndsWith('.dll', [System.StringComparison]::OrdinalIgnoreCase) `
                 -and !$_.target.EndsWith('resources.dll', [System.StringComparison]::OrdinalIgnoreCase) `
                 -and !$_.target.EndsWith('\Accessibility.dll', [System.StringComparison]::OrdinalIgnoreCase)
         } | `
-    ForEach-Object { 
-        $assembly = Split-Path $_.target -Leaf;
-        $assemblies += $assembly;
-    };
+    Select-Object -Unique @{Name="Path";Expression={Split-Path $_.target -Leaf}} | `
+    Select-Object  -ExpandProperty Path;
+
+# this isn't explicitly present in the list
+$assemblies += 'System.Drawing.Common.dll';
+
 
 $needGenerate = $null;
 [bool]::TryParse($GenerateManifest, [ref]$needGenerate) | Out-Null;
@@ -57,17 +57,20 @@ else {
         Update the existing manifest
     #>
     Write-Host "Regenerating the manifest" -ForegroundColor Green
-    $output = "<Project>
-      <ItemGroup Condition=`"'`$(PackageTargetRuntime)' == ''`">
-    ";
+
+    $output = "<!--
+    This props file comes from dotnet/winforms. It gets ingested by dotnet/windowsdesktop and processed by
+    pkg\windowsdesktop\sfx\Microsoft.WindowsDesktop.App.Ref.sfxproj.
+   -->
+<Project>
+  <ItemGroup Condition=`"'`$(PackageTargetRuntime)' == ''`">`r`n";
     $assemblies | `
         Sort-Object | `
         ForEach-Object {
             $assembly = $_;
-            $output += "    <FrameworkListFileClass Include=`"$assembly`" Profile=`"WindowsForms`" />
-    "
+            $output += "    <FrameworkListFileClass Include=`"$assembly`" Profile=`"WindowsForms`" />`r`n"
         }
     $output += "  </ItemGroup>
-    </Project>";
+</Project>";
     $output | Out-File -FilePath $TargetFile -Encoding utf8 -Force;
 }
