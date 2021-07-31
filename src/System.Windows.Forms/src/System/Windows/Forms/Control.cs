9002e3f08bd4ba2aa4b5d590ad0e37ea990c0c24
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2021-07-30 17:32:34 -0700

    Change IsAncestorSiteInDesignMode back to being a property.

--- src/System.Windows.Forms/src/System/Windows/Forms/Control.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Control.cs
@@ -2711,17 +2711,16 @@ namespace System.Windows.Forms
         [Browsable(false)]
         [EditorBrowsable(EditorBrowsableState.Advanced)]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-        public bool IsAncestorSiteInDesignMode(bool checkTopLevelControl) =>
-            GetSitedParentSite(this, checkTopLevelControl) is ISite parentSite ? parentSite.DesignMode : false;
+        public bool IsAncestorSiteInDesignMode =>
+            GetSitedParentSite(this) is ISite parentSite ? parentSite.DesignMode : false;
 
-        private ISite GetSitedParentSite(Control control, bool checkTopLevelControl) =>
+        private ISite GetSitedParentSite(Control control) =>
             control is null
                 ? throw new ArgumentNullException(nameof(control))
-                : control.Site is not null ||
-                  control.Parent is null ||
-                  (!checkTopLevelControl && control.Site.DesignMode)
+                : (control.Site is not null && control.Site.DesignMode) ||
+                  control.Parent is null
                     ? control.Site
-                    : GetSitedParentSite(control.Parent, checkTopLevelControl);
+                    : GetSitedParentSite(control.Parent);
 
         // If the control on which GetContainerControl( ) is called is a ContainerControl, then we don't return the parent
         // but return the same control. This is Everett behavior so we cannot change this since this would be a breaking change.
