61f35f8147e7d90006ae25495820dec3600f118d
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-08-10 17:17:22 +1000

    Migrate to source generators API v2
    Relates to https://github.com/dotnet/runtime/issues/56702#issuecomment-895578440

--- src/System.Windows.Forms.Analyzers.CSharp/src/System/Windows/Forms/Generators/ApplicationConfigurationGenerator.cs
+++ src/System.Windows.Forms.Analyzers.CSharp/src/System/Windows/Forms/Generators/ApplicationConfigurationGenerator.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Collections.Immutable;
 using System.Linq;
 using System.Windows.Forms.Analyzers;
 using Microsoft.CodeAnalysis;
@@ -9,38 +10,33 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace System.Windows.Forms.Generators
 {
-    [Generator]
-    internal class ApplicationConfigurationGenerator : ISourceGenerator
+    [Generator(LanguageNames.CSharp)]
+    internal class ApplicationConfigurationGenerator : IIncrementalGenerator
     {
-        public void Execute(GeneratorExecutionContext context)
+        private void Execute(Compilation compilation, ImmutableArray<SyntaxNode> syntaxNodes, SourceProductionContext context)
         {
-            if (context.SyntaxReceiver is not ApplicationConfigurationSyntaxReceiver syntaxReceiver)
-            {
-                throw new InvalidOperationException("We were given the wrong syntax receiver.");
-            }
-
-            if (syntaxReceiver.Nodes.Count == 0)
+            if (syntaxNodes.IsEmpty)
             {
                 return;
             }
 
-            if (context.Compilation.Options.OutputKind != OutputKind.WindowsApplication &&
+            if (compilation.Options.OutputKind != OutputKind.WindowsApplication &&
                 // Starting in the 5.0.100 version of the .NET SDK, when OutputType is set to Exe, it is automatically changed to WinExe
                 // for WPF and Windows Forms apps that target any framework version, including .NET Framework.
                 // https://docs.microsoft.com/en-us/dotnet/core/compatibility/sdk/5.0/automatically-infer-winexe-output-type
-                context.Compilation.Options.OutputKind != OutputKind.ConsoleApplication)
+                compilation.Options.OutputKind != OutputKind.ConsoleApplication)
             {
                 context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.s_errorUnsupportedProjectType, Location.None, nameof(OutputKind.WindowsApplication)));
                 return;
             }
 
-            ApplicationConfig? projectConfig = ProjectFileReader.ReadApplicationConfig(context);
+            ApplicationConfig? projectConfig = ProjectFileReader.ReadApplicationConfig(compilation);
             if (projectConfig is null)
             {
                 return;
             }
 
-            string? code = ApplicationConfigurationInitializeBuilder.GenerateInitialize(projectNamespace: GetUserProjectNamespace(syntaxReceiver.Nodes[0]), projectConfig);
+            string? code = ApplicationConfigurationInitializeBuilder.GenerateInitialize(projectNamespace: GetUserProjectNamespace(syntaxNodes[0]), projectConfig);
             if (code is not null)
             {
                 context.AddSource("ApplicationConfiguration.g.cs", code);
@@ -59,9 +55,63 @@ namespace System.Windows.Forms.Generators
             return ns;
         }
 
-        public void Initialize(GeneratorInitializationContext context)
+        public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            context.RegisterForSyntaxNotifications(() => new ApplicationConfigurationSyntaxReceiver());
+            var syntaxProvider = context.SyntaxProvider.CreateSyntaxProvider(
+                predicate: (syntaxNode, _) => IsSupportedSyntaxNode(syntaxNode),
+                transform: (generatorSyntaxContext, _) => generatorSyntaxContext.Node
+                );
+
+            IncrementalValueProvider<(Compilation Compilation, ImmutableArray<SyntaxNode> Nodes)> compilationProvider = context.CompilationProvider.Combine(syntaxProvider.Collect());
+
+            context.RegisterSourceOutput(compilationProvider, (spc, source) => Execute(source.Compilation, source.Nodes, spc));
+        }
+
+        public static bool IsSupportedSyntaxNode(SyntaxNode syntaxNode)
+        {
+#pragma warning disable SA1513 // Closing brace should be followed by blank line
+            if (syntaxNode is InvocationExpressionSyntax
+                {
+                    ArgumentList:
+                    {
+                        Arguments: { Count: < 1 }
+                    },
+                    Expression: MemberAccessExpressionSyntax
+                    {
+                        Name:
+                        {
+                            Identifier:
+                            {
+                                ValueText: "Initialize"
+                            }
+                        },
+                        Expression:
+                            MemberAccessExpressionSyntax  // For: SourceGenerated.ApplicationConfiguration.Initialize()
+                            {
+                                Name:
+                                {
+                                    Identifier:
+                                    {
+                                        ValueText: "ApplicationConfiguration"
+                                    }
+                                }
+                            }
+                            or
+                            IdentifierNameSyntax           // For: ApplicationConfiguration.Initialize() with a using statement
+                            {
+                                Identifier:
+                                {
+                                    ValueText: "ApplicationConfiguration"
+                                }
+                            }
+                    }
+                })
+            {
+                return true;
+            }
+#pragma warning restore SA1513 // Closing brace should be followed by blank line
+
+            return false;
         }
     }
 }
