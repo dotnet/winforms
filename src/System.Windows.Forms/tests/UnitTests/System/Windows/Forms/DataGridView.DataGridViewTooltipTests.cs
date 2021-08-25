0fdd6621c025a5b08fc59801c84e83a4cddcf098
Artem Tatarinov <artem.tatarinov@akvelon.com>
2021-08-25 16:49:33 +0500

    InvalidOperationException was thrown at the Hide method of the ToolTip class while creating a new instance of ToolInfoWrapper class. That happened because ToolTip instance (class member named '_window') has a created handle, and ToolTip's DataGridView (method parameter named 'win') has not. Added a check if DataGridView handle is created while DataGridViewToolTip is being activated with its DataGridView (Activate method is used both for showing and hiding DataGridViewToolTip), so no hiding or showing DataGridViewToolTip for DataGridView without a handle would be attempted at the first place.

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataGridView.DataGridViewTooltipTests.cs
@@ -0,0 +1,55 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Data;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.Windows.Forms.Tests
+{
+    public class DataGridView_DataGridViewToolTipTests
+    {
+        [WinFormsFact]
+        public void DataGridViewToolTip_Get_Not_ThrowsException()
+        {
+            DataTable dataTable = new();
+            dataTable.Columns.Add(columnName: "name");
+            dataTable.Rows.Add(values: "name1");
+
+            // create DataGridView with DataSource set to dataTable
+            DataGridView dataGridView = new();
+            dataGridView.ShowCellToolTips = true;
+            dataGridView.DataSource = dataTable;
+
+            // create form and add dataGridView
+            Form dialog = new();
+            dialog.Controls.Add(dataGridView);
+
+            // these two steps can also be done manually after the dialog is shown
+            dialog.Shown += (sender, args) =>
+            {
+                // 1. move mouse cursor over any cell of the first row
+                Cursor.Position = dataGridView.PointToScreen(dataGridView.GetCellDisplayRectangle(columnIndex: 0, rowIndex: 0, cutOverflow: false).Location);
+
+                // 2. close the dialog after a short delay
+                Task.Delay(millisecondsDelay: 100).ContinueWith((t) => dialog.Close(), TaskScheduler.FromCurrentSynchronizationContext());
+            };
+
+            dialog.ShowDialog();
+
+            bool exceptionThrown = false;
+
+            try
+            {
+                dataTable.AcceptChanges();
+            }
+            catch
+            {
+                exceptionThrown = true;
+            }
+
+            Assert.False(exceptionThrown, "Accepting changes on DataTable has thrown an InvalidOperationException because handle of the DataGridView used in Tooltip isn't created");
+        }
+    }
+}
