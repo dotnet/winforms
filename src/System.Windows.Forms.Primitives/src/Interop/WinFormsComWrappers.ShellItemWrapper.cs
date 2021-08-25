96b06803cb2ac6fe0647abe2000e7c74ee878285
Andrii Kurdiumov <kant2002@gmail.com>
2021-08-25 17:59:58 +0600

    Remove explicit namespace prefixes from types

--- src/System.Windows.Forms.Primitives/src/Interop/WinFormsComWrappers.ShellItemWrapper.cs
+++ src/System.Windows.Forms.Primitives/src/Interop/WinFormsComWrappers.ShellItemWrapper.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -28,54 +28,54 @@ internal partial class Interop
                 _wrappedInstance = IntPtr.Zero;
             }
 
-            Interop.HRESULT Interop.Shell32.IShellItem.BindToHandler(IntPtr pbc, ref Guid bhid, ref Guid riid, out IntPtr ppv)
+            HRESULT Shell32.IShellItem.BindToHandler(IntPtr pbc, ref Guid bhid, ref Guid riid, out IntPtr ppv)
             {
                 int retVal;
                 fixed (Guid* local_1 = &bhid)
                 fixed (Guid* local_2 = &riid)
                 fixed (IntPtr* local_3 = &ppv)
-                retVal = ((delegate* unmanaged<System.IntPtr, IntPtr, Guid*, Guid*, IntPtr*, int>)(*(*(void***)_wrappedInstance + 3)))
+                retVal = ((delegate* unmanaged<IntPtr, IntPtr, Guid*, Guid*, IntPtr*, int>)(*(*(void***)_wrappedInstance + 3)))
                     (_wrappedInstance, pbc, local_1, local_2, local_3);
-                return (Interop.HRESULT)retVal;
+                return (HRESULT)retVal;
             }
 
-            Interop.HRESULT Interop.Shell32.IShellItem.GetParent(out Interop.Shell32.IShellItem ppsi)
+            HRESULT Shell32.IShellItem.GetParent(out Shell32.IShellItem ppsi)
             {
-                System.IntPtr local_0;
+                IntPtr local_0;
                 int retVal;
-                retVal = ((delegate* unmanaged<System.IntPtr, System.IntPtr*, int>)(*(*(void***)_wrappedInstance + 4)))
+                retVal = ((delegate* unmanaged<IntPtr, IntPtr*, int>)(*(*(void***)_wrappedInstance + 4)))
                     (_wrappedInstance, &local_0);
-                ppsi = local_0 == System.IntPtr.Zero ? null! : (Interop.Shell32.IShellItem)WinFormsComWrappers.Instance.GetOrCreateObjectForComInstance(local_0, CreateObjectFlags.Unwrap);
-                return (Interop.HRESULT)retVal;
+                ppsi = local_0 == IntPtr.Zero ? null! : (Shell32.IShellItem)WinFormsComWrappers.Instance.GetOrCreateObjectForComInstance(local_0, CreateObjectFlags.Unwrap);
+                return (HRESULT)retVal;
             }
 
-            Interop.HRESULT Interop.Shell32.IShellItem.GetDisplayName(Interop.Shell32.SIGDN sigdnName, out string ppszName)
+            HRESULT Shell32.IShellItem.GetDisplayName(Shell32.SIGDN sigdnName, out string ppszName)
             {
-                System.IntPtr local_1;
+                IntPtr local_1;
                 int retVal;
-                retVal = ((delegate* unmanaged<System.IntPtr, int, System.IntPtr*, int>)(*(*(void***)_wrappedInstance + 5)))
+                retVal = ((delegate* unmanaged<IntPtr, int, IntPtr*, int>)(*(*(void***)_wrappedInstance + 5)))
                     (_wrappedInstance, (int)sigdnName, &local_1);
                 ppszName = Marshal.PtrToStringUni(local_1)!;
-                return (Interop.HRESULT)retVal;
+                return (HRESULT)retVal;
             }
 
-            Interop.HRESULT Interop.Shell32.IShellItem.GetAttributes(Interop.Shell32.SFGAOF sfgaoMask, out Interop.Shell32.SFGAOF psfgaoAttribs)
+            HRESULT Shell32.IShellItem.GetAttributes(Shell32.SFGAOF sfgaoMask, out Shell32.SFGAOF psfgaoAttribs)
             {
                 int local_1;
                 int retVal;
-                retVal = ((delegate* unmanaged<System.IntPtr, int, int*, int>)(*(*(void***)_wrappedInstance + 6)))
+                retVal = ((delegate* unmanaged<IntPtr, int, int*, int>)(*(*(void***)_wrappedInstance + 6)))
                     (_wrappedInstance, (int)sfgaoMask, &local_1);
-                psfgaoAttribs = (Interop.Shell32.SFGAOF)local_1;
-                return (Interop.HRESULT)retVal;
+                psfgaoAttribs = (Shell32.SFGAOF)local_1;
+                return (HRESULT)retVal;
             }
 
-            Interop.HRESULT Interop.Shell32.IShellItem.Compare(Interop.Shell32.IShellItem psi, uint hint, out int piOrder)
+            HRESULT Shell32.IShellItem.Compare(Shell32.IShellItem psi, uint hint, out int piOrder)
             {
                 int result;
-                System.IntPtr local_0;
+                IntPtr local_0;
                 if (psi == null)
                 {
-                    local_0 = System.IntPtr.Zero;
+                    local_0 = IntPtr.Zero;
                 }
                 else
                 {
@@ -90,9 +90,9 @@ internal partial class Interop
 
                 int retVal;
                 fixed (int* local_2 = &piOrder)
-                retVal = ((delegate* unmanaged<System.IntPtr, System.IntPtr, uint, int*, int>)(*(*(void***)_wrappedInstance + 7)))
+                retVal = ((delegate* unmanaged<IntPtr, IntPtr, uint, int*, int>)(*(*(void***)_wrappedInstance + 7)))
                     (_wrappedInstance, local_0, hint, local_2);
-                return (Interop.HRESULT)retVal;
+                return (HRESULT)retVal;
             }
         }
     }
