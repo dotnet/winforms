4d9156ecf0f5cd4652285f7c0ce5ea16b9ef1d7b
TillW <14997061+x789@users.noreply.github.com>
2021-10-02 00:24:05 +0200

    Fixed parameter-name of 'ArgumentNullException' in 'OpenFile'

--- src/System.Windows.Forms/src/System/Windows/Forms/SaveFileDialog.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/SaveFileDialog.cs
@@ -54,7 +54,7 @@ namespace System.Windows.Forms
         public Stream OpenFile()
         {
             string filename = FileNames[0];
-            ArgumentNullException.ThrowIfNull(filename, nameof(filename));
+            ArgumentNullException.ThrowIfNull(filename, nameof(FileName));
             if (string.IsNullOrEmpty(filename))
             {
                 throw new ArgumentNullException(nameof(FileName));
