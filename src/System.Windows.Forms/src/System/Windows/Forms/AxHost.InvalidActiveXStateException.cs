92b4fee80d61e88fb6c71650ae955e769774a7be
gpetrou <gipetrou@gmail.com>
2021-10-03 10:58:48 +0300

    Enable nullability in some AxHost classes

--- src/System.Windows.Forms/src/System/Windows/Forms/AxHost.InvalidActiveXStateException.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AxHost.InvalidActiveXStateException.cs
@@ -2,21 +2,19 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     public abstract partial class AxHost
     {
         public class InvalidActiveXStateException : Exception
         {
-            private readonly string name;
-            private readonly ActiveXInvokeKind kind;
+            private readonly string? _name;
+            private readonly ActiveXInvokeKind _kind;
 
-            public InvalidActiveXStateException(string name, ActiveXInvokeKind kind)
+            public InvalidActiveXStateException(string? name, ActiveXInvokeKind kind)
             {
-                this.name = name;
-                this.kind = kind;
+                _name = name;
+                _kind = kind;
             }
 
             public InvalidActiveXStateException()
@@ -25,14 +23,14 @@ namespace System.Windows.Forms
 
             public override string ToString()
             {
-                switch (kind)
+                switch (_kind)
                 {
                     case ActiveXInvokeKind.MethodInvoke:
-                        return string.Format(SR.AXInvalidMethodInvoke, name);
+                        return string.Format(SR.AXInvalidMethodInvoke, _name);
                     case ActiveXInvokeKind.PropertyGet:
-                        return string.Format(SR.AXInvalidPropertyGet, name);
+                        return string.Format(SR.AXInvalidPropertyGet, _name);
                     case ActiveXInvokeKind.PropertySet:
-                        return string.Format(SR.AXInvalidPropertySet, name);
+                        return string.Format(SR.AXInvalidPropertySet, _name);
                     default:
                         return base.ToString();
                 }
--- src/System.Windows.Forms/src/System/Windows/Forms/AxHost.TypeLibraryTimeStampAttribute.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AxHost.TypeLibraryTimeStampAttribute.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Globalization;
 
 namespace System.Windows.Forms
@@ -13,20 +11,12 @@ namespace System.Windows.Forms
         [AttributeUsage(AttributeTargets.Assembly, Inherited = false)]
         public sealed class TypeLibraryTimeStampAttribute : Attribute
         {
-            private readonly DateTime val;
-
             public TypeLibraryTimeStampAttribute(string timestamp)
             {
-                val = DateTime.Parse(timestamp, CultureInfo.InvariantCulture);
+                Value = DateTime.Parse(timestamp, CultureInfo.InvariantCulture);
             }
 
-            public DateTime Value
-            {
-                get
-                {
-                    return val;
-                }
-            }
+            public DateTime Value { get; }
         }
     }
 }
