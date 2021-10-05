dotnet/winforms

commit ebd609ec783a87cc17f28f0f7f7a314a431e750a

Fixing MonthCalendar a display dates range resets and flickers after calling UpdateBoldedDates (#4317)

src/System.Windows.Forms/src/System/Windows/Forms/MonthCalendar.cs
@@ -9,6 +9,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
 using System.Globalization;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Windows.Forms.Internal;
 using System.Windows.Forms.Layout;
@@ -198,7 +199,7 @@ namespace System.Windows.Forms
                     }
                 }
 
-                RecreateHandle();
+                UpdateBoldedDates();
             }
         }
 
@@ -270,7 +271,7 @@ namespace System.Windows.Forms
                     }
                 }
 
-                RecreateHandle();
+                UpdateBoldedDates();
             }
         }
 
@@ -526,14 +527,14 @@ namespace System.Windows.Forms
                 if (value != null && value.Length > 0)
                 {
                     // Add each bolded date to our List.
-                    foreach (var  dateTime in value)
+                    foreach (var dateTime in value)
                     {
                         _monthlyArrayOfDates.Add(dateTime);
                         _datesToBoldMonthly |= 0x00000001 << (dateTime.Day - 1);
                     }
                 }
 
-                RecreateHandle();
+                UpdateBoldedDates();
             }
         }
 
@@ -1564,7 +1565,6 @@ namespace System.Windows.Forms
                 if (DateTime.Compare(_arrayOfDates[i].Date, date.Date) == 0)
                 {
                     _arrayOfDates.RemoveAt(i);
-                    Invalidate();
                     return;
                 }
             }
@@ -1828,6 +1828,56 @@ namespace System.Windows.Forms
         }
 
         /// <summary>
+        ///  Update states of displayed dates. Make the native control redraw bolded dates.
+        /// </summary>
+        private unsafe void SetMonthViewBoldedDates()
+        {
+            Debug.Assert(_mcCurView == MCMV.MONTH, "This logic should work only in the Month view.");
+
+            // Get the first and the last visible dates even they are in not fully displayed months
+            SelectionRange displayRange = GetDisplayRange(false);
+
+            // Calculate the number of visible months, even though they may be partially visible.
+            // It is necessary to send to Windows correct info about all bolded dates that are visible.
+            int monthsCount = (displayRange.End.Year - displayRange.Start.Year) * 12 + displayRange.End.Month - displayRange.Start.Month + 1;
+
+            // Create a special collection for storage states of dates of some displayed month.
+            // This collection will be send to Windows to update displayed dates states - bolded/unbolded.
+            Span<uint> monthDayStates = stackalloc uint[monthsCount];
+
+            // Run through all displayed dates to set a binary marker that the date is bolded
+            // if BoldedDates, AnnualArrayOfDates, or MonthlyArrayOfDates contain this date.
+            DateTime currentDate = displayRange.Start;
+            while (currentDate <= displayRange.End)
+            {
+                bool currentDateIsBolded = _arrayOfDates.Contains(currentDate)
+                    || _annualArrayOfDates.Any(d => d.Month == currentDate.Month && d.Day == currentDate.Day)
+                    || _monthlyArrayOfDates.Any(d => d.Day == currentDate.Day);
+
+                if (currentDateIsBolded)
+                {
+                    // Calculate an index of a month of the current date in the display range.
+                    // It works as an array, indexes start from 0.
+                    int currentMonthIndex = (12 - displayRange.Start.Month + currentDate.Month) % 12;
+
+                    // Set bolded state for the current date of the current month
+                    // to prepare the states array before sending to Windows
+                    monthDayStates[currentMonthIndex] |= 1U << currentDate.Day - 1;
+                }
+
+                // Set the next day for check
+                currentDate = currentDate.AddDays(1);
+            }
+
+            fixed (uint* arr = monthDayStates)
+            {
+                // Update display dates states.
+                // For more info see docs: https://docs.microsoft.com/windows/win32/controls/mcm-setdaystate
+                User32.SendMessageW(Handle, (User32.WM)ComCtl32.MCM.SETDAYSTATE, (IntPtr)(void*)monthsCount, (IntPtr)arr);
+            }
+        }
+
+        /// <summary>
         ///  Sets the selection for a month calendar control to a given date range.
         ///  The selection range will not be set if the selection range exceeds the
         ///  maximum selection count.
@@ -1964,7 +2014,16 @@ namespace System.Windows.Forms
         /// <summary>
         ///  Forces month calendar to display the current set of bolded dates.
         /// </summary>
-        public void UpdateBoldedDates() => RecreateHandle();
+        public void UpdateBoldedDates()
+        {
+            if (IsHandleCreated && _mcCurView == MCMV.MONTH)
+            {
+                // Use a specific implementation in the Month view
+                // to avoid MonthCalendar display dates range resets
+                // and flickers when a MonthCalendar shows several months.
+                SetMonthViewBoldedDates();
+            }
+        }
 
         /// <summary>
         ///  Updates the current setting for "TODAY" in the MonthCalendar control

src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/MonthCalendarTests.cs
@@ -258,7 +258,7 @@ namespace System.Windows.Forms.Tests
             Assert.True(calendar.IsHandleCreated);
             Assert.Equal(0, invalidatedCallCount);
             Assert.Equal(0, styleChangedCallCount);
-            Assert.Equal(1, createdCallCount);
+            Assert.Equal(0, createdCallCount);
 
             // Set same.
             calendar.AnnuallyBoldedDates = value;
@@ -276,7 +276,7 @@ namespace System.Windows.Forms.Tests
             Assert.True(calendar.IsHandleCreated);
             Assert.Equal(0, invalidatedCallCount);
             Assert.Equal(0, styleChangedCallCount);
-            Assert.Equal(2, createdCallCount);
+            Assert.Equal(0, createdCallCount);
         }
 
         public static IEnumerable<object[]> BackColor_Set_TestData()
@@ -586,7 +586,7 @@ namespace System.Windows.Forms.Tests
             Assert.True(calendar.IsHandleCreated);
             Assert.Equal(0, invalidatedCallCount);
             Assert.Equal(0, styleChangedCallCount);
-            Assert.Equal(1, createdCallCount);
+            Assert.Equal(0, createdCallCount);
 
             // Set same.
             calendar.BoldedDates = value;
@@ -604,7 +604,7 @@ namespace System.Windows.Forms.Tests
             Assert.True(calendar.IsHandleCreated);
             Assert.Equal(0, invalidatedCallCount);
             Assert.Equal(0, styleChangedCallCount);
-            Assert.Equal(2, createdCallCount);
+            Assert.Equal(0, createdCallCount);
         }
 
         public static IEnumerable<object[]> CalendarDimensions_Set_TestData()
@@ -1611,7 +1611,7 @@ namespace System.Windows.Forms.Tests
             Assert.True(calendar.IsHandleCreated);
             Assert.Equal(0, invalidatedCallCount);
             Assert.Equal(0, styleChangedCallCount);
-            Assert.Equal(1, createdCallCount);
+            Assert.Equal(0, createdCallCount);
 
             // Set same.
             calendar.MonthlyBoldedDates = value;
@@ -1629,7 +1629,7 @@ namespace System.Windows.Forms.Tests
             Assert.True(calendar.IsHandleCreated);
             Assert.Equal(0, invalidatedCallCount);
             Assert.Equal(0, styleChangedCallCount);
-            Assert.Equal(2, createdCallCount);
+            Assert.Equal(0, createdCallCount);
         }
 
         [WinFormsTheory]

commit a358bc80da8a2ad6f28cb51f7c02b30c3cf76da3

Fixed getting an month index of a bolded date of a MonthCalendar (#5869)
Fixes #5853

The regression came from #4317, the previous implementation didn't not take into account gray dates of a display range. So the max index was 11, but in real, 13 is the max month index (including the first and last gray months).

## Proposed changes

- Rework getting current month index when setting a new bolded date

<!-- We are in TELL-MODE the following section must be completed -->

## Customer Impact

- A user can bold a MonthCalendar date correctly


## Regression? 

- Yes (from #4317)

## Risk

- Minimal

## Screenshots

### Before
MonthCalendar doesn't show bolded dates:
![K29yyG2JFB](https://user-images.githubusercontent.com/49272759/135065436-c23a25bd-d980-4f09-93d3-4158075be222.gif)

### After
MonthCalendar shows bolded dates:
![bby0rqDoqn](https://user-images.githubusercontent.com/49272759/135065493-50dfa763-47a8-4e94-ad48-9cd9ca9e517b.gif)

## Test methodology <!-- How did you ensure quality? -->

- CTI
- Unit tests
- Manual testing

## Test environment(s) <!-- Remove any that don't apply -->

- .NET 7.0-alpha
- Windows 10

src/System.Windows.Forms/src/System/Windows/Forms/MonthCalendar.cs
@@ -340,6 +340,35 @@ namespace System.Windows.Forms
             set => base.DoubleBuffered = value;
         }
 
+        internal void FillMonthDayStates(Span<uint> monthDayStates, SelectionRange displayRange)
+        {
+            // Run through all displayed dates to set a binary marker that the date is bolded
+            // if BoldedDates, AnnualArrayOfDates, or MonthlyArrayOfDates contain this date.
+            DateTime currentDate = displayRange.Start;
+            while (currentDate <= displayRange.End)
+            {
+                bool currentDateIsBolded = _boldDates.Contains(currentDate)
+                    || _annualBoldDates.Any(d => d.Month == currentDate.Month && d.Day == currentDate.Day)
+                    || _monthlyBoldDates.Any(d => d.Day == currentDate.Day);
+
+                if (currentDateIsBolded)
+                {
+                    // Calculate an index of a month of the current date in the display range,
+                    // starting from the first displayed month.
+                    // The display range may include gray dates of the first and last months.
+                    // So the max count of visible months is 14 and the max index is 13.
+                    int currentMonthIndex = GetIndexInMonths(displayRange.Start, currentDate);
+
+                    // Set bolded state for the current date of the current month
+                    // to prepare the states array before sending to Windows
+                    monthDayStates[currentMonthIndex] |= 1U << currentDate.Day - 1;
+                }
+
+                // Set the next day for check
+                currentDate = currentDate.AddDays(1);
+            }
+        }
+
         /// <summary>
         ///  The first day of the week for the month calendar control.
         /// </summary>
@@ -1238,6 +1267,9 @@ namespace System.Windows.Forms
             }
         }
 
+        private static int GetIndexInMonths(DateTime startDate, DateTime currentDate)
+            => (currentDate.Year - startDate.Year) * MonthsInYear + currentDate.Month - startDate.Month;
+
         private Size GetMinReqRect() => GetMinReqRect(0, false, false);
 
         /// <summary>
@@ -1324,6 +1356,14 @@ namespace System.Windows.Forms
                 End = times[1]
             };
         }
+
+        /// <summary>
+        ///  Calculate the number of visible months, even though they may be partially visible.
+        ///  It is necessary to send to Windows correct info about all bolded dates that are visible.
+        ///  Get an index of the last month, that starts from 0, and add 1 to get months count.
+        /// </summary>
+        private static int GetMonthsCountOfRange(SelectionRange displayRange)
+            => GetIndexInMonths(displayRange.Start, displayRange.End) + 1;
 
         /// <summary>
         ///  Called by SetBoundsCore. If updateRows is true, then the number of rows
@@ -1870,38 +1910,13 @@ namespace System.Windows.Forms
 
             // Get the first and the last visible dates even they are in not fully displayed months
             SelectionRange displayRange = GetDisplayRange(false);
-
-            // Calculate the number of visible months, even though they may be partially visible.
-            // It is necessary to send to Windows correct info about all bolded dates that are visible.
-            int monthsCount = (displayRange.End.Year - displayRange.Start.Year) * 12 + displayRange.End.Month - displayRange.Start.Month + 1;
+            int monthsCount = GetMonthsCountOfRange(displayRange);
 
             // Create a special collection for storage states of dates of some displayed month.
             // This collection will be send to Windows to update displayed dates states - bolded/unbolded.
             Span<uint> monthDayStates = stackalloc uint[monthsCount];
-
-            // Run through all displayed dates to set a binary marker that the date is bolded
-            // if BoldedDates, AnnualArrayOfDates, or MonthlyArrayOfDates contain this date.
-            DateTime currentDate = displayRange.Start;
-            while (currentDate <= displayRange.End)
-            {
-                bool currentDateIsBolded = _boldDates.Contains(currentDate)
-                    || _annualBoldDates.Any(d => d.Month == currentDate.Month && d.Day == currentDate.Day)
-                    || _monthlyBoldDates.Any(d => d.Day == currentDate.Day);
-
-                if (currentDateIsBolded)
-                {
-                    // Calculate an index of a month of the current date in the display range.
-                    // It works as an array, indexes start from 0.
-                    int currentMonthIndex = (12 - displayRange.Start.Month + currentDate.Month) % 12;
-
-                    // Set bolded state for the current date of the current month
-                    // to prepare the states array before sending to Windows
-                    monthDayStates[currentMonthIndex] |= 1U << currentDate.Day - 1;
-                }
-
-                // Set the next day for check
-                currentDate = currentDate.AddDays(1);
-            }
+            // Run through all displayed bolded dates and fill the Span collection
+            FillMonthDayStates(monthDayStates, displayRange);
 
             fixed (uint* arr = monthDayStates)
             {

src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/Calendar.cs
@@ -15,6 +15,21 @@ namespace WinformsControlsTest
             daysOfWeekComboBox.SelectedIndex = (int)monthCalendar1.FirstDayOfWeek;
             showWeekNumbersCheckBox.Checked = monthCalendar1.ShowWeekNumbers;
             showTodayCheckBox.Checked = monthCalendar1.ShowToday;
+            monthCalendar1.DateSelected += monthCalendar1_DateSelected;
+        }
+
+        private unsafe void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            if (monthCalendar1.BoldedDates.Contains(e.Start))
+            {
+                monthCalendar1.RemoveBoldedDate(e.Start);
+            }
+            else
+            {
+                monthCalendar1.AddBoldedDate(e.Start);
+            }
+
+            monthCalendar1.UpdateBoldedDates();
         }
 
         private void setMinDateButton_Click(object sender, EventArgs e)

src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/MonthCalendarTests.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.Windows.Forms.TestUtilities;
 using Xunit;
 using static Interop;
+using static Interop.ComCtl32;
 
 namespace System.Windows.Forms.Tests
 {
@@ -4156,6 +4157,92 @@ namespace System.Windows.Forms.Tests
             calendar.MaxDate = new DateTime(2019, 9, 3);
             Assert.Throws<ArgumentOutOfRangeException>("date1", () => calendar.SetSelectionRange(calendar.MaxDate.AddDays(1), calendar.MaxDate));
             Assert.Throws<ArgumentOutOfRangeException>("date2", () => calendar.SetSelectionRange(calendar.MaxDate, calendar.MaxDate.AddDays(1)));
+        }
+
+        public static IEnumerable<object[]> MonthCalendar_FillMonthDayStates_ReturnsExpected_TestData()
+        {
+            // This test set of dates is designed for a specifict test case:
+            // when a calendar has 12 fully visible months + 2 not fully visible.
+            // This test calendar has (08/29/2021 - 09/10/2022) dates range.
+
+            yield return new object[] { new DateTime(2021, 8, 31) }; // Make this date of the not fully visible previous month bold
+
+            // Make visible dates of 2021 year (Sep - Dec) bold
+            for (int i = 9; i <= 12; i++)
+            {
+                yield return new object[] { new DateTime(2021, i, i) };
+            }
+
+            // Make visible dates of 2022 year (Jan - Aug) bold
+            for (int i = 1; i <= 8; i++)
+            {
+                yield return new object[] { new DateTime(2022, i, i) };
+            }
+
+            yield return new object[] { new DateTime(2022, 9, 1) }; // Make this date of the not fully visible last month bold
+        }
+
+        [WinFormsTheory]
+        [MemberData(nameof(MonthCalendar_FillMonthDayStates_ReturnsExpected_TestData))]
+        public unsafe void MonthCalendar_FillMonthDayStates_ReturnsExpected(DateTime currentDate)
+        {
+            const int MonthsInYear = 12;
+            // Create a calendar with (600x600) size, that contains 3 columns and 4 row of months (12 months total).
+            // Set the first day of week to have a stable test case in different environments.
+            using MonthCalendar calendar = new() { Size = new Size(600, 600), FirstDayOfWeek = Day.Sunday };
+
+            calendar.CreateControl();
+            // Set a visible range (08/29/2021 - 09/10/2022) to have a stable test case
+            calendar.SetSelectionRange(new DateTime(2021, 9, 1), new DateTime(2022, 8, 31));
+            MCMV view = calendar.TestAccessor().Dynamic._mcCurView;
+            SelectionRange displayRange = calendar.GetDisplayRange(visible: false);
+
+            Assert.Equal(MCMV.MONTH, view);
+            Assert.Equal(new DateTime(2021, 8, 29), displayRange.Start);
+            Assert.Equal(new DateTime(2022, 9, 10), displayRange.End);
+
+            int monthsCount = calendar.TestAccessor().Dynamic.GetMonthsCountOfRange(displayRange);
+            int currentMonthIndex = (currentDate.Year - displayRange.Start.Year) * MonthsInYear + currentDate.Month - displayRange.Start.Month;
+            calendar.AddBoldedDate(currentDate);
+            Span<uint> boldedDates = stackalloc uint[monthsCount];
+
+            calendar.FillMonthDayStates(boldedDates, displayRange);
+
+            uint expectedState = 1U << (currentDate.Day - 1);
+            uint actualState = boldedDates[currentMonthIndex] & expectedState;
+
+            Assert.Equal(expectedState, actualState);
+        }
+
+        public static IEnumerable<object[]> MonthCalendar_GetIndexInMonths_ReturnsExpected_TestData()
+        {
+            int expectedIndex = 0;
+
+            // This test set of dates is designed to check dates in different years.
+            // The start date is 08/01/2021.
+
+            // Check dates of 2021 year (Aug - Dec)
+            for (int i = 8; i <= 12; i++)
+            {
+                yield return new object[] { new DateTime(2021, i, i), expectedIndex++ };
+            }
+
+            // Check dates of 2022 year (Jan - Sep) bold
+            for (int i = 1; i <= 9; i++)
+            {
+                yield return new object[] { new DateTime(2022, i, i), expectedIndex++ };
+            }
+        }
+
+        [WinFormsTheory]
+        [MemberData(nameof(MonthCalendar_GetIndexInMonths_ReturnsExpected_TestData))]
+        public unsafe void MonthCalendar_GetIndexInMonths_ReturnsExpected(DateTime currentDate, int expectedIndex)
+        {
+            DateTime startDate = new(2021, 8, 1);
+            using MonthCalendar calendar = new();
+            int actualIndex = calendar.TestAccessor().Dynamic.GetIndexInMonths(startDate, currentDate);
+
+            Assert.Equal(expectedIndex, actualIndex);
         }
 
         private class SubMonthCalendar : MonthCalendar