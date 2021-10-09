8e088ab8b775dd01dd3144385d91130d276d946c
TillW <14997061+x789@users.noreply.github.com>
2021-10-09 14:45:06 +0200

    Disabled CS8604 locally

--- src/System.Windows.Forms.Primitives/tests/UnitTests/System/Windows/Forms/Internals/ArgumentValidationTests.cs
+++ src/System.Windows.Forms.Primitives/tests/UnitTests/System/Windows/Forms/Internals/ArgumentValidationTests.cs
@@ -193,7 +193,9 @@ namespace System.Windows.Forms.Tests
         public void OrThrowIfNullOrEmpty_ParamIsNull()
         {
             string? param = null;
+#pragma warning disable CS8604 // Possible null reference argument.
             var exception = Assert.Throws<ArgumentNullException>(() => _ = param.OrThrowIfNullOrEmpty());
+#pragma warning restore CS8604 // Possible null reference argument.
             Assert.Equal(nameof(param), exception.ParamName);
         }
 
@@ -217,7 +219,9 @@ namespace System.Windows.Forms.Tests
         public void OrThrowIfNullOrEmpty_ParamIsNull_DifferentParamName()
         {
             string? param = null;
+#pragma warning disable CS8604 // Possible null reference argument.
             var exception = Assert.Throws<ArgumentNullException>(() => _ = param.OrThrowIfNullOrEmpty("paramName"));
+#pragma warning restore CS8604 // Possible null reference argument.
             Assert.Equal("paramName", exception.ParamName);
         }
 
