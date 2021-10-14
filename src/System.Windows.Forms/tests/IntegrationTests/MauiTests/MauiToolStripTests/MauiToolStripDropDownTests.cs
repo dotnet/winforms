0a4bf9259e0934bf0e81983321faae309e1511b9
Artem Tatarinov <artem.tatarinov@akvelon.com>
2021-10-14 19:24:00 +0500

    Fixing too large up and down arrows at ToolStripScrollButton

--- src/System.Windows.Forms/tests/IntegrationTests/MauiTests/MauiToolStripTests/MauiToolStripDropDownTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/MauiTests/MauiToolStripTests/MauiToolStripDropDownTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
 using System.Windows.Forms.IntegrationTests.Common;
 using ReflectTools;
 using WFCTestLib.Log;
@@ -44,6 +45,54 @@ namespace System.Windows.Forms.IntegrationTests.MauiTests
             _result &= !_toolStrip.ProcessDialogChar('T');
 
             return new ScenarioResult(_result);
+        }
+
+        [Scenario(true)]
+        public ScenarioResult ToolStripScrollButton_Arrows_Size_ReturnsExpected(TParams p)
+        {
+            Type toolStripScrollButtonType = typeof(ToolStripScrollButton);
+            var accessor = typeof(DpiHelper).TestAccessor();
+            Size defaultSize = new(16, 16);
+            int oldDeviceDpi = DpiHelper.DeviceDpi;
+            List<DpiTestData> dpiTestData = new();
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonDown", Dpi = 96, ExpectedSide = 16 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonDown", Dpi = 120, ExpectedSide = 24 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonDown", Dpi = 144, ExpectedSide = 24 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonDown", Dpi = 168, ExpectedSide = 32 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonDown", Dpi = 288, ExpectedSide = 48 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonUp", Dpi = 96, ExpectedSide = 16 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonUp", Dpi = 120, ExpectedSide = 24 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonUp", Dpi = 144, ExpectedSide = 24 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonUp", Dpi = 168, ExpectedSide = 32 });
+            dpiTestData.Add(new DpiTestData() { ResourceName = "ScrollButtonUp", Dpi = 288, ExpectedSide = 48 });
+
+            try
+            {
+                foreach (DpiTestData data in dpiTestData)
+                {
+                    accessor.Dynamic.DeviceDpi = data.Dpi;
+                    Bitmap bitmap = DpiHelper.GetScaledBitmapFromIcon(toolStripScrollButtonType, data.ResourceName, defaultSize);
+                    if (data.ExpectedSide != bitmap.Width || data.ExpectedSide != bitmap.Height)
+                    {
+                        return new ScenarioResult(false);
+                    }
+                }
+            }
+            finally
+            {
+                accessor.Dynamic.DeviceDpi = oldDeviceDpi;
+            }
+
+            return new ScenarioResult(true);
+        }
+
+        private class DpiTestData
+        {
+            public string ResourceName { get; set; }
+
+            public int Dpi { get; set; }
+
+            public int ExpectedSide { get; set; }
         }
 
         private class SubToolStripDropDown : ToolStripDropDown
