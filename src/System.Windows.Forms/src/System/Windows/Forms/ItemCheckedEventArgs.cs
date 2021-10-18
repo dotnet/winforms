a564afc8d056615f85a06ee4420a6fcef8265dfa
gpetrou <4172445+gpetrou@users.noreply.github.com>
2021-10-18 09:31:12 +0300

    Enable nullability in ItemCheckedEventArgs (#5992)

--- src/System.Windows.Forms/src/System/Windows/Forms/ItemCheckedEventArgs.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ItemCheckedEventArgs.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     /// <summary>
@@ -11,7 +9,7 @@ namespace System.Windows.Forms
     /// </summary>
     public class ItemCheckedEventArgs : EventArgs
     {
-        public ItemCheckedEventArgs(ListViewItem item)
+        public ItemCheckedEventArgs(ListViewItem? item)
         {
             Item = item;
         }
@@ -19,6 +17,6 @@ namespace System.Windows.Forms
         /// <summary>
         ///  The index of the item that is about to change.
         /// </summary>
-        public ListViewItem Item { get; }
+        public ListViewItem? Item { get; }
     }
 }
