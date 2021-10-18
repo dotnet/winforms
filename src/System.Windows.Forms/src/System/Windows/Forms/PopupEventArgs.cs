8c7712d7b9c67277f8cf69f11815245d90158d20
gpetrou <4172445+gpetrou@users.noreply.github.com>
2021-10-18 09:27:35 +0300

    Enable nullability in PopupEventArgs (#5993)

--- src/System.Windows.Forms/src/System/Windows/Forms/PopupEventArgs.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PopupEventArgs.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.ComponentModel;
 using System.Drawing;
 
@@ -17,7 +15,7 @@ namespace System.Windows.Forms
         /// <summary>
         ///  Creates a new PopupEventArgs with the given parameters.
         /// </summary>
-        public PopupEventArgs(IWin32Window associatedWindow, Control associatedControl, bool isBalloon, Size size)
+        public PopupEventArgs(IWin32Window? associatedWindow, Control? associatedControl, bool isBalloon, Size size)
         {
             AssociatedWindow = associatedWindow;
             AssociatedControl = associatedControl;
@@ -28,12 +26,12 @@ namespace System.Windows.Forms
         /// <summary>
         ///  The Associated Window for which the tooltip is being painted.
         /// </summary>
-        public IWin32Window AssociatedWindow { get; }
+        public IWin32Window? AssociatedWindow { get; }
 
         /// <summary>
         ///  The control for which the tooltip is being painted.
         /// </summary>
-        public Control AssociatedControl { get; }
+        public Control? AssociatedControl { get; }
 
         /// <summary>
         ///  The rectangle outlining the area in which the painting should be done.
