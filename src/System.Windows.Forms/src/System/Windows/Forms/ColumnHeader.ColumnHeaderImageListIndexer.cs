b069feff89da5c05e940e895a5e806ef97a8d1ce
gpetrou <4172445+gpetrou@users.noreply.github.com>
2021-11-03 00:53:10 +0200

    Enable nullability in ColumnHeader (#6107)
    ## Proposed changes
    
    - Enable nullability in ColumnHeader

--- src/System.Windows.Forms/src/System/Windows/Forms/ColumnHeader.ColumnHeaderImageListIndexer.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ColumnHeader.ColumnHeaderImageListIndexer.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Diagnostics;
 
 namespace System.Windows.Forms
@@ -19,7 +17,7 @@ namespace System.Windows.Forms
                 _owner = ch;
             }
 
-            public override ImageList ImageList
+            public override ImageList? ImageList
             {
                 get
                 {
