f34b861ce72f7f6eb703fb93f60870b712a0c605
Devendar Reddy Adulla <dreddy@microsoft.com>
2021-11-05 10:11:27 -0700

    Fix anchored controls mis-alignment when DPI of the display is higher than 100% (#6122)
    * Fix anchored controls mis-alignment when DPI of the display is higher than 100%.
    
    Fixes #5774

--- src/System.Windows.Forms/src/System/Windows/Forms/ContainerControl.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ContainerControl.cs
@@ -829,7 +829,15 @@ namespace System.Windows.Forms
             // generator always generates a PerformLayout() right after a
             // ResumeLayout(false), so this seems to be the most opportune place
             // for this.
-            if (!_state[s_stateScalingChild] && !performLayout && AutoScaleMode != AutoScaleMode.None && AutoScaleMode != AutoScaleMode.Inherit && _state[s_stateScalingNeededOnLayout])
+
+            // Skip Scale() when AutoscaleFactor is 100% (evaluated to 1.0F for both width and height) as it is a no-op.
+            // It means the form is designed on the monitor that has same settings as the monitor that
+            // it is being run.
+            if (!_state[s_stateScalingChild]
+                && !performLayout
+                && AutoScaleMode != AutoScaleMode.None && AutoScaleMode != AutoScaleMode.Inherit
+                && _state[s_stateScalingNeededOnLayout]
+                && (AutoScaleFactor.Width != 1.0F || AutoScaleFactor.Height != 1.0F))
             {
                 _state[s_stateScalingChild] = true;
                 try
