9b1fcb861bf2c6004a5db35ca72c357f3795046e
Danil Andrianov <danil.andrianov@akvelon.com>
2021-11-15 13:59:50 +0300

    Rewrote Maui Scrollbar tests and removed unnecessary projects.

--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/ScrollBarTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/ScrollBarTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
 using System.Windows.Forms.UI.IntegrationTests.Infra;
 using Xunit;
 
@@ -378,6 +379,389 @@ namespace System.Windows.Forms.UI.IntegrationTests
             });
         }
 
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_LTR_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width == 0 || VFirstPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnRectangle_LTR_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 50);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Height > 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnRectangle_LTR_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 100);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Height > 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_RTL_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width == 0 || VFirstPageButton(vScrollBar).Bounds.Height == 0);
+            });
+            }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnRectangle_RTL_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 50);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Height > 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnRectangle_RTL_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 100);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Height > 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_LTR_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 0, value: 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width == 0 || VFirstPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_RTL_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 0, value: 0);
+
+                Assert.True(VFirstPageButton(vScrollBar).Bounds.Width == 0 || VFirstPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnInvisible_LTR_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 0);
+
+                Assert.Equal(AccessibleStates.Invisible, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnNone_LTR_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 50);
+
+                Assert.Equal(AccessibleStates.None, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnNone_LTR_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 100);
+
+                Assert.Equal(AccessibleStates.None, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnInvisible_RTL_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 0);
+
+                Assert.Equal(AccessibleStates.Invisible, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnNone_RTL_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 50);
+
+                Assert.Equal(AccessibleStates.None, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnNone_RTL_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 100);
+
+                Assert.Equal(AccessibleStates.None, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnInvisible_LTR_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 0, value: 0);
+
+                Assert.Equal(AccessibleStates.Invisible, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarFirstPageButtonAccessibleObject_State_ReturnInvisible_RTL_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 0, value: 0);
+
+                Assert.Equal(AccessibleStates.Invisible, VFirstPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnRectangle_LTR_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Height > 0);
+            });
+            }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnRectangle_LTR_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 50);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Height > 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_LTR_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 100);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width == 0 || VLastPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnRectangle_RTL_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Height > 0);
+            });
+            }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnRectangle_RTL_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 50);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width > 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Height > 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_RTL_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 100);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width == 0 || VLastPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_LTR_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 0, value: 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width == 0 || VLastPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_Bounds_ReturnEmptyRectangle_RTL_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 0, value: 0);
+
+                Assert.True(VLastPageButton(vScrollBar).Bounds.Width == 0 || VLastPageButton(vScrollBar).Bounds.Height == 0);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnNone_LTR_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 0);
+
+                Assert.Equal(AccessibleStates.None, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnNone_LTR_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 50);
+
+                Assert.Equal(AccessibleStates.None, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnInvisible_LTR_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 100, value: 100);
+
+                Assert.Equal(AccessibleStates.Invisible, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnNone_RTL_Value_0()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 0);
+
+                Assert.Equal(AccessibleStates.None, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnNone_RTL_Value_50()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 50);
+
+                Assert.Equal(AccessibleStates.None, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnNone_Invisible_Value_100()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 100, value: 100);
+
+                Assert.Equal(AccessibleStates.Invisible, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnInvisible_LTR_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.No, minimum: 0, maximum: 0, value: 0);
+
+                Assert.Equal(AccessibleStates.Invisible, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        [StaFact]
+        public void VScrollBarLastPageButtonAccessibleObject_State_ReturnInvisible_RTL_MiniumEqualsMaximum()
+        {
+            RunTestForVScrollBar(vScrollBar =>
+            {
+                SetVScrollBar(vScrollBar, RightToLeft.Yes, minimum: 0, maximum: 0, value: 0);
+
+                Assert.Equal(AccessibleStates.Invisible, VLastPageButton(vScrollBar).State);
+            });
+        }
+
+        private void SetVScrollBar(VScrollBar vScrollBar, RightToLeft rightToLeft, int minimum, int maximum, int value)
+        {
+            vScrollBar.RightToLeft = rightToLeft;
+            vScrollBar.Minimum = minimum;
+            vScrollBar.Maximum = maximum;
+            vScrollBar.Value = value;
+            Application.DoEvents();
+        }
+
+        private ScrollBar.ScrollBarFirstPageButtonAccessibleObject VFirstPageButton(VScrollBar vScrollBar)
+            => ((VScrollBar.ScrollBarAccessibleObject)vScrollBar.AccessibilityObject).FirstPageButtonAccessibleObject;
+
+        private ScrollBar.ScrollBarLastPageButtonAccessibleObject VLastPageButton(VScrollBar vScrollBar)
+            => ((VScrollBar.ScrollBarAccessibleObject)vScrollBar.AccessibilityObject).LastPageButtonAccessibleObject;
+
         private void SetHScrollBar(HScrollBar hScrollBar, RightToLeft rightToLeft, int minimum, int maximum, int value)
         {
             hScrollBar.RightToLeft = rightToLeft;
@@ -401,6 +785,7 @@ namespace System.Windows.Forms.UI.IntegrationTests
                     HScrollBar hScrollBar = new()
                     {
                         Parent = form,
+                        Size = new Size(200, 50),
                     };
                     return hScrollBar;
                 },
@@ -410,6 +795,27 @@ namespace System.Windows.Forms.UI.IntegrationTests
                     await AsyncTestHelper.JoinPendingOperationsAsync(AsyncTestHelper.UnexpectedTimeout);
 
                     runTest(hScrollBar);
+                });
+        }
+
+        private void RunTestForVScrollBar(Action<VScrollBar> runTest)
+        {
+            UITest.RunControl(
+                createControl: form =>
+                {
+                    VScrollBar vScrollBar = new()
+                    {
+                        Parent = form,
+                        Size = new Size(50, 200),
+                    };
+                    return vScrollBar;
+                },
+                runTestAsync: async vScrollBar =>
+                {
+                    // Wait for pending operations so the Control is loaded completely before testing it
+                    await AsyncTestHelper.JoinPendingOperationsAsync(AsyncTestHelper.UnexpectedTimeout);
+
+                    runTest(vScrollBar);
                 });
         }
     }
