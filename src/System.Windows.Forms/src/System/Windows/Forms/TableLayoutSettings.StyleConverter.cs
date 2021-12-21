bfcc63353df0b9f1e289d505bd20d8470a624391
gpetrou <gipetrou@gmail.com>
2021-12-21 19:03:43 +0200

    Enable nullability in StyleConverter

--- src/System.Windows.Forms/src/System/Windows/Forms/TableLayoutSettings.StyleConverter.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/TableLayoutSettings.StyleConverter.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.ComponentModel;
 using System.ComponentModel.Design.Serialization;
 using System.Globalization;
@@ -14,7 +12,7 @@ namespace System.Windows.Forms
     {
         internal class StyleConverter : TypeConverter
         {
-            public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+            public override bool CanConvertTo(ITypeDescriptorContext? context, Type? destinationType)
             {
                 if (destinationType == typeof(InstanceDescriptor))
                 {
@@ -24,7 +22,7 @@ namespace System.Windows.Forms
                 return base.CanConvertTo(context, destinationType);
             }
 
-            public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+            public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
             {
                 if (destinationType == typeof(InstanceDescriptor) && value is TableLayoutStyle style)
                 {
