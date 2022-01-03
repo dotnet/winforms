fa4f0cc31554cf14ea7282a21d9f7420cea79a4a
gpetrou <4172445+gpetrou@users.noreply.github.com>
2022-01-03 23:33:53 +0200

    Enable nullability in DockingAttribute (#6425)

--- src/System.Windows.Forms/src/System/Windows/Forms/DockingAttribute.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/DockingAttribute.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     /// <summary>
@@ -26,14 +24,14 @@ namespace System.Windows.Forms
 
         public DockingBehavior DockingBehavior { get; }
 
-        public override bool Equals(object obj)
+        public override bool Equals(object? obj)
         {
             if (obj == this)
             {
                 return true;
             }
 
-            return (obj is DockingAttribute other && other.DockingBehavior == DockingBehavior);
+            return obj is DockingAttribute other && other.DockingBehavior == DockingBehavior;
         }
 
         public override int GetHashCode() => DockingBehavior.GetHashCode();
