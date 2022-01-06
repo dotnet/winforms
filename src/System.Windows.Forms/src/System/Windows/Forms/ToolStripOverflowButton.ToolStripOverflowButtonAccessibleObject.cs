367880ee492707cf01bf32c0bb407a43127945d7
gpetrou <gipetrou@gmail.com>
2022-01-06 18:56:53 +0200

    Enable nullability in classes that inherit from ToolStripDropDownItemAccessibleObject

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripOverflowButton.ToolStripOverflowButtonAccessibleObject.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripOverflowButton.ToolStripOverflowButtonAccessibleObject.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     public partial class ToolStripOverflowButton
@@ -14,7 +12,7 @@ namespace System.Windows.Forms
             {
             }
 
-            public override string Name
+            public override string? Name
             {
                 get => Owner.AccessibleName ?? SR.ToolStripOptions;
                 set => base.Name = value;
