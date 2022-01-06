a59164c4c6aecc97c4263c24674521a0c02ea8af
gpetrou <4172445+gpetrou@users.noreply.github.com>
2022-01-06 02:23:55 +0200

    Enable nullability in FeatureSupport and IFeatureSupport (#6465)

--- src/System.Windows.Forms/src/System/Windows/Forms/IFeatureSupport.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/IFeatureSupport.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     /// <summary>
@@ -26,6 +24,6 @@ namespace System.Windows.Forms
         /// <summary>
         ///  Retrieves the version of the specified feature.
         /// </summary>
-        Version GetVersionPresent(object feature);
+        Version? GetVersionPresent(object feature);
     }
 }
