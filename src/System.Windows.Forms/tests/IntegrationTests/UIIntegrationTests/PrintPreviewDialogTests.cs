6170cdb7b8d95303cc1ad2775f3bbddb9e658c40
Igor Velikorossov <RussKie@users.noreply.github.com>
2022-01-18 09:30:29 +1100

    Update PrintPreviewDialogTests.cs

--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/PrintPreviewDialogTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/PrintPreviewDialogTests.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -10,6 +10,9 @@ namespace System.Windows.Forms.UITests
 {
     public class PrintPreviewDialogTests : ControlTestBase
     {
+        // This value may need to be adjusted if tests fail in CI/different environment.
+        private const int DelayMS = 100;
+
         public PrintPreviewDialogTests(ITestOutputHelper testOutputHelper)
             : base(testOutputHelper)
         {
@@ -20,10 +23,12 @@ namespace System.Windows.Forms.UITests
         {
             await RunTestAsync(async printPreviewDialog =>
             {
-                printPreviewDialog.Focus();
                 await InputSimulator.SendAsync(
                     printPreviewDialog,
                     inputSimulator => inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_1));
+
+                // Allow a little time to process the keystroke.
+                await Task.Delay(DelayMS);
 
                 Assert.Equal(1, printPreviewDialog.PrintPreviewControl.Rows);
                 Assert.Equal(1, printPreviewDialog.PrintPreviewControl.Columns);
@@ -35,10 +40,12 @@ namespace System.Windows.Forms.UITests
         {
             await RunTestAsync(async printPreviewDialog =>
             {
-                printPreviewDialog.Focus();
                 await InputSimulator.SendAsync(
                     printPreviewDialog,
                     inputSimulator => inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_2));
+
+                // Allow a little time to process the keystroke.
+                await Task.Delay(DelayMS);
 
                 Assert.Equal(1, printPreviewDialog.PrintPreviewControl.Rows);
                 Assert.Equal(2, printPreviewDialog.PrintPreviewControl.Columns);
@@ -50,10 +57,12 @@ namespace System.Windows.Forms.UITests
         {
             await RunTestAsync(async printPreviewDialog =>
             {
-                printPreviewDialog.Focus();
                 await InputSimulator.SendAsync(
                     printPreviewDialog,
                     inputSimulator => inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_3));
+
+                // Allow a little time to process the keystroke.
+                await Task.Delay(DelayMS);
 
                 Assert.Equal(1, printPreviewDialog.PrintPreviewControl.Rows);
                 Assert.Equal(3, printPreviewDialog.PrintPreviewControl.Columns);
@@ -65,31 +74,34 @@ namespace System.Windows.Forms.UITests
         {
             await RunTestAsync(async printPreviewDialog =>
             {
-                printPreviewDialog.Focus();
                 await InputSimulator.SendAsync(
                     printPreviewDialog,
                     inputSimulator => inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_4));
+
+                // Allow a little time to process the keystroke.
+                await Task.Delay(DelayMS);
 
                 Assert.Equal(2, printPreviewDialog.PrintPreviewControl.Rows);
                 Assert.Equal(2, printPreviewDialog.PrintPreviewControl.Columns);
             });
         }
 
-        //[WinFormsFact]
-        //public async Task PrintPreviewDialog_Hotkey_Ctrl_5Async()
-        //{
-        //    await RunTestAsync(async printPreviewDialog =>
-        //    {
-        //        printPreviewDialog.Focus();
-        //        Application.DoEvents();
-        //        await InputSimulator.SendAsync(
-        //            printPreviewDialog,
-        //            inputSimulator => inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_5));
+        [WinFormsFact]
+        public async Task PrintPreviewDialog_Hotkey_Ctrl_5Async()
+        {
+            await RunTestAsync(async printPreviewDialog =>
+            {
+                await InputSimulator.SendAsync(
+                    printPreviewDialog,
+                    inputSimulator => inputSimulator.Keyboard.ModifiedKeyStroke(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_5));
 
-        //        Assert.Equal(2, printPreviewDialog.PrintPreviewControl.Rows);
-        //        Assert.Equal(3, printPreviewDialog.PrintPreviewControl.Columns);
-        //    });
-        //}
+                // Allow a little time to process the keystroke.
+                await Task.Delay(DelayMS);
+
+                Assert.Equal(2, printPreviewDialog.PrintPreviewControl.Rows);
+                Assert.Equal(3, printPreviewDialog.PrintPreviewControl.Columns);
+            });
+        }
 
         private async Task RunTestAsync(Func<PrintPreviewDialog, Task> runTest)
         {
