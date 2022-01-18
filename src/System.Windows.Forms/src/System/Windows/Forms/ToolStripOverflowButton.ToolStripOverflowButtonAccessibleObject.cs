0add28e3a8ce9b2ed8c77f8d39d9524a064d2ddc
gpetrou <gipetrou@gmail.com>
2022-01-18 08:34:56 +0200

    Use AllowNull

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripOverflowButton.ToolStripOverflowButtonAccessibleObject.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripOverflowButton.ToolStripOverflowButtonAccessibleObject.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace System.Windows.Forms
 {
     public partial class ToolStripOverflowButton
@@ -12,7 +14,8 @@ namespace System.Windows.Forms
             {
             }
 
-            public override string? Name
+            [AllowNull]
+            public override string Name
             {
                 get => Owner.AccessibleName ?? SR.ToolStripOptions;
                 set => base.Name = value;
