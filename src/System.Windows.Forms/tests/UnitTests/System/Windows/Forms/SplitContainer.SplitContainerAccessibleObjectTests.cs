ad985bc6d6c2c11d45e5b559c06aa110f051dfb7
Danil-Andrianov <danil.andrianov@akvelon.com>
2022-01-19 14:42:30 +0300

    Added UIA support for split container

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/SplitContainer.SplitContainerAccessibleObjectTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/SplitContainer.SplitContainerAccessibleObjectTests.cs
@@ -3,39 +3,39 @@
 // See the LICENSE file in the project root for more information.
 
 using Xunit;
-using static Interop;
+using static Interop.UiaCore;
 
 namespace System.Windows.Forms.Tests
 {
-    public class SplitContainer_SplitContainerAccessibilityObjectTests : IClassFixture<ThreadExceptionFixture>
+    public class SplitContainer_SplitContainerAccessibleObjectTests : IClassFixture<ThreadExceptionFixture>
     {
         [WinFormsFact]
-        public void SplitContainerAccessibilityObject_Ctor_Default()
+        public void SplitContainerAccessibleObject_Ctor_Default()
         {
-            using SplitContainer splitContainer = new SplitContainer();
-            splitContainer.CreateControl();
+            using SplitContainer splitContainer = new();
+            SplitContainer.SplitContainerAccessibleObject accessibleObject = new(splitContainer);
 
-            Assert.NotNull(splitContainer.AccessibilityObject);
-            Assert.True(splitContainer.IsHandleCreated);
+            Assert.Equal(splitContainer, accessibleObject.Owner);
+            Assert.False(splitContainer.IsHandleCreated);
         }
 
         [WinFormsFact]
-        public void SplitContainerAccessibilityObject_ControlType_IsPane_IfAccessibleRoleIsDefault()
+        public void SplitContainerAccessibleObject_ControlType_IsPane_IfAccessibleRoleIsDefault()
         {
-            using SplitContainer splitContainer = new SplitContainer();
+            using SplitContainer splitContainer = new();
             splitContainer.CreateControl();
             // AccessibleRole is not set = Default
 
-            object actual = splitContainer.AccessibilityObject.GetPropertyValue(UiaCore.UIA.ControlTypePropertyId);
+            object actual = splitContainer.AccessibilityObject.GetPropertyValue(UIA.ControlTypePropertyId);
 
-            Assert.Equal(UiaCore.UIA.PaneControlTypeId, actual);
+            Assert.Equal(UIA.PaneControlTypeId, actual);
             Assert.True(splitContainer.IsHandleCreated);
         }
 
         [WinFormsFact]
-        public void SplitContainerAccessibilityObject_Role_IsClient_ByDefault()
+        public void SplitContainerAccessibleObject_Role_IsClient_ByDefault()
         {
-            using SplitContainer splitContainer = new SplitContainer();
+            using SplitContainer splitContainer = new();
             splitContainer.CreateControl();
             // AccessibleRole is not set = Default
 
@@ -64,14 +64,58 @@ namespace System.Windows.Forms.Tests
         [MemberData(nameof(SplitContainerAccessibleObject_GetPropertyValue_ControlType_IsExpected_ForCustomRole_TestData))]
         public void SplitContainerAccessibleObject_GetPropertyValue_ControlType_IsExpected_ForCustomRole(AccessibleRole role)
         {
-            using SplitContainer splitContainer = new SplitContainer();
+            using SplitContainer splitContainer = new();
             splitContainer.AccessibleRole = role;
 
-            object actual = splitContainer.AccessibilityObject.GetPropertyValue(UiaCore.UIA.ControlTypePropertyId);
-            UiaCore.UIA expected = AccessibleRoleControlTypeMap.GetControlType(role);
+            object actual = splitContainer.AccessibilityObject.GetPropertyValue(UIA.ControlTypePropertyId);
+            UIA expected = AccessibleRoleControlTypeMap.GetControlType(role);
 
             Assert.Equal(expected, actual);
             Assert.False(splitContainer.IsHandleCreated);
+        }
+
+        [WinFormsTheory]
+        [InlineData((int)UIA.NamePropertyId, "TestName")]
+        [InlineData((int)UIA.AutomationIdPropertyId, "SplitContainer1")]
+        public void SplitContainerAccessibleObject_GetPropertyValue_Invoke_ReturnsExpected(int propertyID, object expected)
+        {
+            using SplitContainer control = new()
+            {
+                Name = "SplitContainer1",
+                AccessibleName = "TestName"
+            };
+
+            var accessibleObject = new SplitContainer.SplitContainerAccessibleObject(control);
+            object value = accessibleObject.GetPropertyValue((UIA)propertyID);
+
+            Assert.Equal(expected, value);
+            Assert.False(control.IsHandleCreated);
+        }
+
+        [WinFormsFact]
+        public void SplitContainerAccessibleObject_GetPropertyValue_HasKeyboardFocus_ReturnsTrue_IfControlHasFocus()
+        {
+            using SplitContainer control = new();
+
+            var accessibleObject = new SplitContainer.SplitContainerAccessibleObject(control);
+            Assert.False(control.IsHandleCreated);
+            control.FocusActiveControlInternal();
+            bool value = (bool)accessibleObject.GetPropertyValue(UIA.HasKeyboardFocusPropertyId);
+
+            Assert.True(value);
+            Assert.True(control.IsHandleCreated);
+        }
+
+        [WinFormsFact]
+        public void SplitContainerAccessibleObject_GetPropertyValue_HasKeyboardFocus_ReturnsFalse_IfControlHasNoFocus()
+        {
+            using SplitContainer control = new();
+
+            var accessibleObject = new SplitContainer.SplitContainerAccessibleObject(control);
+            bool value = (bool)accessibleObject.GetPropertyValue(UIA.HasKeyboardFocusPropertyId);
+
+            Assert.False(value);
+            Assert.False(control.IsHandleCreated);
         }
     }
 }
