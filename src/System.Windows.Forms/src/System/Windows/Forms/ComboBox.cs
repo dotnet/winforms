9bf78f682e9740e41f0ec87b97c5a8f088734e98
jbhensley <jbhensley@users.noreply.github.com>
2022-01-19 09:39:51 -0800

    Do not use flat style in high contrast mode

--- src/System.Windows.Forms/src/System/Windows/Forms/ComboBox.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ComboBox.cs
@@ -3903,7 +3903,7 @@ namespace System.Windows.Forms
                     break;
 
                 case WM.PAINT:
-                    if (GetStyle(ControlStyles.UserPaint) == false && (FlatStyle == FlatStyle.Flat || FlatStyle == FlatStyle.Popup))
+                    if (GetStyle(ControlStyles.UserPaint) == false && (FlatStyle == FlatStyle.Flat || FlatStyle == FlatStyle.Popup) && !DisplayInformation.HighContrast)
                     {
                         using var dropDownRegion = new Gdi32.RegionScope(FlatComboBoxAdapter._dropDownRect);
                         using var windowRegion = new Gdi32.RegionScope(Bounds);
