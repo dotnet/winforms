8f51e7253334f3dc333b7fc917e3e2ec8b9fd8eb
Igor Velikorossov <igor.velikorossov@microsoft.com>
2022-02-10 11:29:44 +1100

    Pin assembly versions to prevent revving those in servicing releases
    Relates to #6663

--- pkg/Microsoft.Private.Winforms/sdk/dotnet-windowsdesktop/UpdateFileClassification.ps1
+++ pkg/Microsoft.Private.Winforms/sdk/dotnet-windowsdesktop/UpdateFileClassification.ps1
@@ -6,6 +6,10 @@ Param(
   [string] $TargetFile,
   [Parameter(Mandatory=$True, Position=3)]
   [string] $GenerateManifest,
+  [Parameter(Mandatory=$True, Position=4)]
+  [string] $ExpectedAssemblyVersion,
+  [Parameter(Mandatory=$True, Position=5)]
+  [string] $IsServicingRelease,
   [Parameter(ValueFromRemainingArguments=$true)][String[]] $properties
 )
 
@@ -22,7 +26,7 @@ $assemblies = $xmlDoc.package.files.file | `
                 -and !$_.target.EndsWith('\Accessibility.dll', [System.StringComparison]::OrdinalIgnoreCase)
         } | `
     Select-Object -Unique @{Name="Path";Expression={Split-Path $_.target -Leaf}} | `
-    Select-Object  -ExpandProperty Path;
+    Select-Object -ExpandProperty Path;
 
 # this isn't explicitly present in the list
 $assemblies += 'System.Drawing.Common.dll';
@@ -30,6 +34,9 @@ $assemblies += 'System.Drawing.Common.dll';
 
 $needGenerate = $null;
 [bool]::TryParse($GenerateManifest, [ref]$needGenerate) | Out-Null;
+$servicingRelease = $null;
+[bool]::TryParse($IsServicingRelease, [ref]$servicingRelease) | Out-Null;
+
 
 if (!$needGenerate) {
     <#
@@ -74,3 +81,57 @@ else {
 </Project>";
     $output | Out-File -FilePath $TargetFile -Encoding utf8 -Force;
 }
+
+
+# 
+# Verify that components that are exposed as references in the targeting packs don't have their versions revved.
+# See https://github.com/dotnet/winforms/pull/6667 for more details.
+[xml] $xmlDoc = Get-Content -Path $NuspecFile -Force;
+
+# Iterate over files that MUST NOT have their versions revved with every release
+$nonRevAssemblies = $xmlDoc.package.files.file | `
+    Where-Object { 
+            ($_.target.StartsWith('lib\') -or $_.target.StartsWith('ref\')) `
+                -and $_.target.EndsWith('.dll', [System.StringComparison]::OrdinalIgnoreCase) `
+                -and !$_.target.EndsWith('resources.dll', [System.StringComparison]::OrdinalIgnoreCase) `
+                -and !$_.target.EndsWith('\Accessibility.dll', [System.StringComparison]::OrdinalIgnoreCase) `
+                -and !$_.target.EndsWith('\Microsoft.VisualBasic.dll', [System.StringComparison]::OrdinalIgnoreCase)
+        } | `
+    Select-Object -Unique src | `
+    Select-Object -ExpandProperty src;
+
+$nonRevAssemblies | `
+    sort-object | `
+    foreach-object {
+        $assembly = $_;
+        [string] $version = ([Reflection.AssemblyName]::GetAssemblyName($assembly).Version).ToString()
+
+        Write-Host "$assembly`: $version"
+        if (![string]::Equals($version, $ExpectedAssemblyVersion)) {
+            throw "$assembly is not versioned correctly. Expected: '$ExpectedAssemblyVersion', found: '$version'."
+            exit -1;
+        }
+    }
+
+# Iterate over files that MUST have their versions revved with every release
+$revAssemblies = $xmlDoc.package.files.file | `
+    Where-Object { 
+            $_.target.StartsWith('sdk\analyzers\') `
+                -and $_.target.EndsWith('.dll', [System.StringComparison]::OrdinalIgnoreCase) `
+                -and !$_.target.EndsWith('resources.dll', [System.StringComparison]::OrdinalIgnoreCase)
+        } | `
+    Select-Object -Unique src | `
+    Select-Object -ExpandProperty src;
+
+$revAssemblies | `
+    sort-object | `
+    foreach-object {
+        $assembly = $_;
+        [string] $version = ([Reflection.AssemblyName]::GetAssemblyName($assembly).Version).ToString()
+
+        Write-Host "$assembly`: $version"
+        if ($servicingRelease -and [string]::Equals($version, $ExpectedAssemblyVersion)) {
+            throw "$assembly is not versioned correctly. '$version' is not expected."
+            exit -1;
+        }
+    }
