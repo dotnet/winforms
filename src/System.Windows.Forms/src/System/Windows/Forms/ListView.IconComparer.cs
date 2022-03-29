aa9d3ff768c20c68222aefadebe9990e5bbe8d03
gpetrou <gipetrou@gmail.com>
2022-03-29 20:35:47 +0300

    Enable nullability in IconComparer

--- src/System.Windows.Forms/src/System/Windows/Forms/ListView.IconComparer.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ListView.IconComparer.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections;
 using System.Globalization;
 
@@ -30,18 +28,18 @@ namespace System.Windows.Forms
                 }
             }
 
-            public int Compare(object obj1, object obj2)
+            public int Compare(object? obj1, object? obj2)
             {
                 //subhag
-                ListViewItem currentItem = (ListViewItem)obj1;
-                ListViewItem nextItem = (ListViewItem)obj2;
+                ListViewItem? currentItem = (ListViewItem?)obj1;
+                ListViewItem? nextItem = (ListViewItem?)obj2;
                 if (sortOrder == SortOrder.Ascending)
                 {
-                    return string.Compare(currentItem.Text, nextItem.Text, false, CultureInfo.CurrentCulture);
+                    return string.Compare(currentItem?.Text, nextItem?.Text, false, CultureInfo.CurrentCulture);
                 }
                 else
                 {
-                    return string.Compare(nextItem.Text, currentItem.Text, false, CultureInfo.CurrentCulture);
+                    return string.Compare(nextItem?.Text, currentItem?.Text, false, CultureInfo.CurrentCulture);
                 }
             }
         }
