e9990f65da0695cdc7560c03ffcf6e12e2f472cd
gpetrou <gipetrou@gmail.com>
2022-03-30 08:26:07 +0300

    Add some tests for classes that inherit from Control.ControlCollection

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/TypedControlCollectionTests.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Tests;
+
+public class TypedControlCollectionTests
+{
+    [WinFormsFact]
+    public void TypedControlCollection_Ctor_Control()
+    {
+        using Panel owner = new();
+        TypedControlCollection collection = new(owner, typeof(Panel), false);
+
+        Assert.Empty(collection);
+        Assert.False(collection.IsReadOnly);
+        Assert.Same(owner, collection.Owner);
+    }
+
+    [WinFormsFact]
+    public void TypedControlCollection_Ctor_NullOwner_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>("owner", () => new TypedControlCollection(null, typeof(Panel), false));
+    }
+}
