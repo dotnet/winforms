5041c0a381a705f4d5b97ade6640fa2f52388694
Brandon Williams <willibrandon@gmail.com>
2022-05-13 01:11:31 -0700

    Remove the unused using

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripDropTargetManager.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripDropTargetManager.cs
@@ -10,7 +10,6 @@ using System.Drawing;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using static Interop;
-using IComDataObject = System.Runtime.InteropServices.ComTypes.IDataObject;
 
 namespace System.Windows.Forms
 {
