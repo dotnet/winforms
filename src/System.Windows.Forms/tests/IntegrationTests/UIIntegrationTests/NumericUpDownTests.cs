10409eb8e3ff31f33c7ba4d30f569007b784b992
Andrii Kurdiumov <kant2002@gmail.com>
2022-05-19 00:40:18 +0600

    Test that numeric up-down successfully focused This is successfully catches bug in the https://github.com/dotnet/winforms/pull/7145 implementation

---
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/NumericUpDownTests.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Drawing;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System.Windows.Forms.UITests
+{
+    public class NumericUpDownTests : ControlTestBase
+    {
+        public NumericUpDownTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [WinFormsFact]
+        public async Task NumericUpDownAccessibleObject_Focused_ReturnsCorrectValueAsync()
+        {
+            await RunSingleControlTestAsync<NumericUpDown>((form, control) =>
+            {
+                control.Focus();
+
+                var isFocused = control.Focused;
+                AccessibleObject accessibleObject = control.AccessibilityObject;
+                var focused = accessibleObject.GetFocused();
+                Assert.NotNull(focused);
+
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
