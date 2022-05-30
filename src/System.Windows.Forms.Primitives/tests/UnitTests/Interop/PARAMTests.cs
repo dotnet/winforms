a9f3ddb1bf2203cd25d8ef968ca352dd338ba125
Tanya Solyanik <tanyaso@microsoft.com>
2022-05-30 12:21:51 -0700

    Fix CS8778 warning and CS0221 error when casting to nint

--- src/System.Windows.Forms.Primitives/tests/UnitTests/Interop/PARAMTests.cs
+++ src/System.Windows.Forms.Primitives/tests/UnitTests/Interop/PARAMTests.cs
@@ -40,85 +40,85 @@ namespace System.Windows.Forms.Primitives.Tests.Interop
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is32bit))]
         public unsafe void LOWORD_x32_Result()
         {
-            Assert.Equal(0x0304, PARAM.LOWORD((IntPtr)unchecked((int)0x01020304)));
-            Assert.Equal(0xF3F4, PARAM.LOWORD((IntPtr)unchecked((int)0xF1F2F3F4)));
+            Assert.Equal(0x0304, PARAM.LOWORD((nint)(int)0x01020304));
+            Assert.Equal(0xF3F4, PARAM.LOWORD((nint)unchecked((int)0xF1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is64bit))]
         public unsafe void LOWORD_x64_Result()
         {
-            Assert.Equal(0x0304, PARAM.LOWORD((IntPtr)unchecked((long)0x0506070801020304)));
-            Assert.Equal(0xF3F4, PARAM.LOWORD((IntPtr)unchecked((long)0xF5F6F7F8F1F2F3F4)));
+            Assert.Equal(0x0304, PARAM.LOWORD(unchecked((nint)(long)0x0506070801020304)));
+            Assert.Equal(0xF3F4, PARAM.LOWORD(unchecked((nint)unchecked((long)0xF5F6F7F8F1F2F3F4))));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is32bit))]
         public unsafe void HIWORD_x32_Result()
         {
-            Assert.Equal(0x0102, PARAM.HIWORD((IntPtr)unchecked((int)0x01020304)));
-            Assert.Equal(0xF1F2, PARAM.HIWORD((IntPtr)unchecked((int)0xF1F2F3F4)));
+            Assert.Equal(0x0102, PARAM.HIWORD((nint)(int)0x01020304));
+            Assert.Equal(0xF1F2, PARAM.HIWORD((nint)unchecked((int)0xF1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is64bit))]
         public unsafe void HIWORD_x64_Result()
         {
-            Assert.Equal(0x0102, PARAM.HIWORD((IntPtr)unchecked((long)0x0506070801020304)));
-            Assert.Equal(0xF1F2, PARAM.HIWORD((IntPtr)unchecked((long)0xF5F6F7F8F1F2F3F4)));
+            Assert.Equal(0x0102, PARAM.HIWORD(unchecked((nint)unchecked((long)0x0506070801020304))));
+            Assert.Equal(0xF1F2, PARAM.HIWORD(unchecked((nint)unchecked((long)0xF5F6F7F8F1F2F3F4))));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is32bit))]
         public unsafe void SignedLOWORD_x32_Result()
         {
-            Assert.Equal(unchecked((short)0x0304), PARAM.SignedLOWORD((IntPtr)unchecked((int)0x01020304)));
-            Assert.Equal(unchecked((short)0xF3F4), PARAM.SignedLOWORD((IntPtr)unchecked((int)0xF1F2F3F4)));
+            Assert.Equal(unchecked((short)0x0304), PARAM.SignedLOWORD((nint)(int)0x01020304));
+            Assert.Equal(unchecked((short)0xF3F4), PARAM.SignedLOWORD((nint)unchecked((int)0xF1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is64bit))]
         public unsafe void SignedLOWORD_x64_Result()
         {
-            Assert.Equal(unchecked((short)0x0304), PARAM.SignedLOWORD((IntPtr)unchecked((long)0x0506070801020304)));
-            Assert.Equal(unchecked((short)0xF3F4), PARAM.SignedLOWORD((IntPtr)unchecked((long)0xF5F6F7F8F1F2F3F4)));
+            Assert.Equal(unchecked((short)0x0304), PARAM.SignedLOWORD(unchecked((nint)(long)0x0506070801020304)));
+            Assert.Equal(unchecked((short)0xF3F4), PARAM.SignedLOWORD(unchecked((nint)unchecked((long)0xF5F6F7F8F1F2F3F4))));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is32bit))]
         public unsafe void SignedHIWORD_x32_Result()
         {
-            Assert.Equal(unchecked((short)0x0102), PARAM.SignedHIWORD((IntPtr)unchecked((int)0x01020304)));
-            Assert.Equal(unchecked((short)0xF1F2), PARAM.SignedHIWORD((IntPtr)unchecked((int)0xF1F2F3F4)));
+            Assert.Equal(unchecked((short)0x0102), PARAM.SignedHIWORD((nint)(int)0x01020304));
+            Assert.Equal(unchecked((short)0xF1F2), PARAM.SignedHIWORD((nint)unchecked((int)0xF1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is64bit))]
         public unsafe void SignedHIWORD_x64_Result()
         {
-            Assert.Equal(unchecked((short)0x0102), PARAM.SignedHIWORD((IntPtr)unchecked((long)0x0506070801020304)));
-            Assert.Equal(unchecked((short)0xF1F2), PARAM.SignedHIWORD((IntPtr)unchecked((long)0xF5F6F7F8F1F2F3F4)));
+            Assert.Equal(unchecked((short)0x0102), PARAM.SignedHIWORD(unchecked((nint)unchecked((long)0x0506070801020304))));
+            Assert.Equal(unchecked((short)0xF1F2), PARAM.SignedHIWORD(unchecked((nint)unchecked((long)0xF5F6F7F8F1F2F3F4))));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is32bit))]
         public unsafe void ToInt_x32_Result()
         {
-            Assert.Equal(unchecked((int)0x01020304), PARAM.ToInt((IntPtr)unchecked((int)0x01020304)));
-            Assert.Equal(unchecked((int)0xF1F2F3F4), PARAM.ToInt((IntPtr)unchecked((int)0xF1F2F3F4)));
+            Assert.Equal(unchecked((int)0x01020304), PARAM.ToInt((nint)(int)0x01020304));
+            Assert.Equal(unchecked((int)0xF1F2F3F4), PARAM.ToInt((nint)unchecked((int)0xF1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is64bit))]
         public unsafe void ToInt_x64_Result()
         {
-            Assert.Equal(unchecked((int)0x01020304), PARAM.ToInt((IntPtr)unchecked((long)0x0506070801020304)));
-            Assert.Equal(unchecked((int)0xF1F2F3F4), PARAM.ToInt((IntPtr)unchecked((long)0xF5F6F7F8F1F2F3F4)));
+            Assert.Equal(unchecked((int)0x01020304), PARAM.ToInt(unchecked((nint)(long)0x0506070801020304)));
+            Assert.Equal(unchecked((int)0xF1F2F3F4), PARAM.ToInt(unchecked((nint)(long)0xF5F6F7F8F1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is32bit))]
         public unsafe void ToUInt_x32_Result()
         {
-            Assert.Equal((uint)0x01020304, PARAM.ToUInt((IntPtr)unchecked((int)0x01020304)));
-            Assert.Equal((uint)0xF1F2F3F4, PARAM.ToUInt((IntPtr)unchecked((int)0xF1F2F3F4)));
+            Assert.Equal((uint)0x01020304, PARAM.ToUInt((nint)(int)0x01020304));
+            Assert.Equal((uint)0xF1F2F3F4, PARAM.ToUInt((nint)unchecked((int)0xF1F2F3F4)));
         }
 
         [ConditionalFact(typeof(ArchitectureDetection), nameof(ArchitectureDetection.Is64bit))]
         public unsafe void ToUInt_x64_Result()
         {
-            Assert.Equal((uint)0x01020304, PARAM.ToUInt((IntPtr)unchecked((long)0x0506070801020304)));
-            Assert.Equal((uint)0xF1F2F3F4, PARAM.ToUInt((IntPtr)unchecked((long)0xF5F6F7F8F1F2F3F4)));
+            Assert.Equal((uint)0x01020304, PARAM.ToUInt(unchecked((nint)(long)0x0506070801020304)));
+            Assert.Equal((uint)0xF1F2F3F4, PARAM.ToUInt(unchecked((nint)(long)0xF5F6F7F8F1F2F3F4)));
         }
     }
 }
