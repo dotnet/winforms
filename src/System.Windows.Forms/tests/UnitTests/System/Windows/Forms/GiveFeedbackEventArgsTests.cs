f343c22c2927f8331749cf61cb7e3501eb7ca8d0
Brandon Williams <willibrandon@gmail.com>
2022-06-04 10:28:45 -0700

    Add GiveFeedbackEventArgs unit tests

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/GiveFeedbackEventArgsTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/GiveFeedbackEventArgsTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Drawing;
 using Xunit;
 
 namespace System.Windows.Forms.Tests
@@ -20,6 +21,40 @@ namespace System.Windows.Forms.Tests
         }
 
         [Theory]
+        [MemberData(nameof(CursorOffset_TestData))]
+        public void CursorOffset_Set_GetReturnsExpected(Point value)
+        {
+            var e = new GiveFeedbackEventArgs(DragDropEffects.None, false, new Bitmap(1, 1), new Point(0, 0), false)
+            {
+                CursorOffset = value
+            };
+            Assert.Equal(value, e.CursorOffset);
+        }
+
+        public static IEnumerable<object[]> CursorOffset_TestData()
+        {
+            yield return new object[] { new Point(1, 1) };
+            yield return new object[] { new Point(-1, -1) };
+        }
+
+        [Theory]
+        [MemberData(nameof(DragImage_TestData))]
+        public void DragImage_Set_GetReturnsExpected(Bitmap value)
+        {
+            var e = new GiveFeedbackEventArgs(DragDropEffects.None, false, new Bitmap(2, 2), new Point(0, 0), false)
+            {
+                DragImage = value
+            };
+            Assert.Equal(value, e.DragImage);
+        }
+
+        public static IEnumerable<object[]> DragImage_TestData()
+        {
+            yield return new object[] { new Bitmap(1, 1) };
+            yield return new object[] { null };
+        }
+
+        [Theory]
         [InlineData(true)]
         [InlineData(false)]
         public void UseDefaultCursors_Set_GetReturnsExpected(bool value)
@@ -29,6 +64,18 @@ namespace System.Windows.Forms.Tests
                 UseDefaultCursors = value
             };
             Assert.Equal(value, e.UseDefaultCursors);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void UseDefaultDragImage_Set_GetReturnsExpected(bool value)
+        {
+            var e = new GiveFeedbackEventArgs(DragDropEffects.None, false, new Bitmap(1, 1), new Point(0, 0), false)
+            {
+                UseDefaultDragImage = value
+            };
+            Assert.Equal(value, e.UseDefaultDragImage);
         }
     }
 }
