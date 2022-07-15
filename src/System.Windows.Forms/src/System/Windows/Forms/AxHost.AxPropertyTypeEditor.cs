3f53acb48c2a9b936c1ab274d525afec81905a35
gpetrou <gipetrou@gmail.com>
2022-07-15 11:10:47 +0300

    Enable nullability in AxPropertyTypeEditor

--- src/System.Windows.Forms/src/System/Windows/Forms/AxHost.AxPropertyTypeEditor.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AxHost.AxPropertyTypeEditor.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.ComponentModel;
 using System.Drawing.Design;
 using System.Windows.Forms.Design;
@@ -30,21 +28,21 @@ namespace System.Windows.Forms
             ///  the user to modify the value.  Host assistance in presenting UI to the user
             ///  can be found through the valueAccess.getService function.
             /// </summary>
-            public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+            public override object? EditValue(ITypeDescriptorContext? context, IServiceProvider provider, object? value)
             {
                 try
                 {
-                    object instance = context.Instance;
+                    object? instance = context?.Instance;
                     _propertyDescriptor._owner.ShowPropertyPageForDispid(_propertyDescriptor.Dispid, _guid);
                 }
-                catch (Exception ex1)
+                catch (Exception ex)
                 {
                     if (provider is not null)
                     {
-                        IUIService uiSvc = (IUIService)provider.GetService(typeof(IUIService));
+                        IUIService? uiSvc = (IUIService?)provider.GetService(typeof(IUIService));
                         if (uiSvc is not null)
                         {
-                            uiSvc.ShowError(ex1, SR.ErrorTypeConverterFailed);
+                            uiSvc.ShowError(ex, SR.ErrorTypeConverterFailed);
                         }
                     }
                 }
@@ -56,7 +54,7 @@ namespace System.Windows.Forms
             ///  Retrieves the editing style of the Edit method.  If the method
             ///  is not supported, this will return None.
             /// </summary>
-            public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+            public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context)
             {
                 return UITypeEditorEditStyle.Modal;
             }
