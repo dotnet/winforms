25713e5595129e54ab41e4ccac5c6534ad9f47af
Loni Tra <lonitra@microsoft.com>
2022-07-20 14:43:20 -0700

    Convert CloseEnhMetaFile to Cswin32 PInvoke (#7463)

--- src/System.Windows.Forms.Primitives/src/Interop/Gdi32/Interop.HENHMETAFILE.cs
+++ src/System.Windows.Forms.Primitives/src/Interop/Gdi32/Interop.HENHMETAFILE.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using Gdi = Windows.Win32.Graphics.Gdi;
+
 internal static partial class Interop
 {
     internal static partial class Gdi32
@@ -16,6 +18,8 @@ internal static partial class Interop
 
             public static explicit operator IntPtr(HENHMETAFILE hmetafile) => hmetafile.Handle;
             public static explicit operator HENHMETAFILE(IntPtr hmetafile) => new HENHMETAFILE(hmetafile);
+            public static implicit operator HENHMETAFILE(Gdi.HENHMETAFILE hmetafile) => new(hmetafile.Value);
+            public static implicit operator Gdi.HENHMETAFILE(HENHMETAFILE hmetafile) => new(hmetafile.Handle);
         }
     }
 }
