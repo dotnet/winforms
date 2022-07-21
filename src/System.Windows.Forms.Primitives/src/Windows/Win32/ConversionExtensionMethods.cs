277c45a6efcb56620379bb7ecc133affdbd5807f
Loni Tra <lonitra@microsoft.com>
2022-07-20 17:33:03 -0700

    Convert Begin/EndPaint and AdjustWindowRect
    - Also start on GetClientRect
    - Add conversion extension methods

---
+++ src/System.Windows.Forms.Primitives/src/Windows/Win32/ConversionExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Drawing;
+using Windows.Win32.Foundation;
+
+namespace Windows.Win32
+{
+    internal static class ConversionExtensionMethods
+    {
+        public static Rectangle ToRectangle(this RECT rect) => Rectangle.FromLTRB(rect.left, rect.top, rect.right, rect.bottom);
+
+        public static int Width(this RECT rect) => rect.right - rect.left;
+
+        public static int Height(this RECT rect) => rect.bottom - rect.top;
+
+        public static RECT ToRect(this Size size) => new() { right = size.Width, bottom = size.Height };
+
+        public static Size Size(this RECT rect) => new(rect.Width(), rect.Height());
+    }
+}
--- src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.BitBlt.cs
+++ src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.BitBlt.cs
@@ -9,6 +9,13 @@ namespace Windows.Win32
 {
     internal static partial class PInvoke
     {
+        public static BOOL GetClientRect(IHandle hWnd, out RECT lpRect)
+        {
+            BOOL result = GetClientRect((HWND)hWnd.Handle, out lpRect);
+            GC.KeepAlive(hWnd);
+            return result;
+        }
+
         public static BOOL BitBlt(
             IHandle hdc,
             int x,
