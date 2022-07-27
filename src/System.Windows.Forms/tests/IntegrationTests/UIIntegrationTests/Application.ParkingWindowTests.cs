1038c61fc0f88ce85a397ffc896ab9cad81b0917
Brandon Williams <willibrandon@gmail.com>
2022-07-26 23:34:39 -0700

    Migrate ParkingWindow test to UIIntegration
    -Allows the test to be un-skipped without destablising the test suite.

---
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/Application.ParkingWindowTests.cs
@@ -0,0 +1,60 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System.Windows.Forms.UITests
+{
+    public class ParkingWindowTests : ControlTestBase
+    {
+        public ParkingWindowTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [WinFormsFact]
+        public void ParkingWindow_DoesNotThrowOnGarbageCollecting()
+        {
+            Control.CheckForIllegalCrossThreadCalls = true;
+
+            Form form = InitFormWithControlToGarbageCollect();
+
+            try
+            {
+                // Force garbage collecting to access combobox from another (GC) thread.
+                GC.Collect();
+
+                GC.WaitForPendingFinalizers();
+            }
+            catch (Exception ex)
+            {
+                Assert.True(ex is null, $"Expected no exception, but got: {ex?.Message}"); // Actually need to check whether GC.Collect() does not throw exception.
+            }
+        }
+
+        private Form InitFormWithControlToGarbageCollect()
+        {
+            Form form = new Form();
+            ComboBox? comboBox = new ComboBox
+            {
+                DropDownStyle = ComboBoxStyle.DropDown
+            };
+
+            form.Controls.Add(comboBox);
+            form.Show();
+
+            // Park combobox handle in ParkingWindow.
+            comboBox.Parent = null;
+
+            // Recreate combobox handle to set parent to ParkingWindow.
+            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+
+            // Lose the reference to combobox to allow Garbage collecting combobox.
+            comboBox = null;
+
+            return form;
+        }
+    }
+}
--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/Application.ParkingWindowTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/Application.ParkingWindowTests.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Runtime.InteropServices;
-using Microsoft.DotNet.RemoteExecutor;
 using Xunit;
 using static System.Windows.Forms.Application;
 using static Interop;
@@ -12,53 +11,6 @@ namespace System.Windows.Forms.Tests
 {
     public class ParkingWindowTests
     {
-        [WinFormsFact(Skip = "Crash with AbandonedMutexException. See: https://github.com/dotnet/arcade/issues/5325")]
-        public void ParkingWindow_DoesNotThrowOnGarbageCollecting()
-        {
-            using RemoteInvokeHandle invokerHandle = RemoteExecutor.Invoke(() =>
-            {
-                Control.CheckForIllegalCrossThreadCalls = true;
-
-                Form form = InitFormWithControlToGarbageCollect();
-
-                try
-                {
-                    // Force garbage collecting to access combobox from another (GC) thread.
-                    GC.Collect();
-
-                    GC.WaitForPendingFinalizers();
-                }
-                catch (Exception ex)
-                {
-                    Assert.True(ex is null, "Expected no exception, but got: " + ex.Message); // Actually need to check whether GC.Collect() does not throw exception.
-                }
-            });
-
-            // verify the remote process succeeded
-            Assert.Equal(RemoteExecutor.SuccessExitCode, invokerHandle.ExitCode);
-        }
-
-        private Form InitFormWithControlToGarbageCollect()
-        {
-            Form form = new Form();
-            ComboBox comboBox = new ComboBox();
-            comboBox.DropDownStyle = ComboBoxStyle.DropDown;
-
-            form.Controls.Add(comboBox);
-            form.Show();
-
-            // Park combobox handle in ParkingWindow.
-            comboBox.Parent = null;
-
-            // Recreate combobox handle to set parent to ParkingWindow.
-            comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
-
-            // Lose the reference to combobox to allow Garbage collecting combobox.
-            comboBox = null;
-
-            return form;
-        }
-
         [WinFormsFact]
         public void ParkingWindow_Unaware()
         {
