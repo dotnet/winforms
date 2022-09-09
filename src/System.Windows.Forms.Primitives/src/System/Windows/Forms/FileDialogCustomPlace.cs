09dcb64cacaeff0b8340c88e41e58ca8cecab4c1
Loni Tra <lonitra@microsoft.com>
2022-09-09 16:15:47 -0700

    Convert threads to CsWin32 (#7650)
    * Convert threads to CsWin32
    
    - Converted `SHCreateShellItem`, `SHGetPathFromIDListLongPath` and `SHBrowseForFolderW`.
    - Changed `BrowseInfoFlags` with CsWin32 `BIF_*`
    - Added `IShellItem = Interop.Shell32.IShellItem` and `SIGDN = Interop.Shell32.SIGDN` to resolve ambigous usings.
    
    * Update src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/FolderNameEditor.FolderBrowserStyles.cs
    
    Co-authored-by: Igor Velikorossov <RussKie@users.noreply.github.com>
    
    * Fixed conditions of if statements and changed name of method
    
    * added names for null parameters, change variable name, added spaces, removed unnecessary using.
    
    * removed unnecessary using
    
    * delete unused file
    
    * removed SIGDN from global usings
    
    Co-authored-by: Igor Velikorossov <RussKie@users.noreply.github.com>

--- src/System.Windows.Forms.Primitives/src/System/Windows/Forms/FileDialogCustomPlace.cs
+++ src/System.Windows.Forms.Primitives/src/System/Windows/Forms/FileDialogCustomPlace.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Diagnostics.CodeAnalysis;
-using static Interop.Shell32;
 
 namespace System.Windows.Forms
 {
@@ -80,7 +79,7 @@ namespace System.Windows.Forms
                 }
             }
 
-            return GetShellItemForPath(filePathString);
+            return PInvoke.SHCreateShellItem(filePathString);
         }
     }
 }
