d6c4b4f82eaf4d46eedffba682d9bc104de33510
Loni Tra <lonitra@microsoft.com>
2022-09-09 12:13:39 -0700

    Convert Begin/EndPaint and AdjustWindowRect (#7470)
    * Convert Begin/EndPaint and AdjustWindowRect
    
    - Also start on GetClientRect
    - Add conversion extension methods
    
    * Re-add Interop.PAINSTRUCT and utilize it for PInvoke.BeginPaintScope
    - refactor GetClientRect to own file

---
+++ src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.BeginPaintScope.cs
@@ -0,0 +1,53 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Drawing;
+using Windows.Win32.Foundation;
+using Windows.Win32.Graphics.Gdi;
+
+namespace Windows.Win32
+{
+    internal static partial class PInvoke
+    {
+        /// <summary>
+        ///  Helper to scope lifetime of an HDC retrieved via <see cref="BeginPaint(HWND, out PAINTSTRUCT)"/>
+        /// </summary>
+        /// <remarks>
+        ///  <para>
+        ///   Use in a <see langword="using" /> statement. If you must pass this around, always pass
+        ///   by <see langword="ref" /> to avoid duplicating the handle and risking a double EndPaint.
+        ///  </para>
+        /// </remarks>
+#if DEBUG
+        internal class BeginPaintScope : DisposalTracking.Tracker, IDisposable
+#else
+        internal readonly ref struct BeginPaintScope
+#endif
+        {
+            private readonly PAINTSTRUCT _paintStruct;
+
+            public HDC HDC { get; }
+            public HWND HWND { get; }
+            public Rectangle PaintRectangle => _paintStruct.rcPaint.ToRectangle();
+
+            public BeginPaintScope(HWND hwnd)
+            {
+                HDC = BeginPaint(hwnd, out _paintStruct);
+                HWND = hwnd;
+            }
+
+            public static implicit operator Interop.Gdi32.HDC(in BeginPaintScope scope) => scope.HDC;
+            public static implicit operator HDC(in BeginPaintScope scope) => scope.HDC;
+
+            public void Dispose()
+            {
+                EndPaint(HWND, _paintStruct);
+
+#if DEBUG
+                GC.SuppressFinalize(this);
+#endif
+            }
+        }
+    }
+}
--- src/System.Windows.Forms/src/System/Windows/Forms/Form.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Form.cs
@@ -14,6 +14,7 @@ using System.Windows.Forms.Layout;
 using System.Windows.Forms.VisualStyles;
 using Windows.Win32;
 using Windows.Win32.System.Threading;
+using Windows.Win32.UI.WindowsAndMessaging;
 using static Interop;
 
 namespace System.Windows.Forms
@@ -3091,16 +3092,16 @@ namespace System.Windows.Forms
         private Size ComputeWindowSize(Size clientSize)
         {
             CreateParams cp = CreateParams;
-            return ComputeWindowSize(clientSize, cp.Style, cp.ExStyle);
+            return ComputeWindowSize(clientSize, (WINDOW_STYLE)cp.Style, (WINDOW_EX_STYLE)cp.ExStyle);
         }
 
         /// <summary>
         ///  Computes the window size from the clientSize base on the specified
         ///  window styles. This will not return the correct size if menus wrap.
         /// </summary>
-        private Size ComputeWindowSize(Size clientSize, int style, int exStyle)
+        private Size ComputeWindowSize(Size clientSize, WINDOW_STYLE style, WINDOW_EX_STYLE exStyle)
         {
-            RECT result = new RECT(0, 0, clientSize.Width, clientSize.Height);
+            var result = clientSize.ToRect();
             AdjustWindowRectExForControlDpi(ref result, style, false, exStyle);
             return new Size(result.right - result.left, result.bottom - result.top);
         }
@@ -3517,7 +3518,7 @@ namespace System.Windows.Forms
                 // When computing the client window size, don't tell them that
                 // we are going to be maximized!
                 int maskedStyle = cp.Style & ~(int)(User32.WS.MAXIMIZE | User32.WS.MINIMIZE);
-                Size correct = ComputeWindowSize(ClientSize, maskedStyle, cp.ExStyle);
+                Size correct = ComputeWindowSize(ClientSize, (WINDOW_STYLE)maskedStyle, (WINDOW_EX_STYLE)cp.ExStyle);
                 cp.Width = correct.Width;
                 cp.Height = correct.Height;
             }
@@ -4810,7 +4811,7 @@ namespace System.Windows.Forms
                 Size restoredSize = _restoredWindowBounds.Size;
                 if ((_restoredWindowBoundsSpecified & BoundsSpecified.Size) != 0)
                 {
-                    restoredSize = SizeFromClientSize(restoredSize.Width, restoredSize.Height);
+                    restoredSize = SizeFromClientSizeInternal(restoredSize);
                 }
 
                 SetBounds(_restoredWindowBounds.X, _restoredWindowBounds.Y,
