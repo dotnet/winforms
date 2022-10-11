dotnet/winforms

commit e70fe16639bf582db55ef8f2f0061f6bb26b3ad0

Port System.ComponentModel.Design.* for #469 (#606)
* port DesignerActionPropertyItem

* port DesignerActionMethodItem

* port UndoEngine

* adding inheritance service constructor test

* port DesignerSurface

* PR comments

* PR comments

* adding line ending to SR.resx

* fixed xml line endings

src/System.Windows.Forms.Design/src/System/ComponentModel/Design/StatusCommandUI.cs
@@ -1,0 +1,130 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace System.ComponentModel.Design
+{
+    /// <summary>
+    /// This class provides a single entrypoint used by the Behaviors, KeySize and KeyMoves (in CommandSets) and SelectionService to update the StatusBar Information.  
+    /// </summary>
+    internal class StatusCommandUI
+    {
+        MenuCommand _statusRectCommand = null;
+        IMenuCommandService _menuService = null;
+        readonly IServiceProvider _serviceProvider;
+
+        public StatusCommandUI(IServiceProvider provider)
+        {
+            _serviceProvider = provider;
+        }
+
+        /// <summary>
+        /// Retrieves the menu editor service, which we cache for speed.
+        /// </summary>
+        private IMenuCommandService MenuService
+        {
+            get
+            {
+                if (_menuService == null)
+                {
+                    _menuService = (IMenuCommandService)_serviceProvider.GetService(typeof(IMenuCommandService));
+                }
+                return _menuService;
+            }
+        }
+
+        /// <summary>
+        /// Retrieves the actual StatusRectCommand, which we cache for speed.
+        /// </summary>
+        private MenuCommand StatusRectCommand
+        {
+            get
+            {
+                if (_statusRectCommand == null)
+                {
+                    if (MenuService != null)
+                    {
+                        _statusRectCommand = MenuService.FindCommand(MenuCommands.SetStatusRectangle);
+                    }
+                }
+                return _statusRectCommand;
+            }
+        }
+
+        /// <summary>
+        /// Actual Function which invokes the command.
+        /// </summary>
+        public void SetStatusInformation(Component selectedComponent, Point location)
+        {
+            if (selectedComponent == null)
+            {
+                return;
+            }
+            Rectangle bounds = Rectangle.Empty;
+            if (selectedComponent is Control c)
+            {
+                bounds = c.Bounds;
+            }
+            else
+            {
+                PropertyDescriptor BoundsProp = TypeDescriptor.GetProperties(selectedComponent)["Bounds"];
+                if (BoundsProp != null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
+                {
+                    bounds = (Rectangle)BoundsProp.GetValue(selectedComponent);
+                }
+            }
+            if (location != Point.Empty)
+            {
+                bounds.X = location.X;
+                bounds.Y = location.Y;
+            }
+            if (StatusRectCommand != null)
+            {
+                StatusRectCommand.Invoke(bounds);
+            }
+        }
+
+        /// <summary>
+        /// Actual Function which invokes the command.
+        /// </summary>
+        public void SetStatusInformation(Component selectedComponent)
+        {
+            if (selectedComponent == null)
+            {
+                return;
+            }
+            Rectangle bounds = Rectangle.Empty;
+            if (selectedComponent is Control c)
+            {
+                bounds = c.Bounds;
+            }
+            else
+            {
+                PropertyDescriptor BoundsProp = TypeDescriptor.GetProperties(selectedComponent)["Bounds"];
+                if (BoundsProp != null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
+                {
+                    bounds = (Rectangle)BoundsProp.GetValue(selectedComponent);
+                }
+            }
+            if (StatusRectCommand != null)
+            {
+                StatusRectCommand.Invoke(bounds);
+            }
+        }
+
+        /// <summary>
+        /// Actual Function which invokes the command.
+        /// </summary>
+        public void SetStatusInformation(Rectangle bounds)
+        {
+            if (StatusRectCommand != null)
+            {
+                StatusRectCommand.Invoke(bounds);
+            }
+        }
+    }
+}

src/System.Windows.Forms.Design.Editors/src/System/ComponentModel/Design/CollectionEditor.cs
@@ -1325,7 +1325,6 @@ namespace System.ComponentModel.Design
 
             private void UpdateItemWidths(ListItem item)
             {
-                // VSWhidbey#384112: Its neither safe nor accurate to perform these width calculations prior to normal listbox handle creation. So we nop in this case now.
                 if (!_listbox.IsHandleCreated)
                 {
                     return;
@@ -1485,8 +1484,6 @@ namespace System.ComponentModel.Design
             /// </summary>
             private void Listbox_handleCreated(object sender, EventArgs e)
             {
-                // VSWhidbey#384112: Since we no longer perform width calculations prior to handle
-                // creation now, we need to ensure we do it at least once after handle creation.
                 UpdateItemWidths(null);
             }
 

src/System.Windows.Forms.Design/src/Resources/SR.resx
@@ -213,4 +213,91 @@
   <data name="UnsafeNativeMethodsNotImplemented" xml:space="preserve">
     <value>Not implemented.</value>
   </data>
+  <data name="DesignerActionPanel_CouldNotFindMethod" xml:space="preserve">
+    <value>Could not find method '{0}'</value>
+  </data>
+  <data name="UndoEngineComponentAdd0" xml:space="preserve">
+    <value>Add Component</value>
+  </data>
+  <data name="UndoEngineComponentAdd1" xml:space="preserve">
+    <value>Add {0}</value>
+  </data>
+  <data name="UndoEngineComponentChange0" xml:space="preserve">
+    <value>Change Component</value>
+  </data>
+  <data name="UndoEngineComponentChange1" xml:space="preserve">
+    <value>Change {0}</value>
+  </data>
+  <data name="UndoEngineComponentChange2" xml:space="preserve">
+    <value>Change {0}.{1}</value>
+  </data>
+  <data name="UndoEngineComponentRemove0" xml:space="preserve">
+    <value>Remove Component</value>
+  </data>
+  <data name="UndoEngineComponentRemove1" xml:space="preserve">
+    <value>Remove {0}</value>
+  </data>
+  <data name="UndoEngineComponentRename" xml:space="preserve">
+    <value>Rename {0} to {1}</value>
+  </data>
+  <data name="UndoEngineMissingService" xml:space="preserve">
+    <value>The service {0} is required but could not be found.  If you have removed this service ensure that you provide a replacement.</value>
+  </data>
+  <data name="DesignerHostCantDestroyInheritedComponent" xml:space="preserve">
+    <value>Cannot remove or destroy inherited component '{0}'.</value>
+  </data>
+  <data name="DesignerHostCyclicAdd" xml:space="preserve">
+    <value>Cannot add an instance of {0} to the designer because it would create a circular dependency.  Make sure that the type does not have the same namespace and type name as the root component {1}.</value>
+  </data>
+  <data name="DesignerHostDesignerNeedsComponent" xml:space="preserve">
+    <value>Failure initializing the designer.  It has no Component member.</value>
+  </data>
+  <data name="DesignerHostDestroyComponentTransaction" xml:space="preserve">
+    <value>Deleting '{0}'.</value>
+  </data>
+  <data name="DesignerHostDuplicateName" xml:space="preserve">
+    <value>The name {0} is already in use by another component.</value>
+  </data>
+  <data name="DesignerHostFailedComponentCreate" xml:space="preserve">
+    <value>Component of type {0} could not be created.  Make sure the type implements IComponent and provides an appropriate public constructor.  Appropriate constructors either take no parameters or take a single IContainer parameter.</value>
+  </data>
+  <data name="DesignerHostGenericTransactionName" xml:space="preserve">
+    <value>No Description Available</value>
+  </data>
+  <data name="DesignerHostLoaderSpecified" xml:space="preserve">
+    <value>A designer loader has already been configured for this design surface.</value>
+  </data>
+  <data name="DesignerHostNestedTransaction" xml:space="preserve">
+    <value>The designer transaction '{0}' cannot be committed or canceled because nested transaction '{1}' is still active.  Commit or cancel the nested transaction first.</value>
+  </data>
+  <data name="DesignerHostNoBaseClass" xml:space="preserve">
+    <value>Cannot open a designer for the file because the class within it does not inherit from a class that can be visually designed.</value>
+  </data>
+  <data name="DesignerHostNoTopLevelDesigner" xml:space="preserve">
+    <value>There is no designer for the class {0}.</value>
+  </data>
+  <data name="DesignerHostUnloading" xml:space="preserve">
+    <value>New components cannot be added while a designer is unloading.</value>
+  </data>
+  <data name="DesignSurfaceContainerDispose" xml:space="preserve">
+    <value>The container cannot be disposed at design time.</value>
+  </data>
+  <data name="DesignSurfaceDesignerNotLoaded" xml:space="preserve">
+    <value>Cannot create a view for this design surface because the designer is not loaded.</value>
+  </data>
+  <data name="DesignSurfaceFatalError" xml:space="preserve">
+    <value>An error occurred while loading the document.  Fix the error, and then try loading the document again.  The error message follows:\r\n\r\n{0}</value>
+  </data>
+  <data name="DesignSurfaceNoRootComponent" xml:space="preserve">
+    <value>The designer loader did not provide a root component but has not indicated why.</value>
+  </data>
+  <data name="DesignSurfaceNoSupportedTechnology" xml:space="preserve">
+    <value>The designer loaded, but it does not offer a view compatible with this design surface.</value>
+  </data>
+  <data name="DesignSurfaceServiceIsFixed" xml:space="preserve">
+    <value>The service {0} cannot be removed from the service container.</value>
+  </data>
+  <data name="ExtenderProviderServiceDuplicateProvider" xml:space="preserve">
+    <value>The extender provider {0} has already been added as an extender.  Adding another would result in duplicate properties.</value>
+  </data>
 </root>

src/System.Windows.Forms.Design/src/Resources/xlf/SR.cs.xlf
@@ -22,6 +22,101 @@
         <target state="new">Integer cannot be converted to a float.</target>
         <note />
       </trans-unit>
+      <trans-unit id="DesignSurfaceContainerDispose">
+        <source>The container cannot be disposed at design time.</source>
+        <target state="new">The container cannot be disposed at design time.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignSurfaceDesignerNotLoaded">
+        <source>Cannot create a view for this design surface because the designer is not loaded.</source>
+        <target state="new">Cannot create a view for this design surface because the designer is not loaded.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignSurfaceFatalError">
+        <source>An error occurred while loading the document.  Fix the error, and then try loading the document again.  The error message follows:\r\n\r\n{0}</source>
+        <target state="new">An error occurred while loading the document.  Fix the error, and then try loading the document again.  The error message follows:\r\n\r\n{0}</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignSurfaceNoRootComponent">
+        <source>The designer loader did not provide a root component but has not indicated why.</source>
+        <target state="new">The designer loader did not provide a root component but has not indicated why.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignSurfaceNoSupportedTechnology">
+        <source>The designer loaded, but it does not offer a view compatible with this design surface.</source>
+        <target state="new">The designer loaded, but it does not offer a view compatible with this design surface.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignSurfaceServiceIsFixed">
+        <source>The service {0} cannot be removed from the service container.</source>
+        <target state="new">The service {0} cannot be removed from the service container.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerActionPanel_CouldNotFindMethod">
+        <source>Could not find method '{0}'</source>
+        <target state="new">Could not find method '{0}'</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostCantDestroyInheritedComponent">
+        <source>Cannot remove or destroy inherited component '{0}'.</source>
+        <target state="new">Cannot remove or destroy inherited component '{0}'.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostCyclicAdd">
+        <source>Cannot add an instance of {0} to the designer because it would create a circular dependency.  Make sure that the type does not have the same namespace and type name as the root component {1}.</source>
+        <target state="new">Cannot add an instance of {0} to the designer because it would create a circular dependency.  Make sure that the type does not have the same namespace and type name as the root component {1}.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostDesignerNeedsComponent">
+        <source>Failure initializing the designer.  It has no Component member.</source>
+        <target state="new">Failure initializing the designer.  It has no Component member.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostDestroyComponentTransaction">
+        <source>Deleting '{0}'.</source>
+        <target state="new">Deleting '{0}'.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostDuplicateName">
+        <source>The name {0} is already in use by another component.</source>
+        <target state="new">The name {0} is already in use by another component.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostFailedComponentCreate">
+        <source>Component of type {0} could not be created.  Make sure the type implements IComponent and provides an appropriate public constructor.  Appropriate constructors either take no parameters or take a single IContainer parameter.</source>
+        <target state="new">Component of type {0} could not be created.  Make sure the type implements IComponent and provides an appropriate public constructor.  Appropriate constructors either take no parameters or take a single IContainer parameter.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostGenericTransactionName">
+        <source>No Description Available</source>
+        <target state="new">No Description Available</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostLoaderSpecified">
+        <source>A designer loader has already been configured for this design surface.</source>
+        <target state="new">A designer loader has already been configured for this design surface.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostNestedTransaction">
+        <source>The designer transaction '{0}' cannot be committed or canceled because nested transaction '{1}' is still active.  Commit or cancel the nested transaction first.</source>
+        <target state="new">The designer transaction '{0}' cannot be committed or canceled because nested transaction '{1}' is still active.  Commit or cancel the nested transaction first.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostNoBaseClass">
+        <source>Cannot open a designer for the file because the class within it does not inherit from a class that can be visually designed.</source>
+        <target state="new">Cannot open a designer for the file because the class within it does not inherit from a class that can be visually designed.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostNoTopLevelDesigner">
+        <source>There is no designer for the class {0}.</source>
+        <target state="new">There is no designer for the class {0}.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="DesignerHostUnloading">
+        <source>New components cannot be added while a designer is unloading.</source>
+        <target state="new">New components cannot be added while a designer is unloading.</target>
+        <note />
+      </trans-unit>
       <trans-unit id="DesignerOptions_CodeGenDisplay">
         <source>Optimized Code Generation</source>
         <target state="translated">Generování optimalizovaného kódu</target>
@@ -122,6 +217,11 @@
         <target state="translated">Tento objekt IDataObject nepodporuje funkci SetData.</target>
         <note />
       </trans-unit>
+      <trans-unit id="ExtenderProviderServiceDuplicateProvider">
+        <source>The extender provider {0} has already been added as an extender.  Adding another would result in duplicate properties.</source>
+        <target state="new">The extender provider {0} has already been added as an extender.  Adding another would result in duplicate properties.</target>
+        <note />
+      </trans-unit>
       <trans-unit id="InheritanceServiceReadOnlyCollection">
         <source>Read-Only</source>
         <target state="new">Read-Only</target>
@@ -155,6 +255,51 @@
       <trans-unit id="ToolboxItemValueNotSerializable">
         <source>Data type {0} is not serializable. Items added to a property dictionary must be serializable.</source>
         <target state="new">Data type {0} is not serializable. Items added to a property dictionary must be serializable.</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentAdd0">
+        <source>Add Component</source>
+        <target state="new">Add Component</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentAdd1">
+        <source>Add {0}</source>
+        <target state="new">Add {0}</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentChange0">
+        <source>Change Component</source>
+        <target state="new">Change Component</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentChange1">
+        <source>Change {0}</source>
+        <target state="new">Change {0}</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentChange2">
+        <source>Change {0}.{1}</source>
+        <target state="new">Change {0}.{1}</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentRemove0">
+        <source>Remove Component</source>
+        <target state="new">Remove Component</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentRemove1">
+        <source>Remove {0}</source>
+        <target state="new">Remove {0}</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineComponentRename">
+        <source>Rename {0} to {1}</source>
+        <target state="new">Rename {0} to {1}</target>
+        <note />
+      </trans-unit>
+      <trans-unit id="UndoEngineMissingService">
+        <source>The service {0} is required but could not be found.  If you have removed this service ensure that you provide a replacement.</source>
+        <target state="new">The service {0} is required but could not be found.  If you have removed this service ensure that you provide a replacement.</target>
         <note />
       </trans-unit>
       <trans-unit id="UnsafeNativeMethodsNotImplemented">

commit 4a24fc1a2395406302190ce5261486bb0602e156

Remove duplicate StatusCommandUI
It appears a genuine porting mistake introduced in #606. `StatusCommandUI`
only ever existed under System.Windows.Forms.Design namespace, and the
type was later correctly ported in #632.

src/System.Windows.Forms.Design/src/System/ComponentModel/Design/SelectionService.cs
@@ -7,6 +7,7 @@ using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Windows.Forms.Design;
 
 namespace System.ComponentModel.Design
 {

src/System.Windows.Forms.Design/src/System/ComponentModel/Design/StatusCommandUI.cs
@@ -1,137 +1,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using System.Drawing;
-using System.Windows.Forms;
-using System.Windows.Forms.Design;
-
-namespace System.ComponentModel.Design
-{
-    /// <summary>
-    ///  This class provides a single entrypoint used by the Behaviors, KeySize and KeyMoves (in CommandSets) and SelectionService to update the StatusBar Information.
-    /// </summary>
-    internal class StatusCommandUI
-    {
-        MenuCommand _statusRectCommand;
-        IMenuCommandService _menuService;
-        readonly IServiceProvider _serviceProvider;
-
-        public StatusCommandUI(IServiceProvider provider)
-        {
-            _serviceProvider = provider;
-        }
-
-        /// <summary>
-        ///  Retrieves the menu editor service, which we cache for speed.
-        /// </summary>
-        private IMenuCommandService MenuService
-        {
-            get
-            {
-                if (_menuService is null)
-                {
-                    _menuService = (IMenuCommandService)_serviceProvider.GetService(typeof(IMenuCommandService));
-                }
-
-                return _menuService;
-            }
-        }
-
-        /// <summary>
-        ///  Retrieves the actual StatusRectCommand, which we cache for speed.
-        /// </summary>
-        private MenuCommand StatusRectCommand
-        {
-            get
-            {
-                if (_statusRectCommand is null)
-                {
-                    if (MenuService is not null)
-                    {
-                        _statusRectCommand = MenuService.FindCommand(MenuCommands.SetStatusRectangle);
-                    }
-                }
-
-                return _statusRectCommand;
-            }
-        }
-
-        /// <summary>
-        ///  Actual Function which invokes the command.
-        /// </summary>
-        public void SetStatusInformation(Component selectedComponent, Point location)
-        {
-            if (selectedComponent is null)
-            {
-                return;
-            }
-
-            Rectangle bounds = Rectangle.Empty;
-            if (selectedComponent is Control c)
-            {
-                bounds = c.Bounds;
-            }
-            else
-            {
-                PropertyDescriptor BoundsProp = TypeDescriptor.GetProperties(selectedComponent)["Bounds"];
-                if (BoundsProp is not null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
-                {
-                    bounds = (Rectangle)BoundsProp.GetValue(selectedComponent);
-                }
-            }
-
-            if (location != Point.Empty)
-            {
-                bounds.X = location.X;
-                bounds.Y = location.Y;
-            }
-
-            if (StatusRectCommand is not null)
-            {
-                StatusRectCommand.Invoke(bounds);
-            }
-        }
-
-        /// <summary>
-        ///  Actual Function which invokes the command.
-        /// </summary>
-        public void SetStatusInformation(Component selectedComponent)
-        {
-            if (selectedComponent is null)
-            {
-                return;
-            }
-
-            Rectangle bounds = Rectangle.Empty;
-            if (selectedComponent is Control c)
-            {
-                bounds = c.Bounds;
-            }
-            else
-            {
-                PropertyDescriptor BoundsProp = TypeDescriptor.GetProperties(selectedComponent)["Bounds"];
-                if (BoundsProp is not null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
-                {
-                    bounds = (Rectangle)BoundsProp.GetValue(selectedComponent);
-                }
-            }
-
-            if (StatusRectCommand is not null)
-            {
-                StatusRectCommand.Invoke(bounds);
-            }
-        }
-
-        /// <summary>
-        ///  Actual Function which invokes the command.
-        /// </summary>
-        public void SetStatusInformation(Rectangle bounds)
-        {
-            if (StatusRectCommand is not null)
-            {
-                StatusRectCommand.Invoke(bounds);
-            }
-        }
-    }
-}

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/StatusCommandUI.cs
@@ -9,7 +9,8 @@ using System.Drawing;
 namespace System.Windows.Forms.Design
 {
     /// <summary>
-    ///  This class provides a single entrypoint used by the Behaviors, KeySize and KeyMoves (in CommandSets) and SelectionService to update the StatusBar Information.
+    ///  This class provides a single entrypoint used by the Behaviors, KeySize and KeyMoves (in CommandSets) and
+    ///  SelectionService to update the StatusBar Information.
     /// </summary>
     internal class StatusCommandUI
     {
@@ -47,7 +48,7 @@ namespace System.Windows.Forms.Design
             {
                 if (_statusRectCommand is null)
                 {
-                    if (MenuService != null)
+                    if (MenuService is not null)
                     {
                         _statusRectCommand = MenuService.FindCommand(MenuCommands.SetStatusRectangle);
                     }
@@ -75,7 +76,7 @@ namespace System.Windows.Forms.Design
             else
             {
                 PropertyDescriptor BoundsProp = TypeDescriptor.GetProperties(selectedComponent)["Bounds"];
-                if (BoundsProp != null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
+                if (BoundsProp is not null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
                 {
                     bounds = (Rectangle)BoundsProp.GetValue(selectedComponent);
                 }
@@ -87,7 +88,7 @@ namespace System.Windows.Forms.Design
                 bounds.Y = location.Y;
             }
 
-            if (StatusRectCommand != null)
+            if (StatusRectCommand is not null)
             {
                 StatusRectCommand.Invoke(bounds);
             }
@@ -111,13 +112,13 @@ namespace System.Windows.Forms.Design
             else
             {
                 PropertyDescriptor BoundsProp = TypeDescriptor.GetProperties(selectedComponent)["Bounds"];
-                if (BoundsProp != null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
+                if (BoundsProp is not null && typeof(Rectangle).IsAssignableFrom(BoundsProp.PropertyType))
                 {
                     bounds = (Rectangle)BoundsProp.GetValue(selectedComponent);
                 }
             }
 
-            if (StatusRectCommand != null)
+            if (StatusRectCommand is not null)
             {
                 StatusRectCommand.Invoke(bounds);
             }
@@ -128,7 +129,7 @@ namespace System.Windows.Forms.Design
         /// </summary>
         public void SetStatusInformation(Rectangle bounds)
         {
-            if (StatusRectCommand != null)
+            if (StatusRectCommand is not null)
             {
                 StatusRectCommand.Invoke(bounds);
             }