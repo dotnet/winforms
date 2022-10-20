5d2fae877a7d731ef091a8d626315f83ad0485db
Jeremy Kuhne <jkuhne@microsoft.com>
2022-10-20 16:41:31 -0700

    Fix the original save dialog
    I copied from the Win32 metadata directly and it had `PreserveSig = false`, which is only valid with HRESULT returns.
    
    Fixes #7975

--- src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.GetOpenFileName.cs
+++ src/System.Windows.Forms.Primitives/src/Windows/Win32/PInvoke.GetOpenFileName.cs
@@ -10,10 +10,10 @@ namespace Windows.Win32
     // https://github.com/microsoft/win32metadata/issues/1300
     internal static partial class PInvoke
     {
-        [DllImport("COMDLG32.dll", EntryPoint = "GetOpenFileNameW", ExactSpelling = true, PreserveSig = false)]
-        public static extern unsafe BOOL GetOpenFileName([In][Out] OPENFILENAME* param0);
+        [DllImport("COMDLG32.dll", EntryPoint = "GetOpenFileNameW", ExactSpelling = true)]
+        public static extern unsafe BOOL GetOpenFileName(OPENFILENAME* param0);
 
-        [DllImport("COMDLG32.dll", EntryPoint = "GetSaveFileNameW", ExactSpelling = true, PreserveSig = false)]
-        public static extern unsafe BOOL GetSaveFileName([In][Out] OPENFILENAME* param0);
+        [DllImport("COMDLG32.dll", EntryPoint = "GetSaveFileNameW", ExactSpelling = true)]
+        public static extern unsafe BOOL GetSaveFileName(OPENFILENAME* param0);
     }
 }
