87c7d802a9e88e085d434fc7b92f6ecee29b2553
elachlan <2433737+elachlan@users.noreply.github.com>
2022-10-25 08:05:21 +1000

    Add Test for FolderBrowserDialog

---
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/FolderBrowserDialogTests.cs
@@ -0,0 +1,40 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using WindowsInput;
+using WindowsInput.Native;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace System.Windows.Forms.UITests
+{
+    public class FolderBrowserDialogTests : ControlTestBase
+    {
+        public FolderBrowserDialogTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        [WinFormsFact]
+        public void FolderBrowserDialog_ShowDialog()
+        {
+            FolderBrowserDialog form = new()
+            {
+                AutoUpgradeEnabled = false,
+            };
+
+            Timer timer = new();
+            timer.Interval = 3000;
+            timer.Tick += (s, e) =>
+            {
+                ((Timer)s).Stop();
+                new InputSimulator().Keyboard.KeyPress(VirtualKeyCode.ESCAPE);
+            };
+
+            timer.Start();
+            form.ShowDialog();
+        }
+
+    }
+}
