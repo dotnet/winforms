54a6efe5fe5bfb42adf8faafa98bd3b281e11391
gpetrou <gipetrou@gmail.com>
2022-10-25 19:34:31 +0300

    Enable nullability in ToolStripCustomIComparer

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripCustomIComparer.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripCustomIComparer.cs
@@ -2,14 +2,27 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     internal class ToolStripCustomIComparer : IComparer<ToolStrip>
     {
-        public int Compare(ToolStrip x, ToolStrip y)
+        public int Compare(ToolStrip? x, ToolStrip? y)
         {
+            if (x is null && y is null)
+            {
+                return 0;
+            }
+
+            if (x is null)
+            {
+                return -1;
+            }
+
+            if (y is null)
+            {
+                return 1;
+            }
+
             if (x.GetType() == y.GetType())
             {
                 return 0; // same type
