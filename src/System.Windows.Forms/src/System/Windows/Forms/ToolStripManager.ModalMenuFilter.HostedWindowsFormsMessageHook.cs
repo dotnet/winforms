92fa6230b77fc6862c784af0164561d7cb73735b
gpetrou <4172445+gpetrou@users.noreply.github.com>
2022-11-02 09:09:38 +0200

    Enable nullability in HostedWindowsFormsMessageHook (#8097)

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripManager.ModalMenuFilter.HostedWindowsFormsMessageHook.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripManager.ModalMenuFilter.HostedWindowsFormsMessageHook.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using static Interop;
@@ -18,7 +16,7 @@ namespace System.Windows.Forms
             {
                 private HHOOK _messageHookHandle;
                 private bool _isHooked;
-                private HOOKPROC _callBack;
+                private HOOKPROC? _callBack;
 
                 public HostedWindowsFormsMessageHook()
                 {
