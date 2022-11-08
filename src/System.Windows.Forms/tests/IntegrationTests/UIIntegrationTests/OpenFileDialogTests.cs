2479775c33fa9196e6717770e852b663aba283a7
Loni Tra <lonitra@microsoft.com>
2022-11-08 15:10:38 -0800

    Prevent OpenFileDialog From Throwing When Opening With Non-Existing Initial Directory (#8115)

---
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/OpenFileDialogTests.cs
@@ -0,0 +1,51 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Xunit;
+using Xunit.Abstractions;
+using static Interop;
+
+namespace System.Windows.Forms.UITests
+{
+    public class OpenFileDialogTests : ControlTestBase
+    {
+        public OpenFileDialogTests(ITestOutputHelper testOutputHelper)
+            : base(testOutputHelper)
+        {
+        }
+
+        // Regression test for https://github.com/dotnet/winforms/issues/8108
+        [WinFormsFact]
+        public void OpenFileDialogTests_OpenWithNonExistingInitDirectory_Success()
+        {
+            using TestDialogForm dialogOwnerForm = new();
+            using OpenFileDialog dialog = new();
+            dialog.InitialDirectory = Guid.NewGuid().ToString();
+            Assert.Equal(DialogResult.Cancel, dialog.ShowDialog(dialogOwnerForm));
+        }
+
+        [WinFormsFact]
+        public void OpenFileDialogTests_OpenWithExistingInitDirectory_Success()
+        {
+            using TestDialogForm dialogOwnerForm = new();
+            using OpenFileDialog dialog = new();
+            dialog.InitialDirectory = Path.GetTempPath();
+            Assert.Equal(DialogResult.Cancel, dialog.ShowDialog(dialogOwnerForm));
+        }
+
+        private class TestDialogForm : Form
+        {
+            protected override void WndProc(ref Message m)
+            {
+                if (m.MsgInternal == User32.WM.ENTERIDLE && m.WParamInternal == (uint)MSGF.DIALOGBOX)
+                {
+                    HWND dialogHandle = (HWND)m.LParamInternal;
+                    PInvoke.PostMessage(dialogHandle, User32.WM.CLOSE);
+                }
+
+                base.WndProc(ref m);
+            }
+        }
+    }
+}
