1d1ed62e19a6329aa91692daae2237e0c7903656
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2022-11-09 10:33:38 +1000

    refactor AutoCompleteStringCollection to use List<string> instead of ArrayList

--- src/System.Windows.Forms/src/System/Windows/Forms/AutoCompleteStringCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AutoCompleteStringCollection.cs
@@ -15,7 +15,7 @@ namespace System.Windows.Forms
     public class AutoCompleteStringCollection : IList
     {
         CollectionChangeEventHandler onCollectionChanged;
-        private readonly ArrayList data = new ArrayList();
+        private readonly List<string> data = new List<string>();
 
         public AutoCompleteStringCollection()
         {
@@ -28,7 +28,7 @@ namespace System.Windows.Forms
         {
             get
             {
-                return ((string)data[index]);
+                return data[index];
             }
             set
             {
@@ -42,29 +42,11 @@ namespace System.Windows.Forms
         ///  Gets the number of strings in the
         ///  <see cref="AutoCompleteStringCollection"/> .
         /// </summary>
-        public int Count
-        {
-            get
-            {
-                return data.Count;
-            }
-        }
+        public int Count => data.Count;
 
-        bool IList.IsReadOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
+        bool IList.IsReadOnly => false;
 
-        bool IList.IsFixedSize
-        {
-            get
-            {
-                return false;
-            }
-        }
+        bool IList.IsFixedSize => false;
 
         public event CollectionChangeEventHandler CollectionChanged
         {
@@ -72,10 +54,7 @@ namespace System.Windows.Forms
             remove => onCollectionChanged -= value;
         }
 
-        protected void OnCollectionChanged(CollectionChangeEventArgs e)
-        {
-            onCollectionChanged?.Invoke(this, e);
-        }
+        protected void OnCollectionChanged(CollectionChangeEventArgs e) => onCollectionChanged?.Invoke(this, e);
 
         /// <summary>
         ///  Adds a string with the specified value to the
@@ -83,7 +62,8 @@ namespace System.Windows.Forms
         /// </summary>
         public int Add(string value)
         {
-            int index = data.Add(value);
+            data.Add(value);
+            int index = data.LastIndexOf(value);
             OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Add, value));
             return index;
         }
@@ -114,28 +94,19 @@ namespace System.Windows.Forms
         ///  <see cref="AutoCompleteStringCollection"/> contains a string with the specified
         ///  value.
         /// </summary>
-        public bool Contains(string value)
-        {
-            return data.Contains(value);
-        }
+        public bool Contains(string value) => data.Contains(value);
 
         /// <summary>
         ///  Copies the <see cref="AutoCompleteStringCollection"/> values to a one-dimensional <see cref="Array"/> instance at the
         ///  specified index.
         /// </summary>
-        public void CopyTo(string[] array, int index)
-        {
-            data.CopyTo(array, index);
-        }
+        public void CopyTo(string[] array, int index) => data.CopyTo(array, index);
 
         /// <summary>
         ///  Returns the index of the first occurrence of a string in
         ///  the <see cref="AutoCompleteStringCollection"/> .
         /// </summary>
-        public int IndexOf(string value)
-        {
-            return data.IndexOf(value);
-        }
+        public int IndexOf(string value) => data.IndexOf(value);
 
         /// <summary>
         ///  Inserts a string into the <see cref="AutoCompleteStringCollection"/> at the specified
@@ -150,25 +121,13 @@ namespace System.Windows.Forms
         /// <summary>
         ///  Gets a value indicating whether the <see cref="AutoCompleteStringCollection"/> is read-only.
         /// </summary>
-        public bool IsReadOnly
-        {
-            get
-            {
-                return false;
-            }
-        }
+        public bool IsReadOnly => false;
 
         /// <summary>
         ///  Gets a value indicating whether access to the <see cref="AutoCompleteStringCollection"/>
         ///  is synchronized (thread-safe).
         /// </summary>
-        public bool IsSynchronized
-        {
-            get
-            {
-                return false;
-            }
-        }
+        public bool IsSynchronized => false;
 
         /// <summary>
         ///  Removes a specific string from the <see cref="AutoCompleteStringCollection"/> .
@@ -184,7 +143,7 @@ namespace System.Windows.Forms
         /// </summary>
         public void RemoveAt(int index)
         {
-            string value = (string)data[index];
+            string value = data[index];
             data.RemoveAt(index);
             OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Remove, value));
         }
@@ -196,49 +155,22 @@ namespace System.Windows.Forms
 
         object IList.this[int index]
         {
-            get
-            {
-                return this[index];
-            }
-            set
-            {
-                this[index] = (string)value;
-            }
+            get => this[index];
+            set => this[index] = (string)value;
         }
 
-        int IList.Add(object value)
-        {
-            return Add((string)value);
-        }
+        int IList.Add(object value) => Add((string)value);
 
-        bool IList.Contains(object value)
-        {
-            return Contains((string)value);
-        }
+        bool IList.Contains(object value) => Contains((string)value);
 
-        int IList.IndexOf(object value)
-        {
-            return IndexOf((string)value);
-        }
+        int IList.IndexOf(object value) => IndexOf((string)value);
 
-        void IList.Insert(int index, object value)
-        {
-            Insert(index, (string)value);
-        }
+        void IList.Insert(int index, object value) => Insert(index, (string)value);
 
-        void IList.Remove(object value)
-        {
-            Remove((string)value);
-        }
+        void IList.Remove(object value) => Remove((string)value);
 
-        void ICollection.CopyTo(Array array, int index)
-        {
-            data.CopyTo(array, index);
-        }
+        void ICollection.CopyTo(Array array, int index) => data.CopyTo((string[])array, index);
 
-        public IEnumerator GetEnumerator()
-        {
-            return data.GetEnumerator();
-        }
+        public IEnumerator GetEnumerator() => data.GetEnumerator();
     }
 }
