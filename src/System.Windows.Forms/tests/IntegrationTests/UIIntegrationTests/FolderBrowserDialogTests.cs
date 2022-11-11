d76165bdf4d01b189bee4a905597efe812c94a49
Loni Tra <lonitra@microsoft.com>
2022-11-11 09:21:20 -0800

    Update FolderBrowserDialogTest (#8150)

--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/FolderBrowserDialogTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/FolderBrowserDialogTests.cs
@@ -4,75 +4,28 @@
 
 using Xunit;
 using Xunit.Abstractions;
-using static Interop;
 
-namespace System.Windows.Forms.UITests
+namespace System.Windows.Forms.UITests;
+
+public class FolderBrowserDialogTests : ControlTestBase
 {
-    public class FolderBrowserDialogTests : ControlTestBase
+    public FolderBrowserDialogTests(ITestOutputHelper testOutputHelper)
+        : base(testOutputHelper)
     {
-        public FolderBrowserDialogTests(ITestOutputHelper testOutputHelper)
-            : base(testOutputHelper)
+    }
+
+    // Regression test for https://github.com/dotnet/winforms/issues/7981
+    [WinFormsTheory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void FolderBrowserDialog_ShowDialog(bool autoUpgradeEnabled)
+    {
+        using DialogHostForm dialogOwnerForm = new();
+        using FolderBrowserDialog dialog = new()
         {
-        }
+            AutoUpgradeEnabled = autoUpgradeEnabled,
+        };
 
-        [WinFormsTheory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public void FolderBrowserDialog_ShowDialog(bool autoUpgradeEnabled)
-        {
-            using FolderBrowserDialog dialog = new()
-            {
-                AutoUpgradeEnabled = autoUpgradeEnabled,
-            };
-
-            using Timer timer = new();
-            timer.Interval = 1_000;
-            int counter = 0;
-
-            bool dialogClosed = false;
-            timer.Tick += (s, e) =>
-            {
-                counter++;
-                timer.Stop();
-
-                User32.EnumWindows(FindAndCloseDialog);
-
-                if (!dialogClosed)
-                {
-                    if (counter > 2)
-                    {
-                        Application.Exit();
-                        return;
-                    }
-
-                    TestOutputHelper.WriteLine($"Couldn't find the dialog ({counter}/3). Retrying...");
-                    timer.Start();
-                }
-            };
-
-            timer.Start();
-            dialog.ShowDialog();
-
-            // The dialog has opened and closed successfully
-            Assert.True(dialogClosed, "Failed to close the dialog");
-            return;
-
-            unsafe BOOL FindAndCloseDialog(HWND hwnd)
-            {
-                uint processId;
-                PInvoke.GetWindowThreadProcessId(hwnd, &processId);
-                if (processId == PInvoke.GetCurrentProcessId() && PInvoke.IsWindowVisible(hwnd))
-                {
-                    TestOutputHelper.WriteLine($"Process ID: {processId}");
-                    TestOutputHelper.WriteLine($"Dialog window found: 0x{hwnd.Value:X8}");
-
-                    PInvoke.SendMessage(hwnd, User32.WM.CLOSE);
-
-                    dialogClosed = true;
-                }
-
-                return true;
-            }
-        }
+        Assert.Equal(DialogResult.Cancel, dialog.ShowDialog(dialogOwnerForm));
     }
 }
--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/OpenFileDialogTests.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/OpenFileDialogTests.cs
@@ -4,7 +4,6 @@
 
 using Xunit;
 using Xunit.Abstractions;
-using static Interop;
 
 namespace System.Windows.Forms.UITests
 {
@@ -19,7 +18,7 @@ namespace System.Windows.Forms.UITests
         [WinFormsFact]
         public void OpenFileDialogTests_OpenWithNonExistingInitDirectory_Success()
         {
-            using TestDialogForm dialogOwnerForm = new();
+            using DialogHostForm dialogOwnerForm = new();
             using OpenFileDialog dialog = new();
             dialog.InitialDirectory = Guid.NewGuid().ToString();
             Assert.Equal(DialogResult.Cancel, dialog.ShowDialog(dialogOwnerForm));
@@ -28,24 +27,10 @@ namespace System.Windows.Forms.UITests
         [WinFormsFact]
         public void OpenFileDialogTests_OpenWithExistingInitDirectory_Success()
         {
-            using TestDialogForm dialogOwnerForm = new();
+            using DialogHostForm dialogOwnerForm = new();
             using OpenFileDialog dialog = new();
             dialog.InitialDirectory = Path.GetTempPath();
             Assert.Equal(DialogResult.Cancel, dialog.ShowDialog(dialogOwnerForm));
-        }
-
-        private class TestDialogForm : Form
-        {
-            protected override void WndProc(ref Message m)
-            {
-                if (m.MsgInternal == User32.WM.ENTERIDLE && m.WParamInternal == (uint)MSGF.DIALOGBOX)
-                {
-                    HWND dialogHandle = (HWND)m.LParamInternal;
-                    PInvoke.PostMessage(dialogHandle, User32.WM.CLOSE);
-                }
-
-                base.WndProc(ref m);
-            }
         }
     }
 }
