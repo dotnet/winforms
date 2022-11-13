54dac693b31238aa396dad60143f795116a643e7
gpetrou <gipetrou@gmail.com>
2022-11-13 08:50:01 +0200

    Enable nullability in MergeHistoryItem

--- src/System.Windows.Forms/src/System/Windows/Forms/MergeHistoryItem.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/MergeHistoryItem.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 namespace System.Windows.Forms
 {
     internal class MergeHistoryItem
@@ -15,15 +13,15 @@ namespace System.Windows.Forms
 
         public MergeAction MergeAction { get; }
 
-        public ToolStripItem TargetItem { get; set; }
+        public ToolStripItem? TargetItem { get; set; }
 
         public int Index { get; set; } = -1;
 
         public int PreviousIndex { get; set; } = -1;
 
-        public ToolStripItemCollection PreviousIndexCollection { get; set; }
+        public ToolStripItemCollection? PreviousIndexCollection { get; set; }
 
-        public ToolStripItemCollection IndexCollection { get; set; }
+        public ToolStripItemCollection? IndexCollection { get; set; }
 
         public override string ToString()
             => $"MergeAction: {MergeAction} | TargetItem: {(TargetItem is null ? "null" : TargetItem.Text)} Index: {Index}";
