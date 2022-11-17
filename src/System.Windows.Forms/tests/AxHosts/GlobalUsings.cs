6e8ae0a49be0f2f59979749c726a1cd556e8ea02
Loni Tra <lonitra@microsoft.com>
2022-11-16 17:25:49 -0800

    Add license

--- src/System.Windows.Forms/tests/AxHosts/GlobalUsings.cs
+++ src/System.Windows.Forms/tests/AxHosts/GlobalUsings.cs
@@ -1,2 +1,6 @@
-﻿global using System;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+global using System;
 global using System.Windows.Forms;
