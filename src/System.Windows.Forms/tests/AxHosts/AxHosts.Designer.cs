4e2a4aa6a4555dc5840bb816f0f1e1eabe878663
Loni Tra <lonitra@microsoft.com>
2022-11-17 16:49:46 -0800

    Add msbuildproj to build using Framework version of msbuild

--- src/System.Windows.Forms/tests/AxHosts/AxHosts.Designer.cs
+++ src/System.Windows.Forms/tests/AxHosts/AxHosts.Designer.cs
@@ -32,12 +32,36 @@ namespace AxHosts
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AxHosts));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(800, 450);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // AxHosts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "AxHosts";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
--- src/System.Windows.Forms/tests/AxHosts/AxHosts.cs
+++ src/System.Windows.Forms/tests/AxHosts/AxHosts.cs
@@ -2,28 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.ComponentModel;
 using System.IO;
-using AxWMPLib;
 
 namespace AxHosts
 {
     public partial class AxHosts : Form
     {
-        private AxWindowsMediaPlayer _mediaPlayer;
-
         public AxHosts()
         {
             InitializeComponent();
-            _mediaPlayer = new()
-            {
-                Dock = DockStyle.Fill,
-            };
-
-            ((ISupportInitialize)_mediaPlayer).BeginInit();
-            Controls.Add(_mediaPlayer);
-            ((ISupportInitialize)_mediaPlayer).EndInit();
-            _mediaPlayer.URL = Path.GetFullPath(@".\resources\media.mpg");
+            axWindowsMediaPlayer1.URL = Path.GetFullPath(@"./Resources/media.mpg");
         }
     }
 }
