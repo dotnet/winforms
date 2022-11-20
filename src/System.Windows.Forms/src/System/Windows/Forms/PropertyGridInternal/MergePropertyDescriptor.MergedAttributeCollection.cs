2fcaa0f61c9c39260d866e784681bf322a97e68c
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2022-11-21 09:39:47 +1000

    Fix blank line

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/MergePropertyDescriptor.MergedAttributeCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyGridInternal/MergePropertyDescriptor.MergedAttributeCollection.cs
@@ -68,6 +68,7 @@ namespace System.Windows.Forms.PropertyGridInternal
                 {
                     _foundAttributes[attributeType] = value;
                 }
+
                 return value;
             }
         }
