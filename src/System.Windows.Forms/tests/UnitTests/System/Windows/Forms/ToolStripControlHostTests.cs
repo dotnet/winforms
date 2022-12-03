e7dfb2cdd691e04c77e4e5ebac3e199c3c1384b3
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2022-12-03 10:10:35 +1000

    Refactor tests

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/ToolStripControlHostTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/ToolStripControlHostTests.cs
@@ -1812,6 +1812,69 @@ namespace System.Windows.Forms.Tests
             Assert.False(c.IsHandleCreated);
         }
 
+        [WinFormsFact]
+        public void ToolStripControlHost_Site_GetControlSiteContainerAfterSettingNull_IsNull()
+        {
+            using var c = new Control();
+            using var item = new ToolStripControlHost(c);
+
+            using var container = new Container();
+            var mockSite = new Mock<ISite>(MockBehavior.Strict);
+            mockSite
+                .Setup(s => s.Container)
+                .Returns(container);
+            mockSite
+                .Setup(s => s.GetService(typeof(AmbientProperties)))
+                .Returns(null);
+            item.Site = mockSite.Object;
+            ISite oldSite = c.Site;
+
+            item.Site = null;
+            Assert.Null(oldSite.Container);
+        }
+
+        [WinFormsFact]
+        public void ToolStripControlHost_Site_GetControlSiteDesignModeAfterSettingNull_IsFalse()
+        {
+            using var c = new Control();
+            using var item = new ToolStripControlHost(c);
+
+            using var container = new Container();
+            var mockSite = new Mock<ISite>(MockBehavior.Strict);
+            mockSite
+                .Setup(s => s.Container)
+                .Returns(container);
+            mockSite
+                .Setup(s => s.GetService(typeof(AmbientProperties)))
+                .Returns(null);
+            item.Site = mockSite.Object;
+            ISite oldSite = c.Site;
+
+            item.Site = null;
+            Assert.False(oldSite.DesignMode);
+        }
+
+        [WinFormsFact]
+        public void ToolStripControlHost_Site_GetControlSiteNameAfterSettingNull_IsNull()
+        {
+            using var c = new Control();
+            using var item = new ToolStripControlHost(c);
+
+            using var container = new Container();
+            var mockSite = new Mock<ISite>(MockBehavior.Strict);
+            mockSite
+                .Setup(s => s.Container)
+                .Returns(container);
+            mockSite
+                .Setup(s => s.GetService(typeof(AmbientProperties)))
+                .Returns(null);
+            item.Site = mockSite.Object;
+            ISite oldSite = c.Site;
+
+            item.Site = null;
+            Assert.Null(oldSite.Name);
+        }
+
         [WinFormsTheory]
         [CommonMemberData(typeof(CommonTestHelper), nameof(CommonTestHelper.GetStringNormalizedTheoryData))]
         public void ToolStripControlHost_Site_SetControlSiteName_GetReturnsExpected(string value, string expected)
@@ -1847,6 +1910,28 @@ namespace System.Windows.Forms.Tests
             Assert.Equal(expected, c.Site.Name);
             mockSite.VerifySet(s => s.Name = value, Times.Exactly(2));
             Assert.False(c.IsHandleCreated);
+        }
+
+        [WinFormsFact]
+        public void ToolStripControlHost_Site_SetControlSiteNameAfterSettingNull_IsNull()
+        {
+            using var c = new Control();
+            using var item = new ToolStripControlHost(c);
+
+            using var container = new Container();
+            var mockSite = new Mock<ISite>(MockBehavior.Strict);
+            mockSite
+                .Setup(s => s.Container)
+                .Returns(container);
+            mockSite
+                .Setup(s => s.GetService(typeof(AmbientProperties)))
+                .Returns(null);
+            item.Site = mockSite.Object;
+            ISite oldSite = c.Site;
+
+            item.Site = null;
+            oldSite.Name = "name";
+            Assert.Null(oldSite.Name);
         }
 
         public static IEnumerable<object[]> Site_GetService_TestData()
