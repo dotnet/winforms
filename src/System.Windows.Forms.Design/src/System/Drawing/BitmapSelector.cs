00e6b62faf223b3cf2c2417b888de71c1648fbee
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2022-12-08 09:02:46 +1000

    Enable nullable reference types for `System.Windows.Forms.Design` (#8343)
    * Enable nullable reference types for System.Windows.Forms.Design
    
    * Remove #nullable disable which should not have been added
    
    * fix using

--- src/System.Windows.Forms.Design/src/System/Drawing/BitmapSelector.cs
+++ src/System.Windows.Forms.Design/src/System/Drawing/BitmapSelector.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#nullable disable
+
 using System.Configuration;
 using System.Drawing.Configuration;
 using System.Reflection;
