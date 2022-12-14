fc89cfc3a3f948d5fcbb7addb77579a2f4f8afcd
jericho <desautelsj@hotmail.com>
2022-12-14 10:14:19 -0500

    Add license

--- src/System.Windows.Forms/src/misc/CollectionHelper.cs
+++ src/System.Windows.Forms/src/misc/CollectionHelper.cs
@@ -1,4 +1,8 @@
-﻿namespace System.Collections.Generic
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Collections.Generic
 {
     /// <summary>
     ///  Helpers for working with non generic collections.
