f78c1ba0e0df9a75c8197075a12c858cd82e7ea6
Youssef1313 <youssefvictor00@gmail.com>
2023-01-04 13:42:58 +0200

    fix tests

--- src/System.Windows.Forms.Analyzers.CSharp/tests/UnitTests/System/Windows/Forms/Generators/MockData/ApplicationConfigurationGeneratorTests.GenerateInitialize_user_top_level.cs
+++ src/System.Windows.Forms.Analyzers.CSharp/tests/UnitTests/System/Windows/Forms/Generators/MockData/ApplicationConfigurationGeneratorTests.GenerateInitialize_user_top_level.cs
@@ -17,7 +17,7 @@ internal static partial class ApplicationConfiguration
     ///  global::System.Windows.Forms.Application.EnableVisualStyles();
     ///  global::System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(true);
     ///  global::System.Windows.Forms.Application.SetHighDpiMode(HighDpiMode.DpiUnawareGdiScaled);
-    ///  global::System.Windows.Forms.Application.SetDefaultFont(new Font(new FontFamily("Microsoft Sans Serif"), 8.25f, (FontStyle)0, (GraphicsUnit)2));
+    ///  global::System.Windows.Forms.Application.SetDefaultFont(new global::System.Drawing.Font(new global::System.Drawing.FontFamily("Microsoft Sans Serif"), 8.25f, (global::System.Drawing.FontStyle)0, (global::System.Drawing.GraphicsUnit)2));
     /// </code>
     /// </summary>
     public static void Initialize()
