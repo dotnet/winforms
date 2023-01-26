04e0713d09e64d89d26e3971916a0922b1d5a316
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2023-01-26 10:01:40 +1000

    fix usings

--- src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/FormOwnerTestForm.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/WinformsControlsTest/FormOwnerTestForm.cs
@@ -2,10 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
-using Windows.Win32.System.Ole;
 
 namespace WinformsControlsTest
 {
