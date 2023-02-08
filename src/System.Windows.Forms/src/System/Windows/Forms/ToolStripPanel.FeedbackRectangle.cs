979de7d7a428716a21d7a60924ccc2683dc4ae83
gpetrou <gipetrou@gmail.com>
2023-02-08 19:53:38 +0200

    Enable nullability in FeedbackRectangle

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripPanel.FeedbackRectangle.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripPanel.FeedbackRectangle.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Drawing;
 
 namespace System.Windows.Forms
@@ -60,7 +58,7 @@ namespace System.Windows.Forms
                     {
                         Visible = false;
                         _dropDown.Dispose();
-                        _dropDown = null;
+                        _dropDown = null!;
                     }
                 }
             }
