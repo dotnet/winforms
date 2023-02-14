1e444d5d64c3d3f05293d8a342027bfbc0cfd823
gpetrou <4172445+gpetrou@users.noreply.github.com>
2023-02-14 20:21:25 +0200

    Enable nullability in WindowsFormsSynchronizationContext (#8622)
    * Enable nullability in WindowsFormsSynchronizationContext
    
    * Fix private field names
    
    * Use conditional expression

--- src/System.Windows.Forms/src/System/Windows/Forms/WindowsFormsSynchronizationContext.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/WindowsFormsSynchronizationContext.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.ComponentModel;
 using System.Diagnostics;
 
@@ -14,8 +12,8 @@ namespace System.Windows.Forms
     /// </summary>
     public sealed class WindowsFormsSynchronizationContext : SynchronizationContext, IDisposable
     {
-        private Control controlToSendTo;
-        private WeakReference destinationThreadRef;
+        private Control _controlToSendTo;
+        private WeakReference? _destinationThreadRef;
 
         //ThreadStatics won't get initialized per thread: easiest to just invert the value.
         [ThreadStatic]
@@ -25,76 +23,70 @@ namespace System.Windows.Forms
         private static bool inSyncContextInstallation;
 
         [ThreadStatic]
-        private static SynchronizationContext previousSyncContext;
+        private static SynchronizationContext? previousSyncContext;
 
         public WindowsFormsSynchronizationContext()
         {
             DestinationThread = Thread.CurrentThread;   //store the current thread to ensure its still alive during an invoke.
-            controlToSendTo = Application.ThreadContext.FromCurrent().MarshalingControl;
-            Debug.Assert(controlToSendTo.IsHandleCreated, "Marshaling control should have created its handle in its ctor.");
+            _controlToSendTo = Application.ThreadContext.FromCurrent().MarshalingControl;
+            Debug.Assert(_controlToSendTo.IsHandleCreated, "Marshaling control should have created its handle in its ctor.");
         }
 
-        private WindowsFormsSynchronizationContext(Control marshalingControl, Thread destinationThread)
+        private WindowsFormsSynchronizationContext(Control marshalingControl, Thread? destinationThread)
         {
-            controlToSendTo = marshalingControl;
+            _controlToSendTo = marshalingControl;
             DestinationThread = destinationThread;
-            Debug.Assert(controlToSendTo.IsHandleCreated, "Marshaling control should have created its handle in its ctor.");
+            Debug.Assert(_controlToSendTo.IsHandleCreated, "Marshaling control should have created its handle in its ctor.");
         }
 
         // Directly holding onto the Thread can prevent ThreadStatics from finalizing.
-        private Thread DestinationThread
+        private Thread? DestinationThread
         {
-            get
-            {
-                if ((destinationThreadRef is not null) && (destinationThreadRef.IsAlive))
-                {
-                    return destinationThreadRef.Target as Thread;
-                }
-
-                return null;
-            }
+            get => _destinationThreadRef?.IsAlive == true
+                ? _destinationThreadRef.Target as Thread
+                : null;
             set
             {
                 if (value is not null)
                 {
-                    destinationThreadRef = new WeakReference(value);
+                    _destinationThreadRef = new WeakReference(value);
                 }
             }
         }
 
         public void Dispose()
         {
-            if (controlToSendTo is not null)
+            if (_controlToSendTo is not null)
             {
-                if (!controlToSendTo.IsDisposed)
+                if (!_controlToSendTo.IsDisposed)
                 {
-                    controlToSendTo.Dispose();
+                    _controlToSendTo.Dispose();
                 }
 
-                controlToSendTo = null;
+                _controlToSendTo = null!;
             }
         }
 
         // This is never called because we decide whether to Send or Post and we always post
-        public override void Send(SendOrPostCallback d, object state)
+        public override void Send(SendOrPostCallback d, object? state)
         {
-            Thread destinationThread = DestinationThread;
+            Thread? destinationThread = DestinationThread;
             if (destinationThread is null || !destinationThread.IsAlive)
             {
                 throw new InvalidAsynchronousStateException(SR.ThreadNoLongerValid);
             }
 
-            controlToSendTo?.Invoke(d, new object[] { state });
+            _controlToSendTo?.Invoke(d, new object?[] { state });
         }
 
-        public override void Post(SendOrPostCallback d, object state)
+        public override void Post(SendOrPostCallback d, object? state)
         {
-            controlToSendTo?.BeginInvoke(d, new object[] { state });
+            _controlToSendTo?.BeginInvoke(d, new object?[] { state });
         }
 
         public override SynchronizationContext CreateCopy()
         {
-            return new WindowsFormsSynchronizationContext(controlToSendTo, DestinationThread);
+            return new WindowsFormsSynchronizationContext(_controlToSendTo, DestinationThread);
         }
 
         // Determines whether we install the WindowsFormsSynchronizationContext when we create a control, or
