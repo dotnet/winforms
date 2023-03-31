e614a6401a40b401c1c2ba864cf9baa7e1a69074
Sam Harwell <Sam.Harwell@microsoft.com>
2023-03-31 12:40:05 -0500

    Do not reset cursor position during test discovery

--- src/System.Windows.Forms/tests/UnitTests/ModuleInitializer.cs
+++ src/System.Windows.Forms/tests/UnitTests/ModuleInitializer.cs
@@ -16,12 +16,6 @@ namespace System.Windows.Forms.Tests
             // handling of a Windows message.
             AppContext.SetSwitch("System.Windows.Forms.DoNotCatchUnhandledExceptions", isEnabled: true);
 
-            // Some controls have behavior that changes when the mouse is over them. Make sure we start with the cursor
-            // at a known position.
-            //
-            // See https://github.com/dotnet/winforms/pull/7031#issuecomment-1101339968 for an example of this.
-            Cursor.Position = default;
-
             Application.EnableVisualStyles();
         }
     }
