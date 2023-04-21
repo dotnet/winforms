6b38ba561061ece54192f9e7d65e2c5ea5afdacd
Devendar Reddy Adulla <dreddy@microsoft.com>
2023-04-20 18:29:49 -0700

    Fix null reference in PermonV2 mode.
    Null check should be explicit to avoid further access of the Item object.
    
    Fixes #5125

--- src/System.Windows.Forms/src/System/Windows/Forms/ToolStripRenderer.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ToolStripRenderer.cs
@@ -634,7 +634,7 @@ namespace System.Windows.Forms
                 Point[] arrow = null;
 
                 // We need the Elvis operator here, since at design time at this point Item can be null.
-                if (e.Item?.DeviceDpi != previousDeviceDpi && DpiHelper.IsPerMonitorV2Awareness)
+                if (e.Item is not null && e.Item.DeviceDpi != previousDeviceDpi && DpiHelper.IsPerMonitorV2Awareness)
                 {
                     previousDeviceDpi = e.Item.DeviceDpi;
                     ScaleArrowOffsetsIfNeeded(e.Item.DeviceDpi);
