a11a957972f5c1b05a4bccf82e15fa5365d8ebcd
gpetrou <4172445+gpetrou@users.noreply.github.com>
2023-05-05 01:11:53 +0300

    Enable nullability in AutoCompleteStringCollection (#7047)

--- src/System.Windows.Forms/src/System/Windows/Forms/AutoCompleteStringCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AutoCompleteStringCollection.cs
@@ -2,8 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#nullable disable
-
 using System.Collections;
 using System.ComponentModel;
 
@@ -14,8 +12,8 @@ namespace System.Windows.Forms;
 /// </summary>
 public class AutoCompleteStringCollection : IList
 {
-    private CollectionChangeEventHandler onCollectionChanged;
-    private readonly List<string> data = new();
+    private CollectionChangeEventHandler? _onCollectionChanged;
+    private readonly List<string> _data = new();
 
     public AutoCompleteStringCollection()
     {
@@ -28,12 +26,12 @@ public class AutoCompleteStringCollection : IList
     {
         get
         {
-            return data[index];
+            return _data[index];
         }
         set
         {
-            OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Remove, data[index]));
-            data[index] = value;
+            OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Remove, _data[index]));
+            _data[index] = value;
             OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Add, value));
         }
     }
@@ -42,19 +40,19 @@ public class AutoCompleteStringCollection : IList
     ///  Gets the number of strings in the
     ///  <see cref="AutoCompleteStringCollection"/> .
     /// </summary>
-    public int Count => data.Count;
+    public int Count => _data.Count;
 
-    bool IList.IsReadOnly => ((IList)data).IsReadOnly;
+    bool IList.IsReadOnly => ((IList)_data).IsReadOnly;
 
-    bool IList.IsFixedSize => ((IList)data).IsFixedSize;
+    bool IList.IsFixedSize => ((IList)_data).IsFixedSize;
 
-    public event CollectionChangeEventHandler CollectionChanged
+    public event CollectionChangeEventHandler? CollectionChanged
     {
-        add => onCollectionChanged += value;
-        remove => onCollectionChanged -= value;
+        add => _onCollectionChanged += value;
+        remove => _onCollectionChanged -= value;
     }
 
-    protected void OnCollectionChanged(CollectionChangeEventArgs e) => onCollectionChanged?.Invoke(this, e);
+    protected void OnCollectionChanged(CollectionChangeEventArgs e) => _onCollectionChanged?.Invoke(this, e);
 
     /// <summary>
     ///  Adds a string with the specified value to the
@@ -62,7 +60,7 @@ public class AutoCompleteStringCollection : IList
     /// </summary>
     public int Add(string value)
     {
-        int index = ((IList)data).Add(value);
+        int index = ((IList)_data).Add(value);
         OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Add, value));
         return index;
     }
@@ -74,7 +72,7 @@ public class AutoCompleteStringCollection : IList
     {
         ArgumentNullException.ThrowIfNull(value);
 
-        data.AddRange(value);
+        _data.AddRange(value);
         OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Refresh, null));
     }
 
@@ -84,7 +82,7 @@ public class AutoCompleteStringCollection : IList
     /// </summary>
     public void Clear()
     {
-        data.Clear();
+        _data.Clear();
         OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Refresh, null));
     }
 
@@ -93,19 +91,19 @@ public class AutoCompleteStringCollection : IList
     ///  <see cref="AutoCompleteStringCollection"/> contains a string with the specified
     ///  value.
     /// </summary>
-    public bool Contains(string value) => data.Contains(value);
+    public bool Contains(string value) => _data.Contains(value);
 
     /// <summary>
     ///  Copies the <see cref="AutoCompleteStringCollection"/> values to a one-dimensional <see cref="Array"/> instance at the
     ///  specified index.
     /// </summary>
-    public void CopyTo(string[] array, int index) => data.CopyTo(array, index);
+    public void CopyTo(string[] array, int index) => _data.CopyTo(array, index);
 
     /// <summary>
     ///  Returns the index of the first occurrence of a string in
     ///  the <see cref="AutoCompleteStringCollection"/> .
     /// </summary>
-    public int IndexOf(string value) => data.IndexOf(value);
+    public int IndexOf(string value) => _data.IndexOf(value);
 
     /// <summary>
     ///  Inserts a string into the <see cref="AutoCompleteStringCollection"/> at the specified
@@ -113,27 +111,27 @@ public class AutoCompleteStringCollection : IList
     /// </summary>
     public void Insert(int index, string value)
     {
-        data.Insert(index, value);
+        _data.Insert(index, value);
         OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Add, value));
     }
 
     /// <summary>
     ///  Gets a value indicating whether the <see cref="AutoCompleteStringCollection"/> is read-only.
     /// </summary>
-    public bool IsReadOnly => ((IList)data).IsReadOnly;
+    public bool IsReadOnly => ((IList)_data).IsReadOnly;
 
     /// <summary>
     ///  Gets a value indicating whether access to the <see cref="AutoCompleteStringCollection"/>
     ///  is synchronized (thread-safe).
     /// </summary>
-    public bool IsSynchronized => ((IList)data).IsSynchronized;
+    public bool IsSynchronized => ((IList)_data).IsSynchronized;
 
     /// <summary>
     ///  Removes a specific string from the <see cref="AutoCompleteStringCollection"/> .
     /// </summary>
     public void Remove(string value)
     {
-        data.Remove(value);
+        _data.Remove(value);
         OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Remove, value));
     }
 
@@ -142,33 +140,33 @@ public class AutoCompleteStringCollection : IList
     /// </summary>
     public void RemoveAt(int index)
     {
-        string value = data[index];
-        data.RemoveAt(index);
+        string value = _data[index];
+        _data.RemoveAt(index);
         OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Remove, value));
     }
 
     /// <summary>
     ///  Gets an object that can be used to synchronize access to the <see cref="AutoCompleteStringCollection"/>.
     /// </summary>
-    public object SyncRoot => ((IList)data).SyncRoot;
+    public object SyncRoot => ((IList)_data).SyncRoot;
 
-    object IList.this[int index]
+    object? IList.this[int index]
     {
         get => this[index];
-        set => this[index] = (string)value;
+        set => this[index] = (string)value!;
     }
 
-    int IList.Add(object value) => Add((string)value);
+    int IList.Add(object? value) => Add((string)value!);
 
-    bool IList.Contains(object value) => Contains((string)value);
+    bool IList.Contains(object? value) => Contains((string)value!);
 
-    int IList.IndexOf(object value) => IndexOf((string)value);
+    int IList.IndexOf(object? value) => IndexOf((string)value!);
 
-    void IList.Insert(int index, object value) => Insert(index, (string)value);
+    void IList.Insert(int index, object? value) => Insert(index, (string)value!);
 
-    void IList.Remove(object value) => Remove((string)value);
+    void IList.Remove(object? value) => Remove((string)value!);
 
-    void ICollection.CopyTo(Array array, int index) => ((ICollection)data).CopyTo(array, index);
+    void ICollection.CopyTo(Array array, int index) => ((ICollection)_data).CopyTo(array, index);
 
-    public IEnumerator GetEnumerator() => data.GetEnumerator();
+    public IEnumerator GetEnumerator() => _data.GetEnumerator();
 }
