df2b2d7fb333c3f5dd2953b2061723c3a3b3a4d6
Stephen Flynn <ssflynn@gmail.com>
2023-05-08 13:02:30 -0400

    8295 - ResXResourceWriterTests (#9070)
    * 8295 - ResXResourceWriterTests
    
    Issue# 8295 - Added tests to ResXResourceWriter
    
    * #8295 - Addresses PR concerns for new unit test for ResXResourceWriter
    
    * #8295 - PR suggestions for ResXResourceWriter unit tests
    
    Implements suggestions from PR review related to unit test for the ResXResourceWriter class
    
    ---------
    
    Co-authored-by: Stephen Flynn <stephen@bevdevs.com>

--- src/System.Windows.Forms/tests/UnitTests/System/Resources/ResXResourceWriterTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Resources/ResXResourceWriterTests.cs
@@ -9,6 +9,13 @@ namespace System.Resources.Tests;
 // NB: doesn't require thread affinity
 public class ResXResourceWriterTests
 {
+    private readonly byte[] _testBytes = new byte[] { 1, 2, 3 };
+    private readonly string _testString1 = "TestString1";
+    private readonly string _testString2 = "TestString2";
+    private readonly string _resxFileName = "test.resx";
+    private readonly DateTime _testDateTime = new(2023, 5, 4);
+    private readonly Func<Type, string> _typeNameConverter = type => type?.AssemblyQualifiedName ?? string.Empty;
+
     [Fact]
     public void TestRoundTrip()
     {
@@ -37,5 +44,231 @@ public class ResXResourceWriterTests
                 Assert.Equal(value, pair.Value);
             }
         }
+    }
+
+    [Fact]
+    public void ResXResourceWriter_Constructor_FileName_CreatesInstance()
+    {
+        // Arrange
+        string fileName = _resxFileName;
+
+        // Act
+        using ResXResourceWriter writer = new(fileName);
+
+        // Assert
+        Assert.NotNull(writer);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_Constructor_FileNameAndTypeNameConverter_CreatesInstance()
+    {
+        // Arrange
+        string fileName = _resxFileName;
+
+        // Act
+        using ResXResourceWriter writer = new(fileName, _typeNameConverter);
+
+        // Assert
+        Assert.NotNull(writer);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_Constructor_Stream_CreatesInstance()
+    {
+        // Arrange
+        using MemoryStream stream = new();
+
+        // Act
+        using ResXResourceWriter writer = new(stream);
+
+        // Assert
+        Assert.NotNull(writer);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_Constructor_StreamAndTypeNameConverter_CreatesInstance()
+    {
+        // Arrange
+        using MemoryStream stream = new();
+
+        // Act
+        using ResXResourceWriter writer = new(stream, _typeNameConverter);
+
+        // Assert
+        Assert.NotNull(writer);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_Constructor_TextWriter_CreatesInstance()
+    {
+        // Arrange
+        using StringWriter stringWriter = new();
+
+        // Act
+        using ResXResourceWriter writer = new(stringWriter);
+
+        // Assert
+        Assert.NotNull(writer);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_Constructor_TextWriterAndTypeNameConverter_CreatesInstance()
+    {
+        // Arrange
+        using StringWriter stringWriter = new();
+
+        // Act
+        using ResXResourceWriter writer = new(stringWriter, _typeNameConverter);
+
+        // Assert
+        Assert.NotNull(writer);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddMetadata_ByteValue_AddsMetadataToResourceFile()
+    {
+        // Arrange
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddMetadata(_testString1, _testBytes);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var metadataEnumerator = reader.GetMetadataEnumerator();
+        Assert.True(metadataEnumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)metadataEnumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testBytes, currentEntry.Value);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddMetadata_StringValue_AddsMetadataToResourceFile()
+    {
+        // Arrange
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddMetadata(_testString1, _testString2);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var metadataEnumerator = reader.GetMetadataEnumerator();
+        Assert.True(metadataEnumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)metadataEnumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testString2, currentEntry.Value);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddMetadata_ObjectValue_AddsMetadataToResourceFile()
+    {
+        // Arrange
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddMetadata(_testString1, _testDateTime);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var metadataEnumerator = reader.GetMetadataEnumerator();
+        Assert.True(metadataEnumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)metadataEnumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testDateTime, currentEntry.Value);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddResource_ByteValue_AddsResourceToResourceFile()
+    {
+        // Arrange
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddResource(_testString1, _testBytes);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var enumerator = reader.GetEnumerator();
+        Assert.True(enumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)enumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testBytes, currentEntry.Value);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddResource_StringValue_AddsResourceToResourceFile()
+    {
+        // Arrange
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddResource(_testString1, _testString2);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var enumerator = reader.GetEnumerator();
+        Assert.True(enumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)enumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testString2, currentEntry.Value);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddResource_ObjectValue_AddsResourceToResourceFile()
+    {
+        // Arrange
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddResource(_testString1, _testDateTime);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var enumerator = reader.GetEnumerator();
+        Assert.True(enumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)enumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testDateTime, currentEntry.Value);
+    }
+
+    [Fact]
+    public void ResXResourceWriter_AddResource_ResxDataNodeValue_AddsResourceToResourceFile()
+    {
+        // Arrange
+        ResXDataNode dataNode = new(_testString1, _testDateTime);
+        using ResXResourceWriter writer = new(_resxFileName);
+
+        // Act
+        writer.AddResource(_testString1, dataNode);
+        writer.Generate();
+        writer.Close();
+
+        // Assert
+        using ResXResourceReader reader = new(_resxFileName);
+        var enumerator = reader.GetEnumerator();
+        Assert.True(enumerator.MoveNext());
+
+        var currentEntry = (DictionaryEntry)enumerator.Current;
+        Assert.Equal(_testString1, currentEntry.Key);
+        Assert.Equal(_testDateTime, currentEntry.Value);
     }
 }
