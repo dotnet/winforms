87d15a1dfb9c5f9f0371506f54c4987c89ab8379
gpetrou <gipetrou@gmail.com>
2023-05-10 20:11:14 +0300

    Avoid calling _owningGroup.Items[0].ListView twice in ListViewGroupAccessibleObject constructor

--- src/System.Windows.Forms/src/System/Windows/Forms/ListViewGroup.ListViewGroupAccessibleObject.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/ListViewGroup.ListViewGroupAccessibleObject.cs
@@ -23,8 +23,8 @@ public partial class ListViewGroup
 
             // Using item from group for getting of ListView is a workaround for https://github.com/dotnet/winforms/issues/4019
             _owningListView = owningGroup.ListView
-                ?? (owningGroup.Items.Count > 0 && _owningGroup.Items[0].ListView is not null
-                    ? _owningGroup.Items[0].ListView!
+                ?? (owningGroup.Items.Count > 0 && _owningGroup.Items[0].ListView is ListView listView
+                    ? listView
                     : throw new InvalidOperationException(nameof(owningGroup.ListView)));
 
             _owningListViewAccessibilityObject = _owningListView.AccessibilityObject as ListView.ListViewAccessibleObject
