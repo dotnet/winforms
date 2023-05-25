9cd1406c9672580bf81ad1fc0cf9aaf4a1566f75
Devendar Reddy Adulla <dreddy@microsoft.com>
2023-05-25 15:27:43 -0700

    fix buidl failure.

--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/Infra/SendInput.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/Infra/SendInput.cs
@@ -3,7 +3,6 @@
 // See the LICENSE file in the project root for more information.
 
 using System.Windows.Forms.UITests.Input;
-using Microsoft.VisualStudio.Threading;
 using Windows.Win32.UI.Input.KeyboardAndMouse;
 using Windows.Win32.UI.WindowsAndMessaging;
 using Xunit.Abstractions;
