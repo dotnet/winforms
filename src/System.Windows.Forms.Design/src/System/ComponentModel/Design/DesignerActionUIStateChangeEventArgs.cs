72b3a6724c0047a76db4c6dc2449d836bca4755c
halgab <24685886+halgab@users.noreply.github.com>
2023-05-26 16:53:45 +0200

    annotate DesignerActionUIService

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/DesignerActionUIStateChangeEventArgs.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/DesignerActionUIStateChangeEventArgs.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 namespace System.ComponentModel.Design;
 
 /// <summary>
@@ -13,7 +11,7 @@ public class DesignerActionUIStateChangeEventArgs : EventArgs
     /// <summary>
     ///  Constructor that requires the object in question, the type of change and the remaining actionlists left for the object on the related object.
     /// </summary>
-    public DesignerActionUIStateChangeEventArgs(object relatedObject, DesignerActionUIStateChangeType changeType)
+    public DesignerActionUIStateChangeEventArgs(object? relatedObject, DesignerActionUIStateChangeType changeType)
     {
         RelatedObject = relatedObject;
         ChangeType = changeType;
@@ -22,7 +20,7 @@ public class DesignerActionUIStateChangeEventArgs : EventArgs
     /// <summary>
     ///  The object this change is related to.
     /// </summary>
-    public object RelatedObject { get; }
+    public object? RelatedObject { get; }
 
     /// <summary>
     ///  The type of changed that caused the related event to be thrown.
