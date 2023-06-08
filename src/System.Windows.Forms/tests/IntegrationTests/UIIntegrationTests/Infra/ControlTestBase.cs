005a819c43b4e7a228624d265a6ec5ed5985f456
Devendar Reddy Adulla <dreddy@microsoft.com>
2023-06-08 11:42:20 -0700

    Update WInFormsFact to include RetryCount

--- src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/Infra/ControlTestBase.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/UIIntegrationTests/Infra/ControlTestBase.cs
@@ -14,6 +14,7 @@ using Xunit.Abstractions;
 namespace System.Windows.Forms.UITests;
 
 [UseDefaultXunitCulture]
+[UISettings(MaxAttempts = 3)]
 public abstract class ControlTestBase : IAsyncLifetime, IDisposable
 {
     private const int SPIF_SENDCHANGE = 0x0002;
