3888c2d267f6d9d2242534e62dbcaf54c656c718
halgab <24685886+halgab@users.noreply.github.com>
2023-06-25 16:26:37 +0200

    annotate DesignerActionBehavior

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/DesignerActionBehavior.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/DesignerActionBehavior.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using System.ComponentModel;
 using System.ComponentModel.Design;
 using System.Drawing;
@@ -14,9 +12,6 @@ namespace System.Windows.Forms.Design.Behavior;
 /// </summary>
 internal sealed class DesignerActionBehavior : Behavior
 {
-    private readonly IComponent _relatedComponent; //The component we are bound to
-    private readonly DesignerActionUI _parentUI; //ptr to the parenting UI, used for showing menus and setting selection
-    private DesignerActionListCollection _actionLists; //all the shortcuts!
     private readonly IServiceProvider _serviceProvider; // we need to cache the service provider here to be able to create the panel with the proper arguments
     private bool _ignoreNextMouseUp;
 
@@ -25,36 +20,26 @@ internal sealed class DesignerActionBehavior : Behavior
     /// </summary>
     internal DesignerActionBehavior(IServiceProvider serviceProvider, IComponent relatedComponent, DesignerActionListCollection actionLists, DesignerActionUI parentUI)
     {
-        _actionLists = actionLists;
+        ActionLists = actionLists;
         _serviceProvider = serviceProvider;
-        _relatedComponent = relatedComponent;
-        _parentUI = parentUI;
+        RelatedComponent = relatedComponent;
+        ParentUI = parentUI;
     }
 
     /// <summary>
     ///  Returns the collection of DesignerActionLists this Behavior is managing. These will be dynamically updated (some can be removed, new ones can be added, etc...).
     /// </summary>
-    internal DesignerActionListCollection ActionLists
-    {
-        get => _actionLists;
-        set => _actionLists = value;
-    }
+    internal DesignerActionListCollection ActionLists { get; set; }
 
     /// <summary>
     ///  Returns the parenting UI (a DesignerActionUI)
     /// </summary>
-    internal DesignerActionUI ParentUI
-    {
-        get => _parentUI;
-    }
+    internal DesignerActionUI ParentUI { get; }
 
     /// <summary>
     ///  Returns the Component that this glyph is attached to.
     /// </summary>
-    internal IComponent RelatedComponent
-    {
-        get => _relatedComponent;
-    }
+    internal IComponent RelatedComponent { get; }
 
     /// <summary>
     ///  Hides the designer action panel UI.
@@ -79,7 +64,7 @@ internal sealed class DesignerActionBehavior : Behavior
     /// </summary>
     internal void ShowUI(Glyph g)
     {
-        if (!(g is DesignerActionGlyph glyph))
+        if (g is not DesignerActionGlyph glyph)
         {
             Debug.Fail("Why are we trying to 'showui' on a glyph that's not a DesignerActionGlyph?");
             return;
@@ -97,13 +82,13 @@ internal sealed class DesignerActionBehavior : Behavior
         }
     }
 
-    public override bool OnMouseDoubleClick(Glyph g, MouseButtons button, Point mouseLoc)
+    public override bool OnMouseDoubleClick(Glyph? g, MouseButtons button, Point mouseLoc)
     {
         _ignoreNextMouseUp = true;
         return true;
     }
 
-    public override bool OnMouseDown(Glyph g, MouseButtons button, Point mouseLoc)
+    public override bool OnMouseDown(Glyph? g, MouseButtons button, Point mouseLoc)
     { // we take the msg
         return (!ParentUI.IsDesignerActionPanelVisible);
     }
@@ -111,7 +96,7 @@ internal sealed class DesignerActionBehavior : Behavior
     /// <summary>
     ///  In response to a MouseUp, we will either 1) select the Glyph and control if not selected, or 2) Build up our context menu representing our DesignerActions and show it.
     /// </summary>
-    public override bool OnMouseUp(Glyph g, MouseButtons button)
+    public override bool OnMouseUp(Glyph? g, MouseButtons button)
     {
         if (button != MouseButtons.Left || ParentUI is null)
         {
@@ -125,23 +110,19 @@ internal sealed class DesignerActionBehavior : Behavior
         }
         else if (!_ignoreNextMouseUp)
         {
-            if (_serviceProvider is not null)
+            if (_serviceProvider.TryGetService(out ISelectionService? selectionService))
             {
-                ISelectionService selectionService = (ISelectionService)_serviceProvider.GetService(typeof(ISelectionService));
-                if (selectionService is not null)
+                if (selectionService.PrimarySelection != RelatedComponent)
                 {
-                    if (selectionService.PrimarySelection != RelatedComponent)
+                    List<IComponent> componentList = new List<IComponent>
                     {
-                        List<IComponent> componentList = new List<IComponent>
-                        {
-                            RelatedComponent
-                        };
-                        selectionService.SetSelectedComponents(componentList, SelectionTypes.Primary);
-                    }
+                        RelatedComponent
+                    };
+                    selectionService.SetSelectedComponents(componentList, SelectionTypes.Primary);
                 }
             }
 
-            ShowUI(g);
+            ShowUI(g!);
         }
         else
         {
