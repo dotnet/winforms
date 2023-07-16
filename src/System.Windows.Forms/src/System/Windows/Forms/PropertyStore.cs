732d702806378a169549e92ce90a173fc707597a
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2023-07-17 09:11:22 +1000

    Refactor TryGetObject to handle when null values are returned for non-nullable types

--- src/System.Windows.Forms/src/System/Windows/Forms/PropertyStore.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/PropertyStore.cs
@@ -188,8 +188,10 @@ internal partial class PropertyStore
     public bool TryGetObject<T>(int key, [NotNullWhen(true)] out T? value)
     {
         object? entry = GetObject(key, out bool found);
-        value = !found ? default : (T?)entry;
-        return found;
+        bool isValid = entry is T || Nullable.GetUnderlyingType(typeof(T)) is not null;
+        Debug.Assert(!found || entry is null || entry is T, $"Entry is not of type {typeof(T)}, but of type {entry?.GetType()}");
+        value = found && isValid ? (T?)entry : default;
+        return found && isValid;
     }
 
     public bool ContainsObjectThatIsNotNull(int key)
