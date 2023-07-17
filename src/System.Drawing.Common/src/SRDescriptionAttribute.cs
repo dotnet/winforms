b3f3ecddffc8be4cb2267c55620cdda905fe33ad
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2023-07-18 04:17:55 +1000

    Fix `System.Drawing.Common` `SRDescriptionAttribute` (#9506)
    Fixes #3822

--- src/System.Drawing.Common/src/SRDescriptionAttribute.cs
+++ src/System.Drawing.Common/src/SRDescriptionAttribute.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel;
@@ -17,7 +17,7 @@ internal sealed class SRDescriptionAttribute : DescriptionAttribute
             if (!_replaced)
             {
                 _replaced = true;
-                DescriptionValue = SR.Format(base.Description);
+                base.DescriptionValue = SR.GetResourceString(base.Description);
             }
             return base.Description;
         }
