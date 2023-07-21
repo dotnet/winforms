b7847fc967bf0dbe119eb124e6579672a128160d
Leaf Shi <132890443+LeafShi1@users.noreply.github.com>
2023-07-21 18:30:15 +0800

    Delete unnecessary namespace

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/MonthCalendarDesigner.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/MonthCalendarDesigner.cs
@@ -1,40 +1,35 @@
-﻿// See the LICENSE file in the project root for more information.
-
-using System.Collections;
-using System.Drawing;
-using System.Windows.Forms.Design.Behavior;
-
-namespace System.Windows.Forms.Design;
-
-internal class MonthCalendarDesigner : ControlDesigner
+namespace System.Windows.Forms.Design
 {
-    public MonthCalendarDesigner()
+    internal class MonthCalendarDesigner : ControlDesigner
     {
-        AutoResizeHandles = true;
-    }
-
-    /// <summary>
-    /// Retrieves a set of rules concerning the movement capabilities of a component.
-    /// This should be one or more flags from the SelectionRules class.  If no designer
-    /// provides rules for a component, the component will not get any UI services.
-    /// </summary>
-    public override SelectionRules SelectionRules
-    {
-        get
+        public MonthCalendarDesigner()
         {
-            SelectionRules rules = base.SelectionRules;
+            AutoResizeHandles = true;
+        }
 
-            if (Control.Parent is null || (Control.Parent is not null && !Control.Parent.IsMirrored))
+        /// <summary>
+        /// Retrieves a set of rules concerning the movement capabilities of a component.
+        /// This should be one or more flags from the SelectionRules class.  If no designer
+        /// provides rules for a component, the component will not get any UI services.
+        /// </summary>
+        public override SelectionRules SelectionRules
+        {
+            get
             {
-                rules &= ~(SelectionRules.TopSizeable | SelectionRules.LeftSizeable);
-            }
-            else
-            {
-                Debug.Assert(Control.Parent is not null && Control.Parent.IsMirrored);
-                rules &= ~(SelectionRules.TopSizeable | SelectionRules.RightSizeable);
-            }
+                SelectionRules rules = base.SelectionRules;
 
-            return rules;
+                if (Control.Parent is null || (Control.Parent is not null && !Control.Parent.IsMirrored))
+                {
+                    rules &= ~(SelectionRules.TopSizeable | SelectionRules.LeftSizeable);
+                }
+                else
+                {
+                    Debug.Assert(Control.Parent is not null && Control.Parent.IsMirrored);
+                    rules &= ~(SelectionRules.TopSizeable | SelectionRules.RightSizeable);
+                }
+
+                return rules;
+            }
         }
     }
 }
