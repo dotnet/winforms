cbcf0fac9af825d82e8ea30b9137c5ff9b422491
Leaf Shi <v-leafshi@microsoft.com>
2023-07-21 00:24:10 -0700

    Port MonthCalendarDesigner to runtime

---
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/MonthCalendarDesigner.cs
@@ -0,0 +1,40 @@
+﻿// See the LICENSE file in the project root for more information.
+
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms.Design.Behavior;
+
+namespace System.Windows.Forms.Design;
+
+internal class MonthCalendarDesigner : ControlDesigner
+{
+    public MonthCalendarDesigner()
+    {
+        AutoResizeHandles = true;
+    }
+
+    /// <summary>
+    /// Retrieves a set of rules concerning the movement capabilities of a component.
+    /// This should be one or more flags from the SelectionRules class.  If no designer
+    /// provides rules for a component, the component will not get any UI services.
+    /// </summary>
+    public override SelectionRules SelectionRules
+    {
+        get
+        {
+            SelectionRules rules = base.SelectionRules;
+
+            if (Control.Parent is null || (Control.Parent is not null && !Control.Parent.IsMirrored))
+            {
+                rules &= ~(SelectionRules.TopSizeable | SelectionRules.LeftSizeable);
+            }
+            else
+            {
+                Debug.Assert(Control.Parent is not null && Control.Parent.IsMirrored);
+                rules &= ~(SelectionRules.TopSizeable | SelectionRules.RightSizeable);
+            }
+
+            return rules;
+        }
+    }
+}
