664fa061543f3d88642fd661122972274eea380a
gpetrou <gipetrou@gmail.com>
2023-07-25 07:35:03 +0300

    Make value nullable

--- src/System.Windows.Forms/src/System/Windows/Forms/Binding.BindToObject.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Binding.BindToObject.cs
@@ -167,7 +167,7 @@ public partial class Binding
             }
         }
 
-        internal void SetValue(object value)
+        internal void SetValue(object? value)
         {
             object? obj = null;
 
