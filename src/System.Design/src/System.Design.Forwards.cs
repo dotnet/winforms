7c550aa92e2efebae1bc4eb9d495435bf458acf3
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2023-07-27 06:18:33 +1000

    Implement `ImageListDesigner` (#9493)
    * Implement ImageListDesigner
    
    * Refactoring ImageListActionList
    
    * Move AssemblyRef to common
    
    * Changes from review
    
    * Few missed changes
    
    * Revert "Move AssemblyRef to common" and add required changes
    
    ---------
    
    Co-authored-by: Devendar Reddy Adulla <dreddy@microsoft.com>

--- src/System.Design/src/System.Design.Forwards.cs
+++ src/System.Design/src/System.Design.Forwards.cs
@@ -38,6 +38,7 @@ using System.Runtime.CompilerServices;
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.DateTimePickerDesigner))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.FormDocumentDesigner))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.GroupBoxDesigner))]
+[assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ImageListDesigner))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.LabelDesigner))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ListBoxDesigner))]
 [assembly: TypeForwardedTo(typeof(System.Windows.Forms.Design.ListViewDesigner))]
