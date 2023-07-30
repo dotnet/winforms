9c3e9f0c9302b7084a3b2b1251c9809e4c4c42d2
halgab <24685886+halgab@users.noreply.github.com>
2023-07-30 16:56:22 +0200

    annotate SelectionUIService

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/SelectionUIService.SelectionUIItem.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/SelectionUIService.SelectionUIItem.cs
@@ -66,7 +66,7 @@ internal sealed partial class SelectionUIService
         private readonly Control? _control;
         private SelectionStyles _selectionStyle; // how do we draw this thing?
         private SelectionRules _selectionRules;
-        private readonly ISelectionUIHandler _handler; // the components selection UI handler (can be null)
+        private readonly ISelectionUIHandler? _handler; // the components selection UI handler (can be null)
 
         ///  Its ok to call virtual method as this is a private class.
         public SelectionUIItem(SelectionUIService selUIsvc, object component)
