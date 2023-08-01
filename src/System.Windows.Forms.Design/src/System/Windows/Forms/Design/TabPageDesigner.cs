7a78b7ca73f29053b934b7dfbb000a0730cc6038
Simon Zhao <v-weidzh@microsoft.com>
2023-08-01 08:57:27 +0800

    Add license header to the file

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/TabPageDesigner.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/TabPageDesigner.cs
@@ -1,4 +1,8 @@
-﻿using System.ComponentModel.Design;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.ComponentModel.Design;
 using System.Drawing;
 using System.Windows.Forms.Design.Behavior;
 
