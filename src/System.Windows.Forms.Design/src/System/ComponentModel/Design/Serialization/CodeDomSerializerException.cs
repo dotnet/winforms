a9c8e80c9083981586b8173a982a5911dfda5ffe
halgab <24685886+halgab@users.noreply.github.com>
2023-08-04 15:27:39 +0200

    annotate CodeDomSerializerException

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CodeDomSerializerException.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CodeDomSerializerException.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using System.CodeDom;
 using System.Runtime.Serialization;
 
@@ -13,22 +11,22 @@ namespace System.ComponentModel.Design.Serialization;
 /// </summary>
 public class CodeDomSerializerException : SystemException
 {
-    public CodeDomSerializerException(string message, CodeLinePragma linePragma) : base(message)
+    public CodeDomSerializerException(string? message, CodeLinePragma? linePragma) : base(message)
     {
         LinePragma = linePragma;
     }
 
-    public CodeDomSerializerException(Exception ex, CodeLinePragma linePragma) : base(ex?.Message, ex)
+    public CodeDomSerializerException(Exception? ex, CodeLinePragma? linePragma) : base(ex?.Message, ex)
     {
         LinePragma = linePragma;
     }
 
-    public CodeDomSerializerException(string message, IDesignerSerializationManager manager) : base(message)
+    public CodeDomSerializerException(string? message, IDesignerSerializationManager manager) : base(message)
     {
         ArgumentNullException.ThrowIfNull(manager);
     }
 
-    public CodeDomSerializerException(Exception ex, IDesignerSerializationManager manager) : base(ex?.Message, ex)
+    public CodeDomSerializerException(Exception? ex, IDesignerSerializationManager manager) : base(ex?.Message, ex)
     {
         ArgumentNullException.ThrowIfNull(manager);
     }
@@ -36,7 +34,7 @@ public class CodeDomSerializerException : SystemException
     /// <summary>
     ///  Gets the line pragma object that is related to this error.
     /// </summary>
-    public CodeLinePragma LinePragma { get; }
+    public CodeLinePragma? LinePragma { get; }
 
     [Obsolete(DiagnosticId = "SYSLIB0051")]
     public override void GetObjectData(SerializationInfo info, StreamingContext context)
