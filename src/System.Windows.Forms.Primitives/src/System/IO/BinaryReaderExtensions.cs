e0ce845dee972aecff5f61e1d2987467f2b93fd8
Jeremy Kuhne <jkuhne@microsoft.com>
2023-08-07 14:17:14 -0700

    Don't serialize Hashtable with custom comparer
    We don't support Hashtable manual serialization with custom comparers or hash code generators. Check for this case and allow it to fall through to the BinaryFormatter.  Add a few tests to validate.
    
    Also move BinaryFormatReader code to BinaryReaderExtensions.

--- src/System.Windows.Forms.Primitives/src/System/IO/BinaryReaderExtensions.cs
+++ src/System.Windows.Forms.Primitives/src/System/IO/BinaryReaderExtensions.cs
@@ -1,9 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
-using System.Windows.Forms.BinaryFormat;
-
 namespace System.IO;
 
 internal static class BinaryReaderExtensions
@@ -13,7 +12,34 @@ internal static class BinaryReaderExtensions
     /// </summary>
     /// <exception cref="SerializationException">The data was invalid.</exception>
     public static unsafe DateTime ReadDateTime(this BinaryReader reader)
-        => BinaryFormatReader.CreateDateTimeFromData(reader.ReadInt64());
+        => CreateDateTimeFromData(reader.ReadInt64());
+
+    /// <summary>
+    ///  Creates a <see cref="DateTime"/> object from raw data with validation.
+    /// </summary>
+    /// <exception cref="SerializationException"><paramref name="data"/> was invalid.</exception>
+    private static DateTime CreateDateTimeFromData(long data)
+    {
+        // Copied from System.Runtime.Serialization.Formatters.Binary.BinaryParser
+
+        // Use DateTime's public constructor to validate the input, but we
+        // can't return that result as it strips off the kind. To address
+        // that, store the value directly into a DateTime via an unsafe cast.
+        // See BinaryFormatterWriter.WriteDateTime for details.
+
+        try
+        {
+            const long TicksMask = 0x3FFFFFFFFFFFFFFF;
+            _ = new DateTime(data & TicksMask);
+        }
+        catch (ArgumentException ex)
+        {
+            // Bad data
+            throw new SerializationException(ex.Message, ex);
+        }
+
+        return Unsafe.As<long, DateTime>(ref data);
+    }
 
     /// <summary>
     ///  Returns the remaining amount of bytes in the given <paramref name="reader"/>.
