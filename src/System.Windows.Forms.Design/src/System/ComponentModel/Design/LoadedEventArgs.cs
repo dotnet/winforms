1681c11945faffa7b13bbd4318b4c075dcf68081
halgab <24685886+halgab@users.noreply.github.com>
2023-08-09 10:26:36 +0200

    annotate LoadedEventArgs

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/LoadedEventArgs.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/LoadedEventArgs.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using System.Collections;
 
 namespace System.ComponentModel.Design;
@@ -15,7 +13,7 @@ public sealed class LoadedEventArgs : EventArgs
     /// <summary>
     ///  Creates a new LoadedEventArgs object.
     /// </summary>
-    public LoadedEventArgs(bool succeeded, ICollection errors)
+    public LoadedEventArgs(bool succeeded, ICollection? errors)
     {
         HasSucceeded = succeeded;
         Errors = errors ?? Array.Empty<object>();
