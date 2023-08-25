338ede3eaaac7735ef3bfddcf416222802a020f8
Jeremy Kuhne <jkuhne@microsoft.com>
2023-08-25 11:13:48 -0700

    Handle IErrorInfo false

--- src/System.Windows.Forms/tests/InteropTests/PropertyGridTests.cs
+++ src/System.Windows.Forms/tests/InteropTests/PropertyGridTests.cs
@@ -27,7 +27,7 @@ public class PropertyGridTests
                 try
                 {
                     encodingEntry.SetPropertyTextValue("333");
-                    Assert.False(true, "Invalid set values should produce ExternalException which will be presenttted to the user.");
+                    Assert.False(true, "Invalid set values should produce ExternalException which will be presented to the user.");
                 }
                 catch (ExternalException ex)
                 {
