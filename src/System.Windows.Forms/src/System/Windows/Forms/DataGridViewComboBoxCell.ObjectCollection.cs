48631bbefe39e18c7d5ef6675488df126516ec45
gpetrou <gipetrou@gmail.com>
2023-09-29 18:11:17 +0300

    Fix private field names

--- src/System.Windows.Forms/src/System/Windows/Forms/DataGridViewComboBoxCell.ObjectCollection.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/DataGridViewComboBoxCell.ObjectCollection.cs
@@ -16,23 +16,23 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
     [ListBindable(false)]
     public class ObjectCollection : IList
     {
-        private readonly DataGridViewComboBoxCell owner;
-        private List<object> items;
-        private IComparer<object> comparer;
+        private readonly DataGridViewComboBoxCell _owner;
+        private List<object> _items;
+        private IComparer<object> _comparer;
 
         public ObjectCollection(DataGridViewComboBoxCell owner)
         {
             Debug.Assert(owner is not null);
-            this.owner = owner;
+            _owner = owner;
         }
 
         private IComparer<object> Comparer
         {
             get
             {
-                comparer ??= new ItemComparer(owner);
+                _comparer ??= new ItemComparer(_owner);
 
-                return comparer;
+                return _comparer;
             }
         }
 
@@ -54,9 +54,9 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
         {
             get
             {
-                items ??= new List<object>();
+                _items ??= new List<object>();
 
-                return items;
+                return _items;
             }
         }
 
@@ -78,14 +78,14 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
         public int Add(object item)
         {
             //this.owner.CheckNoSharedCell();
-            owner.CheckNoDataSource();
+            _owner.CheckNoDataSource();
 
             ArgumentNullException.ThrowIfNull(item);
 
             int index = ((IList)InnerArray).Add(item);
 
             bool success = false;
-            if (owner.Sorted)
+            if (_owner.Sorted)
             {
                 try
                 {
@@ -102,7 +102,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
                 }
             }
 
-            owner.OnItemsCollectionChanged();
+            _owner.OnItemsCollectionChanged();
             return index;
         }
 
@@ -114,17 +114,17 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
         public void AddRange(params object[] items)
         {
             //this.owner.CheckNoSharedCell();
-            owner.CheckNoDataSource();
+            _owner.CheckNoDataSource();
             AddRangeInternal(items);
-            owner.OnItemsCollectionChanged();
+            _owner.OnItemsCollectionChanged();
         }
 
         public void AddRange(ObjectCollection value)
         {
             //this.owner.CheckNoSharedCell();
-            owner.CheckNoDataSource();
+            _owner.CheckNoDataSource();
             AddRangeInternal((ICollection<object>)value);
-            owner.OnItemsCollectionChanged();
+            _owner.OnItemsCollectionChanged();
         }
 
         /// <summary>
@@ -144,7 +144,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
 
             // Add everything to the collection first, then sort
             InnerArray.AddRange(items);
-            if (owner.Sorted)
+            if (_owner.Sorted)
             {
                 InnerArray.Sort(Comparer);
             }
@@ -172,7 +172,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
             set
             {
                 //this.owner.CheckNoSharedCell();
-                owner.CheckNoDataSource();
+                _owner.CheckNoDataSource();
 
                 if (index < 0 || index >= InnerArray.Count)
                 {
@@ -180,7 +180,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
                 }
 
                 InnerArray[index] = value.OrThrowIfNull();
-                owner.OnItemsCollectionChanged();
+                _owner.OnItemsCollectionChanged();
             }
         }
 
@@ -192,9 +192,9 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
             if (InnerArray.Count > 0)
             {
                 //this.owner.CheckNoSharedCell();
-                owner.CheckNoDataSource();
+                _owner.CheckNoDataSource();
                 InnerArray.Clear();
-                owner.OnItemsCollectionChanged();
+                _owner.OnItemsCollectionChanged();
             }
         }
 
@@ -246,7 +246,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
         public void Insert(int index, object item)
         {
             //this.owner.CheckNoSharedCell();
-            owner.CheckNoDataSource();
+            _owner.CheckNoDataSource();
 
             ArgumentNullException.ThrowIfNull(item);
 
@@ -257,14 +257,14 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
 
             // If the combo box is sorted, then just treat this like an add
             // because we are going to twiddle the index anyway.
-            if (owner.Sorted)
+            if (_owner.Sorted)
             {
                 Add(item);
             }
             else
             {
                 InnerArray.Insert(index, item);
-                owner.OnItemsCollectionChanged();
+                _owner.OnItemsCollectionChanged();
             }
         }
 
@@ -288,7 +288,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
         public void RemoveAt(int index)
         {
             //this.owner.CheckNoSharedCell();
-            owner.CheckNoDataSource();
+            _owner.CheckNoDataSource();
 
             if (index < 0 || index >= InnerArray.Count)
             {
@@ -296,7 +296,7 @@ public partial class DataGridViewComboBoxCell : DataGridViewCell
             }
 
             InnerArray.RemoveAt(index);
-            owner.OnItemsCollectionChanged();
+            _owner.OnItemsCollectionChanged();
         }
     } // end ObjectCollection
 }
