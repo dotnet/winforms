412255c6b73fcf556bad7d680cc86fc852f0f767
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2023-10-07 12:52:09 -0700

    Add first implementation of Popup control.

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/PopupCloseRequestOrigin.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms;
+
+/// <summary>
+///  Represents the closing request origin in the context of the <see cref="PopupCloseRequestEventArgs"/>.
+/// </summary>
+public enum PopupCloseRequestOrigin
+{
+    /// <summary>
+    ///  An action by the user caused the popup to close.
+    /// </summary>
+    ExternalByUser = 0,
+
+    /// <summary>
+    ///  The popup was closed by the component itself.
+    /// </summary>
+    InternalByComponent = 1
+}
---
+++ src/System.Windows.Forms/src/System/Windows/Forms/PopupOpeningEventArgs.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace System.Windows.Forms;
+
+/// <summary>
+///  Represents the event data for a popup opening event.
+/// </summary>
+/// <seealso cref="CancelEventArgs" />
+public class PopupOpeningEventArgs : CancelEventArgs
+{
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="PopupOpeningEventArgs"/> class.
+    /// </summary>
+    /// <param name="cancel">true if the event should be canceled; otherwise, false.</param>
+    /// <param name="preferredNewSize">The preferred new size of the popup.</param>
+    public PopupOpeningEventArgs(bool cancel, Size preferredNewSize)
+    {
+        base.Cancel = cancel;
+        PreferredNewSize = preferredNewSize;
+    }
+
+    /// <summary>
+    ///  Gets or sets the preferred new size of the popup.
+    /// </summary>
+    /// <value>
+    /// The preferred new size of the popup.
+    /// </value>
+    public Size PreferredNewSize { get; set; }
+
+    /// <summary>
+    ///  Gets or sets a value indicating whether the popup is should be prevented from resizing.
+    /// </summary>
+    /// <value>
+    ///   <see langword="true"/> if the popup is should not be resized; otherwise, <see langword="false"/>.
+    /// </value>
+    public bool PreventResizing { get; set; }
+}
