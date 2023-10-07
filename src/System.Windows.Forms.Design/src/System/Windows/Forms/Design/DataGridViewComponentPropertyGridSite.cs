b7c130bb7b93043b2f254ab25bbab9981b45c48f
halgab <24685886+halgab@users.noreply.github.com>
2023-10-07 23:25:28 +0200

    annotate DataGridViewCellStyleEditor and DataGridViewComponentPropertyGridSite

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DataGridViewComponentPropertyGridSite.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DataGridViewComponentPropertyGridSite.cs
@@ -1,45 +1,42 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using System.ComponentModel;
 
 namespace System.Windows.Forms.Design;
 
 internal class DataGridViewComponentPropertyGridSite : ISite
 {
-    private readonly IServiceProvider _sp;
-    private readonly IComponent _comp;
+    private readonly IServiceProvider? _sp;
     private bool _inGetService;
 
-    public DataGridViewComponentPropertyGridSite(IServiceProvider sp, IComponent comp)
+    public DataGridViewComponentPropertyGridSite(IServiceProvider? sp, IComponent comp)
     {
         _sp = sp;
-        _comp = comp;
+        Component = comp;
     }
 
     /// <summary>
     ///  When implemented by a class, gets the component associated with the <see cref="ISite"/>.
     /// </summary>
-    public IComponent Component { get => _comp; }
+    public IComponent Component { get; }
 
     /// <summary>
     /// When implemented by a class, gets the container associated with the <see cref="ISite"/>.
     /// </summary>
-    public IContainer Container { get => null; }
+    public IContainer? Container => null;
 
     /// <summary>
     ///  When implemented by a class, determines whether the component is in design mode.
     /// </summary>
-    public bool DesignMode { get => false; }
+    public bool DesignMode => false;
 
     /// <summary>
     ///  When implemented by a class, gets or sets the name of the component associated with the <see cref="ISite"/>.
     /// </summary>
-    public string Name { get; set; }
+    public string? Name { get; set; }
 
-    public object GetService(Type t)
+    public object? GetService(Type t)
     {
         if (_inGetService || _sp is null)
         {
