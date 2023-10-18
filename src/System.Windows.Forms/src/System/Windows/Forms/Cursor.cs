826a78731c90ef06792c6505c03132ecd6de2479
Matt Mitchell <mmitche@microsoft.com>
2023-10-18 15:59:37 -0700

    Revert "Don't invalidate Cursors cursors (#9994)"
    This reverts commit e0551d909b052e4066f8dd0f90d560f8d44682ba.

--- src/System.Windows.Forms/src/System/Windows/Forms/Cursor.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Cursor.cs
@@ -72,11 +72,6 @@ namespace System.Windows.Forms
         public Cursor(Type type, string resource)
             : this((type ?? throw new ArgumentNullException(nameof(type))).Module.Assembly.GetManifestResourceStream(type, resource)!)
         {
-            if (type == typeof(Cursor))
-            {
-                // This came from the Cursors class, we shouldn't be disposing them.
-                _ownHandle = false;
-            }
         }
 
         /// <summary>
@@ -231,11 +226,13 @@ namespace System.Windows.Forms
 
         private void Dispose(bool disposing)
         {
-            if (_handle != IntPtr.Zero && _ownHandle)
+            if (_handle != IntPtr.Zero)
             {
-                // If we don't own the handle we should never _actually_ dispose or set to null.
-                // Doing so would make the static cursors in Cursors unusable.
-                User32.DestroyCursor(_handle);
+                if (_ownHandle)
+                {
+                    User32.DestroyCursor(_handle);
+                }
+
                 _handle = IntPtr.Zero;
             }
         }
