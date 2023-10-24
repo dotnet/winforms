423a34b6be780d42a5823a26c85d444b62cd817f
gpetrou <4172445+gpetrou@users.noreply.github.com>
2023-10-24 22:10:08 +0300

    Enable nullability in some DataGridViewComboBoxCell members (#10167)
    * Enable nullability in some DataGridViewComboBoxCell members
    
    * Use expression body
    
    * Add DataGridViewComboBoxCellFlags enum

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/DataGridViewComboBoxCell.DataGridViewComboBoxCellFlags.cs
@@ -0,0 +1,18 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms;
+
+public partial class DataGridViewComboBoxCell
+{
+    [Flags]
+    private enum DataGridViewComboBoxCellFlags : byte
+    {
+        IgnoreNextMouseClick = 0x01,
+        CellSorted = 0x02,
+        CellCreateItemsFromDataSource = 0x04,
+        CellAutoComplete = 0x08,
+        DataSourceInitializedHookedUp = 0x10,
+        DropDownHookedUp = 0x20
+    }
+}
