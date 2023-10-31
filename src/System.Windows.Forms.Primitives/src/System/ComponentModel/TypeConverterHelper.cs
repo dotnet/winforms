6a6b758b89333e1937ac9132c227a24d8b5a3dc0
halgab <24685886+halgab@users.noreply.github.com>
2023-10-31 22:43:54 +0100

    review feedback

--- src/System.Windows.Forms.Primitives/src/System/ComponentModel/TypeConverterHelper.cs
+++ src/System.Windows.Forms.Primitives/src/System/ComponentModel/TypeConverterHelper.cs
@@ -7,11 +7,20 @@ namespace System.ComponentModel;
 
 internal static class TypeConverterHelper
 {
+    /// <summary>
+    /// Converts the given text to list of objects, using the specified context and culture information.
+    /// </summary>
+    /// <param name="context">An ITypeDescriptorContext that provides a format context.</param>
+    /// <param name="culture">A CultureInfo. If null is passed, the current culture is assumed.</param>
+    /// <param name="text">The chars to convert.</param>
+    /// <param name="output">The converted text chunks</param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns>true if text was converted successfully; otherwise, false.</returns>
     public static bool TryParseAsSpan<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(ITypeDescriptorContext? context, CultureInfo? culture, ReadOnlySpan<char> text, Span<T> output)
     {
         culture ??= CultureInfo.CurrentCulture;
 
-        Span<Range> tokens = stackalloc Range[output.Length + 1];
+        using BufferScope<Range> tokens = new BufferScope<Range>(stackalloc Range[16]);
         int tokensCount = text.Split(tokens, culture.TextInfo.ListSeparator[0]);
 
         if (tokensCount != output.Length)
