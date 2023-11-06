fdad5bc2e99e33622b24d4c5265ff24ddd7c8766
gpetrou <4172445+gpetrou@users.noreply.github.com>
2023-11-06 23:34:21 +0200

    Add some tests for classes that inherit from Control.ControlCollection (#6935)

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/SplitContainer.SplitContainerTypedControlCollectionTests.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Tests;
+
+public class SplitContainer_SplitContainerTypedControlCollectionTests
+{
+    [WinFormsFact]
+    public void SplitContainerTypedControlCollection_Ctor_Control()
+    {
+        using SplitContainer owner = new ();
+        SplitContainer.SplitContainerTypedControlCollection collection = new(owner, typeof(SplitterPanel), false);
+
+        Assert.Empty(collection);
+        Assert.False(collection.IsReadOnly);
+        Assert.Same(owner, collection.Owner);
+    }
+
+    [WinFormsFact]
+    public void SplitContainerTypedControlCollection_Ctor_NullOwner_ThrowsArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>("owner", () => new SplitContainer.SplitContainerTypedControlCollection(null, typeof(SplitterPanel), false));
+    }
+}
