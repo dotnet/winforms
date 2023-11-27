4ed39f473417aa667477e1fda99aca1e478543cd
Simon Zhao <v-weidzh@microsoft.com>
2023-11-27 10:42:17 +0800

    update (Variant)string.Empty to Variant.Empty

--- src/System.Windows.Forms/src/System/Windows/Forms/TextBox/TextBoxBase.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/TextBox/TextBoxBase.cs
@@ -1584,7 +1584,7 @@ public abstract partial class TextBoxBase : Control
         if (IsAccessibilityObjectCreated)
         {
             AccessibilityObject.RaiseAutomationEvent(UIA_EVENT_ID.UIA_Text_TextChangedEventId);
-            using var textVariant = PasswordProtect ? (VARIANT)string.Empty : (VARIANT)Text;
+            using var textVariant = PasswordProtect ? VARIANT.Empty : (VARIANT)Text;
             AccessibilityObject.RaiseAutomationPropertyChangedEvent(UIA_PROPERTY_ID.UIA_ValueValuePropertyId, textVariant, textVariant);
         }
     }
