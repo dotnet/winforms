75aaf6a23aed5833272c1d597220cae136b5b09f
halgab <24685886+halgab@users.noreply.github.com>
2023-11-28 19:04:35 +0100

    Enable nullability in ResourceCodeDomSerializer.SerializationResourceManager (#9457)
    * null-annotate ResourceCodeDomSerializer.SerializationResourceManager
    
    * spin out ComponentCache to its own file
    
    * review feedback
    
    * apply review feedback

---
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/ComponentCache.ResourceEntry.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.ComponentModel.Design.Serialization;
+
+internal sealed partial class ComponentCache
+{
+    internal readonly struct ResourceEntry(
+        string name,
+        object? value,
+        bool forceInvariant,
+        bool shouldSerializeValue,
+        bool ensureInvariant,
+        PropertyDescriptor? propertyDescriptor,
+        ExpressionContext? expressionContext)
+    {
+        public readonly bool ForceInvariant = forceInvariant;
+        public readonly bool EnsureInvariant = ensureInvariant;
+        public readonly bool ShouldSerializeValue = shouldSerializeValue;
+        public readonly string Name = name;
+        public readonly object? Value = value;
+        public readonly PropertyDescriptor? PropertyDescriptor = propertyDescriptor;
+        public readonly ExpressionContext? ExpressionContext = expressionContext;
+    }
+}
