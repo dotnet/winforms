938d6d649e4b0e24171937fda1bc7d8a884872bf
Loni Tra <lonitra@microsoft.com>
2023-11-29 13:37:45 -0800

    Cherry pick 172405c

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/TreeViewTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/TreeViewTests.cs
@@ -7114,6 +7114,52 @@ public class TreeViewTests
         treeView.TreeViewNodeSorter = treeSorter;
     }
 
+    [WinFormsTheory]
+    [InlineData("A", "B")]
+    [InlineData("A", "A")]
+    [InlineData("B", "A")]
+    public void Clear_AfterSort_ShouldNotGetStuck(string firstNodeText, string secondNodeText)
+    {
+        using TreeView treeView = new();
+        TreeNode parent = new("Parent");
+        treeView.Nodes.Add(parent);
+
+        TreeNode firstNode = new(firstNodeText);
+        parent.Nodes.Add(firstNode);
+        TreeNode secondNode = new(secondNodeText);
+        parent.Nodes.Add(secondNode);
+
+        treeView.Sort();
+
+        Stopwatch timer = new();
+        timer.Start();
+        parent.Nodes.Clear();
+        timer.Stop();
+        Assert.True(timer.ElapsedMilliseconds <= 500);
+    }
+
+    [WinFormsTheory]
+    [InlineData("A", "B")]
+    [InlineData("A", "A")]
+    [InlineData("B", "A")]
+    public void Remove_AfterSort_ShouldNotThrowException(string firstNodeText, string secondNodeText)
+    {
+        using TreeView treeView = new();
+        TreeNode parent = new("Parent");
+        treeView.Nodes.Add(parent);
+
+        TreeNode firstNode = new(firstNodeText);
+        parent.Nodes.Add(firstNode);
+        TreeNode secondNode = new(secondNodeText);
+        parent.Nodes.Add(secondNode);
+
+        treeView.Sort();
+
+        parent.Nodes.Remove(firstNode);
+
+        parent.Nodes.Remove(secondNode);
+    }
+
     private class SubTreeView : TreeView
     {
         public new bool CanEnableIme => base.CanEnableIme;
