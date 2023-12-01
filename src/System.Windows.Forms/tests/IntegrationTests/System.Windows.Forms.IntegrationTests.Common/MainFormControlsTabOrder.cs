ea489b17d4c1188618dc4b7331263cb79bc88463
Ricardo Bossan <ricardobossan@protonmail.com>
2023-12-01 19:06:22 -0300

    Issue 6514 sample renderer for winforms control test (#10404)
    * Makes minimum size for WinformControlTest's Main Form just enough to render 3 columns of buttons
    
    * Adds control test for the WinformsControlsTest, related to https://github.com/dotnet/winforms/issues/6514
    
    ---------
    
    Co-authored-by: Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>

--- src/System.Windows.Forms/tests/IntegrationTests/System.Windows.Forms.IntegrationTests.Common/MainFormControlsTabOrder.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/System.Windows.Forms.IntegrationTests.Common/MainFormControlsTabOrder.cs
@@ -45,5 +45,6 @@ public enum MainFormControlsTabOrder
     ListBoxTestButton,
     PasswordButton,
     ChartControlButton,
-    ToolStripSeparatorPreferredSize
+    ToolStripSeparatorPreferredSize,
+    CustomComCtl32Button
 }
