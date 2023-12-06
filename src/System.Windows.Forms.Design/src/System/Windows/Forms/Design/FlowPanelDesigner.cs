c51b3fa3a7971923a23f3a592056bd23d183b4be
halgab <24685886+halgab@users.noreply.github.com>
2023-12-06 09:47:49 +0100

    rollback changes addressed in other PRs

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/FlowPanelDesigner.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/FlowPanelDesigner.cs
@@ -19,7 +19,7 @@ internal class FlowPanelDesigner : PanelDesigner
             ArrayList snapLines = (ArrayList)base.SnapLines;
 
             // identify all the paddings to remove
-            List<SnapLine> paddingsToRemove = new();
+            ArrayList paddingsToRemove = new ArrayList(4);
             foreach (SnapLine line in snapLines)
             {
                 if (line.Filter is not null && line.Filter.Contains(SnapLine.Padding))
