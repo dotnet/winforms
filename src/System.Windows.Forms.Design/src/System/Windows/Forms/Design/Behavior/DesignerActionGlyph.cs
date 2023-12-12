6048945918cde0d4fe6d3a38cb9c633cd9b269a6
Leaf Shi <v-leafshi@microsoft.com>
2023-12-11 22:11:11 -0800

    Using function "GetIconResourceAsBitmap(Type type, string resource, Size size)" instaed of "GetIconResourceAsBitmap(Type type, string resource, int dpi)" when initial Image "GlyphImageClosed" and "GlyphImageOpened"

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/DesignerActionGlyph.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/DesignerActionGlyph.cs
@@ -15,6 +15,8 @@ internal sealed class DesignerActionGlyph : Glyph
 {
     internal const int CONTROLOVERLAP_X = 5;                    // number of pixels the anchor should be offset to the left of the control's upper-right
     internal const int CONTROLOVERLAP_Y = 2;                    // number of pixels the anchor overlaps the control in the y-direction
+    private const byte IconsWidth = 10;
+    private const byte IconsHeight = 10;
 
     private Rectangle _alternativeBounds = Rectangle.Empty;     // if !empty, this represents the bounds of the tray control this glyph is related to
     private Rectangle _bounds;                                  // the bounds of our glyph
@@ -98,12 +100,12 @@ internal sealed class DesignerActionGlyph : Glyph
     private Image GlyphImageClosed => _glyphImageClosed ??= ScaleHelper.GetIconResourceAsBitmap(
         typeof(DesignerActionGlyph),
         "Close_left",
-        ScaleHelper.InitialSystemDpi);
+        new Size(IconsWidth, IconsHeight));
 
     private Image GlyphImageOpened => _glyphImageOpened ??= ScaleHelper.GetIconResourceAsBitmap(
         typeof(DesignerActionGlyph),
         "Open_left",
-        ScaleHelper.InitialSystemDpi);
+        new Size(IconsWidth, IconsHeight));
 
     internal void InvalidateOwnerLocation()
     {
--- src/System.Windows.Forms.Primitives/src/System/Windows/Forms/Internals/ScaleHelper.cs
+++ src/System.Windows.Forms.Primitives/src/System/Windows/Forms/Internals/ScaleHelper.cs
@@ -332,7 +332,7 @@ internal static partial class ScaleHelper
     ///  Gets the given icon resource as a <see cref="Bitmap"/> scaled to the specified dpi.
     /// </summary>
     internal static Bitmap GetIconResourceAsBitmap(Type type, string resource, int dpi)
-        => GetIconResourceAsBestMatchBitmap(type, resource, ScaleToDpi(SystemIconSize, dpi));
+        => GetIconResourceAsBitmap(type, resource, ScaleToDpi(SystemIconSize, dpi));
 
     /// <summary>
     ///  Gets the given icon resource as a <see cref="Bitmap"/> of the given size.
