153ac08da0f9a7579ad5476e7ec9991d566c24bc
Simon Zhao <v-weidzh@microsoft.com>
2024-01-04 21:19:06 +0800

    Test the path of reportTool

--- eng/MergeCoverageData.ps1
+++ eng/MergeCoverageData.ps1
@@ -8,8 +8,16 @@ $converageToolsPath = Join-Path $artifactsDir "tools"
 if (!(Test-Path -Path $converageToolsPath)) {
   New-Item -Path $converageToolsPath -ItemType Directory
 }
+if(!(Test-Path -Path $(PkgReportGenerator))){
+ echo 'Can not find the path'
+}
+if(Test-Path -Path $(PkgReportGenerator)){
+ $reportTools = Join-Path $converageToolsPath "tools\net47\ReportGenerator.exe"
+  if(Test-Path -Path $reportTools){
+  echo 'Can find the ReportGenerator'
+  }
+}
 dotnet tool install dotnet-reportgenerator-globaltool --tool-path $converageToolsPath
-
 $converageTools = Join-Path $converageToolsPath "reportgenerator.exe"
 Test-Path -Path $converageTools
 $reportCoverage = Join-Path $artifactsDir "bin\*\Debug\*\coverage\*.coverage"
