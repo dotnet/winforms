dotnet/winforms

commit a83ef804bdc6fde593302ea0628c04171b2afac9

Refactor `SnapLines` to use `IList` directly instead of casting to `ArrayList` (#10279)
* Refactor SnapLines to use IList directly instead of casting to ArrayList
Minor refactors

* Refactor Add ListAdapter and use IList<SnapLine>

* Refactor to use IList<SnapLine>

* Add IWrapper and AdapterHelpers and use them throughout

* Fix up missed usages of Unwrap

* Extract out files and add comments

* Fix comment indent

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/ControlDesigner.cs
@@ -356,13 +356,15 @@ public partial class ControlDesigner : ComponentDesigner
     ///  Returns a list of SnapLine objects representing interesting alignment points for this control.
     ///  These SnapLines are used to assist in the positioning of the control on a parent's surface.
     /// </summary>
-    public virtual IList SnapLines => SnapLinesInternal();
+    public virtual IList SnapLines => EdgeAndMarginSnapLines().Unwrap();
 
-    internal IList SnapLinesInternal() => SnapLinesInternal(Control.Margin);
+    internal IList<SnapLine> SnapLinesInternal => SnapLines.Adapt<SnapLine>();
 
-    internal IList SnapLinesInternal(Padding margin)
+    internal IList<SnapLine> EdgeAndMarginSnapLines() => EdgeAndMarginSnapLines(Control.Margin);
+
+    internal IList<SnapLine> EdgeAndMarginSnapLines(Padding margin)
     {
-        ArrayList snapLines = new(4);
+        List<SnapLine> snapLines = new(8);
         int width = Control.Width;
         int height = Control.Height;
 

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/AdapterHelpers.cs
@@ -1,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections;
+
+namespace System.Windows.Forms.Design;
+
+/// <summary>
+///  Helpers for adapting between generic and non-generic lists.
+/// </summary>
+internal static class AdapterHelpers
+{
+    /// <summary>
+    ///  Provides an extension method to safely extract the underlying non-generic <see cref="IList"/>
+    ///  from a wrapped <see cref="IList&lt;T&gt;"/> when available.
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the <see cref="IList&lt;T&gt;"/>.</typeparam>
+    /// <param name="list">The IList&lt;T&gt; to unwrap.</param>
+    /// <returns>The underlying non-generic <see cref="IList"/> if available; otherwise, the original <see cref="IList&lt;T&gt;"/>.</returns>
+    internal static IList Unwrap<T>(this IList<T> list) => list is IWrapper<IList> wrapper ? wrapper.Unwrap() : (IList)list;
+
+    /// <summary>
+    ///  Provides an extension method to adapt a non-generic IList to a generic <see cref="IList&lt;T&gt;"/> by creating
+    ///  a <see cref="ListAdapter&lt;T&gt;"/> when needed.
+    /// </summary>
+    /// <typeparam name="T">The desired type of elements in the resulting <see cref="IList&lt;T&gt;"/>.</typeparam>
+    /// <param name="list">The non-generic <see cref="IList"/> to adapt.</param>
+    /// <returns>
+    /// A generic <see cref="IList&lt;T&gt;"/> if the input IList can be cast to <see cref="IList&lt;T&gt;"/> otherwise, a new <see cref="ListAdapter&lt;T&gt;"/> wrapping the input IList.
+    /// </returns>
+    internal static IList<T> Adapt<T>(this IList list) => list is IList<T> iList ? iList : new ListAdapter<T>(list);
+}

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/BehaviorService.cs
@@ -816,7 +816,7 @@ public sealed partial class BehaviorService : IDisposable
 
             if (host.GetDesigner(comp) is ControlDesigner designer)
             {
-                foreach (SnapLine line in designer.SnapLines)
+                foreach (SnapLine line in designer.SnapLinesInternal)
                 {
                     snapLineInfo.Append($"{line}\tAssociated Control = {designer.Control.Name}:::");
                 }

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/DragAssistanceManager.cs
@@ -130,7 +130,7 @@ internal sealed class DragAssistanceManager
     /// </summary>
     private void AddSnapLines(ControlDesigner controlDesigner, List<SnapLine> horizontalList, List<SnapLine> verticalList, bool isTarget, bool validTarget)
     {
-        IList snapLines = controlDesigner.SnapLines;
+        IList<SnapLine> snapLines = controlDesigner.SnapLinesInternal;
         // Used for padding snaplines
         Rectangle controlRect = controlDesigner.Control.ClientRectangle;
         // Used for all others

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/ButtonBaseDesigner.cs
@@ -49,7 +49,7 @@ internal class ButtonBaseDesigner : ControlDesigner
     {
         get
         {
-            ArrayList snapLines = base.SnapLines as ArrayList;
+            IList<SnapLine> snapLines = SnapLinesInternal;
             FlatStyle flatStyle = FlatStyle.Standard;
             ContentAlignment alignment = ContentAlignment.MiddleCenter;
 
@@ -103,7 +103,7 @@ internal class ButtonBaseDesigner : ControlDesigner
 
             snapLines.Add(new SnapLine(SnapLineType.Baseline, baseline, SnapLinePriority.Medium));
 
-            return snapLines;
+            return snapLines.Unwrap();
         }
     }
 

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/ComboBoxDesigner.cs
@@ -27,14 +27,14 @@ internal class ComboBoxDesigner : ControlDesigner
     {
         get
         {
-            ArrayList snapLines = base.SnapLines as ArrayList;
+            IList<SnapLine> snapLines = SnapLinesInternal;
 
             // a single text-baseline for the label (and linklabel) control
             int baseline = DesignerUtils.GetTextBaseline(Control, Drawing.ContentAlignment.TopLeft);
             baseline += 3;
             snapLines.Add(new SnapLine(SnapLineType.Baseline, baseline, SnapLinePriority.Medium));
 
-            return snapLines;
+            return snapLines.Unwrap();
         }
     }
 

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DateTimePickerDesigner.cs
@@ -21,7 +21,7 @@ internal class DateTimePickerDesigner : ControlDesigner
     {
         get
         {
-            IList snapLines = base.SnapLines;
+            IList<SnapLine> snapLines = SnapLinesInternal;
 
             // A single text-baseline for the label (and linklabel) control.
             int baseline = DesignerUtils.GetTextBaseline(Control, ContentAlignment.MiddleLeft);
@@ -30,7 +30,7 @@ internal class DateTimePickerDesigner : ControlDesigner
             baseline += 2;
             snapLines.Add(new SnapLine(SnapLineType.Baseline, baseline, SnapLinePriority.Medium));
 
-            return snapLines;
+            return snapLines.Unwrap();
         }
     }
 

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/FlowPanelDesigner.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using System.Collections;
 using System.Windows.Forms.Design.Behavior;
 
@@ -16,25 +14,19 @@ internal class FlowPanelDesigner : PanelDesigner
     {
         get
         {
-            ArrayList snapLines = (ArrayList)base.SnapLines;
+            IList<SnapLine> snapLines = SnapLinesInternal;
 
-            // identify all the paddings to remove
-            ArrayList paddingsToRemove = new(4);
-            foreach (SnapLine line in snapLines)
+            // identify and remove all paddings
+            for (int i = snapLines.Count - 1; i >= 0; i--)
             {
-                if (line.Filter is not null && line.Filter.Contains(SnapLine.Padding))
+                if (snapLines[i] is SnapLine line
+                    && line.Filter?.Contains(SnapLine.Padding) == true)
                 {
-                    paddingsToRemove.Add(line);
+                    snapLines.RemoveAt(i);
                 }
             }
 
-            // remove all padding
-            foreach (SnapLine line in paddingsToRemove)
-            {
-                snapLines.Remove(line);
-            }
-
-            return snapLines;
+            return snapLines.Unwrap();
         }
     }
 
@@ -48,7 +40,7 @@ internal class FlowPanelDesigner : PanelDesigner
     {
         base.OnDragDrop(de);
 
-        SelectionManager sm = GetService(typeof(SelectionManager)) as SelectionManager;
+        SelectionManager? sm = GetService(typeof(SelectionManager)) as SelectionManager;
         sm?.Refresh();
     }
 }

commit 1091dcfbb5198b702e080c7dfcc356a73ade43db

Fixes #10580 (stackoverflow in ControlDesigner) caused by #10279

src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/ControlDesigner.cs
@@ -358,7 +358,7 @@ public partial class ControlDesigner : ComponentDesigner
     /// </summary>
     public virtual IList SnapLines => EdgeAndMarginSnapLines().Unwrap();
 
-    internal IList<SnapLine> SnapLinesInternal => SnapLines.Adapt<SnapLine>();
+    internal IList<SnapLine> SnapLinesInternal => EdgeAndMarginSnapLines();
 
     internal IList<SnapLine> EdgeAndMarginSnapLines() => EdgeAndMarginSnapLines(Control.Margin);
 