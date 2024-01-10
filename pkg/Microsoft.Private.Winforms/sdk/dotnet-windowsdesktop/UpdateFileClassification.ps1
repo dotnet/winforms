2c1fe88136d0647e26256aaff0a68c21774aeb39
Loni Tra <lonitra@microsoft.com>
2024-01-10 15:09:33 -0800

    Update UpdateFileClassification.ps1

--- pkg/Microsoft.Private.Winforms/sdk/dotnet-windowsdesktop/UpdateFileClassification.ps1
+++ pkg/Microsoft.Private.Winforms/sdk/dotnet-windowsdesktop/UpdateFileClassification.ps1
@@ -23,7 +23,8 @@ $assemblies = $xmlDoc.package.files.file | `
             ($_.target.StartsWith('lib\') -or $_.target.StartsWith('ref\') -or $_.target.StartsWith('sdk\analyzers\'))`
                 -and $_.target.EndsWith('.dll', [System.StringComparison]::OrdinalIgnoreCase) `
                 -and !$_.target.EndsWith('resources.dll', [System.StringComparison]::OrdinalIgnoreCase) `
-                -and !$_.target.EndsWith('\Accessibility.dll', [System.StringComparison]::OrdinalIgnoreCase)
+                -and !$_.target.EndsWith('\Accessibility.dll', [System.StringComparison]::OrdinalIgnoreCase) `
+                -and !$_.target.EndsWith('\System.Private.Windows.Core.dll', [System.StringComparison]::OrdinalIgnoreCase)
         } | `
     Select-Object -Unique @{Name="Path";Expression={Split-Path $_.target -Leaf}} | `
     Select-Object -ExpandProperty Path;
@@ -74,6 +75,12 @@ else {
             $output += "    <FrameworkListFileClass Include=`"$assembly`" Profile=`"WindowsForms`" />`r`n"
         }
     $output += "  </ItemGroup>
+  <!--
+    Private assemblies should not be referenced.
+  -->
+  <ItemGroup>
+    <IgnoredReference Include=`"System.Private.Windows.Core`" />
+  </ItemGroup>
 </Project>";
     $output | Out-File -FilePath $TargetFile -Encoding utf8 -Force;
 }
