08197d0491077dbd641df4e6801bc0411c4c1450
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2024-01-16 20:57:15 -0300

    Adds tests for AxSystemMonitor

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/AxSystemMonitorTests.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+
+namespace System.Windows.Forms.Tests;
+
+public class AxSystemMonitorTests
+{
+    private readonly Form form;
+    private readonly AxSystemMonitor.AxSystemMonitor control;
+
+    public AxSystemMonitorTests()
+    {
+        form = new Form();
+        control = new AxSystemMonitor.AxSystemMonitor();
+        ((ISupportInitialize)control).BeginInit();
+        form.Controls.Add(control);
+        ((ISupportInitialize)control).EndInit();
+    }
+
+    [WinFormsFact]
+    public void AxSystemMonitor_WhenInitialized_ExpectsProperties()
+    {
+        var properties = TypeDescriptor.GetProperties(control);
+        Assert.NotEmpty(properties);
+
+        var events = TypeDescriptor.GetEvents(control);
+        Assert.NotEmpty(events);
+    }
+
+    [WinFormsFact]
+    public void AxSystemMonitor_WhenInitialized_IsEnabled()
+    {
+        Assert.True(control.Enabled);
+    }
+
+    [WinFormsFact]
+    public void AxSystemMonitor_Counter_ReturnsDefaultCounter()
+    {
+        Assert.Equal(0, control.Counters.Count);
+    }
+}
