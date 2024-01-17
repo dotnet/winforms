bec10f66f62977e697aaa1e0a028f344027c5649
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2024-01-17 19:56:37 -0300

    Clean test

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/AxSystemMonitorTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/AxSystemMonitorTests.cs
@@ -4,37 +4,28 @@ namespace System.Windows.Forms.Tests;
 
 public class AxSystemMonitorTests
 {
-    private readonly Form form;
-    private readonly AxSystemMonitor.AxSystemMonitor control;
+    private readonly Form _form;
+    private readonly AxSystemMonitor.AxSystemMonitor _control;
 
     public AxSystemMonitorTests()
     {
-        form = new Form();
-        control = new AxSystemMonitor.AxSystemMonitor();
-        ((ISupportInitialize)control).BeginInit();
-        form.Controls.Add(control);
-        ((ISupportInitialize)control).EndInit();
+        _form = new Form();
+        _control = new AxSystemMonitor.AxSystemMonitor();
+        ((ISupportInitialize)_control).BeginInit();
+        _form.Controls.Add(_control);
+        ((ISupportInitialize)_control).EndInit();
     }
 
     [WinFormsFact]
     public void AxSystemMonitor_WhenInitialized_ExpectsProperties()
     {
-        var properties = TypeDescriptor.GetProperties(control);
+        var properties = TypeDescriptor.GetProperties(_control);
         Assert.NotEmpty(properties);
 
-        var events = TypeDescriptor.GetEvents(control);
+        var events = TypeDescriptor.GetEvents(_control);
         Assert.NotEmpty(events);
-    }
 
-    [WinFormsFact]
-    public void AxSystemMonitor_WhenInitialized_IsEnabled()
-    {
-        Assert.True(control.Enabled);
-    }
-
-    [WinFormsFact]
-    public void AxSystemMonitor_Counter_ReturnsDefaultCounter()
-    {
-        Assert.Equal(0, control.Counters.Count);
+        Assert.True(_control.Enabled);
+        Assert.Equal(0, _control.Counters.Count);
     }
 }
