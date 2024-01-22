c8e6de0792482384512bb4b0c555155a74223843
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2024-01-22 09:40:41 -0300

    Checks if properties from testing control match with propper assembly

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/AxSystemMonitorTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/AxSystemMonitorTests.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Reflection;
+using Castle.Core.Internal;
 
 namespace System.Windows.Forms.Tests;
 
@@ -27,6 +29,34 @@ public class AxSystemMonitorTests : IDisposable
 
         Assert.True(_control.Enabled);
         Assert.Equal(0, _control.Counters.Count);
+
+        // Finds out desired assembly properties
+        const string ClassName = "AxSystemMonitor.AxSystemMonitor";
+        Assembly assembly = Assembly.GetAssembly(typeof(AxSystemMonitor.AxSystemMonitor));
+        Type classType = assembly.GetType(ClassName);
+        TypeInfo classTypeInfo = classType.GetTypeInfo();
+        var assemblyClassProperties = classTypeInfo.DeclaredProperties;
+        List<string> assemblyClassPropertyNames = new();
+        assemblyClassPropertyNames = assemblyClassProperties.Select(p => p.Name).ToList();
+
+        // Finds out testing control properties that come from the right assembly
+        List<string> testingControlProps = new();
+        for (int i = 0; i < properties.Count; i++)
+        {
+            var property = properties[i];
+            string assemblyNameFromControl = property.ComponentType.AssemblyQualifiedName.Split(", ")[0];
+            if (!assemblyNameFromControl.IsNullOrEmpty() && assemblyNameFromControl == assembly.GetName().Name)
+            {
+                testingControlProps.Add(property.Name);
+            }
+        }
+
+        // Tests if the properties from control and assembly properly match
+        foreach (string testControlProp in testingControlProps)
+        {
+            string assemblyProp = assemblyClassPropertyNames.Where(p => p == testControlProp).First();
+            Assert.Contains(testControlProp, assemblyProp);
+        }
     }
 
     public void Dispose()
