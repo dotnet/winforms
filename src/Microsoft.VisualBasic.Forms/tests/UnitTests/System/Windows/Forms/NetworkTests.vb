8306f0ee33ce17df1b0abc36cac3330ddad0b77f
Paul M Cohen <paul1956@users.noreply.github.com>
2024-01-24 19:38:25 -0800

    Address PR Review by fixing spelling and grammer errors

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/NetworkTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/NetworkTests.vb
@@ -95,7 +95,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
                                                         )
                     Finally
                         Assert.True(Directory.Exists(tmpFilePath))
-                        Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 1)
+                        Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 1)
                         CleanupTempDirectory(tmpFilePath)
                     End Try
                 End Sub
@@ -118,7 +118,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
                                                 )
             Finally
                 Assert.True(Directory.Exists(tmpFilePath))
-                Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 18135)
+                Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 18135)
                 CleanupTempDirectory(tmpFilePath)
             End Try
 
@@ -192,7 +192,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
                                                 "",
                                                 Nothing
                                             )
-            Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 18135)
+            Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 18135)
             CleanupTempDirectory(tmpFilePath)
         End Sub
 
@@ -226,7 +226,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
                                                 100000,
                                                 False
                                             )
-            Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 18135)
+            Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 18135)
             CleanupTempDirectory(tmpFilePath)
         End Sub
 
@@ -255,7 +255,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
                                              100000,
                                              True
                                             )
-            Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 104857600)
+            Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 104857600)
             CleanupTempDirectory(tmpFilePath)
         End Sub
 
@@ -265,7 +265,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
             Dim destinationFilename As String = CreateTempFile(tmpFilePath, -1)
             My.Computer.Network.DownloadFile(New Uri(DownloadFileUrl),
                                              destinationFilename)
-            Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 18135)
+            Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 18135)
             CleanupTempDirectory(tmpFilePath)
         End Sub
 
@@ -277,7 +277,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
                                              destinationFilename,
                                              "TDB",
                                              "TBD")
-            Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 18135)
+            Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 18135)
             CleanupTempDirectory(tmpFilePath)
         End Sub
 
@@ -287,7 +287,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
             Dim destinationFilename As String = CreateTempFile(tmpFilePath, -1)
             My.Computer.Network.DownloadFile(DownloadFileUrl,
                                              destinationFilename)
-            Assert.Equal(ValidateDownLoad(tmpFilePath, destinationFilename), 18135)
+            Assert.Equal(ValidateDownload(tmpFilePath, destinationFilename), 18135)
             CleanupTempDirectory(tmpFilePath)
         End Sub
 
