d483dcf8159d2973b99b3294978f538bb219d226
gpetrou <gipetrou@gmail.com>
2024-01-31 21:48:41 +0200

    Add RadioButtonDesigner tests

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/RadioButtonDesignerTests.cs
@@ -0,0 +1,39 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Design.Tests;
+
+public sealed class RadioButtonDesignerTests
+{
+    [Fact]
+    public void ActionLists_WithDefaultRadioButton_ShouldReturnExpectedCount()
+    {
+        using RadioButtonDesigner radioButtonDesigner = new();
+        using RadioButton radioButton = new();
+        radioButtonDesigner.Initialize(radioButton);
+
+        radioButtonDesigner.ActionLists.Count.Should().Be(0);
+    }
+
+    [Fact]
+    public void SnapLines_WithDefaultRadioButton_ShouldReturnExpectedCount()
+    {
+        using RadioButtonDesigner radioButtonDesigner = new();
+        using RadioButton radioButton = new();
+        radioButtonDesigner.Initialize(radioButton);
+
+        radioButtonDesigner.SnapLines.Count.Should().Be(9);
+    }
+
+    [Fact]
+    public void SelectionRules_WithDefaultRadioButton_ShouldThrowNullReferenceException()
+    {
+        using RadioButtonDesigner radioButtonDesigner = new();
+        using RadioButton radioButton = new();
+        radioButtonDesigner.Initialize(radioButton);
+
+        Action action = () => _ = radioButtonDesigner.SelectionRules;
+
+        action.Should().ThrowExactly<NullReferenceException>();
+    }
+}
