8ffa229122e82ff1f09d6e038784b556c4488194
gpetrou <4172445+gpetrou@users.noreply.github.com>
2024-02-02 19:30:16 +0200

    Add MaskedTextBoxDesigner tests (#10780)

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ComboBoxDesignerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ComboBoxDesignerTests.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-namespace System.Windows.Forms.Design.Tests.System.Windows.Forms.Design;
+namespace System.Windows.Forms.Design.Tests;
 
 public sealed class ComboBoxDesignerTests
 {
