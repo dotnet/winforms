55b38f78f1036427870223c8ef8ab74fca7e45e8
gpetrou <gipetrou@gmail.com>
2024-02-03 17:34:52 +0200

    Add RichTextBoxDesigner tests

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/RichTextBoxDesignerTests.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Design.Tests;
+
+public sealed class RichTextBoxDesignerTests
+{
+    [Fact]
+    public void ActionLists_WithDefaultRichTextBox_ShouldReturnExpectedCount()
+    {
+        using RichTextBoxDesigner richTextBoxDesigner = new();
+        using RichTextBox richTextBox = new();
+        richTextBoxDesigner.Initialize(richTextBox);
+
+        richTextBoxDesigner.ActionLists.Count.Should().Be(1);
+    }
+
+    [Fact]
+    public void SnapLines_WithDefaultRichTextBox_ShouldReturnExpectedCount()
+    {
+        using RichTextBoxDesigner richTextBoxDesigner = new();
+        using RichTextBox richTextBox = new();
+        richTextBoxDesigner.Initialize(richTextBox);
+
+        richTextBoxDesigner.SnapLines.Count.Should().Be(9);
+    }
+}
