52e9fc4151dbc9bd950407cc27e8ac1912be5d4d
gpetrou <4172445+gpetrou@users.noreply.github.com>
2024-02-05 20:14:06 +0200

    Add PictureBoxDesigner tests (#10820)

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/PictureBoxDesignerTests.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Design.Tests;
+
+public sealed class PictureBoxDesignerTests
+{
+    [Fact]
+    public void AutoResizeHandles_WithCtor_ShouldBeTrue()
+    {
+        using PictureBoxDesigner pictureBoxDesigner = new();
+
+        pictureBoxDesigner.AutoResizeHandles.Should().BeTrue();
+    }
+
+    [Fact]
+    public void SelectionRules_WithDefaultPictureBox_ShouldThrowNullReferenceException()
+    {
+        using PictureBoxDesigner pictureBoxDesigner = new();
+        using PictureBox pictureBox = new();
+        pictureBoxDesigner.Initialize(pictureBox);
+
+        Action action = () => _ = pictureBoxDesigner.SelectionRules;
+
+        action.Should().ThrowExactly<NullReferenceException>();
+    }
+
+    [Fact]
+    public void ActionLists_WithDefaultPictureBox_ShouldReturnExpectedCount()
+    {
+        using PictureBoxDesigner pictureBoxDesigner = new();
+        using PictureBox pictureBox = new();
+        pictureBoxDesigner.Initialize(pictureBox);
+
+        pictureBoxDesigner.ActionLists.Count.Should().Be(1);
+    }
+}
