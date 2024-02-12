2fa60da41bc8384c961f237993ee223319efc5c2
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2024-02-12 15:48:03 +1000

    Fix build error

--- src/System.Windows.Forms.Primitives/src/GlobalSuppressions.cs
+++ src/System.Windows.Forms.Primitives/src/GlobalSuppressions.cs
@@ -3,5 +3,4 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-
 [assembly: SuppressMessage("Performance", "CA1815:Override equals and operator equals on value types", Justification = "<Pending>", Scope = "type", Target = "~T:System.Windows.Forms.VisualStyles.TextMetrics")]
