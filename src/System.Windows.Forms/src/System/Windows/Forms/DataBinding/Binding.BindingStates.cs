ea93d8646f71a280879fc58a981064222e3017a9
Lachlan Ennis <2433737+elachlan@users.noreply.github.com>
2024-02-16 03:14:23 +1000

    Refactor `BindingSource` and `CurrencyManager` to use flags enum field instead of individual boolean fields (#10843)
    * Refactor BindingSource and CurrencyManager to use flags enum field instead of individual boolean fields
    
    * Change BindingSourceStates to short
    
    * change flags to use binary constants and refactor to inline field access
    
    * Refactor Binding to use flags enum
    
    * Fix Binding.BindingStates values
    
    * fix build error

---
+++ src/System.Windows.Forms/src/System/Windows/Forms/DataBinding/Binding.BindingStates.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms;
+
+public partial class Binding
+{
+    private enum BindingStates : byte
+    {
+        FormattingEnabled      = 0b0000_0001,
+        Modified               = 0b0000_0010,
+        InSetPropValue         = 0b0000_0100,
+        InPushOrPull           = 0b0000_1000,
+        InOnBindingComplete    = 0b0001_0000,
+        DataSourceNullValueSet = 0b0010_0000
+    }
+}
---
+++ src/System.Windows.Forms/src/System/Windows/Forms/DataBinding/BindingSource.BindingSourceStates.cs
@@ -0,0 +1,24 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms;
+
+public partial class BindingSource
+{
+    [Flags]
+    private enum BindingSourceStates : short
+    {
+        ParentsCurrentItemChanging  = 0b0000_0000_0000_0001,
+        DisposedOrFinalized         = 0b0000_0000_0000_0010,
+        IsBindingList               = 0b0000_0000_0000_0100,
+        ListRaisesItemChangedEvents = 0b0000_0000_0000_1000,
+        ListExtractedFromEnumerable = 0b0000_0000_0001_0000,
+        AllowNewIsSet               = 0b0000_0000_0010_0000,
+        AllowNewSetValue            = 0b0000_0000_0100_0000,
+        Initializing                = 0b0000_0000_1000_0000,
+        NeedToSetList               = 0b0000_0001_0000_0000,
+        RecursionDetectionFlag      = 0b0000_0010_0000_0000,
+        InnerListChanging           = 0b0000_0100_0000_0000,
+        EndingEdit                  = 0b0000_1000_0000_0000,
+    }
+}
