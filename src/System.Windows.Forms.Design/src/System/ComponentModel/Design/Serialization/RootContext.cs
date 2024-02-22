6917f7323b0296f8ab7fe8d577ac9969d1d0e680
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-02-21 20:59:59 -0800

    More usings

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/RootContext.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/RootContext.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.CodeDom;
-using System.Windows.Forms;
 
 namespace System.ComponentModel.Design.Serialization;
 
