0000e45ec7916c6734c5b532c43686d8ce75c7b8
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2024-03-26 22:19:46 -0300

    Adds boilerplate for new ActiveX control tests

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/AxWebBrowserTests.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using AxSHDocVw;
+
+namespace System.Windows.Forms.Tests.System.Windows.Forms;
+public class AxMicrosoftWebBrowserTests
+{
+    private readonly Form _form;
+    private readonly AxMicrosoftWebBrowserTests _control;
+
+   public AxMicrosoftWebBrowserTests()
+    {
+        using Form form = new();
+        using AxWebBrowser control = new();
+        ((ISupportInitialize)control).BeginInit();
+        form.Controls.Add(control);
+        ((ISupportInitialize)control).EndInit();
+    } 
+}
