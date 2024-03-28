8840ce266b0f6f1f0473808bef25c6f4a720bcc9
Paul M Cohen <paul1956@users.noreply.github.com>
2024-03-28 00:11:12 -0700

    Remove from Source files, its in Project file Option Explicit On Option Strict On Option Infer On

--- src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/ApplicationServices/ApplyApplicationDefaultsEventArgs.vb
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/ApplicationServices/ApplyApplicationDefaultsEventArgs.vb
@@ -1,8 +1,6 @@
 ﻿' Licensed to the .NET Foundation under one or more agreements.
 ' The .NET Foundation licenses this file to you under the MIT license.
 
-Option Infer On
-
 Imports System.ComponentModel
 Imports System.Drawing
 Imports System.Runtime.InteropServices
