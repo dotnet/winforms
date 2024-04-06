7fcf0524a2d56196ecf9bf055230197b4498e890
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-06 03:49:54 -0700

    Fix ClipBoard tests to avoid race condition

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/ClipboardProxyTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/ClipboardProxyTests.vb
@@ -12,7 +12,6 @@ Namespace Microsoft.VisualBasic.Forms.Tests
     ''' These are just checking the Proxy functions, the underlaying functions are tested elsewhere
     ''' </summary>
     Partial Public Class ClipboardProxyTests
-        Private ReadOnly _lockObject As New Object
 
         Private Function GetUniqueText() As String
             Return Guid.NewGuid().ToString("D")
@@ -20,71 +19,54 @@ Namespace Microsoft.VisualBasic.Forms.Tests
 
         <WinFormsFact>
         Public Sub Clipboard_ContainsText()
-            SyncLock _lockObject
-                Dim text As String = GetUniqueText()
-                Dim clipboardProxy As New MyServices.ClipboardProxy
-                clipboardProxy.SetText(text)
-                Assert.Equal(text, clipboardProxy.GetText())
-            End SyncLock
-        End Sub
-
-        <WinFormsFact>
-        Public Sub Clipboard_ContainsTextWithFormat()
-            SyncLock _lockObject
-                Dim text As String = GetUniqueText()
-                Dim clipboardProxy As New MyServices.ClipboardProxy
-                clipboardProxy.SetText(text, TextDataFormat.Text)
-                Assert.Equal(text, clipboardProxy.GetText(TextDataFormat.Text))
-            End SyncLock
+            Dim text As String = GetUniqueText()
+            Dim clipboardProxy As New MyServices.ClipboardProxy
+            clipboardProxy.SetText(text)
+            Assert.Equal(text, clipboardProxy.GetText())
+            text = GetUniqueText()
+            clipboardProxy.SetText(text, TextDataFormat.Text)
+            Assert.Equal(text, clipboardProxy.GetText(TextDataFormat.Text))
         End Sub
 
         <WinFormsFact>
         Public Sub Clipboard_GetAudioStream_InvokeMultipleTimes_Success()
-            SyncLock _lockObject
-                Dim clipboardProxy As New MyServices.ClipboardProxy
-                Dim result As Stream = clipboardProxy.GetAudioStream()
-                Assert.Equal(result.Length, clipboardProxy.GetAudioStream().Length)
-            End SyncLock
+            Dim clipboardProxy As New MyServices.ClipboardProxy
+            Dim result As Stream = clipboardProxy.GetAudioStream()
+            Assert.Equal(result.Length, clipboardProxy.GetAudioStream().Length)
         End Sub
 
         <WinFormsFact>
         Public Sub Clipboard_SetAudio_InvokeByteArray_GetReturnsExpected()
-            SyncLock _lockObject
-                Dim audioBytes As Byte() = {1, 2, 3}
-                Dim clipboardProxy As New MyServices.ClipboardProxy
-                clipboardProxy.SetAudio(audioBytes)
-                Assert.True(clipboardProxy.ContainsAudio())
-                Assert.True(clipboardProxy.ContainsData(DataFormats.WaveAudio))
-                Assert.Equal(audioBytes.Length, Assert.IsType(Of MemoryStream)(Assert.IsType(Of MemoryStream)(CType(clipboardProxy.GetAudioStream(), MemoryStream))).Length)
-                Assert.Equal(audioBytes.Length, CType(clipboardProxy.GetData(DataFormats.WaveAudio), MemoryStream).Length)
-            End SyncLock
+            Dim audioBytes As Byte() = {1, 2, 3}
+            Dim clipboardProxy As New MyServices.ClipboardProxy
+            clipboardProxy.SetAudio(audioBytes)
+            Assert.True(clipboardProxy.ContainsAudio())
+            Assert.True(clipboardProxy.ContainsData(DataFormats.WaveAudio))
+            Assert.Equal(audioBytes.Length, Assert.IsType(Of MemoryStream)(Assert.IsType(Of MemoryStream)(CType(clipboardProxy.GetAudioStream(), MemoryStream))).Length)
+            Assert.Equal(audioBytes.Length, CType(clipboardProxy.GetData(DataFormats.WaveAudio), MemoryStream).Length)
         End Sub
 
         <WinFormsFact>
         Public Sub Clipboard_SetAudio_InvokeStream_GetReturnsExpected()
-            SyncLock _lockObject
-                Dim audioBytes() As Byte = {1, 2, 3}
-                Dim clipboardProxy As New MyServices.ClipboardProxy
-                Using audioStream As New MemoryStream(audioBytes)
-                    clipboardProxy.SetAudio(audioStream)
-                    Assert.Equal(audioBytes.Length, Assert.IsType(Of MemoryStream)(clipboardProxy.GetAudioStream()).ToArray().Length)
-                    Assert.Equal(audioBytes.Length, Assert.IsType(Of MemoryStream)(clipboardProxy.GetData(DataFormats.WaveAudio)).ToArray().Length)
-                    Assert.True(clipboardProxy.ContainsAudio())
-                    Assert.True(clipboardProxy.ContainsData(DataFormats.WaveAudio))
-                End Using
-            End SyncLock
+            Dim audioBytes() As Byte = {1, 2, 3}
+            Dim clipboardProxy As New MyServices.ClipboardProxy
+            Using audioStream As New MemoryStream(audioBytes)
+                clipboardProxy.SetAudio(audioStream)
+                Assert.Equal(audioBytes.Length, Assert.IsType(Of MemoryStream)(clipboardProxy.GetAudioStream()).ToArray().Length)
+                Assert.Equal(audioBytes.Length, Assert.IsType(Of MemoryStream)(clipboardProxy.GetData(DataFormats.WaveAudio)).ToArray().Length)
+                Assert.True(clipboardProxy.ContainsAudio())
+                Assert.True(clipboardProxy.ContainsData(DataFormats.WaveAudio))
+            End Using
         End Sub
 
         <WinFormsFact>
         Public Sub Clipboard_SetFileDropList_Invoke_GetReturnsExpected()
-            SyncLock _lockObject
-                Dim clipboardProxy As New MyServices.ClipboardProxy
-                Dim filePaths As New StringCollection From
+            Dim clipboardProxy As New MyServices.ClipboardProxy
+            Dim filePaths As New StringCollection From
                 {"filePath", "filePath2"}
-                clipboardProxy.SetFileDropList(filePaths)
-                Assert.True(clipboardProxy.ContainsFileDropList())
-                Assert.Equal(filePaths, clipboardProxy.GetFileDropList())
-            End SyncLock
+            clipboardProxy.SetFileDropList(filePaths)
+            Assert.True(clipboardProxy.ContainsFileDropList())
+            Assert.Equal(filePaths, clipboardProxy.GetFileDropList())
         End Sub
 
     End Class
