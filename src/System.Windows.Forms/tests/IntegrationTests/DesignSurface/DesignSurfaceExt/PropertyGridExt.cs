36671d064e7a5487a19352876c2364fcccf722c6
Epica3055 <v-epicawang@microsoft.com>
2024-04-09 15:26:45 +0800

    fix issue 10579: The selected icon for Icon property cannot be refreshed in time when adding the icon by using "Choose Icon..." command line until clicked any where in the propertyGide Window

--- src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DesignSurfaceExt/PropertyGridExt.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DesignSurfaceExt/PropertyGridExt.cs
@@ -2,13 +2,40 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.ComponentModel.Design;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace DesignSurfaceExt;
 
 public class PropertyGridExt : PropertyGrid
 {
-    public IDesignerHost DesignerHost { get; set; }
+    private IDesignerHost _host;
+    private IComponentChangeService _componentChangeService;
+    public IDesignerHost DesignerHost
+    {
+        get
+        {
+            return _host;
+        }
+        set
+        {
+            if (value is not null)
+            {
+                _componentChangeService ??= (IComponentChangeService)value.GetService(typeof(IComponentChangeService));
+                if (_componentChangeService is not null)
+                {
+                    _componentChangeService.ComponentChanged += (sender, e) =>
+                    {
+                        MethodInfo methodInfo = typeof(PropertyGrid).GetMethod("OnComponentChanged", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                        methodInfo.Invoke(this, [sender, e]);
+                    };
+                }
+            }
+
+            _host = value;
+        }
+    }
 
     protected override void OnSelectedObjectsChanged(EventArgs e) => base.OnSelectedObjectsChanged(e);
 
