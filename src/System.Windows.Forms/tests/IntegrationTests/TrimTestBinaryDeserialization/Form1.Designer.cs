9313f05e84328c3176eb7d80497c9505cd8aaebf
Loni Tra <lonitra@microsoft.com>
2024-04-09 15:37:50 -0700

    Add New Trim Project (#11182)
    add trim project for another scenario

---
+++ src/System.Windows.Forms/tests/IntegrationTests/TrimTestBinaryDeserialization/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace TrimTestBinaryDeserialization
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            ListViewItem listViewItem1 = new ListViewItem("This is a ListViewItem with an image", 0);
+            imageList1 = new ImageList(components);
+            listView1 = new ListView();
+            SuspendLayout();
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "dotnet.png");
+            // 
+            // listView1
+            // 
+            listView1.Items.AddRange(new ListViewItem[] { listViewItem1 });
+            listView1.LargeImageList = imageList1;
+            listView1.Location = new Point(0, 0);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(269, 139);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Tile;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listView1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ImageList imageList1;
+        private ListView listView1;
+    }
+}
