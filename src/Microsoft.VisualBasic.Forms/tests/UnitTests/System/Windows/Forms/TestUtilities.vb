35981e091ab79e9e234e0a1b05ccad13c495b9c1
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-08 23:27:39 -0700

    Update TestUtilities

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/TestUtilities.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/TestUtilities.vb
@@ -4,6 +4,13 @@
 Namespace Microsoft.VisualBasic.Forms.Tests
     Friend Module TestUtilities
 
+        Friend Function GetUniqueInteger(positiveOnly As Boolean) As Integer
+            If positiveOnly Then
+                Return Math.Abs(Guid.NewGuid().GetHashCode())
+            End If
+            Return Guid.NewGuid().GetHashCode()
+        End Function
+
         Friend Function GetUniqueText() As String
             Return Guid.NewGuid().ToString("D")
         End Function
