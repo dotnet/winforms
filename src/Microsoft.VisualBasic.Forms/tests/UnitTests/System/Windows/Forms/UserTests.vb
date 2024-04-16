44ed0ef5003fbbdd9ec848159e9827dfc800c920
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-16 02:19:45 -0700

    Improve User Testing

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/UserTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/UserTests.vb
@@ -1,7 +1,8 @@
 ﻿' Licensed to the .NET Foundation under one or more agreements.
 ' The .NET Foundation licenses this file to you under the MIT license.
 
-Imports System.Threading
+Imports Microsoft.VisualBasic.ApplicationServices
+
 Imports Xunit
 
 Namespace Microsoft.VisualBasic.Forms.Tests
@@ -9,20 +10,52 @@ Namespace Microsoft.VisualBasic.Forms.Tests
     Partial Public Class UserTests
 
         <WinFormsFact>
+        Public Sub UserBaseTest()
+            Dim testUser As New User
+            Assert.Null(testUser.CurrentPrincipal)
+            Dim userPrincipal As UserPrincipal = New UserPrincipal(
+                            authenticationType:="Basic",
+                            name:="Test",
+                            isAuthenticated:=True,
+                            role:="Guest")
+            testUser.CurrentPrincipal = userPrincipal
+            Assert.NotNull(testUser.CurrentPrincipal)
+            Assert.Equal(userPrincipal, testUser.CurrentPrincipal)
+        End Sub
+
+        <WinFormsFact>
         Public Sub UserGetNameTest()
-            Dim testUser As New ProxyUser("Basic", "Test", True, "Guest")
+            Dim testUser As New User With {
+                            .CurrentPrincipal = New UserPrincipal(
+                            authenticationType:="Basic",
+                            name:="Test",
+                            isAuthenticated:=True,
+                            role:="Guest")
+            }
             Assert.Equal("Test", testUser.Name)
         End Sub
 
         <WinFormsFact>
         Public Sub UserIsAuthenticatedTest()
-            Dim testUser As New ProxyUser("Basic", "Test", True, "Guest")
+            Dim testUser As New User With {
+                            .CurrentPrincipal = New UserPrincipal(
+                            authenticationType:="Basic",
+                            name:="Test",
+                            isAuthenticated:=True,
+                            role:="Guest")
+            }
             Assert.Equal(True, testUser.IsAuthenticated)
         End Sub
 
         <WinFormsFact>
         Public Sub UserIsInRoleTest()
-            Dim testUser As New ProxyUser("Basic", "Test", True, "Guest")
+            Dim testUser As New User With {
+                            .CurrentPrincipal = New UserPrincipal(
+                            authenticationType:="Basic",
+                            name:="Test",
+                            isAuthenticated:=True,
+                            role:="Guest")
+            }
             Assert.True(testUser.IsInRole("Guest"))
             Assert.False(testUser.IsInRole("Basic"))
         End Sub
