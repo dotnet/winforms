a7cd9230f5a1ff677a5fff3fe5ac652dce3c7e83
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-15 18:05:15 -0700

    Add Ping Tests

--- src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/Devices/NetworkTests.cs
+++ src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/Devices/NetworkTests.cs
@@ -13,13 +13,47 @@ public class NetworkTests
     }
 
     [Fact]
-    public void Ping()
+    public void Ping_ShortTimeout_Success()
+    {
+        Network network = new();
+        Assert.True(network.Ping("127.0.0.1", 1));
+    }
+
+    [Fact]
+    public void Ping_Success()
     {
         Network network = new();
         Assert.True(network.Ping("127.0.0.1"));
     }
 
+    [Fact]
+    public void Ping_Throw()
+    {
+        Network network = new();
+        Assert.Throws<ArgumentNullException>(() => network.Ping((string)null));
+    }
+
+    [Fact]
+    public void PingUri_ShortTimeout_Success()
+    {
+        Network network = new();
+        Assert.True(network.Ping(new Uri("http://127.0.0.1"), 1));
+    }
+
+    [Fact]
+    public void PingUri_Success()
+    {
+        Network network = new();
+        Assert.True(network.Ping(new Uri("http://127.0.0.1")));
+    }
+
+    [Fact]
+    public void PingUri_Throw()
+    {
+        Network network = new();
+        Assert.Throws<ArgumentNullException>(() => network.Ping((Uri)null));
+    }
+
     // Not tested:
-    //    Public Sub DownloadFile(...) [multiple overloads]
     //    Public Sub UploadFile(...) [multiple overloads]
 }
