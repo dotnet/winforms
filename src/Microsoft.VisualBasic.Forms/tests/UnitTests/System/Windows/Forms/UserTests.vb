7c45150cb799a6dade3c4622cf06121c276178dd
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-16 02:20:44 -0700

    Improve User Testing

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/UserTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/UserTests.vb
@@ -10,6 +10,20 @@ Namespace Microsoft.VisualBasic.Forms.Tests
     Partial Public Class UserTests
 
         <WinFormsFact>
+        Public Sub UserBaseTest()
+            Dim testUser As New User
+            Assert.Null(testUser.CurrentPrincipal)
+            Dim userPrincipal As UserPrincipal = New UserPrincipal(
+                            authenticationType:="Basic",
+                            name:="Test",
+                            isAuthenticated:=True,
+                            role:="Guest")
+            testUser.CurrentPrincipal = userPrincipal
+            Assert.NotNull(testUser.CurrentPrincipal)
+            Assert.Equal(userPrincipal, testUser.CurrentPrincipal)
+        End Sub
+
+        <WinFormsFact>
         Public Sub UserGetNameTest()
             Dim testUser As New User With {
                             .CurrentPrincipal = New UserPrincipal(
