f5a0e72345e6b3db0e42ed2ce58721e51a6ac62e
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-17 16:53:26 -0700

    Add ComputerInfoDebugView Tests

---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/ComputerInfoDebugViewTests.vb
@@ -0,0 +1,32 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports Microsoft.VisualBasic.Devices
+Imports Microsoft.VisualBasic.Devices.ComputerInfo
+Imports Xunit
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+
+    Public Class ComputerInfoDebugViewTests
+
+        <Fact>
+        Public Sub Memory()
+            Dim info As New ComputerInfoDebugView(New ComputerInfo)
+            If PlatformDetection.IsWindows Then
+                Assert.NotEqual(0UI, info.AvailablePhysicalMemory)
+                Assert.NotEqual(0UI, info.AvailableVirtualMemory)
+                Assert.NotEqual(0UI, info.TotalPhysicalMemory)
+                Assert.NotEqual(0UI, info.TotalVirtualMemory)
+            End If
+        End Sub
+
+        <Fact>
+        Public Sub Properties()
+            Dim info As New ComputerInfoDebugView(New ComputerInfo)
+            Assert.Equal(Globalization.CultureInfo.InstalledUICulture, info.InstalledUICulture)
+            Assert.Equal(Environment.OSVersion.Platform.ToString(), info.OSPlatform)
+            Assert.Equal(Environment.OSVersion.Version.ToString(), info.OSVersion)
+        End Sub
+
+    End Class
+End Namespace
