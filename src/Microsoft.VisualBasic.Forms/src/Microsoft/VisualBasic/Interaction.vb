61061cb5815db698a1559fadd330c4a8834ba275
Paul M Cohen <paul1956@users.noreply.github.com>
2024-04-22 20:54:16 -0700

    Move InputBoxHandler to separate file

--- src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/Interaction.vb
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/Interaction.vb
@@ -236,46 +236,6 @@ Namespace Microsoft.VisualBasic
             NativeMethods.AttachThreadInput(0, SafeNativeMethods.GetWindowThreadProcessId(hwndApp, dwDummy), 0)
         End Sub
 
-        Friend NotInheritable Class InputBoxHandler
-            Private ReadOnly _prompt As String
-            Private ReadOnly _title As String
-            Private ReadOnly _defaultResponse As String
-            Private ReadOnly _xPos As Integer
-            Private ReadOnly _yPos As Integer
-            Private _result As String
-            Private ReadOnly _parentWindow As IWin32Window
-            Private _exception As Exception
-
-            Public Sub New(Prompt As String, Title As String, DefaultResponse As String, XPos As Integer, YPos As Integer, ParentWindow As IWin32Window)
-                _prompt = Prompt
-                _title = Title
-                _defaultResponse = DefaultResponse
-                _xPos = XPos
-                _yPos = YPos
-                _parentWindow = ParentWindow
-            End Sub
-
-            Public Sub StartHere()
-                Try
-                    _result = InternalInputBox(_prompt, _title, _defaultResponse, _xPos, _yPos, _parentWindow)
-                Catch ex As Exception
-                    _exception = ex
-                End Try
-            End Sub
-
-            Public ReadOnly Property Result() As String
-                Get
-                    Return _result
-                End Get
-            End Property
-
-            Friend ReadOnly Property Exception As Exception
-                Get
-                    Return _exception
-                End Get
-            End Property
-        End Class
-
         Public Function InputBox(Prompt As String, Title As String, DefaultResponse As String, XPos As Integer, YPos As Integer) As String
             Dim vbHost As IVbHost
             Dim parentWindow As IWin32Window = Nothing
