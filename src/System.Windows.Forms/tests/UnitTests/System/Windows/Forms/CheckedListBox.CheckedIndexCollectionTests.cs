5ea3dd602ab428d3dd20418e9076082173f50903
Olina-Zhang <v-olzhan@microsoft.com>
2024-05-09 08:10:11 +0000

    Handle feedback

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/CheckedListBox.CheckedIndexCollectionTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/CheckedListBox.CheckedIndexCollectionTests.cs
@@ -5,8 +5,23 @@ using System.Collections;
 
 namespace System.Windows.Forms.Tests;
 
-public class CheckedListBox_CheckedIndexCollectionTests
+public class CheckedListBox_CheckedIndexCollectionTests: IDisposable
 {
+    private readonly CheckedListBox _checkedListBox;
+    private readonly CheckedListBox.CheckedIndexCollection _collection;
+
+    public CheckedListBox_CheckedIndexCollectionTests()
+    {
+        _checkedListBox = new();
+        _collection = new(_checkedListBox);
+    }
+
+    public void Dispose()
+    {
+        _checkedListBox.Items.Clear();
+        _checkedListBox.Dispose();
+    }
+
     [WinFormsFact]
     public void CheckedIndexCollection_Ctor_OwnerIsNull_ThrowsArgumentNullException()
     {
@@ -16,100 +31,95 @@ public class CheckedListBox_CheckedIndexCollectionTests
     [WinFormsFact]
     public void CheckedIndexCollection_Properties_ReturnExpected()
     {
-        using CheckedListBox checkedListBox = new();
-        CheckedListBox.CheckedIndexCollection collection = new(checkedListBox);
-
         // Test properties: Count, SyncRoot, IsSynchronized, IsFixedSize, IsReadOnly
-        collection.Count.Should().Be(0);
-        ((ICollection)collection).SyncRoot.Should().BeSameAs(collection);
-        ((ICollection)collection).IsSynchronized.Should().BeFalse();
-        ((IList)collection).IsFixedSize.Should().BeTrue();
-        collection.IsReadOnly.Should().BeTrue();
+        _collection.Count.Should().Be(0);
+        ((ICollection)_collection).SyncRoot.Should().BeSameAs(_collection);
+        ((ICollection)_collection).IsSynchronized.Should().BeFalse();
+        ((IList)_collection).IsFixedSize.Should().BeTrue();
+        _collection.IsReadOnly.Should().BeTrue();
     }
 
     [WinFormsFact]
     public void CheckedIndexCollection_Methods_ThrowNotSupportedException()
     {
-        using CheckedListBox checkedListBox = new();
-        CheckedListBox.CheckedIndexCollection collection = new(checkedListBox);
-
         // Test methods: Add, Clear, Insert, Remove, RemoveAt that should throw NotSupportedException
-        ((Action)(() => ((IList)collection)[0] = 1)).Should().Throw<NotSupportedException>();
-        ((IList)collection).Invoking(c => c.Add(1)).Should().Throw<NotSupportedException>();
-        ((IList)collection).Invoking(c => c.Clear()).Should().Throw<NotSupportedException>();
-        ((IList)collection).Invoking(c => c.Insert(0, 1)).Should().Throw<NotSupportedException>();
-        ((IList)collection).Invoking(c => c.Remove(1)).Should().Throw<NotSupportedException>();
-        ((IList)collection).Invoking(c => c.RemoveAt(0)).Should().Throw<NotSupportedException>();
+        ((Action)(() => ((IList)_collection)[0] = 1)).Should().Throw<NotSupportedException>();
+        ((IList)_collection).Invoking(c => c.Add(1)).Should().Throw<NotSupportedException>();
+        ((IList)_collection).Invoking(c => c.Clear()).Should().Throw<NotSupportedException>();
+        ((IList)_collection).Invoking(c => c.Insert(0, 1)).Should().Throw<NotSupportedException>();
+        ((IList)_collection).Invoking(c => c.Remove(1)).Should().Throw<NotSupportedException>();
+        ((IList)_collection).Invoking(c => c.RemoveAt(0)).Should().Throw<NotSupportedException>();
     }
 
-    [WinFormsFact]
-    public void CheckedIndexCollection_Contains_ReturnsExpected()
+    [WinFormsTheory]
+    [InlineData([new string[] { "item1", "item2" }, new bool[] { true, false }, 0, true])]
+    [InlineData([new string[] { "item1", "item2" }, new bool[] { true, false }, 1, false])]
+    [InlineData([new string[] { "item1", "item2" }, new bool[] { false, false }, 0, false])]
+    public void CheckedIndexCollection_Contains_ReturnsExpected(string[] items, bool[] checkedStatus, int index, bool expected)
     {
-        using CheckedListBox checkedListBox = new();
-        CheckedListBox.CheckedIndexCollection collection = new(checkedListBox);
-        checkedListBox.Items.Add("item1", true);
-        checkedListBox.Items.Add("item2", false);
+        for (int i = 0; i < items.Length; i++)
+        {
+            _checkedListBox.Items.Add(items[i], checkedStatus[i]);
+        }
 
         // Test Contains method
-        collection.Contains(0).Should().BeTrue();
-        collection.Contains(1).Should().BeFalse();
-        ((IList)collection).Contains(0).Should().BeTrue();
-        ((IList)collection).Contains(1).Should().BeFalse();
-        ((IList)collection).Contains("invalid").Should().BeFalse();
+        _collection.Contains(index).Should().Be(expected);
+        ((IList)_collection).Contains(index).Should().Be(expected);
     }
 
-    [WinFormsFact]
-    public void CheckedIndexCollection_IndexOf_ReturnsExpected()
+    [WinFormsTheory]
+    [InlineData(true, 0)]
+    [InlineData(false, -1)]
+    public void CheckedIndexCollection_IndexOf_ReturnsExpected(bool isChecked, int expectedIndex)
     {
-        using CheckedListBox checkedListBox = new();
-        CheckedListBox.CheckedIndexCollection collection = new(checkedListBox);
-        checkedListBox.Items.Add("item1", true);
-        checkedListBox.Items.Add("item2", false);
+        _checkedListBox.Items.Add("item1", isChecked);
 
         // Test IndexOf method
-        collection.IndexOf(0).Should().Be(0);
-        collection.IndexOf(1).Should().Be(-1);
-        ((IList)collection).IndexOf(0).Should().Be(0);
-        ((IList)collection).IndexOf(1).Should().Be(-1);
-        ((IList)collection).IndexOf("invalid").Should().Be(-1);
+        _collection.IndexOf(0).Should().Be(expectedIndex);
+        ((IList)_collection).IndexOf(0).Should().Be(expectedIndex);
+        ((IList)_collection).IndexOf("invalid").Should().Be(-1);
     }
 
-    [WinFormsFact]
-    public void CheckedIndexCollection_CopyTo_CopiesExpectedValues()
+    [WinFormsTheory]
+    [InlineData([new string[] { "item1", "item2", "item3" }, new bool[] { true, false, true }, new int[] { 0, 2, 0 }])]
+    [InlineData([new string[] { "item1", "item2" }, new bool[] { true, true }, new int[] { 0, 1 }])]
+    [InlineData([new string[] { "item1", "item2", "item3" }, new bool[] { false, false, false }, new int[] { 0, 0, 0 }])]
+    public void CheckedIndexCollection_CopyTo_CopiesExpectedValues(string[] items, bool[] checkedStatus, int[] expected)
     {
-        using CheckedListBox checkedListBox = new();
-        CheckedListBox.CheckedIndexCollection collection = new(checkedListBox);
-        checkedListBox.Items.Add("item1", true);
-        checkedListBox.Items.Add("item2", false);
-        checkedListBox.Items.Add("item3", true);
+        for (int i = 0; i < items.Length; i++)
+        {
+            _checkedListBox.Items.Add(items[i], checkedStatus[i]);
+        }
 
         // Create an array to copy to
-        int[] array = new int[3];
+        int[] array = new int[items.Length];
 
         // Copy the collection to the array
-        collection.CopyTo(array, 0);
+        _collection.CopyTo(array, 0);
 
         // Test that the copied values are correct
-        array[0].Should().Be(0); // Index of first checked item
-        array[1].Should().Be(2); // Index of second checked item
-        array[2].Should().Be(0); // Should remain 0 as there are only two checked items
+        array.Should().Equal(expected);
     }
 
-    [WinFormsFact]
-    public void CheckedIndexCollection_GetEnumerator_ReturnsExpected()
+    [WinFormsTheory]
+    [InlineData([new string[] { "item1", "item2", "item3" }, new bool[] { true, false, true }, new int[] { 0, 2 }])]
+    [InlineData([new string[] { "item1", "item2" }, new bool[] { true, true }, new int[] { 0, 1 }])]
+    [InlineData([new string[] { "item1", "item2", "item3" }, new bool[] { false, false, false }, new int[0]])]
+    public void CheckedIndexCollection_GetEnumerator_ReturnsExpected(string[] items, bool[] checkedStatus, int[] expected)
     {
-        using CheckedListBox checkedListBox = new();
-        CheckedListBox.CheckedIndexCollection collection = new(checkedListBox);
-        checkedListBox.Items.Add("item1", true);
-        checkedListBox.Items.Add("item2", false);
-        checkedListBox.Items.Add("item3", true);
+        for (int i = 0; i < items.Length; i++)
+        {
+            _checkedListBox.Items.Add(items[i], checkedStatus[i]);
+        }
 
         // Test GetEnumerator method
-        IEnumerator enumerator = collection.GetEnumerator();
-        enumerator.MoveNext().Should().BeTrue();
-        enumerator.Current.Should().Be(0);
-        enumerator.MoveNext().Should().BeTrue();
-        enumerator.Current.Should().Be(2);
+        IEnumerator enumerator = _collection.GetEnumerator();
+        foreach (int expectedIndex in expected)
+        {
+            enumerator.MoveNext().Should().BeTrue();
+            enumerator.Current.Should().Be(expectedIndex);
+        }
+
         enumerator.MoveNext().Should().BeFalse();
     }
 }
