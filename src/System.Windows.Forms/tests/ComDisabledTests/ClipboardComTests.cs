aa6177871546030140d82c1e0ebcf80f9291e0d9
Tanya Solyanik <tanyaso@microsoft.com>
2024-06-05 10:49:52 -0700

    Run Clipboard test sequentialy, this is not eliminating potential interference, but is an improvement in my observations, the next step would be to add a Clear in the try/finally block in every test, as I didn't figure out how to put the test class constructor into an STA. enabled nullability in Clipboard ests Used fluent assertions, particularly BeEquivalentTo was helpful to make some tests more specific. Several test names didn't match the content due to a copy/paste mistake

--- src/System.Windows.Forms/tests/ComDisabledTests/ClipboardComTests.cs
+++ src/System.Windows.Forms/tests/ComDisabledTests/ClipboardComTests.cs
@@ -1,26 +1,31 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System.Drawing;
 
 namespace System.Windows.Forms.Tests;
 
+[Collection("Sequential")]
 public partial class ClipboardTests
 {
     [WinFormsFact]
     public void Clipboard_SetText_InvokeString_GetReturnsExpected()
     {
         Clipboard.SetText("text");
-        Assert.Equal("text", Clipboard.GetText());
-        Assert.True(Clipboard.ContainsText());
+
+        Clipboard.GetText().Should().Be("text");
+        Clipboard.ContainsText().Should().BeTrue();
     }
 
     [WinFormsFact]
-    public void ClipBoard_SetData_CustomFormat_Color()
+    public void Clipboard_SetData_CustomFormat_Color()
     {
-        string format = nameof(ClipBoard_SetData_CustomFormat_Color);
+        string format = nameof(Clipboard_SetData_CustomFormat_Color);
         Clipboard.SetData(format, Color.Black);
-        Assert.True(Clipboard.ContainsData(format));
-        Assert.Equal(Color.Black, Clipboard.GetData(format));
+
+        Clipboard.ContainsData(format).Should().BeTrue();
+        Clipboard.GetData(format).Should().Be(Color.Black);
     }
 }
