65da424ad6f5af158dc51e64c556e88fa550b084
Olina-Zhang <v-olzhan@microsoft.com>
2024-06-13 07:28:29 +0000

    Update Accessibility test application

--- src/System.Windows.Forms/tests/AccessibilityTests/ToolStripContainer.cs
+++ src/System.Windows.Forms/tests/AccessibilityTests/ToolStripContainer.cs
@@ -10,5 +10,8 @@ public partial class ToolStripContainer : Form
     public ToolStripContainer()
     {
         InitializeComponent();
+        toolStripContainer2.TopToolStripPanel.TabStop = true;
+        toolStripContainer2.TopToolStripPanel.TabIndex = 0;
+        toolStripContainer2.ContentPanel.TabIndex = 1;
     }
 }
