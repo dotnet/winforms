af83b4a830afcbe7911f9db6ff3f5bf7ccb18f31
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2024-06-15 18:02:16 -0700

    Add basic test infra for CSharp Analyzers and CodeFixes.

---
+++ src/System.Windows.Forms.Analyzers.CSharp/tests/UnitTests/System/Windows/Forms/Analyzers/ControlPropertySerializationDiagnosticAnalyzerTest/ControlPropertySerializationDiagnosticAnalyzerTest.cs
@@ -0,0 +1,58 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+using VerifyCS = System.Windows.Forms.Analyzers.Tests
+    .CSharpCodeFixVerifier<
+        System.Windows.Forms.CSharp.Analyzers.ControlPropertySerialization.ControlPropertySerializationDiagnosticAnalyzer,
+        System.Windows.Forms.CSharp.CodeFixes.AddDesignerSerializationVisibility.AddDesignerSerializationVisibilityCodeFixProvider>;
+
+namespace System.Windows.Forms.Analyzers.CSharp.Tests;
+
+public class ControlPropertySerializationDiagnosticAnalyzer
+{
+    private const string ProblematicCode = """
+
+        namespace CSharpControls;
+
+        public class ScalableControlNoSerializationConfiguration : Control
+        {
+            public float ScaleFactor { get; set; } = 1.0f;
+
+            public SizeF ScaledSize { get; set; }
+
+            public PointF ScaledLocation { get; set; }
+        }
+
+        """;
+
+    private const string FixedCode = """
+
+        using System.ComponentModel;
+
+        namespace CSharpControls;
+
+        public class ScalableControlResolved : Control
+        {
+            [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+            public float ScaleFactor { get; set; } = 1.0f;
+
+            [DefaultValue(typeof(SizeF), "0,0")]
+            public SizeF ScaledSize { get; set; }
+
+            public PointF ScaledLocation { get; set; }
+            private bool ShouldSerializeScaledSize() => this.ScaledSize != SizeF.Empty;
+        }
+        
+        """;
+
+    [Fact]
+    public async Task Test_CSharp_AddDesignerSerializationVisibility()
+    {
+        var expected = VerifyCS.Diagnostic(nameof(ControlPropertySerializationDiagnosticAnalyzer));
+
+        await VerifyCS
+            .VerifyCodeFixAsync(ProblematicCode, expected, FixedCode);
+    }
+}
