317c08f1d1845c2f37c295f50333a11083aae2e5
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-07-12 13:46:54 -0700

    Fix usings

--- src/System.Private.Windows.Core/tests/System.Private.Windows.Core.Tests/GlobalUsings.cs
+++ src/System.Private.Windows.Core/tests/System.Private.Windows.Core.Tests/GlobalUsings.cs
@@ -1,9 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-global using Windows.Win32;
-global using Windows.Win32.Foundation;
-global using Windows.Win32.Graphics.Gdi;
-global using Windows.Win32.UI.WindowsAndMessaging;
 global using Xunit;
+#pragma warning disable IDE0005 // Using directive is unnecessary.
 global using FluentAssertions;
+#pragma warning restore IDE0005 // Using directive is unnecessary.
