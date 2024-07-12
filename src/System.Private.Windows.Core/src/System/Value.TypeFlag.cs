cdcdf0201a20042fc81b74b1fa0e0a017298a700
Jeremy Kuhne <jkuhne@microsoft.com>
2024-07-12 14:47:18 -0700

    Adds Value type (#11666)
    This gives us the ability to store primitives without boxing/unboxing them.
    
    Replacing PropertyStore's internals with this will greatly simplify it's implementation and handle far more than just int.

---
+++ src/System.Private.Windows.Core/src/System/Value.TypeFlag.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+
+namespace System;
+
+internal readonly partial struct Value
+{
+    /// <summary>
+    ///  A flag that represents the <see cref="System.Type"/> of a <see cref="Union"/> in a <see cref="Value"/>.
+    ///  Also provides the functionality to convert any <see cref="Value"/> to an <see langword="object"/> that
+    ///  already isn't an <see langword="object"/>.
+    /// </summary>
+    private abstract class TypeFlag
+    {
+        public abstract Type Type
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+        }
+
+        /// <summary>
+        ///  Converts the given <see cref="Value"/> to an <see langword="object"/>.
+        /// </summary>
+        public abstract object ToObject(in Value value);
+    }
+
+    /// <summary>
+    ///  Strongly typed <see cref="TypeFlag"/>.
+    /// </summary>
+    private abstract class TypeFlag<T> : TypeFlag
+    {
+        public sealed override Type Type
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => typeof(T);
+        }
+
+        public override object ToObject(in Value value) => To(value)!;
+        public abstract T To(in Value value);
+    }
+}
---
+++ src/System.Private.Windows.Core/tests/System.Private.Windows.Core.Tests/System/Value/MemoryWatch.cs
@@ -0,0 +1,92 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.ValueTests;
+
+public ref struct MemoryWatch
+{
+    private static bool s_jit;
+    private long _allocations;
+
+    public static void JIT()
+    {
+        if (s_jit)
+        {
+            return;
+        }
+
+        // JITing allocates, so make sure we've got all of our <T> methods created.
+
+        Value.Create((bool)default).GetValue<bool>();
+        Value.Create((byte)default).GetValue<byte>();
+        Value.Create((sbyte)default).GetValue<sbyte>();
+        Value.Create((char)default).GetValue<char>();
+        Value.Create((double)default).GetValue<double>();
+        Value.Create((short)default).GetValue<short>();
+        Value.Create((int)default).GetValue<int>();
+        Value.Create((long)default).GetValue<long>();
+        Value.Create((ushort)default).GetValue<ushort>();
+        Value.Create((uint)default).GetValue<uint>();
+        Value.Create((ulong)default).GetValue<ulong>();
+        Value.Create((float)default).GetValue<float>();
+        Value.Create((double)default).GetValue<double>();
+        Value.Create((DateTime)default).GetValue<DateTime>();
+        Value.Create((DateTimeOffset)default).GetValue<DateTimeOffset>();
+
+        Value.Create((bool?)default).GetValue<bool?>();
+        Value.Create((byte?)default).GetValue<byte?>();
+        Value.Create((sbyte?)default).GetValue<sbyte?>();
+        Value.Create((char?)default).GetValue<char?>();
+        Value.Create((double?)default).GetValue<double?>();
+        Value.Create((short?)default).GetValue<short?>();
+        Value.Create((int?)default).GetValue<int?>();
+        Value.Create((long?)default).GetValue<long?>();
+        Value.Create((ushort?)default).GetValue<ushort?>();
+        Value.Create((uint?)default).GetValue<uint?>();
+        Value.Create((ulong?)default).GetValue<ulong?>();
+        Value.Create((float?)default).GetValue<float?>();
+        Value.Create((double?)default).GetValue<double?>();
+        Value.Create((DateTime?)default).GetValue<DateTime?>();
+        Value.Create((DateTimeOffset?)default).GetValue<DateTimeOffset?>();
+
+        Value value = default;
+        value.TryGetValue(out bool _);
+        value.TryGetValue(out byte _);
+        value.TryGetValue(out sbyte _);
+        value.TryGetValue(out char _);
+        value.TryGetValue(out double _);
+        value.TryGetValue(out short _);
+        value.TryGetValue(out int _);
+        value.TryGetValue(out long _);
+        value.TryGetValue(out ushort _);
+        value.TryGetValue(out uint _);
+        value.TryGetValue(out ulong _);
+        value.TryGetValue(out float _);
+        value.TryGetValue(out double _);
+        value.TryGetValue(out DateTime _);
+        value.TryGetValue(out DateTimeOffset _);
+
+        s_jit = true;
+    }
+
+    public MemoryWatch(long allocations) => _allocations = allocations;
+
+    public static MemoryWatch Create
+    {
+        get
+        {
+            JIT();
+            return new(GC.GetAllocatedBytesForCurrentThread());
+        }
+    }
+
+    public void Dispose() => Validate();
+
+    public void Validate()
+    {
+        Assert.Equal(0, GC.GetAllocatedBytesForCurrentThread() - _allocations);
+
+        // Assert.Equal allocates
+        _allocations = GC.GetAllocatedBytesForCurrentThread();
+    }
+}
