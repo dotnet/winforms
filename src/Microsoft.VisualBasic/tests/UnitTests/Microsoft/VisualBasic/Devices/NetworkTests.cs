68fc7732ac109a77fce4d56cdcd4eb4573dbb949
Paul M Cohen <paul1956@users.noreply.github.com>
2024-07-16 16:56:15 -0700

    Fix C# tests to be Fluent

--- src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/Devices/NetworkTests.cs
+++ src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/Devices/NetworkTests.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using FluentAssertions;
+
 namespace Microsoft.VisualBasic.Devices.Tests;
 
 public class NetworkTests
@@ -16,14 +18,14 @@ public class NetworkTests
     public void Ping_ShortTimeout_Success()
     {
         Network network = new();
-        Assert.True(network.Ping("127.0.0.1", 1));
+        network.Ping("127.0.0.1", 1).Should().BeTrue();
     }
 
     [Fact]
     public void Ping_Success()
     {
         Network network = new();
-        Assert.True(network.Ping("127.0.0.1"));
+        network.Ping("127.0.0.1").Should().BeTrue();
     }
 
     [Fact]
@@ -37,28 +39,30 @@ public class NetworkTests
     public void PingUri_ShortTimeout_Success()
     {
         Network network = new();
-        Assert.True(network.Ping(new Uri("http://127.0.0.1"), 1));
+        network.Ping(new Uri("http://127.0.0.1"), 1).Should().BeTrue();
     }
 
     [Fact]
     public void PingUri_Success()
     {
         Network network = new();
-        Assert.True(network.Ping(new Uri("http://127.0.0.1")));
+        network.Ping(new Uri("http://127.0.0.1")).Should().BeTrue();
     }
 
     [Fact]
     public void PingUri_Throw()
     {
         Network network = new();
-        Assert.Throws<ArgumentNullException>(() => network.Ping((Uri)null));
+        Action action = () => network.Ping((Uri)null);
+        action.Should().Throw<ArgumentNullException>();
     }
 
     [Fact]
     public void PingUriTimeout_Throw()
     {
         Network network = new();
-        Assert.Throws<ArgumentNullException>(() => network.Ping((Uri)null, 1));
+        Action action = () => network.Ping((Uri)null, 1);
+        action.Should().Throw<ArgumentNullException>();
     }
 
     // Not tested:
