56b3487577893997aeef6a2be5f4ab184155ca6f
Paul M Cohen <paul1956@users.noreply.github.com>
2024-07-20 01:30:27 -0700

    Cleanup NativeMethids Split NativeTypes

---
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/Helpers/NativeTypes.LateInitSafeHandleZeroOrMinusOneIsInvalid.vb
@@ -0,0 +1,31 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports Microsoft.Win32.SafeHandles
+
+Namespace Microsoft.VisualBasic.CompilerServices
+    Partial Friend NotInheritable Class NativeTypes
+        ''' <summary>
+        '''  Inherits <see cref="SafeHandleZeroOrMinusOneIsInvalid"/>, with additional InitialSetHandle method.
+        '''  This is required because call to constructor of SafeHandle is not allowed in constrained region.
+        ''' </summary>
+        Friend NotInheritable Class LateInitSafeHandleZeroOrMinusOneIsInvalid
+            Inherits SafeHandleZeroOrMinusOneIsInvalid
+
+            Friend Sub New()
+                MyBase.New(True)
+            End Sub
+
+            Friend Sub InitialSetHandle(h As IntPtr)
+                Debug.Assert(IsInvalid, "Safe handle should only be set once.")
+                SetHandle(h)
+            End Sub
+
+            Protected Overrides Function ReleaseHandle() As Boolean
+                Return NativeMethods.CloseHandle(handle) <> 0
+            End Function
+
+        End Class
+
+    End Class
+End Namespace
