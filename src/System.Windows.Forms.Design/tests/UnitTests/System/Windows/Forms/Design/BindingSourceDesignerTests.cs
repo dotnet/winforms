c2daba11cac15beb8f43c08a5c1ad920d71b7065
Olina-Zhang <v-olzhan@microsoft.com>
2024-07-30 10:06:00 +0000

    Add unit test for BindingSourceDesigner.cs file

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/BindingSourceDesignerTests.cs
@@ -0,0 +1,59 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using Moq;
+using System.ComponentModel.Design;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class BindingSourceDesignerTests
+{
+    [Fact]
+    public void BindingUpdatedByUser_SetValue_ShouldUpdateField()
+    {
+        BindingSourceDesigner designer = new()
+        {
+            BindingUpdatedByUser = true
+        };
+
+        FieldInfo? fieldInfo = designer.GetType().GetField("_bindingUpdatedByUser", BindingFlags.NonPublic | BindingFlags.Instance);
+        fieldInfo.Should().NotBeNull();
+
+        object? fieldValue = fieldInfo?.GetValue(designer);
+
+        fieldValue.Should().NotBeNull();
+        fieldValue.Should().Be(true);
+    }
+
+    [Fact]
+    public void Initialize_ShouldSubscribeToComponentChangeServiceEvents()
+    {
+        Mock<IComponentChangeService> componentChangeServiceMock = new();
+        Mock<IServiceProvider> serviceProviderMock = new();
+        serviceProviderMock.Setup(sp => sp.GetService(typeof(IComponentChangeService))).Returns(componentChangeServiceMock.Object);
+
+        Mock<IComponent> componentMock = new();
+        Mock<ISite> siteMock = new();
+        siteMock.Setup(s => s.GetService(typeof(IComponentChangeService))).Returns(componentChangeServiceMock.Object);
+        componentMock.Setup(c => c.Site).Returns(siteMock.Object);
+
+        BindingSource bindingSource = new() { DataSource = componentMock.Object };
+        BindingSourceDesigner designer = new();
+        designer.Initialize(bindingSource);
+
+        ComponentEventArgs args = new(componentMock.Object);
+
+        MethodInfo? onComponentRemovingMethod = typeof(BindingSourceDesigner).GetMethod("OnComponentRemoving", BindingFlags.NonPublic | BindingFlags.Instance);
+        onComponentRemovingMethod?.Invoke(designer, [null, args]);
+
+        bindingSource.DataSource.Should().BeNull();
+    }
+}
+
+
+
+
