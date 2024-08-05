71f7d1300aaf4142cc16e1e1cac90782155af4d3
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-04 18:49:19 -0700

    Remove unkneeded Import

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/NntiveTypesTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/NntiveTypesTests.vb
@@ -2,7 +2,6 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 
 Imports System.Runtime.InteropServices
-Imports System.Text
 Imports FluentAssertions
 Imports Microsoft.VisualBasic.CompilerServices.NativeTypes
 Imports Xunit
