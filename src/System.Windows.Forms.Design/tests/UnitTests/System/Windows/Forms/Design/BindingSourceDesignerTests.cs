2bd379f69f0784592e75e544ba8c126dd87c8640
Olina-Zhang <v-olzhan@microsoft.com>
2024-08-12 08:38:58 +0000

    update

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/BindingSourceDesignerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/BindingSourceDesignerTests.cs
@@ -15,7 +15,7 @@ public class BindingSourceDesignerTests
     [Fact]
     public void BindingUpdatedByUser_SetValue_ShouldUpdateField()
     {
-        BindingSourceDesigner designer = new()
+        using BindingSourceDesigner designer = new()
         {
             BindingUpdatedByUser = true
         };
@@ -41,8 +41,8 @@ public class BindingSourceDesignerTests
         siteMock.Setup(s => s.GetService(typeof(IComponentChangeService))).Returns(componentChangeServiceMock.Object);
         componentMock.Setup(c => c.Site).Returns(siteMock.Object);
 
-        BindingSource bindingSource = new() { DataSource = componentMock.Object };
-        BindingSourceDesigner designer = new();
+        using BindingSource bindingSource = new() { DataSource = componentMock.Object };
+        using BindingSourceDesigner designer = new();
         designer.Initialize(bindingSource);
 
         ComponentEventArgs args = new(componentMock.Object);
