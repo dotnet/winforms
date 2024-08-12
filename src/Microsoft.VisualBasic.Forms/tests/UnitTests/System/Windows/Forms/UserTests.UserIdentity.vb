f12da23b6a72bc6db29fdfaba9f86a7c93af844b
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-12 16:32:41 -0700

    Merge changes from VB-Runtime-TestsOnly-Review-First

---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/UserTests.UserIdentity.vb
@@ -0,0 +1,42 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports System.Security.Principal
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+
+    Partial Public Class UserTests
+
+        Public Class UserIdentity
+            Implements IIdentity
+
+            Private ReadOnly _authenticationType As String
+            Private ReadOnly _name As String
+
+            Public Sub New(authenticationType As String, name As String, isAuthenticated As Boolean)
+                _authenticationType = authenticationType
+                _name = name
+            End Sub
+
+            Public ReadOnly Property AuthenticationType As String Implements IIdentity.AuthenticationType
+                Get
+                    Return _authenticationType
+                End Get
+            End Property
+
+            Public ReadOnly Property IsAuthenticated As Boolean Implements IIdentity.IsAuthenticated
+                Get
+                    Return True
+                End Get
+            End Property
+
+            Public ReadOnly Property Name As String Implements IIdentity.Name
+                Get
+                    Return _name
+                End Get
+            End Property
+
+        End Class
+
+    End Class
+End Namespace
