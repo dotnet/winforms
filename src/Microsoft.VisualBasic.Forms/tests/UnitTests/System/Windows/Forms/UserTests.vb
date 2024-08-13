43bb645df1d36515c611d5d46aacc25ee12bdb73
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-13 00:22:04 -0700

    Recreate VB tests only based on Features 10

---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/UserTests.vb
@@ -0,0 +1,65 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports FluentAssertions
+Imports Microsoft.VisualBasic.ApplicationServices
+Imports Xunit
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+
+    Partial Public Class UserTests
+
+        <WinFormsFact>
+        Public Sub UserBaseTest()
+            Dim testUser As New User
+            testUser.CurrentPrincipal.Should.BeNull()
+            Dim userPrincipal As New UserPrincipal(
+                authenticationType:="Basic",
+                name:="Test",
+                isAuthenticated:=True,
+                role:="Guest")
+            testUser.CurrentPrincipal = userPrincipal
+            testUser.CurrentPrincipal.Should.NotBeNull()
+            testUser.CurrentPrincipal.Should.Be(userPrincipal)
+            userPrincipal.Identity.AuthenticationType.Should.Be("Basic")
+        End Sub
+
+        <WinFormsFact>
+        Public Sub UserGetNameTest()
+            Dim testUser As New User With {
+                .CurrentPrincipal = New UserPrincipal(
+                authenticationType:="Basic",
+                name:="Test",
+                isAuthenticated:=True,
+                role:="Guest")
+            }
+            testUser.Name.Should.Be("Test")
+        End Sub
+
+        <WinFormsFact>
+        Public Sub UserIsAuthenticatedTest()
+            Dim testUser As New User With {
+                .CurrentPrincipal = New UserPrincipal(
+                authenticationType:="Basic",
+                name:="Test",
+                isAuthenticated:=True,
+                role:="Guest")
+            }
+            testUser.IsAuthenticated.Should.BeTrue()
+        End Sub
+
+        <WinFormsFact>
+        Public Sub UserIsInRoleTest()
+            Dim testUser As New User With {
+                .CurrentPrincipal = New UserPrincipal(
+                authenticationType:="Basic",
+                name:="Test",
+                isAuthenticated:=True,
+                role:="Guest")
+            }
+            testUser.IsInRole("Guest").Should.BeTrue()
+            testUser.IsInRole("Basic").Should.BeFalse()
+        End Sub
+
+    End Class
+End Namespace
