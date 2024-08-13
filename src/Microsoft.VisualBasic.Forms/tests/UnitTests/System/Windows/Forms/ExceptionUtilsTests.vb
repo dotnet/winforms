875198e9b868f22cbce8c3e4987bc39d983fb57c
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-13 03:27:04 -0700

    Add Missing Tests from previous PR

---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/ExceptionUtilsTests.vb
@@ -0,0 +1,82 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports FluentAssertions
+Imports Microsoft.VisualBasic.CompilerServices
+Imports Xunit
+
+Imports ExUtils = Microsoft.VisualBasic.CompilerServices.ExceptionUtils
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+
+    ''' <summary>
+    '''  These are just checking the Proxy functions, the underlying functions are tested elsewhere.
+    ''' </summary>
+    Public Class ExceptionUtilsTests
+
+        <WinFormsFact>
+        Public Sub GetArgumentNullExceptionTest_Succeed()
+            Const ArgumentName As String = "MainForm"
+            Dim ex As Exception = ExUtils.GetArgumentNullException(
+                ArgumentName,
+                resourceID:=SR.General_PropertyNothing,
+                ArgumentName)
+            ex.Should.BeOfType(Of ArgumentNullException)()
+            CType(ex, ArgumentNullException).ParamName.Should.Be(ArgumentName)
+            ex.Message.Should.Be($"Property MainForm cannot be set to Nothing. (Parameter '{ArgumentName}')")
+        End Sub
+
+        <WinFormsFact>
+        Public Sub GetDirectoryNotFoundExceptionTest_Succeed()
+            Dim resourceString As String = ExUtils.GetResourceString(VbErrors.FileNotFound)
+            Dim ex As Exception = ExUtils.GetDirectoryNotFoundException(resourceString)
+            ex.Should.BeOfType(Of IO.DirectoryNotFoundException)()
+            ex.Message.Should.Be("File not found.")
+        End Sub
+
+        <WinFormsFact>
+        Public Sub GetFileNotFoundExceptionTest_Succeed()
+            Dim resourceString As String = ExUtils.GetResourceString(VbErrors.FileNotFound)
+            Dim ex As Exception = ExUtils.GetFileNotFoundException("Test", resourceString)
+            ex.Should.BeOfType(Of IO.FileNotFoundException)()
+            ex.Message.Should.Be("File not found.")
+            CType(ex, IO.FileNotFoundException).FileName.Should.Be("Test")
+        End Sub
+
+        <WinFormsFact>
+        Public Sub GetInvalidOperationExceptionTest_Succeed()
+            ExUtils.GetInvalidOperationException(SR.Mouse_NoMouseIsPresent).Should.BeOfType(Of InvalidOperationException)()
+        End Sub
+
+        <WinFormsFact>
+        Public Sub GetIOExceptionTest_Succeed()
+            Dim ex As Exception = ExUtils.GetIOException(SR.IO_FileExists_Path, IO.Path.GetTempPath)
+            ex.Should.BeOfType(Of IO.IOException)()
+            ex.Message.Should.Be(
+                $"Could not complete operation since a file already exists in this path '{IO.Path.GetTempPath}'.")
+        End Sub
+
+        <WinFormsFact>
+        Public Sub GetWin32ExceptionTest_Succeed()
+            Dim ex As Exception = ExUtils.GetWin32Exception(SR.DiagnosticInfo_Memory)
+            ex.Should.BeOfType(Of ComponentModel.Win32Exception)()
+        End Sub
+
+        <WinFormsTheory>
+        <InlineData(-1)>
+        <InlineData(0)>
+        Public Sub VbMakeExceptionInvalidValuesTest_Succeed(BadResourceId As Integer)
+            Dim id As String = $"ID{BadResourceId}"
+            VbMakeException(BadResourceId).Message.Should.Be($"{SR.GetResourceString(id, id)}")
+        End Sub
+
+        <WinFormsTheory>
+        <InlineData(VbErrors.FileNotFound, "File not found.")>
+        <InlineData(VbErrors.PermissionDenied, "Permission denied.")>
+        <InlineData(VbErrors.None, "")>
+        Public Sub VbMakeExceptionTest_Succeed(errorCode As Integer, expected As String)
+            ExUtils.VbMakeException(errorCode).Message.Should.Be(expected)
+        End Sub
+
+    End Class
+End Namespace
---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/SingleInstanceHelpersTests.vb
@@ -0,0 +1,75 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports System.IO.Pipes
+Imports System.Runtime.CompilerServices
+Imports System.Threading
+Imports FluentAssertions
+Imports Microsoft.VisualBasic.ApplicationServices
+
+Imports Xunit
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+
+    Public Class SingleInstanceHelpersTests
+        Private _resultArgs As String()
+
+        Private Sub OnStartupNextInstanceMarshallingAdaptor(args As String())
+            If args.Length = 1 Then
+                _resultArgs = {"Hello"}
+            End If
+        End Sub
+
+        <WinFormsFact>
+        Public Sub TryCreatePipeServerTests()
+            Dim pipeName As String = GetUniqueText()
+            Dim pipeServer As NamedPipeServerStream = Nothing
+            TryCreatePipeServer(pipeName, pipeServer).Should.BeTrue()
+            pipeServer.CanRead.Should.BeTrue()
+            pipeServer.CanSeek.Should.BeFalse()
+            pipeServer.CanWrite.Should.BeFalse()
+            pipeServer.TransmissionMode.Should.Be(PipeTransmissionMode.Byte)
+            pipeServer.Close()
+            pipeServer.Dispose()
+        End Sub
+
+        <WinFormsFact>
+        Public Sub TryCreatePipeServerTwiceTests_Fail()
+            Dim pipeName As String = GetUniqueText()
+            Dim pipeServer As NamedPipeServerStream = Nothing
+            TryCreatePipeServer(pipeName, pipeServer).Should.BeTrue()
+            Dim pipeServer1 As NamedPipeServerStream = Nothing
+            TryCreatePipeServer(pipeName, pipeServer1).Should.BeFalse()
+            pipeServer1.Should.BeNull()
+            pipeServer.Close()
+            pipeServer.Dispose()
+        End Sub
+
+        <WinFormsFact>
+        Public Async Function WaitForClientConnectionsAsyncTests() As Task
+            Dim pipeName As String = GetUniqueText()
+            Dim pipeServer As NamedPipeServerStream = Nothing
+            If TryCreatePipeServer(pipeName, pipeServer) Then
+
+                Using pipeServer
+                    Dim tokenSource As New CancellationTokenSource()
+                    Dim clientConnection As Task = WaitForClientConnectionsAsync(pipeServer, AddressOf OnStartupNextInstanceMarshallingAdaptor, cancellationToken:=tokenSource.Token)
+                    Dim commandLine As String() = {"Hello"}
+                    Dim awaitable As ConfiguredTaskAwaitable = SendSecondInstanceArgsAsync(pipeName, commandLine, cancellationToken:=tokenSource.Token).ConfigureAwait(False)
+                    awaitable.GetAwaiter().GetResult()
+                    Dim CancelToken As New CancellationToken
+                    Dim buffer As Byte() = New Byte(commandLine.Length) {}
+
+                    Dim count As Integer = Await pipeServer.ReadAsync(buffer.AsMemory(0, commandLine.Length))
+                    ' Ensure the result is set
+                    Do
+                        Await Task.Delay(5)
+                    Loop Until _resultArgs IsNot Nothing
+                    _resultArgs(0).Should.Be("Hello")
+                    Await tokenSource.CancelAsync()
+                End Using
+            End If
+        End Function
+
+    End Class
+End Namespace
