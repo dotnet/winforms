1b02a76487304a03ce036f01897e840a6cbea674
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-24 16:17:27 -0700

    Cleanup Imports in ExceptionUtilsTests

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/ExceptionUtilsTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/ExceptionUtilsTests.vb
@@ -2,7 +2,6 @@
 ' The .NET Foundation licenses this file to you under the MIT license.
 
 Imports FluentAssertions
-Imports Microsoft.VisualBasic.CompilerServices
 Imports Xunit
 
 Imports VbUtils = Microsoft.VisualBasic.CompilerServices.ExceptionUtils
