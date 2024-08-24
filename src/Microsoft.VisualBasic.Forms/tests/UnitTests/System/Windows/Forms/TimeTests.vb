8d1e1b460c160f48fb3023fe6a70dac908881a74
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-24 14:41:41 -0700

    Fix broken Time Tests

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/TimeTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/TimeTests.vb
@@ -24,44 +24,32 @@ Namespace Microsoft.VisualBasic.Forms.Tests
         End Sub
 
         <WinFormsFact>
-        Public Sub TimeTestsDataIteratorTests()
-            Dim testClass As New TimeData
-            testClass.IEnumerable_GetEnumerator.Should.NotBeNull()
-        End Sub
-
-        <WinFormsFact>
         Public Sub VbTickCountCloseToEnvironmentTickCount()
             Dim tickCount As Integer = Math.Abs(Environment.TickCount - My.Computer.Clock.TickCount)
             Call (tickCount < PrecisionTickLimit).Should.BeTrue()
         End Sub
 
-        <WinFormsTheory>
-        <ClassData(GetType(TimeData))>
-        Public Sub VbTimeCloseToDateUtcNow(systemTime As Date, vbTime As Date)
+        <WinFormsFact>
+        Public Sub VbTimeCloseToSystemTime()
+            Dim systemTime As Date = Date.UtcNow
+            Dim vbTime As Date = My.Computer.Clock.GmtTime
             TimesEqual(
-            vbTime,
-            systemTime,
-            acceptableDifferenceInTicks:=PrecisionTickLimit).Should.BeTrue()
+                vbTime,
+                systemTime,
+                acceptableDifferenceInTicks:=PrecisionTickLimit).Should.BeTrue()
+
+            systemTime = Date.Now
+            vbTime = My.Computer.Clock.LocalTime
+            TimesEqual(
+                vbTime,
+                systemTime,
+                acceptableDifferenceInTicks:=PrecisionTickLimit).Should.BeTrue()
         End Sub
 
         <WinFormsFact>
         Public Sub VbTimeNotNew()
             My.Computer.Clock.LocalTime.Should.BeAfter(New Date)
         End Sub
-
-        Protected Friend Class TimeData
-            Implements IEnumerable(Of Object())
-
-            Public Iterator Function GetEnumerator() As IEnumerator(Of Object()) Implements IEnumerable(Of Object()).GetEnumerator
-                Yield {My.Computer.Clock.GmtTime, Date.UtcNow}
-                Yield {My.Computer.Clock.LocalTime, Date.Now}
-            End Function
-
-            Public Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-                Return GetEnumerator()
-            End Function
-
-        End Class
 
     End Class
 End Namespace
