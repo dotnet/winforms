1d10243272de2a77930b50dfffb02632bb69cf3f
Mohmed abdel-fattah <memoarfaa@gmail.com>
2024-08-25 18:32:57 +0300

    Add Dark Mode theme to rows/columns and ColumnHeader/RowHeader in the DataGridView

--- src/System.Windows.Forms/System/Windows/Forms/Controls/DataGridView/DataGridViewCheckBoxCell.cs
+++ src/System.Windows.Forms/System/Windows/Forms/Controls/DataGridView/DataGridViewCheckBoxCell.cs
@@ -20,7 +20,12 @@ public partial class DataGridViewCheckBoxCell : DataGridViewCell, IDataGridViewE
     private const DataGridViewContentAlignment AnyBottom = DataGridViewContentAlignment.BottomRight | DataGridViewContentAlignment.BottomCenter | DataGridViewContentAlignment.BottomLeft;
     private const DataGridViewContentAlignment AnyMiddle = DataGridViewContentAlignment.MiddleRight | DataGridViewContentAlignment.MiddleCenter | DataGridViewContentAlignment.MiddleLeft;
 
-    private static readonly VisualStyleElement s_checkBoxElement = VisualStyleElement.Button.CheckBox.UncheckedNormal;
+#pragma warning disable WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+    private static readonly VisualStyleElement s_darkCheckBoxElement = VisualStyleElement.CreateElement("DarkMode_Explorer::BUTTON", 3, 1);
+    private static readonly VisualStyleElement s_lightCheckBoxElement = VisualStyleElement.Button.CheckBox.UncheckedNormal;
+    private static readonly VisualStyleElement s_checkBoxElement = Application.IsDarkModeEnabled ? s_darkCheckBoxElement : s_lightCheckBoxElement;
+#pragma warning restore WFO5001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
     private static readonly int s_propButtonCellState = PropertyStore.CreateKey();
     private static readonly int s_propTrueValue = PropertyStore.CreateKey();
     private static readonly int s_propFalseValue = PropertyStore.CreateKey();
