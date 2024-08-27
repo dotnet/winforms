a4c830a254cd76f722e39516579ddf5f780e2c2a
Paul M Cohen <paul1956@users.noreply.github.com>
2024-08-27 02:42:22 -0700

    Rename ControlTests.vb to  ControlInvokeTests.vb

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/AssemblyPublicKeyIsAsExpectedTest.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/AssemblyPublicKeyIsAsExpectedTest.vb
@@ -15,7 +15,7 @@ Namespace Microsoft.VisualBasic.Forms.Tests
         ''' </summary>
         <WinFormsFact>
         Public Sub AssemblyPublicKeyIsAsExpected()
-            Dim publicKey() As Byte = GetType(ControlTests).Assembly.GetName().GetPublicKey()
+            Dim publicKey() As Byte = GetType(ControlInvokeTests).Assembly.GetName().GetPublicKey()
             Dim sb As New StringBuilder
             For Each [byte] As Byte In publicKey
                 sb.AppendFormat("{0:x2}", [byte])
