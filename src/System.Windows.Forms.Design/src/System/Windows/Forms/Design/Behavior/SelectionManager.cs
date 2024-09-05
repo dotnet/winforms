044531aee27a693e81ade361cdb60974f331a4aa
Paul M Cohen <paul1956@users.noreply.github.com>
2024-09-04 22:44:11 -0700

    Update src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/SelectionManager.cs
    Co-authored-by: Tanya Solyanik <tanyaso@microsoft.com>

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/SelectionManager.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/Behavior/SelectionManager.cs
@@ -36,7 +36,7 @@ internal sealed class SelectionManager : IDisposable
 
     /// <summary>
     ///  Here we query for necessary services and cache them for performance reasons.
-    ///  We also hook to Component Added/Removed/Changed notifications so we can keep in sync when the designers'
+    ///  We also hook to <see cref="Component" /> Added/Removed/Changed notifications so we can keep in sync when the designers'
     ///  components change. Also, we create our custom Adorner and add it to the BehaviorService.
     /// </summary>
     public SelectionManager(IServiceProvider serviceProvider, BehaviorService behaviorService)
