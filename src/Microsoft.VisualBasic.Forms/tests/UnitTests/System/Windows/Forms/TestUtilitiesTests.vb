7616b3faa66c51e34496c0636605dae2d8bed662
Paul M Cohen <paul1956@users.noreply.github.com>
2024-09-05 18:44:25 -0700

    Fix brken test

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/TestUtilitiesTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/TestUtilitiesTests.vb
@@ -15,10 +15,14 @@ Namespace Microsoft.VisualBasic.Forms.Tests
         End Sub
 
         <WinFormsFact>
-        Public Sub WrongPasswordTestDataIteratorTests()
-            Dim testClass As New WrongPasswordTestData
         Public Sub TimeTestDataIteratorTests()
             Dim testClass As New TimeTestData
+            testClass.IEnumerable_GetEnumerator.Should.NotBeNull()
+        End Sub
+
+        <WinFormsFact>
+        Public Sub WrongPasswordTestDataIteratorTests()
+            Dim testClass As New WrongPasswordTestData
             testClass.IEnumerable_GetEnumerator.Should.NotBeNull()
         End Sub
 
