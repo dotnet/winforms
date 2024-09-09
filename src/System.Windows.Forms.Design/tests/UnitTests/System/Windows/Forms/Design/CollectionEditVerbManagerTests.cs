985626a0aa0f911920bf4512140c600dacac2384
Leaf Shi (BEYONDSOFT CONSULTING INC) <v-leafshi@microsoft.com>
2024-09-09 18:26:33 +0800

    Add test for CollectionEditVerbManager.cs

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/CollectionEditVerbManagerTests.cs
@@ -0,0 +1,38 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.ComponentModel;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class CollectionEditVerbManagerTests
+{
+    public static TheoryData<string?, string> ValuesTheoryData()
+        => new()
+        {
+            { null, SR.ToolStripItemCollectionEditorVerb },
+            { "TestText", "TestText" }
+        };
+
+    [Theory]
+    [MemberData(nameof(ValuesTheoryData))]
+    public void CollectionEditVerbManager_EditItemsVerb_ReturnExpectedValue(string? text, string expectedValue)
+    {
+        using ToolStripButton toolStripButton = new();
+        ToolStripItemDesigner toolStripItemDesigner = new();
+        toolStripItemDesigner.Initialize(toolStripButton);
+
+        PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(ToolStripButton));
+        PropertyDescriptor? propertyDescriptor = properties["Text"];
+
+        CollectionEditVerbManager editor = new(text, toolStripItemDesigner, propertyDescriptor, true);
+        editor.EditItemsVerb.Text.Should().Be(expectedValue);
+        editor.EditItemsVerb.Enabled.Should().BeTrue();
+        editor.EditItemsVerb.Supported.Should().BeTrue();
+        editor.EditItemsVerb.Visible.Should().BeTrue();
+        editor.EditItemsVerb.CommandID!.ID.Should().Be(8192);
+    }
+}
+
