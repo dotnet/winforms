c26f018258c4a702a759464674164c7f7ab9481e
Leaf Shi (BEYONDSOFT CONSULTING INC) <v-leafshi@microsoft.com>
2024-09-10 10:24:35 +0800

    Update the code format and Assert

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/CollectionEditVerbManagerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/CollectionEditVerbManagerTests.cs
@@ -4,35 +4,35 @@
 #nullable enable
 
 using System.ComponentModel;
+using System.ComponentModel.Design;
 
 namespace System.Windows.Forms.Design.Tests;
 
 public class CollectionEditVerbManagerTests
 {
-    public static TheoryData<string?, string> ValuesTheoryData()
-        => new()
-        {
-            { null, SR.ToolStripItemCollectionEditorVerb },
-            { "TestText", "TestText" }
-        };
+    public static TheoryData<string?, string> ValuesTheoryData() => new()
+    {
+        { null, SR.ToolStripItemCollectionEditorVerb },
+        { "TestText", "TestText" }
+    };
 
     [Theory]
     [MemberData(nameof(ValuesTheoryData))]
-    public void CollectionEditVerbManager_EditItemsVerb_ReturnExpectedValue(string? text, string expectedValue)
+    public void CollectionEditVerbManager_EditItemsVerb_ReturnExpectedValue(string? text, string expected)
     {
         using ToolStripButton toolStripButton = new();
-        ToolStripItemDesigner toolStripItemDesigner = new();
+        using ToolStripItemDesigner toolStripItemDesigner = new();
         toolStripItemDesigner.Initialize(toolStripButton);
 
         PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(ToolStripButton));
-        PropertyDescriptor? propertyDescriptor = properties["Text"];
+        PropertyDescriptor? propertyDescriptor = properties[nameof(ToolStripButton.Text)];
 
-        CollectionEditVerbManager editor = new(text, toolStripItemDesigner, propertyDescriptor, true);
-        editor.EditItemsVerb.Text.Should().Be(expectedValue);
+        CollectionEditVerbManager editor = new(text: text, designer: toolStripItemDesigner, prop: propertyDescriptor, addToDesignerVerbs: true);
+        editor.EditItemsVerb.Text.Should().Be(expected);
         editor.EditItemsVerb.Enabled.Should().BeTrue();
         editor.EditItemsVerb.Supported.Should().BeTrue();
         editor.EditItemsVerb.Visible.Should().BeTrue();
-        editor.EditItemsVerb.CommandID!.ID.Should().Be(8192);
+        editor.EditItemsVerb.CommandID.Should().Be(new CommandID(new Guid("74d21313-2aee-11d1-8bfb-00a0c90f26f7"), 8192));
     }
 }
 
