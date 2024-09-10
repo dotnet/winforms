5367f7de5d8b4290b71ca032ffcd3c89cebd3e0c
Loni Tra <lonitra@microsoft.com>
2024-09-10 15:30:47 -0700

    Change IPointer interface method to return nint (#12103)
    * Change IPointer interface method to return nint
    
    * remove IPointer interface and update extension to have IPointer<T>
    
    * update one more to use extension method
    
    * obsolete IPointer.Pointer to discourage usage internally
    
    * Add comment about the change to nint
    
    * Update src/System.Private.Windows.Core/src/Windows/Win32/Foundation/IPointer.cs
    
    Co-authored-by: Jeremy Kuhne <jeremy.kuhne@microsoft.com>
    
    ---------
    
    Co-authored-by: Jeremy Kuhne <jeremy.kuhne@microsoft.com>

---
+++ src/System.Private.Windows.Core/src/Windows/Win32/Foundation/PointerExtensions.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Windows.Win32.Foundation;
+
+internal static unsafe class PointerExtensions
+{
+#pragma warning disable CS0618 // Type or member is obsolete
+    public static T* GetPointer<T>(this IPointer<T> pointer) where T : unmanaged => (T*)pointer.Pointer;
+#pragma warning restore CS0618
+}
