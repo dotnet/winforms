db21a4a748d3d7b119161ed259dd18e0e31a9859
Nora Zhou <v-norazhou@microsoft.com>
2024-09-10 06:43:01 +0000

    Add-core-coverage-for-ListAdapter

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ListAdapterTest.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.Collections;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class ListAdapterTest
+{
+    [Fact]
+    public void ListAdapter_Unwrap_ReturnsOriginalList()
+    {
+        // Arrange
+        var originalList = new ArrayList { 1, 2, 3 };
+        ListAdapter<int> adapter = new(originalList);
+
+        // Act
+        var unwrappedList = adapter.Unwrap();
+
+        // Assert
+        Assert.Same(originalList, unwrappedList);
+    }
+}
+
+
