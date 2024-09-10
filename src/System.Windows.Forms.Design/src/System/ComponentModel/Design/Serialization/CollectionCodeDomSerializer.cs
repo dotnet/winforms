1fc227e0a0f0ff0064ee0be76de2d89849630f5a
Paul M Cohen <paul1956@users.noreply.github.com>
2024-09-09 20:30:40 -0700

    Update src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CollectionCodeDomSerializer.cs
    Co-authored-by: Epica3055 <135201996+Epica3055@users.noreply.github.com>

--- src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CollectionCodeDomSerializer.cs
+++ src/System.Windows.Forms.Design/src/System/ComponentModel/Design/Serialization/CollectionCodeDomSerializer.cs
@@ -135,9 +135,9 @@ public class CollectionCodeDomSerializer : CodeDomSerializer
 
         // We serialize collections as follows:
         //      If the collection is an array, we write out the array.
-        //      If the collection has a method called AddRange, we will call that, providing an array.
-        //      If the collection has an Add method, we will call it repeatedly.
-        //      If the collection is an IList, we will cast to IList and add to it.
+        // If the collection has a method called AddRange, we will call that, providing an array.
+        // If the collection has an Add method, we will call it repeatedly.
+        // If the collection is an IList, we will cast to IList and add to it.
         //      If the collection has no add method, but is marked with PersistContents,
         //      we will enumerate the collection and serialize each element.
         // Check to see if there is a CodePropertyReferenceExpression on the stack.
