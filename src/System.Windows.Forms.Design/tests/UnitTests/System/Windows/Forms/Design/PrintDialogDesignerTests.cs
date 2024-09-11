d54ee91befdb75dfa1e2a4a644c6ea9a81b6e87d
Syareel Md Sukeri <v-smdsukeri@microsoft.com>
2024-09-11 16:33:35 +0800

    Add unit tests for PrintDialogDesigner

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/PrintDialogDesignerTests.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+namespace System.Windows.Forms.Design.Tests;
+public class PrintDialogDesignerTests
+{
+    [Fact]
+    public void InitializeNewComponent_SetsUseEXDialogToTrue()
+    {
+        PrintDialogDesigner designer = new();
+        PrintDialog printDialog = new();
+        designer.Initialize(printDialog);
+
+        designer.InitializeNewComponent(null);
+
+        printDialog.UseEXDialog.Should().BeTrue();
+    }
+}
