72429cb15ea803d9a84eb7eaf9d09909e93a5aa3
Nora Zhou <104609169+Nora-Zhou01@users.noreply.github.com>
2024-09-13 17:44:08 +0000

    Add unit tests for PanelDesigner (#12113)
    Related #10773
    
    Add unit test PanelDesignerTests.cs for public properties and method of the PanelDesigner.cs
    Enable nullability in PanelDesignerTests.cs

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/PanelDesignerTests.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class PanelDesignerTests
+{
+    [Fact]
+    public void Constructor_SetsAutoResizeHandlesToTrue()
+    {
+        using Panel panel = new();
+        using PanelDesigner designer = new();
+        designer.Initialize(panel);
+
+        designer.AutoResizeHandles.Should().BeTrue();
+    }
+}
