a67dc20168a31684c5ac8255d47d99ba23227f96
Olina Zhang (Beyondsoft Corporation) <v-olzhan@microsoft.com>
2024-09-14 05:43:52 +0000

    handle dispose control

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/TabControlDesignerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/TabControlDesignerTests.cs
@@ -22,7 +22,7 @@ namespace System.Windows.Forms.Design.Tests
             return (designer, tabControl);
         }
 
-        private TabControlDesigner CreateDesignerWithMockedHost()
+        private (TabControlDesigner designer, TabControl tabControl, TabPage tabPage) CreateDesignerWithMockedHost()
         {
             TabControlDesigner designer = new();
             TabControl tabControl = new();
@@ -32,25 +32,24 @@ namespace System.Windows.Forms.Design.Tests
             serviceProvider.Setup(s => s.GetService(typeof(IDesignerHost))).Returns(designerHost.Object);
             designerHost.Setup(h => h.CreateComponent(typeof(TabPage))).Returns(tabPage);
             designer.Initialize(tabControl);
-            return designer;
-        }
-
-        private void SetForwardOnDrag(TabControlDesigner designer, bool value)
-        {
-            designer.GetType().GetField("_forwardOnDrag", Reflection.BindingFlags.NonPublic | Reflection.BindingFlags.Instance)?.SetValue(designer, value);
+            return (designer, tabControl, tabPage);
         }
 
         [Fact]
         public void InitializeNewComponent_ShouldAddTabPageToTabControl()
         {
             var (designer, tabControl) = CreateInitializedDesignerWithServiceProvider(out _, out var designerHost);
-            using TabPage tabPage = new();
-            tabControl.TabPages.Add(tabPage);
-            designerHost.Setup(h => h.CreateComponent(typeof(TabPage))).Returns(tabPage);
+            using (designer)
+            using (tabControl)
+            {
+                using TabPage tabPage = new();
+                tabControl.TabPages.Add(tabPage);
+                designerHost.Setup(h => h.CreateComponent(typeof(TabPage))).Returns(tabPage);
 
-            designer.InitializeNewComponent(new Hashtable());
+                designer.InitializeNewComponent(new Hashtable());
 
-            tabControl.TabPages.Count.Should().Be(1);
+                tabControl.TabPages.Count.Should().Be(1);
+            }
         }
 
         [Theory]
@@ -58,99 +57,125 @@ namespace System.Windows.Forms.Design.Tests
         [InlineData(typeof(Button), false)]
         public void CanParent_ReturnsExpectedResult(Type controlType, bool expected)
         {
-            using TabControl tabControl = new();
-            using TabControlDesigner designer = new();
-            designer.Initialize(tabControl);
-            Control? control = Activator.CreateInstance(controlType) as Control;
-
-            if (control is TabPage tabPage && expected == false)
+            var (designer, tabControl) = CreateInitializedDesignerWithServiceProvider(out _, out _);
+            using (designer)
+            using (tabControl)
             {
-                tabControl.TabPages.Add(tabPage);
+                designer.Initialize(tabControl);
+                Control? control = Activator.CreateInstance(controlType) as Control;
+
+                if (control is TabPage tabPage && expected == false)
+                {
+                    tabControl.TabPages.Add(tabPage);
+                }
+
+                bool result = designer.CanParent(control);
+
+                result.Should().Be(expected);
             }
-
-            bool result = designer.CanParent(control);
-
-            result.Should().Be(expected);
         }
 
         [Fact]
         public void Verbs_ContainsAddAndRemoveVerbs()
         {
-            var (designer, _) = CreateInitializedDesignerWithServiceProvider(out _, out _);
+            var (designer, tabControl, tabPage) = CreateDesignerWithMockedHost();
+            using (designer)
+            using (tabControl)
+            using (tabPage)
+            {
+                var verbs = designer.Verbs.Cast<DesignerVerb>();
 
-            var verbs = designer.Verbs.Cast<DesignerVerb>();
-
-            verbs.Should().Contain(v => v.Text == SR.TabControlAdd);
-            verbs.Should().Contain(v => v.Text == SR.TabControlRemove);
+                verbs.Should().Contain(v => v.Text == SR.TabControlAdd);
+                verbs.Should().Contain(v => v.Text == SR.TabControlRemove);
+            }
         }
 
         [Fact]
         public void Verbs_RemoveVerbEnabledWhenTabPagesExist()
         {
-            var (designer, tabControl) = CreateInitializedDesignerWithServiceProvider(out _, out _);
-            using TabPage tabPage = new();
-            tabControl.TabPages.Add(tabPage);
+            var (designer, tabControl, tabPage) = CreateDesignerWithMockedHost();
+            using (designer)
+            using (tabControl)
+            using (tabPage)
+            {
+                tabControl.TabPages.Add(tabPage);
 
-            var verbs = designer.Verbs;
+                var verbs = designer.Verbs;
+                var removeVerb = verbs.Cast<DesignerVerb>().FirstOrDefault(v => v.Text == SR.TabControlRemove);
 
-            var removeVerb = verbs.Cast<DesignerVerb>().FirstOrDefault(v => v.Text == SR.TabControlRemove);
-            removeVerb.Should().NotBeNull();
-            removeVerb!.Enabled.Should().BeTrue();
+                removeVerb.Should().NotBeNull();
+                removeVerb!.Enabled.Should().BeTrue();
+            }
         }
 
         [Fact]
         public void Verbs_RemoveVerbDisabledWhenNoTabPagesExist()
         {
-            var (designer, _) = CreateInitializedDesignerWithServiceProvider(out _, out _);
+            var (designer, tabControl, tabPage) = CreateDesignerWithMockedHost();
+            using (designer)
+            using (tabControl)
+            using (tabPage)
+            {
+                var verbs = designer.Verbs;
+                var removeVerb = verbs.Cast<DesignerVerb>().FirstOrDefault(v => v.Text == SR.TabControlRemove);
 
-            var verbs = designer.Verbs;
-
-            var removeVerb = verbs.Cast<DesignerVerb>().FirstOrDefault(v => v.Text == SR.TabControlRemove);
-            removeVerb.Should().NotBeNull();
-            removeVerb!.Enabled.Should().BeFalse();
+                removeVerb.Should().NotBeNull();
+                removeVerb!.Enabled.Should().BeFalse();
+            }
         }
 
         [Fact]
         public void ParticipatesWithSnapLines_ReturnsFalseWhenNotForwardOnDrag()
         {
-            using var designer = CreateDesignerWithMockedHost();
+            var (designer, tabControl, tabPage) = CreateDesignerWithMockedHost();
+            using (designer)
+            using (tabControl)
+            using (tabPage)
+            {
+                bool result = designer.ParticipatesWithSnapLines;
 
-            bool result = designer.ParticipatesWithSnapLines;
-
-            result.Should().BeFalse();
+                result.Should().BeFalse();
+            }
         }
 
         [Fact]
         public void ParticipatesWithSnapLines_ReturnsTrueWhenForwardOnDragAndPageDesignerParticipates()
         {
-            using var designer = CreateDesignerWithMockedHost();
-            var tabControl = (TabControl)designer.Component;
-            using TabPage tabPage = new();
-            tabControl.TabPages.Add(tabPage);
+            var (designer, tabControl, tabPage) = CreateDesignerWithMockedHost();
+            using (designer)
+            using (tabControl)
+            using (tabPage)
+            {
+                tabControl.TabPages.Add(tabPage);
 
-            Mock<TabPageDesigner> pageDesignerMock = new(MockBehavior.Strict);
-            pageDesignerMock.Setup(p => p.ParticipatesWithSnapLines).Returns(true);
+                Mock<TabPageDesigner> pageDesignerMock = new(MockBehavior.Strict);
+                pageDesignerMock.Setup(p => p.ParticipatesWithSnapLines).Returns(true);
 
-            SetForwardOnDrag(designer, true);
+                designer.TestAccessor().Dynamic._forwardOnDrag = true;
 
-            bool result = designer.ParticipatesWithSnapLines;
+                bool result = designer.ParticipatesWithSnapLines;
 
-            result.Should().BeTrue();
+                result.Should().BeTrue();
+            }
         }
 
         [Fact]
         public void ParticipatesWithSnapLines_ReturnsTrueWhenForwardOnDragAndPageDesignerDoesNotParticipate()
         {
-            using var designer = CreateDesignerWithMockedHost();
+            var (designer, tabControl, tabPage) = CreateDesignerWithMockedHost();
+            using (designer)
+            using (tabControl)
+            using (tabPage)
+            {
+                Mock<TabPageDesigner> pageDesignerMock = new(MockBehavior.Strict);
+                pageDesignerMock.Setup(p => p.ParticipatesWithSnapLines).Returns(false);
 
-            Mock<TabPageDesigner> pageDesignerMock = new(MockBehavior.Strict);
-            pageDesignerMock.Setup(p => p.ParticipatesWithSnapLines).Returns(false);
+                designer.TestAccessor().Dynamic._forwardOnDrag = true;
 
-            SetForwardOnDrag(designer, true);
+                bool result = designer.ParticipatesWithSnapLines;
 
-            bool result = designer.ParticipatesWithSnapLines;
-
-            result.Should().BeTrue();
+                result.Should().BeTrue();
+            }
         }
     }
 }
