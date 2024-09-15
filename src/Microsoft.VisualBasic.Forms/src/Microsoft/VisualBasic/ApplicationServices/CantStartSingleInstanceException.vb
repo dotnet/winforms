c7a9e09add81ac090cf621311534d96a9bb711f5
Paul M Cohen <paul1956@users.noreply.github.com>
2024-09-15 15:19:14 -0700

    Add XML Comments to CantStartSingleInstanceException

--- src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/ApplicationServices/CantStartSingleInstanceException.vb
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/ApplicationServices/CantStartSingleInstanceException.vb
@@ -29,10 +29,19 @@ Namespace Microsoft.VisualBasic.ApplicationServices
             MyBase.New(VbUtils.GetResourceString(SR.AppModel_SingleInstanceCantConnect))
         End Sub
 
+        ''' <summary>
+        '''  Creates a new <see cref="Exception"/>.
+        ''' </summary>
+        ''' <param name="message"></param>
         Public Sub New(message As String)
             MyBase.New(message)
         End Sub
 
+        ''' <summary>
+        '''  Creates a new <see cref="Exception"/>.
+        ''' </summary>
+        ''' <param name="message"></param>
+        ''' <param name="inner"> Inner <see cref="Exception"/></param>
         Public Sub New(message As String, inner As Exception)
             MyBase.New(message, inner)
         End Sub
