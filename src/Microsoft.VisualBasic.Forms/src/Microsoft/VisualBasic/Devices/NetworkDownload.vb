7164f516e6a5cfde5a04b3cd4637cb2a56e81fdf
Paul M Cohen <paul1956@users.noreply.github.com>
2024-09-16 22:51:35 -0700

    Fix formating

--- src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/Devices/NetworkDownload.vb
+++ src/Microsoft.VisualBasic.Forms/src/Microsoft/VisualBasic/Devices/NetworkDownload.vb
@@ -115,7 +115,9 @@ Namespace Microsoft.VisualBasic.Devices
                 End If
             Catch ex As Exception
                 If ex.InnerException IsNot Nothing Then
-                    If TryCast(ex.InnerException, OperationCanceledException) IsNot Nothing AndAlso Environment.UserInteractive Then
+                    If TryCast(ex.InnerException, OperationCanceledException) IsNot Nothing AndAlso
+                        Environment.UserInteractive Then
+
                         If showUI AndAlso Environment.UserInteractive Then
                             Try
                                 IO.File.Delete(destinationFileName)
