058c50651f2226fe8e8177df30607f3d693f2602
Leaf Shi (BEYONDSOFT CONSULTING INC) <v-leafshi@microsoft.com>
2024-09-27 11:12:59 +0800

    Add retry mechanism instead of Sleeping before testing

--- src/System.Windows.Forms/tests/UnitTests/ClipboardTestBase.cs
+++
@@ -1,9 +0,0 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-
-namespace System.Windows.Forms.Tests;
-
-public class ClipboardTestBase
-{
-    protected static void Sleep() => Thread.Sleep(100);
-}
