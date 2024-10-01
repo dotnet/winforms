4d16b4833ba52db8782e48e52bae254dcb139ef1
Syareel Md Sukeri <v-smdsukeri@microsoft.com>
2024-10-01 09:05:24 +0800

    Indentation update

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ItemTypeToolStripMenuItemTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ItemTypeToolStripMenuItemTests.cs
@@ -8,49 +8,49 @@ using System.Drawing.Design;
 
 namespace System.Windows.Forms.Design.Tests;
 
-    public class ItemTypeToolStripMenuItemTests
+public class ItemTypeToolStripMenuItemTests
+{
+    [Fact]
+    public void Constructor_SetsItemType()
     {
-        [Fact]
-        public void Constructor_SetsItemType()
-        {
-            using ItemTypeToolStripMenuItem item = new(typeof(string));
-            item.ItemType.Should().Be(typeof(string));
-        }
-
-        [Fact]
-        public void ConvertTo_SetAndGet()
-        {
-            using ItemTypeToolStripMenuItem item = new(typeof(string)) { ConvertTo = true };
-            item.ConvertTo.Should().BeTrue();
-        }
-
-        [Fact]
-        public void Image_Get_ReturnsCorrectImage()
-        {
-            using ItemTypeToolStripMenuItem item = new(typeof(string));
-            item.Image.Should().BeOfType<Bitmap>().Which.Should().Be(ToolStripDesignerUtils.GetToolboxBitmap(typeof(string)));
-        }
-
-        [Fact]
-        public void Text_ReturnsCorrectDescription()
-        {
-            using ItemTypeToolStripMenuItem item = new(typeof(string));
-            item.Text.Should().BeOfType<string>().Which.Should().Be(ToolStripDesignerUtils.GetToolboxDescription(typeof(string)));
-        }
-
-        [Fact]
-        public void TbxItem_SetAndGet()
-        {
-            ToolboxItem toolboxItem = new(typeof(string));
-            using ItemTypeToolStripMenuItem item = new(typeof(string)) { TbxItem = toolboxItem };
-            item.TbxItem.Should().Be(toolboxItem);
-        }
-
-        [Fact]
-        public void Dispose_SetsTbxItemToNull()
-        {
-            ItemTypeToolStripMenuItem item = new(typeof(string));
-            item.Dispose();
-            item.TbxItem.Should().BeNull();
-        }
+        using ItemTypeToolStripMenuItem item = new(typeof(string));
+        item.ItemType.Should().Be(typeof(string));
     }
+
+    [Fact]
+    public void ConvertTo_SetAndGet()
+    {
+        using ItemTypeToolStripMenuItem item = new(typeof(string)) { ConvertTo = true };
+        item.ConvertTo.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Image_Get_ReturnsCorrectImage()
+    {
+        using ItemTypeToolStripMenuItem item = new(typeof(string));
+        item.Image.Should().BeOfType<Bitmap>().Which.Should().Be(ToolStripDesignerUtils.GetToolboxBitmap(typeof(string)));
+    }
+
+    [Fact]
+    public void Text_ReturnsCorrectDescription()
+    {
+        using ItemTypeToolStripMenuItem item = new(typeof(string));
+        item.Text.Should().BeOfType<string>().Which.Should().Be(ToolStripDesignerUtils.GetToolboxDescription(typeof(string)));
+    }
+
+    [Fact]
+    public void TbxItem_SetAndGet()
+    {
+        ToolboxItem toolboxItem = new(typeof(string));
+        using ItemTypeToolStripMenuItem item = new(typeof(string)) { TbxItem = toolboxItem };
+        item.TbxItem.Should().Be(toolboxItem);
+    }
+
+    [Fact]
+    public void Dispose_SetsTbxItemToNull()
+    {
+        ItemTypeToolStripMenuItem item = new(typeof(string));
+        item.Dispose();
+        item.TbxItem.Should().BeNull();
+    }
+}
