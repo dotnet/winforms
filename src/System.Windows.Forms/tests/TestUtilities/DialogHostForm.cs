3b2d3461234f707c56a9c48f2b9d0dfbc41296e0
Tanya Solyanik <tanyaso@microsoft.com>
2024-10-02 18:24:54 -0700

    adding an interop with a framework assembly test case

--- src/System.Windows.Forms/tests/TestUtilities/DialogHostForm.cs
+++ src/System.Windows.Forms/tests/TestUtilities/DialogHostForm.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.ComponentModel;
 using System.Windows.Forms;
 using Windows.Win32.Foundation;
 using Windows.Win32.UI.WindowsAndMessaging;
@@ -11,6 +12,7 @@ namespace System;
 ///  Helper class to host dialogs for testing.
 ///  This class is typically passed as a parameter to a call to <see cref="CommonDialog.ShowDialog(IWin32Window?)"/>.
 /// </summary>
+[DesignerCategory("Default")]
 internal class DialogHostForm : Form
 {
     protected override void WndProc(ref Message m)
