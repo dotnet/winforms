75c7fb7551b1620e53026b2a6222b0755b5dc30d
Loni Tra <lonitra@microsoft.com>
2024-10-11 12:00:41 -0700

    Add more details  for custom json serialization

--- src/System.Windows.Forms/src/System/Windows/Forms/OLE/Clipboard.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/OLE/Clipboard.cs
@@ -537,8 +537,11 @@ public static class Clipboard
     ///   for more details on default <see cref="JsonSerializer"/> behavior.
     ///  </para>
     ///  <para>
-    ///   If custom behavior is needed, manually JSON serialize the data and then use <see cref="SetData"/>
-    ///   to save the data onto the clipboard.
+    ///   If custom JSON serialization behavior is needed, manually JSON serialize the data and then use <see cref="SetData"/>
+    ///   to save the data onto the clipboard, or create a custom <see cref="Text.Json.Serialization.JsonConverter"/>, attach the
+    ///   <see cref="Text.Json.Serialization.JsonConverterAttribute"/>, and then recall this method.
+    ///   See <see href="https://learn.microsoft.com/dotnet/standard/serialization/system-text-json/converters-how-to"/> for more details
+    ///   on custom converters for JSON serialization.
     ///  </para>
     /// </remarks>
     public static void SetDataAsJson<T>(string format, T data)
