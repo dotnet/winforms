d5112a4bfea336e472297eaf1e38c4b83d8913d1
Paul M Cohen <paul1956@users.noreply.github.com>
2024-10-11 16:26:37 -1000

    Improve VB Audio test coverage

--- src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/Devices/AudioTests.cs
+++ src/Microsoft.VisualBasic/tests/UnitTests/Microsoft/VisualBasic/Devices/AudioTests.cs
@@ -18,6 +18,37 @@ public class AudioTests
         testCode.Should().Throw<FileNotFoundException>();
     }
 
+    [Theory]
+    [EnumData<AudioPlayMode>]
+    public void PlayAllModes_Throws(AudioPlayMode mode)
+    {
+        string location = Path.Combine(Path.GetTempPath(), GetUniqueName());
+        Audio audio = new();
+        Action testCode = () => audio.Play(location, mode);
+        testCode.Should().Throw<FileNotFoundException>();
+    }
+
+    [Fact]
+    public void PlayBytes_Throws()
+    {
+        byte[] data = Array.Empty<byte>();
+        Audio audio = new();
+        Action testCode = () => audio.Play(data, AudioPlayMode.Background);
+        testCode.Should().Throw<InvalidOperationException>();
+
+        data = null;
+        testCode = () => audio.Play(data, AudioPlayMode.Background);
+        testCode.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void PlayEmptyFileName_Throws()
+    {
+        Audio audio = new();
+        Action testCode = () => audio.Play(string.Empty);
+        testCode.Should().Throw<ArgumentNullException>();
+    }
+
     [Fact]
     public void PlayInvalidMode_Throws()
     {
@@ -27,16 +58,17 @@ public class AudioTests
         testCode.Should().Throw<InvalidEnumArgumentException>();
     }
 
-    [Theory]
-    [InlineData(AudioPlayMode.Background)]
-    [InlineData(AudioPlayMode.BackgroundLoop)]
-    [InlineData(AudioPlayMode.WaitToComplete)]
-    public void PlayAllModes_Throws(AudioPlayMode mode)
+    [Fact]
+    public void PlayStream()
     {
-        string location = Path.Combine(Path.GetTempPath(), GetUniqueName());
+        var data = new MemoryStream();
         Audio audio = new();
-        Action testCode = () => audio.Play(location, mode);
-        testCode.Should().Throw<FileNotFoundException>();
+        Action testCode = () => audio.Play(data, AudioPlayMode.Background);
+        testCode.Should().Throw<InvalidOperationException>();
+
+        data = null;
+        testCode = () => audio.Play(data, AudioPlayMode.Background);
+        testCode.Should().Throw<ArgumentNullException>();
     }
 
     // Not tested:
