90647fc7e988b39d3d8fe9989cf743f88cc0cace
Olina Zhang (Beyondsoft Corporation) <v-olzhan@microsoft.com>
2024-10-12 08:31:13 +0000

    Add unit tests for DataGridViewCellStyleBuilder

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DataGridViewCellStyleBuilderTests.cs
@@ -0,0 +1,43 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using Moq;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class DataGridViewCellStyleBuilderTests
+{
+    [Fact]
+    public void CellStyle_SetAndGet_ReturnsCorrectValue()
+    {
+        Mock<IServiceProvider> serviceProvider = new();
+        Mock<IComponent> component = new();
+        using DataGridViewCellStyleBuilder builder = new(serviceProvider.Object, component.Object);
+        var cellStyle = new DataGridViewCellStyle { BackColor = Color.Red };
+
+        builder.CellStyle = cellStyle;
+        var result = builder.CellStyle;
+
+        result.Should().NotBeNull();
+        result.BackColor.Should().Be(Color.Red);
+    }
+
+    [Fact]
+    public void Context_Set_ReturnsCorrectValue()
+    {
+        Mock<IServiceProvider> serviceProvider = new();
+        Mock<IComponent> component = new();
+        using DataGridViewCellStyleBuilder builder = new(serviceProvider.Object, component.Object);
+        Mock<ITypeDescriptorContext> context = new();
+
+        builder.Context = context.Object;
+
+        var field = typeof(DataGridViewCellStyleBuilder).GetField("_context", Reflection.BindingFlags.NonPublic | Reflection.BindingFlags.Instance);
+        var result = field?.GetValue(builder);
+        result.Should().Be(context.Object);
+    }
+}
