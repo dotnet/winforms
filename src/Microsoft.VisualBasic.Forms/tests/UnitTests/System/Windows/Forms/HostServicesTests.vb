025d3a0eac820c4c5573968acc1659d119bcaf65
Paul M Cohen <paul1956@users.noreply.github.com>
2024-10-12 00:15:05 -1000

    Code coverage for HostServices from PR #11863

---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/HostServicesTests.vb
@@ -0,0 +1,41 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Option Strict Off
+
+Imports System.Windows.Forms
+Imports FluentAssertions
+Imports Microsoft.VisualBasic.CompilerServices
+Imports Xunit
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+    Partial Public Class HostServicesTests
+
+        <WinFormsFact>
+        Public Sub InputHandlerTests_Success()
+            Dim prompt As String = GetUniqueText()
+            Dim title As String = GetUniqueText()
+            Dim defaultResponse As String = GetUniqueText()
+            Dim xPos As Integer = -1
+            Dim yPos As Integer = -1
+            Dim parentWindow As IWin32Window = Nothing
+            Dim vbHost As IVbHost = HostServices.VBHost
+            Dim inputHandler As New InputBoxHandler(prompt, title, defaultResponse, xPos, yPos, parentWindow)
+            CType(inputHandler.TestAccessor.Dynamic()._prompt, String).Should.Be(prompt)
+            CType(inputHandler.TestAccessor.Dynamic()._title, String).Should.Be(title)
+            CType(inputHandler.TestAccessor.Dynamic()._defaultResponse, String).Should.Be(defaultResponse)
+            CType(inputHandler.TestAccessor.Dynamic()._xPos, String).Should.Be(xPos)
+            CType(inputHandler.TestAccessor.Dynamic()._yPos, String).Should.Be(yPos)
+            vbHost.Should.BeNull()
+            inputHandler.Exception.Should.BeNull()
+            inputHandler.Result.Should.BeNull()
+        End Sub
+
+        <WinFormsFact>
+        Public Sub VbHostTests_Success()
+            Dim vbHost As IVbHost = New TestVbHost
+            CType(vbHost.GetParentWindow, Control).Should.Be(s_control)
+            vbHost.GetWindowTitle.Should.Be(s_title)
+        End Sub
+    End Class
+End Namespace
