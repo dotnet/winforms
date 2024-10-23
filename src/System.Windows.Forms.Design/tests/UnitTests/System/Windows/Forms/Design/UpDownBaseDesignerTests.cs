cec055dc6de6522a0ff0da52300e21b724b14bd6
Liv Jia Xian <v-livjiaxian@microsoft.com>
2024-10-23 09:34:18 +0800

    Update suggested changes and modify SnapLine testcase to increase coverage

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/UpDownBaseDesignerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/UpDownBaseDesignerTests.cs
@@ -9,10 +9,11 @@ using System.Windows.Forms.Design.Behavior;
 using Moq;
 
 namespace System.Windows.Forms.Design.Tests;
+
 public class UpDownBaseDesignerTests
 {
     [Fact]
-    public void Constructor_SetsAutoResizeHandlesToTrue_And_SelectionRules_ShouldNotIncludeTopOrBottomSizeable_SnapLines_ReturnsCorrectSnapLines()
+    public void Constructor_SetsAutoResizeHandlesToTrue_And_SelectionRules_ShouldNotIncludeTopOrBottomSizeable()
     {
         using UpDownBaseDesigner designer = new();
 
@@ -26,8 +27,22 @@ public class UpDownBaseDesignerTests
         designer.Initialize(numericUpDown);
         SelectionRules rules = designer.SelectionRules;
 
-        rules.Should().NotHaveFlag(SelectionRules.TopSizeable);
-        rules.Should().NotHaveFlag(SelectionRules.BottomSizeable);
+        rules.Should().NotHaveFlag(SelectionRules.TopSizeable | SelectionRules.BottomSizeable);
+    }
+
+    [WinFormsTheory]
+    [InlineData(BorderStyle.None, -1)]
+    [InlineData(BorderStyle.Fixed3D, 2)]
+    public void SnapLines_ReturnsCorrectSnapLine(BorderStyle borderStyle, int expectedBaselineOffset)
+    {
+        using UpDownBaseDesigner designer = new();
+        using NumericUpDown numericUpDown = new();
+        numericUpDown.BorderStyle = borderStyle;
+        Mock<ISite> site = new();
+        site.Setup(s => s.GetService(typeof(IDesignerHost))).Returns(new Mock<IDesignerHost>().Object);
+        numericUpDown.Site = site.Object;
+
+        designer.Initialize(numericUpDown);
 
         List<SnapLine> snapLines = (List<SnapLine>)designer.SnapLines;
 
@@ -35,5 +50,8 @@ public class UpDownBaseDesignerTests
         SnapLine? baselineSnapLine = snapLines.Cast<SnapLine>().FirstOrDefault(sl => sl.SnapLineType == SnapLineType.Baseline);
         baselineSnapLine.Should().NotBeNull();
         baselineSnapLine.Should().BeOfType<SnapLine>().Which.Priority.Should().Be(SnapLinePriority.Medium);
+
+        int expectedBaseline = DesignerUtils.GetTextBaseline(numericUpDown, Drawing.ContentAlignment.TopLeft) + expectedBaselineOffset;
+        baselineSnapLine.Should().BeOfType<SnapLine>().Which.Offset.Should().Be(expectedBaseline);
     }
 }
