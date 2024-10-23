7c1b82cd70ae5f3ffc6c5166701ab667dbbc1ab5
Liv Jia Xian <v-livjiaxian@microsoft.com>
2024-10-23 11:13:10 +0800

    Add IDisposable and split SnapLine Testcase

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/UpDownBaseDesignerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/UpDownBaseDesignerTests.cs
@@ -10,23 +10,38 @@ using Moq;
 
 namespace System.Windows.Forms.Design.Tests;
 
-public class UpDownBaseDesignerTests
+public class UpDownBaseDesignerTests : IDisposable
 {
+    private readonly UpDownBaseDesigner _designer;
+    private readonly NumericUpDown _numericUpDown;
+
+    public UpDownBaseDesignerTests()
+    {
+        _numericUpDown = new();
+        _designer = new();
+        Mock<ISite> site = new();
+        site.Setup(s => s.GetService(typeof(IDesignerHost))).Returns(new Mock<IDesignerHost>().Object);
+        _numericUpDown.Site = site.Object;
+    }
+
+    private void InitializeDesigner(BorderStyle borderStyle)
+    {
+        _numericUpDown.BorderStyle = borderStyle;
+        _designer.Initialize(_numericUpDown);
+    }
+
+    public void Dispose()
+    {
+        _designer.Dispose();
+        _numericUpDown.Dispose();
+    }
+
     [Fact]
     public void Constructor_SetsAutoResizeHandlesToTrue_And_SelectionRules_ShouldNotIncludeTopOrBottomSizeable()
     {
-        using UpDownBaseDesigner designer = new();
-
-        designer.AutoResizeHandles.Should().BeTrue();
-
-        using NumericUpDown numericUpDown = new();
-        Mock<ISite> site = new();
-        site.Setup(s => s.GetService(typeof(IDesignerHost))).Returns(new Mock<IDesignerHost>().Object);
-        numericUpDown.Site = site.Object;
-
-        designer.Initialize(numericUpDown);
-        SelectionRules rules = designer.SelectionRules;
-
+        InitializeDesigner(BorderStyle.None);
+        SelectionRules rules = _designer.SelectionRules;
+        _designer.AutoResizeHandles.Should().BeTrue();
         rules.Should().NotHaveFlag(SelectionRules.TopSizeable | SelectionRules.BottomSizeable);
     }
 
@@ -35,23 +50,15 @@ public class UpDownBaseDesignerTests
     [InlineData(BorderStyle.Fixed3D, 2)]
     public void SnapLines_ReturnsCorrectSnapLine(BorderStyle borderStyle, int expectedBaselineOffset)
     {
-        using UpDownBaseDesigner designer = new();
-        using NumericUpDown numericUpDown = new();
-        numericUpDown.BorderStyle = borderStyle;
-        Mock<ISite> site = new();
-        site.Setup(s => s.GetService(typeof(IDesignerHost))).Returns(new Mock<IDesignerHost>().Object);
-        numericUpDown.Site = site.Object;
-
-        designer.Initialize(numericUpDown);
-
-        List<SnapLine> snapLines = (List<SnapLine>)designer.SnapLines;
+        InitializeDesigner(borderStyle);
+        List<SnapLine> snapLines = (List<SnapLine>)_designer.SnapLines;
 
         snapLines.Should().NotBeNull();
         SnapLine? baselineSnapLine = snapLines.Cast<SnapLine>().FirstOrDefault(sl => sl.SnapLineType == SnapLineType.Baseline);
         baselineSnapLine.Should().NotBeNull();
         baselineSnapLine.Should().BeOfType<SnapLine>().Which.Priority.Should().Be(SnapLinePriority.Medium);
 
-        int expectedBaseline = DesignerUtils.GetTextBaseline(numericUpDown, Drawing.ContentAlignment.TopLeft) + expectedBaselineOffset;
+        int expectedBaseline = DesignerUtils.GetTextBaseline(_numericUpDown, Drawing.ContentAlignment.TopLeft) + expectedBaselineOffset;
         baselineSnapLine.Should().BeOfType<SnapLine>().Which.Offset.Should().Be(expectedBaseline);
     }
 }
