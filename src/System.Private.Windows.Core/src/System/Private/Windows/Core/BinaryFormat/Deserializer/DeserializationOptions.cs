501aeda9c36c9322584b6233a8f7d3a7d3c8a39e
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-10-24 11:34:05 -0700

    Add the DefaultTypeResolver
    Copy the DefaultTypeResolver back from the Runtime.
    
    This also replaces SerializationBinder in the options with ITypeResolver.
    
    Additionally, use a bool instead of FormatterAssemblyStyle.

--- src/System.Private.Windows.Core/src/System/Private/Windows/Core/BinaryFormat/Deserializer/DeserializationOptions.cs
+++ src/System.Private.Windows.Core/src/System/Private/Windows/Core/BinaryFormat/Deserializer/DeserializationOptions.cs
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters;
 
 namespace System.Private.Windows.Core.BinaryFormat;
 
@@ -11,26 +10,30 @@ internal sealed class DeserializationOptions
     /// <summary>
     ///  How exactly assembly names need to match for deserialization.
     /// </summary>
-#pragma warning disable SYSLIB0050 // Type or member is obsolete
-    public FormatterAssemblyStyle AssemblyMatching { get; set; } = FormatterAssemblyStyle.Simple;
-#pragma warning restore SYSLIB0050 // Type or member is obsolete
+    /// <remarks>
+    ///  <para>
+    ///   Aligned with <see href="https://learn.microsoft.com/dotnet/api/system.runtime.serialization.formatters.formatterassemblystyle">
+    ///   FormatterAssemblyStyle</see> behavior.
+    ///  </para>
+    /// </remarks>
+    public bool SimpleAssemblyMatching { get; set; } = true;
 
     /// <summary>
     ///  Type name binder.
     /// </summary>
-    public SerializationBinder? Binder { get; set; }
+    public ITypeResolver? TypeResolver { get; set; }
 
     /// <summary>
     ///  Optional type <see cref="ISerializationSurrogate"/> provider.
     /// </summary>
 #pragma warning disable SYSLIB0050 // Type or member is obsolete
     public ISurrogateSelector? SurrogateSelector { get; set; }
-#pragma warning restore SYSLIB0050 // Type or member is obsolete
+#pragma warning restore SYSLIB0050
 
     /// <summary>
     ///  Streaming context.
     /// </summary>
 #pragma warning disable SYSLIB0050 // Type or member is obsolete
     public StreamingContext StreamingContext { get; set; } = new(StreamingContextStates.All);
-#pragma warning restore SYSLIB0050 // Type or member is obsolete
+#pragma warning restore SYSLIB0050
 }
