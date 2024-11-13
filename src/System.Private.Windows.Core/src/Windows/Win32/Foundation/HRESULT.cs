177abcc37b734b59a1946f4a7bceec589ff5c167
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-11-12 19:30:56 -0800

    Error interop tweaks
    Adds some macros that WPF needs, and moves NTSTATUS to the core assembly.

--- src/System.Private.Windows.Core/src/Windows/Win32/Foundation/HRESULT.cs
+++ src/System.Private.Windows.Core/src/Windows/Win32/Foundation/HRESULT.cs
@@ -1,11 +1,34 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Windows.Win32.System.Diagnostics.Debug;
+
 namespace Windows.Win32.Foundation;
 
 internal readonly partial struct HRESULT
 {
-    public static HRESULT HRESULT_FROM_WIN32(WIN32_ERROR error)
+    /// <summary>
+    ///  Extracts the facility code of the HRESULT. [HRESULT_FACILITY]
+    /// </summary>
+    public FACILITY_CODE Facility
+        // https://learn.microsoft.com/windows/win32/api/winerror/nf-winerror-hresult_facility
+        // #define HRESULT_FACILITY(hr)  (((hr) >> 16) & 0x1fff)
+        => (FACILITY_CODE)((Value >> 16) & 0x1fff);
+
+    /// <summary>
+    ///  Extracts the code portion of the HRESULT. [HRESULT_CODE]
+    /// </summary>
+    public int Code
+        // https://learn.microsoft.com/windows/win32/api/winerror/nf-winerror-hresult_code
+        // #define HRESULT_CODE(hr)    ((hr) & 0xFFFF)
+        => Value & 0xFFFF;
+
+    /// <summary>
+    ///  Converts a Win32 error code into an HRESULT. [HRESULT_FROM_WIN32]
+    /// </summary>
+    public static HRESULT FromWin32(WIN32_ERROR error)
+        // https://learn.microsoft.com/windows/win32/api/winerror/nf-winerror-hresult_from_win32
+        // return (HRESULT)(x) <= 0 ? (HRESULT)(x) : (HRESULT) (((x) & 0x0000FFFF) | (FACILITY_WIN32 << 16) | 0x80000000);
         => new(((int)error & 0x0000FFFF) | unchecked((int)0x80070000));
 
     public static implicit operator HRESULT(Exception ex)
