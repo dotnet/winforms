8a962d55fd04f9640a756be62ed948cc9c121e3a
Nora Zhou <104609169+Nora-Zhou01@users.noreply.github.com>
2024-11-25 22:13:03 +0000

    Add unit tests for ListControlStringCollectionEditor (#12516)
    * Add unit tests for ListControlStringCollectionEditor
    
    * Update

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ListControlStringCollectionEditorTests.cs
@@ -0,0 +1,70 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using Moq;
+using System.ComponentModel;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class ListControlStringCollectionEditorTests
+{
+    [Fact]
+    public void EditValue_WithNullContext_ReturnsBaseEditValue()
+    {
+        ListControlStringCollectionEditor editor = new(typeof(string));
+        IServiceProvider provider = new Mock<IServiceProvider>().Object;
+        object? value = new();
+
+        object? result = editor.EditValue(null, provider, value);
+
+        result.Should().Be(value);
+    }
+
+    [Fact]
+    public void EditValue_WithContextInstanceNotListControl_ReturnsBaseEditValue()
+    {
+        ListControlStringCollectionEditor editor = new(typeof(string));
+        Mock<ITypeDescriptorContext> context = new();
+        context.Setup(c => c.Instance).Returns(new object());
+        IServiceProvider provider = new Mock<IServiceProvider>().Object;
+        object? value = new();
+
+        object? result = editor.EditValue(context.Object, provider, value);
+
+        result.Should().Be(value);
+    }
+
+    [Fact]
+    public void EditValue_WithListControlAndNullDataSource_ReturnsBaseEditValue()
+    {
+        ListControlStringCollectionEditor editor = new(typeof(string));
+        using ListBox listControl = new();
+        Mock<ITypeDescriptorContext> context = new();
+        context.Setup(c => c.Instance).Returns(listControl);
+        IServiceProvider provider = new Mock<IServiceProvider>().Object;
+        object? value = new();
+
+        object? result = editor.EditValue(context.Object, provider, value);
+
+        result.Should().Be(value);
+    }
+
+    [Fact]
+    public void EditValue_WithListControlAndNonNullDataSource_ThrowsArgumentException()
+    {
+        ListControlStringCollectionEditor editor = new(typeof(string));
+
+        using ListBox listControl = new() { DataSource = new List<string> { "item1", "item2", "item3" } };
+
+        Mock<ITypeDescriptorContext> context = new();
+        context.Setup(c => c.Instance).Returns(listControl);
+
+        IServiceProvider provider = new Mock<IServiceProvider>().Object;
+        object? value = new();
+
+        ArgumentException exception = ((Action)(() => editor.EditValue(context.Object, provider, value))).Should().Throw<ArgumentException>().Which;
+        exception.Message.Should().Be(SR.DataSourceLocksItems);
+    }
+}
