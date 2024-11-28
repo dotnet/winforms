1bf35911e98c9580100616ffc0ff8ec6b511120b
Paul M Cohen <paul1956@users.noreply.github.com>
2024-11-27 22:05:44 -0800

    Add some Async Tests

--- src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/DownloadFileTests.vb
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/Forms/DownloadFileTests.vb
@@ -12,53 +12,6 @@ Namespace Microsoft.VisualBasic.Forms.Tests
     Public Class DownloadFileTests
         Inherits VbFileCleanupTestBase
 
-        ''' <summary>
-        '''  Verify that testDirectory exists, that destinationFileName exist and what its length is.
-        ''' </summary>
-        ''' <param name="testDirectory">A Unique directory under the systems Temp directory.</param>
-        ''' <param name="destinationFileName">The full path and filename of the new file.</param>
-        ''' <param name="listener"></param>
-        Private Shared Sub VerifyFailedDownload(
-            testDirectory As String,
-            destinationFileName As String,
-            listener As HttpListener)
-
-            If Not String.IsNullOrWhiteSpace(testDirectory) Then
-                Directory.Exists(testDirectory).Should.BeTrue()
-            End If
-            If Not String.IsNullOrWhiteSpace(destinationFileName) Then
-                Call New FileInfo(destinationFileName).Exists.Should.BeFalse()
-            End If
-            listener.Stop()
-            listener.Close()
-        End Sub
-
-        ''' <summary>
-        '''  Verify that testDirectory exists, that destinationFileName exist and what its length is.
-        ''' </summary>
-        ''' <param name="testDirectory">A Unique directory under the systems Temp directory.</param>
-        ''' <returns>
-        '''  The size in bytes of the destination file, this saves the caller from having to
-        '''  do another FileInfo call.
-        ''' </returns>
-        ''' <param name="destinationFileName">The full path and filename of the new file.</param>
-        ''' <param name="listener"></param>
-        Private Shared Function VerifySuccessfulDownload(
-            testDirectory As String,
-            destinationFileName As String,
-            listener As HttpListener) As Long
-
-            Directory.Exists(testDirectory).Should.BeTrue()
-            Dim fileInfo As New FileInfo(destinationFileName)
-            fileInfo.Exists.Should.BeTrue()
-            Directory.Exists(fileInfo.DirectoryName).Should.BeTrue()
-            ' This directory should not be systems Temp Directory because it must be created
-            Path.GetTempPath.Should.NotBe(fileInfo.DirectoryName)
-            listener.Stop()
-            listener.Close()
-            Return fileInfo.Length
-        End Function
-
         <WinFormsFact>
         Public Sub DownloadFile_UriOnly_Success()
             Dim testDirectory As String = CreateTempDirectory()
---
+++ src/Microsoft.VisualBasic.Forms/tests/UnitTests/System/Windows/TestUtilities/DownloadFileVerifiers.vb
@@ -0,0 +1,59 @@
+﻿' Licensed to the .NET Foundation under one or more agreements.
+' The .NET Foundation licenses this file to you under the MIT license.
+
+Imports System.IO
+Imports System.Net
+Imports FluentAssertions
+
+Namespace Microsoft.VisualBasic.Forms.Tests
+
+    Public Module DownloadFileVerifiers
+        ''' <summary>
+        '''  Verify that testDirectory exists, that destinationFileName exist and what its length is.
+        ''' </summary>
+        ''' <param name="testDirectory">A Unique directory under the systems Temp directory.</param>
+        ''' <param name="destinationFileName">The full path and filename of the new file.</param>
+        ''' <param name="listener"></param>
+        Friend Sub VerifyFailedDownload(
+                testDirectory As String,
+                destinationFileName As String,
+                listener As HttpListener)
+
+            If Not String.IsNullOrWhiteSpace(testDirectory) Then
+                Directory.Exists(testDirectory).Should.BeTrue()
+            End If
+            If Not String.IsNullOrWhiteSpace(destinationFileName) Then
+                Call New FileInfo(destinationFileName).Exists.Should.BeFalse()
+            End If
+            listener.Stop()
+            listener.Close()
+        End Sub
+
+        ''' <summary>
+        '''  Verify that testDirectory exists, that destinationFileName exist and what its length is.
+        ''' </summary>
+        ''' <param name="testDirectory">A Unique directory under the systems Temp directory.</param>
+        ''' <returns>
+        '''  The size in bytes of the destination file, this saves the caller from having to
+        '''  do another FileInfo call.
+        ''' </returns>
+        ''' <param name="destinationFileName">The full path and filename of the new file.</param>
+        ''' <param name="listener"></param>
+        Friend Function VerifySuccessfulDownload(
+                testDirectory As String,
+                destinationFileName As String,
+                listener As HttpListener) As Long
+
+            Directory.Exists(testDirectory).Should.BeTrue()
+            Dim fileInfo As New FileInfo(destinationFileName)
+            fileInfo.Exists.Should.BeTrue()
+            Directory.Exists(fileInfo.DirectoryName).Should.BeTrue()
+            ' This directory should not be systems Temp Directory because it must be created
+            Path.GetTempPath.Should.NotBe(fileInfo.DirectoryName)
+            listener.Stop()
+            listener.Close()
+            Return fileInfo.Length
+        End Function
+
+    End Module
+End Namespace
