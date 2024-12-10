82d2d1c71d9f89b04fbcaeba1593eb7ad52c57f0
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-12-09 16:23:19 -0800

    Add additional error defines
    Add a number of additional defines that are coming up in WPF.
    
    Also add a few more NtStatus related helpers to HRESULT.

--- src/System.Private.Windows.Core/src/Windows/Win32/Foundation/HRESULT.cs
+++ src/System.Private.Windows.Core/src/Windows/Win32/Foundation/HRESULT.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Windows.Win32.System.Diagnostics.Debug;
+using Windows.Win32.System.Variant;
+using Windows.Win32.System.Com;
 
 namespace Windows.Win32.Foundation;
 
@@ -24,12 +26,28 @@ internal readonly partial struct HRESULT
         => Value & 0xFFFF;
 
     /// <summary>
+    ///  <see langword="true"/> if the HRESULT represents an NTSTATUS code.
+    /// </summary>
+    public bool IsNtStatus => (Value & (int)FACILITY_CODE.FACILITY_NT_BIT) == (int)FACILITY_CODE.FACILITY_NT_BIT;
+
+    /// <summary>
+    ///  Extracts the <see cref="NTSTATUS"/> code. Check <see cref="IsNtStatus"/> before calling this method.
+    /// </summary>
+    public NTSTATUS ToNtStatus()
+    {
+        Debug.Assert(IsNtStatus);
+        return new NTSTATUS(Value & ~(int)FACILITY_CODE.FACILITY_NT_BIT);
+    }
+
+    /// <summary>
     ///  Converts a Win32 error code into an HRESULT. [HRESULT_FROM_WIN32]
     /// </summary>
     public static HRESULT FromWin32(WIN32_ERROR error)
         // https://learn.microsoft.com/windows/win32/api/winerror/nf-winerror-hresult_from_win32
         // return (HRESULT)(x) <= 0 ? (HRESULT)(x) : (HRESULT) (((x) & 0x0000FFFF) | (FACILITY_WIN32 << 16) | 0x80000000);
-        => new(((int)error & 0x0000FFFF) | unchecked((int)0x80070000));
+        => error == WIN32_ERROR.NO_ERROR
+            ? S_OK
+            : new HRESULT(((int)error & 0x0000FFFF) | unchecked((int)0x80070000));
 
     public static implicit operator HRESULT(Exception ex)
     {
@@ -54,6 +72,35 @@ internal readonly partial struct HRESULT
     public static readonly HRESULT COR_E_SAFEARRAYTYPEMISMATCH  = (HRESULT)unchecked((int)0x80131533);
     public static readonly HRESULT COR_E_TARGETINVOCATION       = (HRESULT)unchecked((int)0x80131604);
     public static readonly HRESULT COR_E_OBJECTDISPOSED         = (HRESULT)unchecked((int)0x80131622);
+
+    /// <summary>
+    ///  There is no associated handler for the given item registered by the specified application.
+    ///  (Windows Jump List)
+    /// </summary>
+    public static readonly HRESULT DESTS_E_NO_MATCHING_ASSOC_HANDLER = (HRESULT)unchecked((int)0x80040F03);
+
+    /// <summary>
+    ///  Object required.
+    /// </summary>
+    /// <remarks>
+    ///  <para>
+    ///   Visual Basic 6 and VBA raise this when objects have been collected or <see cref="VARIANT"/> values
+    ///   aren't actually <see cref="IUnknown"/> or <see cref="IDispatch"/> object pointers.
+    ///  </para>
+    ///  <para>
+    ///   Seen in various places as E_UNKNOWNWORDERROR, VBA_E_NOTOBJECT, or the VB native EBERR_NotObject.
+    ///   <see cref="FACILITY_CODE.FACILITY_CONTROL"/> is used for all VB errors raised as <see cref="HRESULT"/>s.
+    ///  </para>
+    ///  <para>
+    ///   <see href="https://learn.microsoft.com/office/vba/language/reference/user-interface-help/object-required-error-424">
+    ///    Object required (Error 424)
+    ///   </see>
+    ///  </para>
+    /// </remarks>
+    /// <devdoc>
+    ///  Closing Word when a search is running was a way this could be generated per legacy comments.
+    /// </devdoc>
+    public static readonly HRESULT VB_E_NOTOBJECT = (HRESULT)unchecked((int)0x800A01A8);
 
 #pragma warning restore format
 #pragma warning restore IDE1006
