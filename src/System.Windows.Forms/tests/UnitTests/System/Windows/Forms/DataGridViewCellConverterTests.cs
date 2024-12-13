405475559b627f737373a795e7f217e4198715aa
Zheng Li (Beyondsoft Corporation) <v-zhgl@microsoft.com>
2024-12-13 09:46:39 +0800

    Handle FeedBacks

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataGridViewCellConverterTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataGridViewCellConverterTests.cs
@@ -15,10 +15,8 @@ public class DataGridViewCellConverterTests
     [WinFormsTheory]
     [InlineData(typeof(InstanceDescriptor), true)]
     [InlineData(typeof(string), true)]
-    public void CanConvertTo_ReturnsExpected(Type destinationType, bool expected)
-    {
-        _converter.CanConvertTo(null, destinationType).Should().Be(expected);
-    }
+    public void CanConvertTo_ReturnsExpected(Type destinationType, bool expected) =>
+        _converter.CanConvertTo(context: null, destinationType).Should().Be(expected);
 
     [WinFormsFact]
     public void ConvertTo_InstanceDescriptorWithDataGridViewCell_ReturnsInstanceDescriptor()
@@ -26,8 +24,8 @@ public class DataGridViewCellConverterTests
         using DataGridViewTextBoxCell cell = new();
         Type destinationType = typeof(InstanceDescriptor);
 
-        _converter.ConvertTo(null, CultureInfo.InvariantCulture, cell, destinationType).Should().NotBeNull();
-        _converter.ConvertTo(null, CultureInfo.InvariantCulture, cell, destinationType).Should().BeOfType<InstanceDescriptor>();
+        _converter.ConvertTo(context: null, CultureInfo.InvariantCulture, cell, destinationType).Should().NotBeNull();
+        _converter.ConvertTo(context: null, CultureInfo.InvariantCulture, cell, destinationType).Should().BeOfType<InstanceDescriptor>();
     }
 
     [WinFormsFact]
@@ -36,7 +34,7 @@ public class DataGridViewCellConverterTests
         object value = new();
         Type destinationType = typeof(InstanceDescriptor);
 
-        Action action = () => _converter.ConvertTo(null, CultureInfo.InvariantCulture, value, destinationType);
+        Action action = () => _converter.ConvertTo(context: null, CultureInfo.InvariantCulture, value, destinationType);
         action.Should().Throw<NotSupportedException>()
             .WithMessage("'DataGridViewCellConverter' is unable to convert 'System.Object' to 'System.ComponentModel.Design.Serialization.InstanceDescriptor'.");
     }
@@ -47,8 +45,8 @@ public class DataGridViewCellConverterTests
         using DataGridViewTextBoxCell cell = new();
         Type destinationType = typeof(string);
 
-        _converter.ConvertTo(null, CultureInfo.InvariantCulture, cell, destinationType).Should().NotBeNull();
-        _converter.ConvertTo(null, CultureInfo.InvariantCulture, cell, destinationType).Should().BeOfType<string>();
-        _converter.ConvertTo(null, CultureInfo.InvariantCulture, cell, destinationType).Should().Be(cell.ToString());
+        _converter.ConvertTo(context: null, CultureInfo.InvariantCulture, cell, destinationType).Should().NotBeNull();
+        _converter.ConvertTo(context: null, CultureInfo.InvariantCulture, cell, destinationType).Should().BeOfType<string>();
+        _converter.ConvertTo(context: null, CultureInfo.InvariantCulture, cell, destinationType).Should().Be(cell.ToString());
     }
 }
