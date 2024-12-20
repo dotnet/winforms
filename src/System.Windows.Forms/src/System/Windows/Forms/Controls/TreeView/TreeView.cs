5ac73f9cf3006fd60fec261c4bb5b45840d09dd2
Mark Daniel Osborne <markosborne@pcblues.com>
2024-12-20 21:01:50 +1000

    Set to try different DWM values and calls.

--- src/System.Windows.Forms/src/System/Windows/Forms/Controls/TreeView/TreeView.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/Controls/TreeView/TreeView.cs
@@ -2555,6 +2555,38 @@ public partial class TreeView : Control
         }
     }
 
+    private enum DWMWINDOWATTRIBUTE
+    {
+        DWMWA_NCRENDERING_ENABLED = 1,
+        DWMWA_NCRENDERING_POLICY,
+        DWMWA_TRANSITIONS_FORCEDISABLED,
+        DWMWA_ALLOW_NCPAINT,
+        DWMWA_CAPTION_BUTTON_BOUNDS,
+        DWMWA_NONCLIENT_RTL_LAYOUT,
+        DWMWA_FORCE_ICONIC_REPRESENTATION,
+        DWMWA_FLIP3D_POLICY,
+        DWMWA_EXTENDED_FRAME_BOUNDS,
+        DWMWA_HAS_ICONIC_BITMAP,
+        DWMWA_DISALLOW_PEEK,
+        DWMWA_EXCLUDED_FROM_PEEK,
+        DWMWA_CLOAK,
+        DWMWA_CLOAKED,
+        DWMWA_FREEZE_REPRESENTATION,
+        DWMWA_PASSIVE_UPDATE_MODE,
+        DWMWA_USE_HOSTBACKDROPBRUSH,
+        DWMWA_USE_IMMERSIVE_DARK_MODE_BEFORE_20H1 = 19,
+        DWMWA_USE_IMMERSIVE_DARK_MODE = 20,
+        DWMWA_WINDOW_CORNER_PREFERENCE = 33,
+        DWMWA_BORDER_COLOR,
+        DWMWA_CAPTION_COLOR,
+        DWMWA_TEXT_COLOR,
+        DWMWA_VISIBLE_FRAME_BORDER_THICKNESS,
+        DWMWA_SYSTEMBACKDROP_TYPE,
+        DWMWA_LAST
+    }
+    
+
+
     private LRESULT TvnBeginLabelEdit(NMTVDISPINFOW nmtvdi)
     {
         // Check for invalid node handle
@@ -2604,15 +2636,21 @@ public partial class TreeView : Control
 
             if (thisColorMode == SystemColorMode.Dark)
             {
-                int trueVal = 1;
-                const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
 
-                IntPtr handle = _labelEdit.Handle;
-                Debug.WriteLine(handle.ToString());
 
                 try
                 {
-                    DwmSetWindowAttribute(handle, DWMWA_USE_IMMERSIVE_DARK_MODE, ref trueVal, sizeof(int));
+                    // Try different dwm attributes and values
+                    // May be a problem with WIN 10 compatibility
+
+                    int trueVal = 1;
+                    var attr = (int)DWMWINDOWATTRIBUTE.DWMWA_CAPTION_COLOR;
+                    var attrVal = 0x00ff0000;
+
+                    IntPtr handle = _labelEdit.Handle;
+                    Debug.WriteLine(handle.ToString());
+
+                    DwmSetWindowAttribute(handle, attr, ref attrVal, Marshal.SizeOf(attrVal) );
                 }
                 catch (Exception dwmEx)
                 {
