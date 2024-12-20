6632fc2817a435b63c66f6100f6b7f08bb926032
Mark Daniel Osborne <markosborne@pcblues.com>
2024-12-20 16:15:27 +1000

    Add ScratchProject2 project for testing #12042

---
+++ src/System.Windows.Forms/tests/IntegrationTests/ScratchProject2/Form1.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ScratchProject2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object? sender, EventArgs e)     
+        {
+            treeView1.BeforeLabelEdit += treeView1_BeforeLabelEdit;
+        }
+
+        private void treeView1_BeforeLabelEdit(object? sender, NodeLabelEditEventArgs e)
+        {
+            Console.WriteLine("Here!");
+        } 
+
+    }
+}
---
+++ src/System.Windows.Forms/tests/IntegrationTests/ScratchProject2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ScratchProject2;
+
+// This project is meant for temporary testing and experimenting and should be kept as simple as possible.
+
+internal static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+#pragma warning disable WFO5001
+        Application.SetColorMode(SystemColorMode.Dark);
+        Form1 form = new();
+        Application.Run(form);
+    }
+}
