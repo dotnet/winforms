2d1e0a2e45362858501a10047267e1bba67086da
Wendy Chao (BEYONDSOFT CONSULTING INC) <v-chaowendy@microsoft.com>
2025-01-06 01:52:02 +0000

    add unit test for UserControlDocumentDesigner

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/UserControlDocumentDesignerTests.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+namespace System.Windows.Forms.Design.Tests;
+public class UserControlDocumentDesignerTests
+{
+    [Fact]
+    public void Constructor_ShouldSetAutoResizeHandlesToTrue()
+    {
+        UserControlDocumentDesigner userControl = new();
+
+        userControl.AutoResizeHandles.Should().BeTrue();
+    }
+}
