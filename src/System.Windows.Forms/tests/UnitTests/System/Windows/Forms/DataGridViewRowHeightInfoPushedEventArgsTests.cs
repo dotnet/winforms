7c956a3d654f9910d25d3353817411cbe23b67ee
v-zhgl <38325459+Zheng-Li01@users.noreply.github.com>
2025-01-09 02:34:01 +0800

    Add code coverage for DataGridViewRowHeightInfoPushedEventArgs (#12673)
    related #10453
    
    Proposed changes
    Add unit tests for DataGridViewRowHeightInfoPushedEventArgs.cs to test its properties

---
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/DataGridViewRowHeightInfoPushedEventArgsTests.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+namespace System.Windows.Forms.Tests;
+
+public class DataGridViewRowHeightInfoPushedEventArgsTests
+{
+    [Theory]
+    [InlineData(1, 20, 10)]
+    [InlineData(0, 20, 10)]
+    [InlineData(1, 0, 10)]
+    [InlineData(1, 20, 0)]
+    [InlineData(-1, 20, 10)]
+    [InlineData(1, 20, 5)]
+    public void Ctor_ValidArguments_SetsProperties(int rowIndex, int height, int minimumHeight)
+    {
+        DataGridViewRowHeightInfoPushedEventArgs e = new(rowIndex, height, minimumHeight);
+
+        e.RowIndex.Should().Be(rowIndex);
+        e.Height.Should().Be(height);
+        e.MinimumHeight.Should().Be(minimumHeight);
+    }
+}
