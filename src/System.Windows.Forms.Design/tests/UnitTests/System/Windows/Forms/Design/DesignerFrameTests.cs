6ffca659cc925f6eb8d8aa34688e3aa81415a796
v-norazhou <v-norazhou@win1122h21226>
2025-01-09 08:24:01 +0000

    Cancel reflection on OverlayControl

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DesignerFrameTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DesignerFrameTests.cs
@@ -24,11 +24,8 @@ public class DesignerFrameTests
 
         designerFrame.Text.Should().Be("DesignerFrame");
         designerFrame.BackColor.Should().Be(Color.Red);
-        designerFrame.Controls.Cast<Control>().Should().HaveCount(1);
 
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
-        designerFrame.Controls[0].Should().BeOfType(overlayControlType);
+        designerFrame.Controls.Cast<Control>().Should().HaveCount(1);
     }
 
     [Fact]
@@ -41,15 +38,11 @@ public class DesignerFrameTests
         designerFrame.Initialize(control);
 
         FieldInfo? designerField = typeof(DesignerFrame).GetField("_designer", BindingFlags.NonPublic | BindingFlags.Instance);
-
         object designer = designerField?.GetValue(designerFrame) ?? throw new InvalidOperationException("Field '_designer' not found in DesignerFrame.");
 
         designer.Should().Be(control);
         control.Visible.Should().BeTrue();
         control.Enabled.Should().BeTrue();
-
-        designerFrame.Controls.Cast<Control>().ToList().ForEach(c =>
-            Console.WriteLine($"Control Type: {c.GetType()}, Control Name: {c.Name}"));
 
         designerFrame.Controls.Cast<Control>().Should().NotContain(control);
     }
@@ -100,108 +93,21 @@ public class DesignerFrameTests
         Mock<ISite> mockSite = new();
         using DesignerFrame designerFrame = new(mockSite.Object);
 
-        designerFrame.Resize += (sender, args) => { /* Some behavior on resize */ };
+        designerFrame.Resize += (sender, args) => { /* Handle resize if needed */ };
+
         designerFrame.Width = 500;
 
         designerFrame.Width.Should().Be(500);
     }
 
     [Fact]
-    public void CanReflectOverlayControlType()
+    public void Designer_ProcessDialogKey()
     {
-        Type designerFrameType = typeof(DesignerFrame);
+        Mock<ISite> mockSite = new();
+        using DesignerFrame designerFrame = new(mockSite.Object);
 
-        Type? overlayControlType = designerFrameType.GetNestedType("OverlayControl", BindingFlags.NonPublic);
+        bool result = designerFrame.TestAccessor().Dynamic.ProcessDialogKey(Keys.Enter);
 
-        overlayControlType.Should().NotBeNull();
-        if (overlayControlType is not null)
-        {
-            Console.WriteLine($"OverlayControl type found: {overlayControlType.FullName}");
-        }
-    }
-
-    [Fact]
-    public void CanReflectOverlayControlConstructor()
-    {
-        Type designerFrameType = typeof(DesignerFrame);
-        Type? overlayControlType = designerFrameType.GetNestedType("OverlayControl", BindingFlags.NonPublic);
-
-        overlayControlType.Should().NotBeNull();
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-
-        constructor.Should().NotBeNull();
-    }
-
-    [Fact]
-    public void CanCreateOverlayControlInstance()
-    {
-        Mock<IServiceProvider> mockProvider = new();
-
-        Type designerFrameType = typeof(DesignerFrame);
-
-        Type? overlayControlType = designerFrameType.GetNestedType("OverlayControl", BindingFlags.NonPublic);
-
-        overlayControlType.Should().NotBeNull();
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-
-        constructor.Should().NotBeNull();
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-
-        overlayControl.Should().NotBeNull();
-    }
-
-    [Fact]
-    public void CanAccessOverlayControlControlsProperty()
-    {
-        Mock<IServiceProvider> mockProvider = new();
-
-        Type designerFrameType = typeof(DesignerFrame);
-
-        Type? overlayControlType = designerFrameType.GetNestedType("OverlayControl", BindingFlags.NonPublic);
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-
-        PropertyInfo? controlsProperty = overlayControlType?.GetProperty("Controls", BindingFlags.Instance | BindingFlags.Public);
-
-        Control.ControlCollection? controls = controlsProperty?.GetValue(overlayControl) as Control.ControlCollection;
-
-        controls.Should().NotBeNull();
-    }
-
-    [Fact]
-    public void CanAddControlsToOverlayControl()
-    {
-        Mock<IServiceProvider> mockProvider = new();
-
-        Type designerFrameType = typeof(DesignerFrame);
-
-        Type? overlayControlType = designerFrameType.GetNestedType("OverlayControl", BindingFlags.NonPublic);
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-
-        PropertyInfo? controlsProperty = overlayControlType?.GetProperty("Controls", BindingFlags.Instance | BindingFlags.Public);
-
-        Control.ControlCollection? controls = controlsProperty?.GetValue(overlayControl) as Control.ControlCollection;
-
-        controls.Should().NotBeNull();
-        controls.Should().BeOfType<Control.ControlCollection>();
-
-        Control control1 = new() { Bounds = new Rectangle(0, 0, 100, 100) };
-        Control control2 = new() { Bounds = new Rectangle(100, 100, 100, 100) };
-
-        controls?.Add(control1);
-        controls?.Add(control2);
-
-        List<Control>? controlsList = controls?.Cast<Control>().ToList();
-
-        controlsList.Should().Contain(control1);
-        controlsList.Should().Contain(control2);
+        result.Should().BeFalse();
     }
 }
