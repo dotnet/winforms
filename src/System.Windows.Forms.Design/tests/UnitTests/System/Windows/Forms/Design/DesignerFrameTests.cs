a979bd545173b604e470f0124d658abc31c334e5
v-norazhou <v-norazhou@win1122h21226>
2025-01-09 02:20:36 +0000

    update

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DesignerFrameTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DesignerFrameTests.cs
@@ -55,225 +55,55 @@ public class DesignerFrameTests
     }
 
     [Fact]
-    public void OverlayControl_Constructor_SetsProperties()
+    public void DesignerFrame_ProcessDialogKey_ReturnsExpectedResult()
     {
-        Mock<IServiceProvider> mockProvider = new();
+        Mock<ISite> mockSite = new();
+        using DesignerFrame designerFrame = new(mockSite.Object);
 
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-        constructor.Should().NotBeNull();
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-        overlayControl.Should().NotBeNull();
+        bool result = designerFrame.TestAccessor().Dynamic.ProcessDialogKey(Keys.Enter);
 
-        FieldInfo? providerField = overlayControlType?.GetField("_provider", BindingFlags.NonPublic | BindingFlags.Instance);
-        FieldInfo? overlayListField = overlayControlType?.GetField("_overlayList", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        providerField?.GetValue(overlayControl).Should().Be(mockProvider.Object);
-        overlayListField?.GetValue(overlayControl).Should().NotBeNull();
-
-        ((bool)overlayControlType?.GetProperty("AutoScroll")?.GetValue(overlayControl)!).Should().BeTrue();
-        overlayControlType?.GetProperty("Text")?.GetValue(overlayControl).Should().Be("OverlayControl");
+        result.Should().BeFalse();
     }
 
     [Fact]
-    public void OverlayControl_PushOverlay_AddsControl()
+    public void DesignerFrame_Constructor_SetsSiteProperty()
     {
-        Mock<IServiceProvider> mockProvider = new();
+        Mock<ISite> mockSite = new();
 
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
+        using DesignerFrame designerFrame = new(mockSite.Object);
 
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-        constructor.Should().NotBeNull();
+        if (designerFrame.Site is null)
+        {
+            PropertyInfo? siteProperty = typeof(DesignerFrame).GetProperty("Site", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            siteProperty?.SetValue(designerFrame, mockSite.Object);
+        }
 
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-        overlayControl.Should().NotBeNull();
+        designerFrame.Should().NotBeNull();
+        designerFrame.Site.Should().Be(mockSite.Object);
+    }
 
+    [Fact]
+    public void DesignerFrame_AddControl_AddsControlToFrame()
+    {
+        Mock<ISite> mockSite = new();
+        using DesignerFrame designerFrame = new(mockSite.Object);
         Control control = new();
 
-        MethodInfo? pushOverlayMethod = overlayControlType?.GetMethod("PushOverlay");
-        pushOverlayMethod.Should().NotBeNull();
+        designerFrame.Controls.Add(control);
 
-        object? index = pushOverlayMethod?.Invoke(overlayControl, [control]);
-
-        FieldInfo? overlayListField = overlayControlType?.GetField("_overlayList", BindingFlags.NonPublic | BindingFlags.Instance);
-        List<Control>? overlayList = overlayListField?.GetValue(overlayControl) as List<Control>;
-
-        overlayList.Should().NotBeNull();
-        overlayList.Should().Contain(control);
-        index.Should().Be(0);
-
-        if ((bool)overlayControlType?.GetProperty("IsHandleCreated")?.GetValue(overlayControl)!)
-        {
-            FieldInfo? parentField = typeof(Control).GetField("_parent", BindingFlags.NonPublic | BindingFlags.Instance);
-            parentField?.GetValue(control).Should().Be(overlayControl);
-
-            PropertyInfo? displayRectangleProperty = overlayControlType?.GetProperty("DisplayRectangle");
-            displayRectangleProperty?.GetValue(overlayControl).Should().Be(control.Bounds);
-        }
+        designerFrame.Controls.Cast<Control>().Should().Contain(control);
     }
 
     [Fact]
-    public void OverlayControl_RemoveOverlay_RemovesControl()
+    public void DesignerFrame_Resize_TriggersExpectedBehavior()
     {
-        Mock<IServiceProvider> mockProvider = new();
+        Mock<ISite> mockSite = new();
+        using DesignerFrame designerFrame = new(mockSite.Object);
 
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
+        designerFrame.Resize += (sender, args) => { /* Some behavior on resize */ };
+        designerFrame.Width = 500;
 
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-        constructor.Should().NotBeNull();
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-        overlayControl.Should().NotBeNull();
-
-        Control control = new();
-
-        MethodInfo? pushOverlayMethod = overlayControlType?.GetMethod("PushOverlay");
-        pushOverlayMethod.Should().NotBeNull();
-        pushOverlayMethod?.Invoke(overlayControl, [control]);
-
-        MethodInfo? removeOverlayMethod = overlayControlType?.GetMethod("RemoveOverlay");
-        removeOverlayMethod.Should().NotBeNull();
-        removeOverlayMethod?.Invoke(overlayControl, [control]);
-
-        FieldInfo? overlayListField = overlayControlType?.GetField("_overlayList", BindingFlags.NonPublic | BindingFlags.Instance);
-        List<Control>? overlayList = overlayListField?.GetValue(overlayControl) as List<Control>;
-
-        overlayList.Should().NotBeNull();
-        overlayList.Should().NotContain(control);
-        control.Visible.Should().BeFalse();
-        control.Parent.Should().BeNull();
-    }
-
-    [Fact]
-    public void OverlayControl_InsertOverlay_InsertsControlAtIndex()
-    {
-        Mock<IServiceProvider> mockProvider = new();
-
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-        constructor.Should().NotBeNull();
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-        overlayControl.Should().NotBeNull();
-
-        Control control1 = new();
-        Control control2 = new();
-
-        MethodInfo? pushOverlayMethod = overlayControlType?.GetMethod("PushOverlay");
-        pushOverlayMethod.Should().NotBeNull();
-        pushOverlayMethod?.Invoke(overlayControl, [control1]);
-
-        MethodInfo? insertOverlayMethod = overlayControlType?.GetMethod("InsertOverlay");
-        insertOverlayMethod.Should().NotBeNull();
-        insertOverlayMethod?.Invoke(overlayControl, [control2, 0]);
-
-        FieldInfo? overlayListField = overlayControlType?.GetField("_overlayList", BindingFlags.NonPublic | BindingFlags.Instance);
-        List<Control>? overlayList = overlayListField?.GetValue(overlayControl) as List<Control>;
-
-        overlayList.Should().NotBeNull();
-        overlayList?[0].Should().Be(control2);
-        overlayList?[1].Should().Be(control1);
-
-        if ((bool)overlayControlType?.GetProperty("IsHandleCreated")?.GetValue(overlayControl)!)
-        {
-            FieldInfo? parentField = typeof(Control).GetField("_parent", BindingFlags.NonPublic | BindingFlags.Instance);
-            parentField?.GetValue(control2).Should().Be(overlayControl);
-
-            PropertyInfo? displayRectangleProperty = overlayControlType?.GetProperty("DisplayRectangle");
-            displayRectangleProperty?.GetValue(overlayControl).Should().Be(control2.Bounds);
-        }
-    }
-
-    [Fact]
-    public void OverlayControl_InvalidateOverlays_InvokesInvalidateOnIntersectingControls()
-    {
-        Mock<IServiceProvider> mockProvider = new();
-
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-        constructor.Should().NotBeNull();
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-        overlayControl.Should().NotBeNull();
-
-        Control control1 = new() { Bounds = new Rectangle(0, 0, 100, 100) };
-        Control control2 = new() { Bounds = new Rectangle(50, 50, 100, 100) };
-
-        MethodInfo? pushOverlayMethod = overlayControlType?.GetMethod("PushOverlay");
-        pushOverlayMethod.Should().NotBeNull();
-        pushOverlayMethod?.Invoke(overlayControl, [control1]);
-        pushOverlayMethod?.Invoke(overlayControl, [control2]);
-
-        MethodInfo? invalidateOverlaysMethod = overlayControlType?.GetMethod("InvalidateOverlays", [typeof(Rectangle)]);
-        invalidateOverlaysMethod.Should().NotBeNull();
-        invalidateOverlaysMethod?.Invoke(overlayControl, [new Rectangle(25, 25, 50, 50)]);
-
-        FieldInfo? overlayListField = overlayControlType?.GetField("_overlayList", BindingFlags.NonPublic | BindingFlags.Instance);
-        List<Control>? overlayList = overlayListField?.GetValue(overlayControl) as List<Control>;
-
-        overlayList.Should().NotBeNull();
-        overlayList.Should().Contain(control1);
-        overlayList.Should().Contain(control2);
-
-        MethodInfo? invalidateMethod = typeof(Control).GetMethod("Invalidate", [typeof(Rectangle)]);
-        invalidateMethod.Should().NotBeNull();
-
-        invalidateMethod?.Invoke(control1, [new Rectangle(25, 25, 50, 50)]);
-        invalidateMethod?.Invoke(control2, [new Rectangle(25, 25, 50, 50)]);
-    }
-
-    [Fact]
-    public void OverlayControl_InvalidateOverlays_InvokesInvalidateOnIntersectingControlsWithRegion()
-    {
-        Mock<IServiceProvider> mockProvider = new();
-
-        Type? overlayControlType = typeof(DesignerFrame).GetNestedType("OverlayControl", BindingFlags.NonPublic);
-        overlayControlType.Should().NotBeNull();
-
-        ConstructorInfo? constructor = overlayControlType?.GetConstructor([typeof(IServiceProvider)]);
-        constructor.Should().NotBeNull();
-
-        object? overlayControl = constructor?.Invoke([mockProvider.Object]);
-        overlayControl.Should().NotBeNull();
-
-        Control control1 = new() { Bounds = new Rectangle(0, 0, 100, 100) };
-        Control control2 = new() { Bounds = new Rectangle(50, 50, 100, 100) };
-
-        MethodInfo? pushOverlayMethod = overlayControlType?.GetMethod("PushOverlay");
-        pushOverlayMethod.Should().NotBeNull();
-        pushOverlayMethod?.Invoke(overlayControl, [control1]);
-        pushOverlayMethod?.Invoke(overlayControl, [control2]);
-
-        MethodInfo? invalidateOverlaysMethod = overlayControlType?.GetMethod("InvalidateOverlays", [typeof(Region)]);
-        invalidateOverlaysMethod.Should().NotBeNull();
-
-        using (var region = new Region(new Rectangle(25, 25, 50, 50)))
-        {
-            invalidateOverlaysMethod?.Invoke(overlayControl, [region]);
-        }
-
-        FieldInfo? overlayListField = overlayControlType?.GetField("_overlayList", BindingFlags.NonPublic | BindingFlags.Instance);
-        List<Control>? overlayList = overlayListField?.GetValue(overlayControl) as List<Control>;
-
-        overlayList.Should().NotBeNull();
-        overlayList.Should().Contain(control1);
-        overlayList.Should().Contain(control2);
-
-        MethodInfo? invalidateMethod = typeof(Control).GetMethod("Invalidate", [typeof(Region)]);
-        invalidateMethod.Should().NotBeNull();
-
-        using (var region = new Region(new Rectangle(25, 25, 50, 50)))
-        {
-            invalidateMethod?.Invoke(control1, [region]);
-            invalidateMethod?.Invoke(control2, [region]);
-        }
+        designerFrame.Width.Should().Be(500);
     }
 
     [Fact]
