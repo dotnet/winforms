59e0f00e2f110dc831da7359c0793d81eabaccc1
v-chaowendy <v-chaowendy@24H2W11>
2025-01-14 03:35:14 +0000

    grammar error

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/FocusableLabelTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/FocusableLabelTests.cs
@@ -4,31 +4,26 @@
 #nullable enable
 
 namespace System.Windows.Forms;
+
 public class FocusableLabelTests
 {
     [WinFormsFact]
     public void UnderlineWhenFocused_DefaultValue_ShouldBeTrue()
     {
-        // Arrange
-        var label = new FocusableLabel();
+        FocusableLabel label = new();
 
-        // Act
         var result = label.UnderlineWhenFocused;
 
-        // Assert
         result.Should().BeTrue();
     }
 
     [WinFormsFact]
     public void FocusableLabel_ShouldBeSelectableAndTabStop()
     {
-        // Arrange
-        var label = new FocusableLabel();
+        FocusableLabel label = new();
 
-        // Act
         var isTabStop = label.TabStop;
 
-        // Assert
         isTabStop.Should().BeTrue("Expected TabStop to be true, indicating the control is selectable.");
     }
 }
