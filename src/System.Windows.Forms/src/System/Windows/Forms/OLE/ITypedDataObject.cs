77d7bfd435423bdb05a2db793f0a4ce396741f01
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2025-01-23 13:25:35 -0800

    Refactor Clipboard code to internal interface
    This refactors the Clipboard code to utilize an internal interface to facilitate moving most logic to the Core assembly.
    
    - Add and utilize IDataObjectInternal that "composes" ITypedDataObject and IDataObject
    - Move mapped formats to DataFormatNames
    - Move RestrictedTypeDesrializationException
    - De-nest internal DataObject classes to prepare for moving
    - Add DataObject derived internal class for tracking user provided data

--- src/System.Windows.Forms/src/System/Windows/Forms/OLE/ITypedDataObject.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/OLE/ITypedDataObject.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Reflection.Metadata;
-using System.Runtime.Serialization.Formatters.Binary;
+using System.Private.Windows.Core.Ole;
 
 namespace System.Windows.Forms;
 
@@ -21,61 +21,22 @@ namespace System.Windows.Forms;
 /// </remarks>
 public interface ITypedDataObject : IDataObject
 {
-    /// <summary>
-    ///  Retrieves data associated with data format named after <typeparamref name="T"/>,
-    ///  if that data is of type <typeparamref name="T"/>.
-    /// </summary>
-    /// <inheritdoc cref="TryGetData{T}(string, Func{TypeName, Type}, bool, out T)"/>
+    /// <inheritdoc cref="IDataObjectInternal.TryGetData{T}(out T)"/>
     bool TryGetData<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(
         [NotNullWhen(true), MaybeNullWhen(false)] out T data);
 
-    /// <summary>
-    ///  Retrieves data associated with the specified format if that data is of type <typeparamref name="T"/>.
-    /// </summary>
-    /// <inheritdoc cref="TryGetData{T}(string, Func{TypeName, Type}, bool, out T)"/>
+    /// <inheritdoc cref="IDataObjectInternal.TryGetData{T}(string, out T)" />
     bool TryGetData<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(
         string format,
         [NotNullWhen(true), MaybeNullWhen(false)] out T data);
 
-    /// <summary>
-    ///  Retrieves data in a specified format if that data is of type <typeparamref name="T"/>,
-    ///  optionally converting the data to the specified format.
-    /// </summary>
-    /// <inheritdoc cref="TryGetData{T}(string, Func{TypeName, Type}, bool, out T)"/>
+    /// <inheritdoc cref="IDataObjectInternal.TryGetData{T}(string, bool, out T)" />
     bool TryGetData<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(
         string format,
         bool autoConvert,
         [NotNullWhen(true), MaybeNullWhen(false)] out T data);
 
-    /// <summary>
-    ///  <see cref="BinaryFormatter"/> compatible overload that retrieves typed data associated with the specified data format.
-    /// </summary>
-    /// <param name="resolver">
-    ///  A user-provided function that defines a closure of <see cref="Type"/>s that can be retrieved from
-    ///  the exchange medium.
-    /// </param>
-    /// <param name="format">
-    ///  A string that specifies what format to retrieve the data as. See the <see cref="DataFormats"/> class for
-    ///  a set of predefined data formats.
-    /// </param>
-    /// <param name="autoConvert">
-    ///  <see langword="true"/> to attempt to automatically convert the data to the specified format;
-    ///  <see langword="false"/> for no data format conversion.
-    /// </param>
-    /// <param name="data">
-    ///  A data object with the data in the specified format, or <see langword="null"/> if the data is not available
-    ///  in the specified format or is of a wrong type.
-    /// </param>
-    /// <returns>
-    ///  <see langword="true"/> if the data of this format is present and the value is
-    ///  of a matching type and that value can be successfully retrieved, or <see langword="false"/>
-    ///  if the format is not present or the value is not of the right type.
-    /// </returns>
-    /// <remarks>
-    ///  <para>
-    ///   Implement this method for backward compatibility with binary formatted data when binary formatters are enabled.
-    ///  </para>
-    /// </remarks>
+    /// <inheritdoc cref="IDataObjectInternal.TryGetData{T}(string, Func{TypeName, Type}, bool, out T)" />
     bool TryGetData<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>(
         string format,
 #pragma warning disable CS3001 // Argument type is not CLS-compliant
