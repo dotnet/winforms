e3305421b3a14d5d295f5073cfbbfa7388c5d755
Epica3055 <v-epicawang@microsoft.com>
2025-02-08 13:58:19 +0800

    port DataGridViewColumnDataPropertyNameEditor   and DataMemberListEditor

---
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DataGridViewColumnDataPropertyNameEditor.cs
@@ -0,0 +1,78 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace System.Windows.Forms.Design;
+internal class DataGridViewColumnDataPropertyNameEditor : UITypeEditor
+{
+    // FxCop made me add this constructor.
+    public DataGridViewColumnDataPropertyNameEditor() : base() { }
+
+    private DesignBindingPicker? _designBindingPicker;
+
+    public override bool IsDropDownResizable
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public override object? EditValue(ITypeDescriptorContext? context, IServiceProvider provider, object? value)
+    {
+        if (provider is not null && context is not null && context.Instance is not null)
+        {
+            DataGridViewColumnCollectionDialog.ListBoxItem? item = context.Instance as DataGridViewColumnCollectionDialog.ListBoxItem;
+            DataGridView? dataGridView;
+            if (item is not null)
+            {
+                dataGridView = item.DataGridViewColumn.DataGridView;
+            }
+            else
+            {
+                DataGridViewColumn? col = context.Instance as DataGridViewColumn;
+                dataGridView = col?.DataGridView;
+            }
+
+            if (dataGridView is null)
+            {
+                return value;
+            }
+
+            object? dataSource = dataGridView.DataSource;
+            string? dataMember = dataGridView.DataMember;
+            string? valueString = value as string;
+            string? selectedMember = dataMember + "." + valueString;
+            if (dataSource is null)
+            {
+                dataMember = string.Empty;
+                selectedMember = valueString;
+            }
+
+            _designBindingPicker ??= new DesignBindingPicker();
+
+            DesignBinding oldSelection = new DesignBinding(dataSource, selectedMember);
+            DesignBinding? newSelection = _designBindingPicker.Pick(context,
+                provider,
+                showDataSources: false,
+                showDataMembers: true,
+                selectListMembers: false,
+                dataSource,
+                dataMember,
+                oldSelection);
+            if (dataSource is not null && newSelection is not null)
+            {
+                value = newSelection.DataField;
+            }
+        }
+
+        return value;
+    }
+
+    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context)
+    {
+        return UITypeEditorEditStyle.DropDown;
+    }
+}
---
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DataMemberListEditor.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.Drawing.Design;
+
+namespace System.Windows.Forms.Design;
+
+internal class DataMemberListEditor : UITypeEditor
+{
+    private DesignBindingPicker? _designBindingPicker;
+
+    public override bool IsDropDownResizable
+    {
+        get
+        {
+            return true;
+        }
+    }
+
+    public override object? EditValue(ITypeDescriptorContext? context, IServiceProvider provider, object? value)
+    {
+        if (provider is not null && context is not null && context.Instance is not null)
+        {
+            PropertyDescriptor? dataSourceProperty = TypeDescriptor.GetProperties(context.Instance)["DataSource"];
+            if (dataSourceProperty is not null)
+            {
+                object? dataSource = dataSourceProperty.GetValue(context.Instance);
+                _designBindingPicker ??= new DesignBindingPicker();
+
+                DesignBinding oldSelection = new DesignBinding(dataSource, value as string);
+                DesignBinding? newSelection = _designBindingPicker.Pick(context,
+                    provider,
+                    showDataSources: false,
+                    showDataMembers: true,
+                    selectListMembers: true,
+                    dataSource,
+                    string.Empty,
+                    oldSelection);
+                if (dataSource is not null && newSelection is not null)
+                {
+                    value = newSelection.DataMember;
+                }
+            }
+        }
+
+        return value;
+    }
+
+    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext? context)
+    {
+        return UITypeEditorEditStyle.DropDown;
+    }
+}
