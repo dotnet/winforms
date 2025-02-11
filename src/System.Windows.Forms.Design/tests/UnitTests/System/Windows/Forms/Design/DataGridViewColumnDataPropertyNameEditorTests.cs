2dc194bd7a3332f45691ee48107e956d80ce87e3
Epica3055 <v-epicawang@microsoft.com>
2025-02-11 16:22:48 +0800

    add unit test

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DataGridViewColumnDataPropertyNameEditorTests.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.Reflection;
+using Castle.Core.Internal;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class DataGridViewColumnDataPropertyNameEditorTests
+{
+    [Fact]
+    public void DataGridViewColumnDataPropertyNameEditorExist()
+    {
+        PropertyInfo propertyInfo = typeof(DataGridViewColumn).GetProperty(nameof(DataGridViewColumn.DataPropertyName));
+
+        string editorTypeName = propertyInfo.GetAttribute<EditorAttribute>().EditorTypeName;
+
+        Type.GetType(editorTypeName).Should().NotBeNull();
+    }
+}
---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DataMemberListEditorTests.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+using System.Reflection;
+using Castle.Core.Internal;
+
+namespace System.Windows.Forms.Design.Tests;
+
+public class DataMemberListEditorTests
+{
+    [Fact]
+    public void DataMemberListEditorExist()
+    {
+        PropertyInfo propertyInfo = typeof(DataGridView).GetProperty(nameof(DataGridView.DataMember));
+
+        string editorTypeName = propertyInfo.GetAttribute<EditorAttribute>().EditorTypeName;
+
+        Type.GetType(editorTypeName).Should().NotBeNull();
+    }
+}
