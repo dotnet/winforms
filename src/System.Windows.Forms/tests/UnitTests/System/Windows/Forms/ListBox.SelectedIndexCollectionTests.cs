9c1a960599ad822ed8c1d845e7800c4035865850
v-zhgl <38325459+Zheng-Li01@users.noreply.github.com>
2025-02-12 21:57:37 +0000

    Addd code coverage for ListBox.SelectedIndexCollection (#12907)
    * Addd code coverage for ListBox.SelectedIndexCollection

--- src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/ListBox.SelectedIndexCollectionTests.cs
+++ src/System.Windows.Forms/tests/UnitTests/System/Windows/Forms/ListBox.SelectedIndexCollectionTests.cs
@@ -1,13 +1,15 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#nullable enable
+
 using System.Collections;
 
 namespace System.Windows.Forms.Tests;
 
 public class ListBoxSelectedIndexCollectionTests
 {
-    [WinFormsFact]
+    [Fact]
     public void ListBoxSelectedIndexCollection_Ctor_ListBox()
     {
         using ListBox owner = new();
@@ -16,14 +18,14 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.True(collection.IsReadOnly);
     }
 
-    [WinFormsFact]
+    [Fact]
     public void ListBoxSelectedIndexCollection_Ctor_NullOwner_ThrowsArgumentNullException()
     {
         using ListBox owner = new();
-        Assert.Throws<ArgumentNullException>("owner", () => new ListBox.SelectedIndexCollection(null));
+        Assert.Throws<ArgumentNullException>("owner", () => new ListBox.SelectedIndexCollection(null!));
     }
 
-    [WinFormsFact]
+    [Fact]
     public void ListBoxSelectedIndexCollection_ICollection_Properties_GetReturnsExpected()
     {
         using ListBox owner = new();
@@ -33,7 +35,7 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Same(collection, collection.SyncRoot);
     }
 
-    [WinFormsFact]
+    [Fact]
     public void ListBoxSelectedIndexCollection_IList_Properties_GetReturnsExpected()
     {
         using ListBox owner = new();
@@ -45,10 +47,10 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Same(collection, collection.SyncRoot);
     }
 
-    [WinFormsTheory]
-    [InlineData(-1, null)]
-    [InlineData(0, null)]
-    [InlineData(1, null)]
+    [Theory]
+    [InlineData(-1, 0)]
+    [InlineData(0, 0)]
+    [InlineData(1, 0)]
     [InlineData(-1, 1)]
     [InlineData(0, 1)]
     [InlineData(1, 1)]
@@ -62,8 +64,8 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Throws<NotSupportedException>(() => collection[index] = value);
     }
 
-    [WinFormsTheory]
-    [InlineData(null)]
+    [Theory]
+    [InlineData(0)]
     [InlineData(1)]
     [InlineData("1")]
     public void ListBoxSelectedIndexCollection_IListAdd_Invoke_ThrowsNotSupportedException(object value)
@@ -73,7 +75,7 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Throws<NotSupportedException>(() => collection.Add(value));
     }
 
-    [WinFormsFact]
+    [Fact]
     public void ListBoxSelectedIndexCollection_IListClear_Invoke_ThrowsNotSupportedException()
     {
         using ListBox owner = new();
@@ -81,10 +83,10 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Throws<NotSupportedException>(collection.Clear);
     }
 
-    [WinFormsTheory]
-    [InlineData(-1, null)]
-    [InlineData(0, null)]
-    [InlineData(1, null)]
+    [Theory]
+    [InlineData(-1, 0)]
+    [InlineData(0, 0)]
+    [InlineData(1, 0)]
     [InlineData(-1, 1)]
     [InlineData(0, 1)]
     [InlineData(1, 1)]
@@ -98,8 +100,8 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Throws<NotSupportedException>(() => collection.Insert(index, value));
     }
 
-    [WinFormsTheory]
-    [InlineData(null)]
+    [Theory]
+    [InlineData(0)]
     [InlineData(1)]
     [InlineData("1")]
     public void ListBoxSelectedIndexCollection_IListRemove_Invoke_ThrowsNotSupportedException(object value)
@@ -109,7 +111,7 @@ public class ListBoxSelectedIndexCollectionTests
         Assert.Throws<NotSupportedException>(() => collection.Remove(value));
     }
 
-    [WinFormsTheory]
+    [Theory]
     [InlineData(-1)]
     [InlineData(0)]
     [InlineData(1)]
@@ -118,5 +120,52 @@ public class ListBoxSelectedIndexCollectionTests
         using ListBox owner = new();
         IList collection = new ListBox.SelectedIndexCollection(owner);
         Assert.Throws<NotSupportedException>(() => collection.RemoveAt(index));
+    }
+
+    [Fact]
+    public void CopyTo_CopiesItemsCorrectly()
+    {
+        using ListBox listBox = new()
+        {
+            Items = { "Item1", "Item2", "Item3" },
+            SelectedIndices = { 0, 2 }
+        };
+
+        int[] destination = new int[2];
+        listBox.SelectedIndices.CopyTo(destination, 0);
+
+        destination.Should().BeEquivalentTo(new int[] { 0, 2 });
+    }
+
+    [Fact]
+    public void Clear_ClearsSelectedIndices()
+    {
+        using ListBox listBox = new()
+        {
+            Items = { "Item1", "Item2", "Item3" },
+            SelectedIndices = { 0, 2 }
+        };
+
+        listBox.SelectedIndices.Clear();
+
+        listBox.SelectedIndices.Count.Should().Be(0);
+        listBox.SelectedIndices.Contains(0).Should().BeFalse();
+        listBox.SelectedIndices.Contains(2).Should().BeFalse();
+    }
+
+    [Fact]
+    public void Remove_RemovesSelectedIndex()
+    {
+        using ListBox listBox = new()
+        {
+            Items = { "Item1", "Item2", "Item3" },
+            SelectedIndices = { 0, 2 }
+        };
+
+        listBox.SelectedIndices.Remove(2);
+
+        listBox.SelectedIndices.Count.Should().Be(1);
+        listBox.SelectedIndices.Contains(0).Should().BeTrue();
+        listBox.SelectedIndices.Contains(2).Should().BeFalse();
     }
 }
