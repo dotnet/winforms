baf3a3ee0cf86c4225281d3969914960e9fb92fc
Zheng Li (Beyondsoft Corporation) <v-zhgl@microsoft.com>
2025-02-19 10:05:31 +0800

    Handle FeedBacks

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ToolStripCustomTypeDescriptorTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ToolStripCustomTypeDescriptorTests.cs
@@ -18,7 +18,8 @@ public class ToolStripCustomTypeDescriptorTests : IDisposable
         _descriptor = new(_toolStrip);
     }
 
-    public void Dispose() => _toolStrip.Dispose();
+    public void Dispose()
+        => _toolStrip.Dispose();
 
     [Fact]
     public void Constructor_InitializesInstance()
@@ -28,8 +29,8 @@ public class ToolStripCustomTypeDescriptorTests : IDisposable
     }
 
     [Fact]
-    public void GetPropertyOwner_ReturnsInstance() =>
-        _descriptor.GetPropertyOwner(null).Should().Be(_toolStrip);
+    public void GetPropertyOwner_ReturnsInstance()
+        => _descriptor.GetPropertyOwner(null).Should().Be(_toolStrip);
 
     [Fact]
     public void GetProperties_ReturnsCachedCollection()
@@ -52,8 +53,8 @@ public class ToolStripCustomTypeDescriptorTests : IDisposable
     }
 
     [Fact]
-    public void GetProperties_RemovesItemsProperty() =>
-        _descriptor.GetProperties().Cast<PropertyDescriptor>().Should().NotContain(p => p.Name == "Items");
+    public void GetProperties_RemovesItemsProperty()
+        => _descriptor.GetProperties().Cast<PropertyDescriptor>().Should().NotContain(p => p.Name == "Items");
 
     [Fact]
     public void GetProperties_WithAttributes_RemovesItemsProperty()
