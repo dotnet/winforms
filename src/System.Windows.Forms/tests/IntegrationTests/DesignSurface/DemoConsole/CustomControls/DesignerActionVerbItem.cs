8d68587b7ee6eef801781d24787b61d8b17f2a0e
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2025-02-19 16:27:49 -0300

    Adds implicit usings to DemoConsole and DesignSurfaceExt projects

--- src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DemoConsole/CustomControls/DesignerActionVerbItem.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DemoConsole/CustomControls/DesignerActionVerbItem.cs
@@ -1,9 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.ComponentModel.Design;
-using System.Diagnostics;
-
 namespace TestConsole;
 
 internal class DesignerActionVerbItem : DesignerActionMethodItem
--- src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DemoConsole/MainForm.cs
+++ src/System.Windows.Forms/tests/IntegrationTests/DesignSurface/DemoConsole/MainForm.cs
@@ -1,14 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Drawing;
-using System.Windows.Forms;
-using System.ComponentModel.Design;
-
-using DesignSurfaceExt;
-using Timer = System.Windows.Forms.Timer;
-using System.ComponentModel;
-
 namespace TestConsole;
 
 public partial class MainForm : Form
