bb7645a363d6a388f3b7e89dfbb49333523604f2
Nora Zhou (BEYONDSOFT CONSULTING INC) <v-norazhou@microsoft.com>
2025-02-28 05:50:30 +0000

    Update

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ToolStripAdornerWindowServiceTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ToolStripAdornerWindowServiceTests.cs
@@ -12,65 +12,25 @@ namespace System.Windows.Forms.Design.Tests;
 
 public class ToolStripAdornerWindowServiceTests
 {
-    private class TestDesignerFrame : DesignerFrame
-    {
-        public TestDesignerFrame(ISite site) : base(site) { }
-    }
-
-    private class BehaviorServiceWrapper : IDisposable
-    {
-        public Mock<IServiceProvider> ServiceProviderMock { get; }
-        public Mock<IOverlayService> OverlayServiceMock { get; }
-        public BehaviorService BehaviorService { get; }
-
-        public BehaviorServiceWrapper()
-        {
-            ServiceProviderMock = new Mock<IServiceProvider>();
-            OverlayServiceMock = new Mock<IOverlayService>();
-
-            Mock<ISite> siteMock = new();
-            siteMock.Setup(s => s.GetService(typeof(IOverlayService))).Returns(OverlayServiceMock.Object);
-
-            TestDesignerFrame designerFrame = new(siteMock.Object);
-            BehaviorService = new(ServiceProviderMock.Object, designerFrame);
-
-            SetupCommonMocks(ServiceProviderMock, OverlayServiceMock);
-        }
-
-        private void SetupCommonMocks(Mock<IServiceProvider> serviceProviderMock, Mock<IOverlayService> overlayServiceMock)
-        {
-            serviceProviderMock.Setup(sp => sp.GetService(typeof(IOverlayService))).Returns(overlayServiceMock.Object);
-            serviceProviderMock.Setup(sp => sp.GetService(typeof(BehaviorService))).Returns(BehaviorService);
-        }
-
-        public void Dispose()
-        {
-            BehaviorService?.Dispose();
-        }
-    }
-
-    private class TestControl : Control
-    {
-        private readonly Graphics _graphics;
-
-        public TestControl(Graphics graphics) => _graphics = graphics;
-
-        public Graphics TestGraphics => _graphics;
-
-        public new Point PointToScreen(Point point) => new Point(point.X + 8, point.Y + 31);
-    }
-
-    private readonly BehaviorServiceWrapper _behaviorServiceWrapper;
+    private readonly Mock<IServiceProvider> _serviceProviderMock;
+    private readonly Mock<IOverlayService> _overlayServiceMock;
+    private readonly BehaviorService _behaviorService;
     private readonly ToolStripAdornerWindowService _service;
 
-    public ToolStripAdornerWindowServiceTests() => (_behaviorServiceWrapper, _service) = Initialize();
-
-    private (BehaviorServiceWrapper, ToolStripAdornerWindowService) Initialize()
+    public ToolStripAdornerWindowServiceTests()
     {
-        BehaviorServiceWrapper behaviorServiceWrapper = new();
+        _serviceProviderMock = new();
+        _overlayServiceMock = new();
+
+        Mock<ISite> siteMock = new();
+        siteMock.Setup(s => s.GetService(typeof(IOverlayService))).Returns(_overlayServiceMock.Object);
+        _behaviorService = new(_serviceProviderMock.Object, new(siteMock.Object));
+
+        _serviceProviderMock.Setup(sp => sp.GetService(typeof(IOverlayService))).Returns(_overlayServiceMock.Object);
+        _serviceProviderMock.Setup(sp => sp.GetService(typeof(BehaviorService))).Returns(_behaviorService);
+
         using Control control = new();
-        var service = new ToolStripAdornerWindowService(behaviorServiceWrapper.ServiceProviderMock.Object, control);
-        return (behaviorServiceWrapper, service);
+        _service = new(_serviceProviderMock.Object, control);
     }
 
     private void RunInStaThread(Action action)
@@ -96,56 +56,63 @@ public class ToolStripAdornerWindowServiceTests
         }
     }
 
-    private void RunTest(Action test) => RunInStaThread(test);
-
     private TestControl CreateTestControlWithGraphics()
     {
         Bitmap bitmap = new(100, 100);
         Graphics graphics = Graphics.FromImage(bitmap);
-        return new(graphics);
+        return new TestControl(graphics);
+    }
+
+    private class TestControl : Control
+    {
+        private readonly Graphics _graphics;
+
+        public TestControl(Graphics graphics) => _graphics = graphics;
+
+        public Graphics TestGraphics => _graphics;
+
+        public new Point PointToScreen(Point point) => new Point(point.X + 8, point.Y + 31);
     }
 
     [Fact]
-    public void ToolStripAdornerWindowGraphics_BasicTest() => RunTest(() =>
-    {
-        TestControl testControl = CreateTestControlWithGraphics();
-
-        Graphics resultGraphics = testControl.TestGraphics;
-
-        resultGraphics.Should().NotBeNull();
-    });
-
-    [Fact]
-    public void DropDownAdorner_ReturnsAdornerObject() => RunTest(() =>
+    public void DropDownAdorner_ReturnsAdornerObject() => RunInStaThread(() =>
     {
         Adorner adorner = _service.DropDownAdorner;
-
         adorner.Should().NotBeNull();
     });
 
     [Fact]
-    public void ToolStripAdornerWindowGraphics_ReturnsGraphicsObject() => RunTest(() =>
+    public void ToolStripAdornerWindowGraphics_ReturnsGraphicsObject() => RunInStaThread(() =>
     {
         using Graphics graphics = _service.ToolStripAdornerWindowGraphics;
-
         graphics.Should().NotBeNull();
     });
 
     [Fact]
-    public void Dispose_DisposesResourcesCorrectly() => RunTest(() =>
+    public void Dispose_DisposesResourcesCorrectly() => RunInStaThread(() =>
     {
-        _behaviorServiceWrapper.BehaviorService.Adorners.Add(_service.DropDownAdorner);
+        _behaviorService.Adorners.Add(_service.DropDownAdorner);
 
         _service.Dispose();
 
-        _behaviorServiceWrapper.OverlayServiceMock.Verify(o => o.RemoveOverlay(It.IsAny<Control>()), Times.Once);
-        _behaviorServiceWrapper.ServiceProviderMock.Verify(sp => sp.GetService(typeof(BehaviorService)), Times.Once);
-        _behaviorServiceWrapper.ServiceProviderMock.Verify(sp => sp.GetService(typeof(IOverlayService)), Times.Exactly(2));
-        _behaviorServiceWrapper.BehaviorService.Adorners.Cast<Adorner>().Should().NotContain(_service.DropDownAdorner);
+        _overlayServiceMock.Verify(o => o.RemoveOverlay(It.IsAny<Control>()), Times.Once);
+        _serviceProviderMock.Verify(sp => sp.GetService(typeof(BehaviorService)), Times.Once);
+        _serviceProviderMock.Verify(sp => sp.GetService(typeof(IOverlayService)), Times.Exactly(2));
+        _behaviorService.Adorners.Cast<Adorner>().Should().NotContain(_service.DropDownAdorner);
     });
 
     [Fact]
-    public void AdornerWindowPointToScreen_TranslatesPointCorrectly() => RunTest(() =>
+    public void Invalidate_InvokesInvalidateOnAdornerWindow() => RunInStaThread(_service.Invalidate);
+
+    [Fact]
+    public void InvalidateRegion_InvokesInvalidateOnAdornerWindow() => RunInStaThread(() =>
+    {
+        Region region = new(new Rectangle(10, 10, 50, 50));
+        _service.Invalidate(region);
+    });
+
+    [Fact]
+    public void AdornerWindowPointToScreen_TranslatesPointCorrectly() => RunInStaThread(() =>
     {
         TestControl testControl = CreateTestControlWithGraphics();
         Point point = new(10, 20);
@@ -157,7 +124,7 @@ public class ToolStripAdornerWindowServiceTests
     });
 
     [Fact]
-    public void AdornerWindowToScreen_ReturnsCorrectScreenCoordinates() => RunTest(() =>
+    public void AdornerWindowToScreen_ReturnsCorrectScreenCoordinates() => RunInStaThread(() =>
     {
         TestControl control = new TestControl(Graphics.FromImage(new Bitmap(100, 100)));
 
@@ -168,7 +135,7 @@ public class ToolStripAdornerWindowServiceTests
     });
 
     [Fact]
-    public void ControlToAdornerWindow_TranslatesPointCorrectly() => RunTest(() =>
+    public void ControlToAdornerWindow_TranslatesPointCorrectly() => RunInStaThread(() =>
     {
         Control parentControl = new Control { Left = 10, Top = 20 };
         Control control = new Control { Parent = parentControl, Left = 30, Top = 40 };
@@ -180,21 +147,9 @@ public class ToolStripAdornerWindowServiceTests
     });
 
     [Fact]
-    public void Invalidate_InvokesInvalidateOnAdornerWindow() => RunTest(_service.Invalidate);
-
-    [Fact]
-    public void InvalidateRegion_InvokesInvalidateOnAdornerWindow() => RunTest(() =>
-    {
-        Region region = new(new Rectangle(10, 10, 50, 50));
-
-        _service.Invalidate(region);
-    });
-
-    [Fact]
-    public void ProcessPaintMessage_InvokesInvalidateOnAdornerWindow() => RunTest(() =>
+    public void ProcessPaintMessage_InvokesInvalidateOnAdornerWindow() => RunInStaThread(() =>
     {
         Rectangle paintRect = new Rectangle(10, 10, 50, 50);
-
         _service.ProcessPaintMessage(paintRect);
     });
 }
