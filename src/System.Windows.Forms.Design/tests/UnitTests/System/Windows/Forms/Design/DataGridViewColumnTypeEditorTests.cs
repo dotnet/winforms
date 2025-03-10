45e13598829182175463d969f982b96aa2a2ff10
Zheng Li (BEYONDSOFT CONSULTING INC) <v-zhgl@microsoft.com>
2025-03-10 09:25:28 +0800

    Handle FeedBacks

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DataGridViewColumnTypeEditorTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/DataGridViewColumnTypeEditorTests.cs
@@ -1,6 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Drawing.Design;
 using Moq;
 
@@ -51,5 +54,40 @@ public class DataGridViewColumnTypeEditorTests
         serviceProviderMock.Setup(sp => sp.GetService(typeof(IWindowsFormsEditorService))).Returns(editorServiceMock.Object);
 
         _editor.EditValue(null, serviceProviderMock.Object, typeof(DataGridViewTextBoxColumn)).Should().Be(typeof(DataGridViewTextBoxColumn));
+    }
+
+    [Fact]
+    public void EditValue_ShouldReturnOriginalValue_WhenSelectedTypeIsNull()
+    {
+        Mock<IServiceProvider> mockProvider = new();
+        Mock<IWindowsFormsEditorService> mockEditorService = new();
+        Mock<ITypeDescriptorContext> mockContext = new();
+        Mock<IDesignerHost> mockDesignerHost = new();
+        Mock<ITypeDiscoveryService> mockDiscoveryService = new();
+
+        mockProvider.Setup(p => p.GetService(typeof(IWindowsFormsEditorService)))
+                    .Returns(mockEditorService.Object);
+        mockProvider.Setup(p => p.GetService(typeof(IDesignerHost)))
+                    .Returns(mockDesignerHost.Object);
+        mockDesignerHost.Setup(d => d.GetService(typeof(ITypeDiscoveryService)))
+                        .Returns(mockDiscoveryService.Object);
+
+        ArrayList mockTypeCollection = [typeof(DataGridViewTextBoxColumn), typeof(DataGridViewCheckBoxColumn)];
+        mockDiscoveryService.Setup(d => d.GetTypes(typeof(DataGridViewColumn), false))
+                            .Returns(mockTypeCollection);
+
+        DataGridViewTextBoxColumn column = new();
+        SubListBoxItem listBoxItem = new(column);
+        mockContext.Setup(c => c.Instance).Returns(listBoxItem);
+
+        object? result = _editor.EditValue(mockContext.Object, mockProvider.Object, null);
+
+        mockEditorService.Verify(s => s.DropDownControl(It.IsAny<Control>()), Times.Once);
+        result.Should().Be(null);
+    }
+
+    private class SubListBoxItem : DataGridViewColumnCollectionDialog.ListBoxItem
+    {
+        public SubListBoxItem(DataGridViewColumn column) : base(column, null!, null!) { }
     }
 }
