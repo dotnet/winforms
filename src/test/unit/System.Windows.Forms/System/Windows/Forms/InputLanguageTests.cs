8cf8055319331d9a68b1e63d43f3f43ff902a564
Leaf Shi (BEYONDSOFT CONSULTING INC) <v-leafshi@microsoft.com>
2025-03-19 11:56:29 +0800

    Moving OS Condition to SupplementalInputLanguages_TestData for test InputLanguage_FromCulture_SupplementalInputLanguages_Expected

--- src/test/unit/System.Windows.Forms/System/Windows/Forms/InputLanguageTests.cs
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/InputLanguageTests.cs
@@ -132,22 +132,27 @@ public class InputLanguageTests
 
     public static IEnumerable<object[]> SupplementalInputLanguages_TestData()
     {
-        yield return new object[] { "got-Goth", "000C0C00", "Gothic" };
-        yield return new object[] { "jv-Java", "00110C00", "Javanese" };
-        yield return new object[] { "nqo", "00090C00", "N’Ko" };
-        yield return new object[] { "zgh-Tfng", "0000105F", "Tifinagh (Basic)" };
+        // N’Ko input test failed in Windows 11 version 21H2.
+        // This condition should be removed once https://github.com/dotnet/winforms/issues/10150 is resolved.
+        if (OsVersion.IsWindows11_22H2OrGreater())
+        {
+            yield return new object[] { "got-Goth", "000C0C00", "Gothic" };
+            yield return new object[] { "jv-Java", "00110C00", "Javanese" };
+            yield return new object[] { "nqo", "00090C00", "N’Ko" };
+            yield return new object[] { "zgh-Tfng", "0000105F", "Tifinagh (Basic)" };
+        }
+        else
+        {
+            yield return new object[] { "got-Goth", "000C0C00", "Gothic" };
+            yield return new object[] { "jv-Java", "00110C00", "Javanese" };
+            yield return new object[] { "zgh-Tfng", "0000105F", "Tifinagh (Basic)" };
+        }
     }
 
     [Theory]
     [MemberData(nameof(SupplementalInputLanguages_TestData))]
     public void InputLanguage_FromCulture_SupplementalInputLanguages_Expected(string languageTag, string layoutId, string layoutName)
     {
-        // This condition should be removed once https://github.com/dotnet/winforms/issues/10150 is resolved.
-        if (languageTag == "nqo" && !OsVersion.IsWindows11_22H2OrGreater())
-        {
-            return;
-        }
-
         // Also installs default keyboard layout for this language
         // https://learn.microsoft.com/windows-hardware/manufacture/desktop/default-input-locales-for-windows-language-packs
         InstallUserLanguage(languageTag);
