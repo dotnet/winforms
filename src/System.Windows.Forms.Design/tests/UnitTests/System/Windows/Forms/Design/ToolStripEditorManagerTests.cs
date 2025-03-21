3d81c02f919ea0aefb048cd6d363467413566a56
John Qiao (Beyondsoft Corporation) <v-peqiao@microsoft.com>
2025-03-21 02:56:12 -0700

    Updated the code lines to resolve the conversations.

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ToolStripEditorManagerTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ToolStripEditorManagerTests.cs
@@ -6,6 +6,7 @@ using System.ComponentModel.Design;
 using System.Drawing;
 using System.Reflection;
 using System.Windows.Forms.Design.Behavior;
+using Microsoft.CodeAnalysis;
 using Moq;
 
 namespace System.Windows.Forms.Design.Tests;
@@ -23,6 +24,8 @@ public sealed class ToolStripEditorManagerTests : IDisposable
     private readonly object _toolStripEditorControl;
     private readonly Type _toolStripEditorControlType;
     private readonly ConstructorInfo _constructor;
+    private readonly ToolStripItem _toolStripItem;
+    private readonly Mock<DesignerFrame> _mockDesignerFrame;
 
     public ToolStripEditorManagerTests()
     {
@@ -30,7 +33,9 @@ public sealed class ToolStripEditorManagerTests : IDisposable
         _mockSite = new();
         _mockDesignerHost = new();
         _mockComponent = new();
-        _behaviorService = new(_mockServiceProvider.Object, new(_mockSite.Object));
+        _mockDesignerFrame = new(_mockSite.Object) { CallBase = true };
+        _behaviorService = new(_mockServiceProvider.Object, _mockDesignerFrame.Object);
+        _toolStripItem = new ToolStripButton("Sample Button");
 
         _mockComponent.Setup(c => c.Site).Returns(_mockSite.Object);
         _mockSite.Setup(s => s.GetService(typeof(BehaviorService))).Returns(_behaviorService);
@@ -56,6 +61,99 @@ public sealed class ToolStripEditorManagerTests : IDisposable
     }
 
     [Fact]
+    public void Constructor_InitializesBehaviorServiceAndDesignerHost()
+    {
+        _editorManager.Should().NotBeNull();
+
+        BehaviorService? behaviorService = _editorManager.TestAccessor().Dynamic._behaviorService;
+        behaviorService.Should().Be(_behaviorService);
+
+        IDesignerHost? designerHost = _editorManager.TestAccessor().Dynamic._designerHost;
+        designerHost.Should().Be(_mockDesignerHost.Object);
+    }
+
+    [Fact]
+    public void ActivateEditor_ShouldNotAddNewEditor_WhenItemIsNull()
+    {
+        Action act = () => _editorManager.ActivateEditor(null);
+        act.Should().NotThrow();
+    }
+
+    [Fact]
+    public void ActivateEditor_ShouldReturn_WhenItemIsSameAsCurrentItem()
+    {
+        _editorManager.TestAccessor().Dynamic._behaviorService = _behaviorService;
+        _editorManager.TestAccessor().Dynamic._currentItem = _toolStripItem;
+
+        Action act = () => _editorManager.ActivateEditor(_toolStripItem);
+        act.Should().NotThrow();
+
+        ToolStripItem currentItem = _editorManager.TestAccessor().Dynamic._currentItem;
+        currentItem.Should().Be(_toolStripItem);
+    }
+
+    [Fact]
+    public void ActivateEditor_ShouldDeactivateCurrentEditor_WhenEditorIsNotNull()
+    {
+        _editorManager.TestAccessor().Dynamic._behaviorService = _behaviorService;
+        _editorManager.TestAccessor().Dynamic._editor = _toolStripEditorControl;
+        _editorManager.TestAccessor().Dynamic._itemDesigner = new Mock<ToolStripItemDesigner>().Object;
+        _editorManager.TestAccessor().Dynamic._currentItem = new ToolStripButton();
+
+        _editorManager.ActivateEditor(null);
+
+        _behaviorService.AdornerWindowControl.Controls.Cast<Control>().Should().NotContain((Control)_toolStripEditorControl);
+
+        ToolStripTemplateNode editorUI = _editorManager.TestAccessor().Dynamic._editorUI;
+        editorUI.Should().BeNull();
+
+        object? editor = _editorManager.TestAccessor().Dynamic._editor;
+        editor.Should().BeNull();
+
+        ToolStripItem currentItem = _editorManager.TestAccessor().Dynamic._currentItem;
+        currentItem.Should().BeNull();
+
+        bool? isEditorActive = _editorManager.TestAccessor().Dynamic._itemDesigner.IsEditorActive;
+        isEditorActive.Should().BeFalse();
+    }
+
+    [Fact]
+    public void ActivateEditor_ShouldAddNewEditor_WhenItemIsNotNull()
+    {
+        Mock<ToolStrip> mockToolStrip = new();
+        Mock<IComponent> mockComponent = mockToolStrip.As<IComponent>();
+        Mock<ISite> mockSite = new();
+        Mock<IDesignerHost> mockDesignerHost = new();
+        Mock<DesignSurface> mockDesignSurface = new();
+        mockComponent.Setup(c => c.Site).Returns(mockSite.Object);
+        mockSite.Setup(s => s.GetService(typeof(IDesignerHost))).Returns(mockDesignerHost.Object);
+        mockSite.Setup(s => s.GetService(typeof(DesignSurface))).Returns(mockDesignSurface.Object);
+        Mock<Control> mockRootComponent = new();
+        mockDesignerHost.Setup(dh => dh.RootComponent).Returns(mockRootComponent.Object);
+
+        Mock<ToolStripItemDesigner> mockToolStripItemDesigner = new();
+        Mock<ToolStripTemplateNode> mockToolStripTemplateNode = new(MockBehavior.Default, mockComponent.Object, "SampleText");
+        FieldInfo? centerLabelField = typeof(ToolStripTemplateNode).GetField("_centerLabel", BindingFlags.NonPublic | BindingFlags.Instance);
+        centerLabelField?.SetValue(mockToolStripTemplateNode.Object, new ToolStripLabel("Test"));
+        mockToolStripItemDesigner.Setup(d => d.Editor).Returns(mockToolStripTemplateNode.Object);
+        _mockDesignerHost.Setup(dh => dh.GetDesigner(It.IsAny<ToolStripItem>())).Returns(mockToolStripItemDesigner.Object);
+
+        _editorManager.ActivateEditor(_toolStripItem);
+
+        ToolStripItem currentItem = _editorManager.TestAccessor().Dynamic._currentItem;
+        currentItem.Should().Be(_toolStripItem);
+
+        ToolStripTemplateNode editorUI = _editorManager.TestAccessor().Dynamic._editorUI;
+        editorUI.Should().Be(mockToolStripTemplateNode.Object);
+
+        ToolStripItemDesigner itemDesigner = _editorManager.TestAccessor().Dynamic._itemDesigner;
+        itemDesigner.Should().Be(mockToolStripItemDesigner.Object);
+
+        _mockDesignerHost.Verify(dh => dh.GetDesigner(_toolStripItem), Times.Once);
+        mockToolStripItemDesigner.Object.IsEditorActive.Should().BeTrue();
+    }
+
+    [Fact]
     public void CloseManager_ShouldNotThrowException()
     {
         Action act = ToolStripEditorManager.CloseManager;
@@ -76,17 +174,17 @@ public sealed class ToolStripEditorManagerTests : IDisposable
     [Fact]
     public void ToolStripEditorControl_Constructor_InitializesProperties()
     {
-        PropertyInfo? boundsProperty = _toolStripEditorControlType?.GetProperty("Bounds1");
-        PropertyInfo? locationProperty = _toolStripEditorControlType?.GetProperty("Location");
-        PropertyInfo? textProperty = _toolStripEditorControlType?.GetProperty("Text");
-        PropertyInfo? controlsProperty = _toolStripEditorControlType?.GetProperty("Controls");
-        PropertyInfo? sizeProperty = _toolStripEditorControlType?.GetProperty("Size");
+        VerifyProperty("Bounds1", _toolStripEditorControl, _bounds);
+        VerifyProperty(nameof(Location), _toolStripEditorControl, new Point(_bounds.X, _bounds.Y));
+        VerifyProperty(nameof(Text), _toolStripEditorControl, "InSituEditorWrapper");
+        VerifyProperty(nameof(Size), _toolStripEditorControl, new Size(_editorControl.Size.Width, _editorControl.Size.Height));
+        _toolStripEditorControlType?.GetProperty("Controls")?.GetValue(_toolStripEditorControl).Should().BeOfType<Control.ControlCollection>();
+    }
 
-        boundsProperty?.GetValue(_toolStripEditorControl).Should().Be(_bounds);
-        locationProperty?.GetValue(_toolStripEditorControl).Should().Be(new Point(_bounds.X, _bounds.Y));
-        textProperty?.GetValue(_toolStripEditorControl).Should().Be("InSituEditorWrapper");
-        controlsProperty?.GetValue(_toolStripEditorControl).Should().BeOfType<Control.ControlCollection>();
-        sizeProperty?.GetValue(_toolStripEditorControl).Should().Be(new Size(_editorControl.Size.Width, _editorControl.Size.Height));
+    private void VerifyProperty<T>(string propertyName, object targetObject, T expectedValue)
+    {
+        var propertyValue = _toolStripEditorControlType?.GetProperty(propertyName)?.GetValue(targetObject);
+        propertyValue.Should().Be(expectedValue);
     }
 
     [Fact]
@@ -101,15 +199,5 @@ public sealed class ToolStripEditorManagerTests : IDisposable
             Rectangle? actualBounds = boundsProperty.GetValue(_toolStripEditorControl) as Rectangle?;
             actualBounds.Should().Be(newBounds);
         }
-    }
-
-    [Fact]
-    public void ToolStripEditorControl_OnWrappedEditorResize_DoesNotThrow()
-    {
-        EventArgs eventArgs = EventArgs.Empty;
-        MethodInfo? methodInfo = _toolStripEditorControl.GetType().GetMethod("OnWrappedEditorResize", BindingFlags.NonPublic | BindingFlags.Instance);
-
-        Action act = () => methodInfo?.Invoke(_toolStripEditorControl, [_toolStripEditorControl, eventArgs]);
-        act.Should().NotThrow();
     }
 }
