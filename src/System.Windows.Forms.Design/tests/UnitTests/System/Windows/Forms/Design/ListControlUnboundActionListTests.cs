b08cd6d5d1f569e69ca1ccc5b30eef1558131a03
Leaf Shi (BEYONDSOFT CONSULTING INC) <v-leafshi@microsoft.com>
2025-03-27 18:32:14 +0800

    Moving the test case "InvokeItemsDialog_EditValueInvoked_NoException' to UI Integration Tests

--- src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ListControlUnboundActionListTests.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/ListControlUnboundActionListTests.cs
@@ -39,28 +39,4 @@ public sealed class ListControlUnboundActionListTests : IDisposable
         methodItem.Category.Should().Be(SR.ItemsCategoryName);
         methodItem.Description.Should().Be(SR.ListControlUnboundActionListEditItemsDescription);
     }
-
-    [WinFormsFact]
-    public void InvokeItemsDialog_EditValueInvoked_NoException()
-    {
-        using ListView listView = new();
-        using ListViewDesigner listViewDesigner = new();
-        listViewDesigner.Initialize(listView);
-        var actionList = new ListControlUnboundActionList(listViewDesigner);
-        var task = Task.Run(async () =>
-        {
-            await Task.Delay(1000).ConfigureAwait(false);
-            foreach (Form form in Application.OpenForms)
-            {
-                if (form.Text.Contains("ListViewItem"))
-                {
-                    await form.InvokeAsync(new Action(() => form.Dispose())).ConfigureAwait(false);
-                    break;
-                }
-            }
-        });
-
-        Action action = actionList.InvokeItemsDialog;
-        action.Should().NotThrow();
-    }
 }
---
+++ src/test/integration/UIIntegrationTests/ListControlUnboundActionListTests.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using FluentAssertions;
+using System.Windows.Forms.Design;
+
+namespace System.Windows.Forms.UITests;
+
+public class ListControlUnboundActionListTests
+{
+    [WinFormsFact]
+    public void InvokeItemsDialog_EditValueInvoked_NoException()
+    {
+        using ListView listView = new();
+        using ListViewDesigner listViewDesigner = new();
+        listViewDesigner.Initialize(listView);
+        var actionList = new ListControlUnboundActionList(listViewDesigner);
+        var task = Task.Run(async () =>
+        {
+            await Task.Delay(1000).ConfigureAwait(false);
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form.Text.Contains("ListViewItem"))
+                {
+                    await form.InvokeAsync(new Action(() => form.Dispose())).ConfigureAwait(false);
+                    break;
+                }
+            }
+        });
+
+        Action action = actionList.InvokeItemsDialog;
+        action.Should().NotThrow();
+    }
+}
