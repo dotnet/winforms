05d079c504750b760f6b06650e1d52e23be15f95
Leaf Shi <132890443+LeafShi1@users.noreply.github.com>
2025-04-02 18:17:39 -0700

    Add test for TableLayoutPanelCodeDomSerializer (#13212)
    * Add test for TableLayoutPanelCodeDomSerializer
    
    * Delete the result assertion from test "Deserialize_ValidManager_CallsBaseSerializer"

---
+++ src/System.Windows.Forms.Design/tests/UnitTests/System/Windows/Forms/Design/TableLayoutPanelCodeDomSerializerTests.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel.Design.Serialization;
+using Moq;
+
+namespace System.Windows.Forms.Design.Tests.System.Windows.Forms.Design;
+
+public class TableLayoutPanelCodeDomSerializerTests
+{
+    private readonly Mock<IDesignerSerializationManager> _mockManager;
+    private readonly Mock<CodeDomSerializer> _mockBaseSerializer;
+    private readonly TableLayoutPanelCodeDomSerializer _serializer;
+
+    public TableLayoutPanelCodeDomSerializerTests()
+    {
+        _mockManager = new();
+        _mockBaseSerializer = new();
+        _serializer = new();
+        _mockManager
+            .Setup(m => m.GetSerializer(typeof(TableLayoutPanel).BaseType, typeof(CodeDomSerializer)))
+            .Returns(_mockBaseSerializer.Object);
+    }
+
+    [Fact]
+    public void Deserialize_ValidManager_CallsBaseSerializer()
+    {
+        object codeObject = new();
+        _serializer.Deserialize(_mockManager.Object, codeObject);
+
+        _mockBaseSerializer.Verify(s => s.Deserialize(_mockManager.Object, codeObject), Times.Once);
+    }
+
+    [Fact]
+    public void Serialize_TableLayoutPanel_Not_Localizable_ReturnsBaseSerializerResult()
+    {
+        using TableLayoutPanel tableLayoutPanel = new();
+
+        object? result = _serializer.Serialize(_mockManager.Object, tableLayoutPanel);
+
+        _mockBaseSerializer.Verify(s => s.Serialize(_mockManager.Object, tableLayoutPanel), Times.Once);
+        result.Should().Be(_mockBaseSerializer.Object.Serialize(_mockManager.Object, tableLayoutPanel));
+    }
+}
