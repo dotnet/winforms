7f9c487e57959223a9051dc15214e1aab17f0c25
Leaf Shi <132890443+LeafShi1@users.noreply.github.com>
2025-04-09 00:54:19 -0700

    Disable validation of InputLanguage.LayoutName in non-English modes in test InputLanguage_FromCulture_SupplementalInputLanguages_Expected  (#13255)
    * Only run InputLanguage.LayoutName verification in en-US
    
    * Update the assert to verify language.LayoutName in non-english os

--- src/test/unit/System.Windows.Forms/System/Windows/Forms/InputLanguageTests.cs
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/InputLanguageTests.cs
@@ -183,11 +183,20 @@ public class InputLanguageTests
 
     private static void VerifyInputLanguage(InputLanguage language, string languageTag, string layoutId, string layoutName)
     {
-        Assert.NotNull(language);
-        Assert.NotEqual(IntPtr.Zero, language.Handle);
-        Assert.Equal(languageTag, language.Culture.Name);
-        Assert.Equal(layoutId, language.LayoutId);
-        Assert.Equal(layoutName, language.LayoutName);
+        language.Should().NotBeNull();
+        language.Handle.Should().NotBe(IntPtr.Zero);
+        language.Culture.Name.Should().Be(languageTag);
+        language.LayoutId.Should().Be(layoutId);
+
+        if (CultureInfo.InstalledUICulture.Name.StartsWith("en-", StringComparison.OrdinalIgnoreCase))
+        {
+            language.LayoutName.Should().Be(layoutName);
+        }
+        else
+        {
+            language.LayoutName.Should().NotBeNullOrEmpty();
+            language.LayoutName.Should().NotBeEquivalentTo(SR.UnknownInputLanguageLayout);
+        }
     }
 
     private static void VerifyInputLanguage(InputLanguage language)
