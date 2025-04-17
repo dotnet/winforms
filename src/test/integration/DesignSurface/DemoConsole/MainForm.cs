7b5b816b9d9b04a61963f61ca1a43158575552de
Ricardo Bossan (BEYONDSOFT CONSULTING INC) <v-rbossan@microsoft.com>
2025-04-17 14:51:44 -0300

    Unwrap long comment and reindent for better auto formatting

--- src/test/integration/DesignSurface/DemoConsole/MainForm.cs
+++ src/test/integration/DesignSurface/DemoConsole/MainForm.cs
@@ -365,7 +365,10 @@ public partial class MainForm : Form
                         NumericUpDown numericUpDown = surface.CreateControl<NumericUpDown>(new(50, 10), new(10, 10));
                         panel.Controls.Add(numericUpDown);
 
-#if NETCOREAPP // A nested host is required for .NET Framework. See #11866 and https://github.com/dotnet/winforms/issues/13248#issuecomment-2803589449
+                        // A nested host is required for .NET Framework.
+                        // See https://github.com/dotnet/winforms/issues/11866#issuecomment-2673266564
+                        // and https://github.com/dotnet/winforms/issues/13248#issuecomment-2803589449
+#if NETCOREAPP
                         BindingNavigator bindingNavigator = surface.CreateControl<BindingNavigator>(new(0, 0), new(0, 0));
 
                         BindingSource bindingSource = new()
