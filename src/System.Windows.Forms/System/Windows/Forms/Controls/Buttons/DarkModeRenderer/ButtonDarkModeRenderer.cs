93561550e83862ec32d0ae3111e25b9e1be154d2
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2025-05-01 15:38:19 -0700

    First version of Prompt for DarkModeRenderer and class scaffolding.

---
+++ src/System.Windows.Forms/System/Windows/Forms/Controls/Buttons/DarkModeRenderer/ButtonDarkModeRenderer.cs
@@ -0,0 +1,11 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms;
+
+/// <summary>
+///  Provides methods used to render a button control with or without visual styles.
+/// </summary>
+internal static class ButtonDarkModeRenderer
+{
+}
