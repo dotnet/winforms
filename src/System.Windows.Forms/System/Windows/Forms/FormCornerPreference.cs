ed8c70cfead464c3bbaedef7f394e7e357376ffa
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2025-05-06 21:14:07 -0700

    Undo Experimental tagging of Form-APIs..

--- src/System.Windows.Forms/System/Windows/Forms/FormCornerPreference.cs
+++ src/System.Windows.Forms/System/Windows/Forms/FormCornerPreference.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Windows.Forms.Analyzers.Diagnostics;
 using Windows.Win32.Graphics.Dwm;
 
 namespace System.Windows.Forms;
@@ -10,7 +9,6 @@ namespace System.Windows.Forms;
 ///  Specifies the corner preference for a <see cref="Form"/> which can be
 ///  set using the <see cref="Form.FormCornerPreference"/> property.
 /// </summary>
-[Experimental(DiagnosticIDs.ExperimentalDarkMode, UrlFormat = DiagnosticIDs.UrlFormat)]
 public enum FormCornerPreference
 {
     /// <summary>
