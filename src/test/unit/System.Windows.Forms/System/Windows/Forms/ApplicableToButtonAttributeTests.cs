5aa950ab0ef29eb757e6f8c67953151d4001bc30
Zheng Li (BEYONDSOFT CONSULTING INC) <v-zhgl@microsoft.com>
2025-05-14 15:33:04 +0800

    Add code coverage for ApplicableToButtonAttribute

---
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/ApplicableToButtonAttributeTests.cs
@@ -0,0 +1,15 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Controls.Buttons.Tests;
+
+public class ApplicableToButtonAttributeTests
+{
+    [Fact]
+    public void Ctor_Default_CreatesInstance()
+    {
+        ApplicableToButtonAttribute attr = new();
+        attr.Should().NotBeNull();
+        attr.Should().BeAssignableTo<Attribute>();
+    }
+}
