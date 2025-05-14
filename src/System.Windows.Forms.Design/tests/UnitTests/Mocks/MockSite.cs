43e47b486679652b08baafb584441a457d24f478
Ricardo Bossan (BEYONDSOFT CONSULTING INC) (from Dev Box) <v-rbossan@microsoft.com>
2025-05-13 23:55:01 -0300

    Adds more tests

--- src/System.Windows.Forms.Design/tests/UnitTests/Mocks/MockSite.cs
+++ src/System.Windows.Forms.Design/tests/UnitTests/Mocks/MockSite.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #nullable disable
@@ -11,9 +11,9 @@ namespace System.Windows.Forms.Design.Tests.Mocks
 {
     public class MockSite
     {
-        public static Mock<ISite> CreateMockSiteWithDesignerHost(object designerHost)
+        public static Mock<ISite> CreateMockSiteWithDesignerHost(object designerHost, MockBehavior mockBehavior = MockBehavior.Strict)
         {
-            Mock<ISite> mockSite = new(MockBehavior.Strict);
+            Mock<ISite> mockSite = new(mockBehavior);
             mockSite
                 .Setup(s => s.GetService(typeof(IDesignerHost)))
                 .Returns(designerHost);
@@ -51,7 +51,7 @@ namespace System.Windows.Forms.Design.Tests.Mocks
                 .Setup(s => s.GetService(typeof(ToolStripMenuItem)))
                 .Returns(null);
 
-            Mock<IServiceProvider> mockServiceProvider = new(MockBehavior.Strict);
+            Mock<IServiceProvider> mockServiceProvider = new(mockBehavior);
 
             mockSite
                 .Setup(s => s.GetService(typeof(IServiceProvider)))
