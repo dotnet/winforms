20655b6efd42dde93502d9f25a62d4767fee21dc
Zheng Li (BEYONDSOFT CONSULTING INC) <v-zhgl@microsoft.com>
2025-05-23 10:15:25 +0800

    Add code coverage for ImageListConverter & RelatedImageListAttribute

---
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/RelatedImageListAttributeTests.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.Controls.Tests;
+
+public class RelatedImageListAttributeTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("MainImageList")]
+    public void RelatedImageList_ReturnsConstructorValue(string? value)
+    {
+        RelatedImageListAttribute attr = new(value);
+        attr.RelatedImageList.Should().Be(value);
+    }
+}
