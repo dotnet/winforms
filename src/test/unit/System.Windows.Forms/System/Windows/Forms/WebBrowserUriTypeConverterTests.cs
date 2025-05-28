f7669bd554aa27c9a8c9fcbe1cffa260b521be44
Philip Wang <v-philipwang@microsoft.com>
2025-05-28 15:55:58 +0800

    Update

--- src/test/unit/System.Windows.Forms/System/Windows/Forms/WebBrowserUriTypeConverterTests.cs
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/WebBrowserUriTypeConverterTests.cs
@@ -14,16 +14,16 @@ public class WebBrowserUriTypeConverterTests
     [InlineData("www.microsoft.com", "http://www.microsoft.com/")]
     public void WebBrowserUri_ConvertFrom_UriString(string input, string expected)
     {
-        var result = _converter.ConvertFrom(null, CultureInfo.InvariantCulture, input);
+        Uri result = (Uri)_converter.ConvertFrom(null, CultureInfo.InvariantCulture, input)!;
 
         result.Should().BeOfType<Uri>();
-        ((Uri)result!).AbsoluteUri.Should().Be(expected);
+        result.Should().Be(expected);
     }
 
     [Fact]
     public void WebBrowserUri_ConvertFrom_EmptyString_ReturnsNull()
     {
-        var result = _converter.ConvertFrom(null, CultureInfo.InvariantCulture, "");
+        Uri result = (Uri)_converter.ConvertFrom(null, CultureInfo.InvariantCulture, "")!;
 
         result.Should().BeNull();
     }
@@ -31,8 +31,7 @@ public class WebBrowserUriTypeConverterTests
     [Fact]
     public void WebBrowserUri_ConvertFrom_InvalidUri_ThrowsUriFormatException()
     {
-        string invalid = "http://inv@lid uri";
-        Action action = () => _converter.ConvertFrom(null, CultureInfo.InvariantCulture, invalid);
+        Action action = () => _converter.ConvertFrom(null, CultureInfo.InvariantCulture, "http://inv@lid uri");
 
         action.Should().Throw<UriFormatException>();
     }
@@ -41,7 +40,7 @@ public class WebBrowserUriTypeConverterTests
     public void WebBrowserUri_ConvertFrom_AlreadyUri_ReturnsSameUri()
     {
         Uri uri = new("http://www.microsoft.com/");
-        var result = _converter.ConvertFrom(null, CultureInfo.InvariantCulture, uri);
+        Uri result = (Uri)_converter.ConvertFrom(null, CultureInfo.InvariantCulture, uri)!;
 
         result.Should().Be(uri);
     }
