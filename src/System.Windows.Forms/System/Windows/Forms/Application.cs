4804ffcad2833b8f215a47cbf19941730ae54969
Mohmed abdel-fattah <memoarfaa@gmail.com>
2025-06-15 15:47:45 +0300

    Complete theme change notification event at Application.cs

--- src/System.Windows.Forms/System/Windows/Forms/Application.cs
+++ src/System.Windows.Forms/System/Windows/Forms/Application.cs
@@ -724,8 +724,8 @@ public sealed partial class Application
     {
         // First send to all children.
         PInvokeCore.EnumChildWindows(handle, SendThemeChangedRecursive);
-
-        // Then send to ourself.
+        // Send the theme changed message to this window.
+        PInvokeCore.SendMessage(handle, PInvokeCore.WM_SYSCOLORCHANGE);
         PInvokeCore.SendMessage(handle, PInvokeCore.WM_THEMECHANGED);
 
         return true;
