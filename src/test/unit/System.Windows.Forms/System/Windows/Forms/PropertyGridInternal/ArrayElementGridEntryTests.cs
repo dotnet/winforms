e91dcd8165b6155cc155aee5e3b92f8fb0ae963c
v-philipwang <86937911+Philip-Wang01@users.noreply.github.com>
2025-06-16 10:26:13 +0800

    Add unit test for ArrayElementGridEntry.cs file (#13537)
    * Add unit test for ArrayElementGridEntry.cs file

---
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/PropertyGridInternal/ArrayElementGridEntryTests.cs
@@ -0,0 +1,44 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms.PropertyGridInternal.Tests;
+
+public class ArrayElementGridEntryTests
+{
+    private class TestGridEntry : GridEntry
+    {
+        public TestGridEntry(PropertyGrid ownerGrid, GridEntry? parent = null)
+            : base(ownerGrid, parent)
+        {
+            PropertyValue = new int[] { 1, 2, 3 };
+        }
+
+        public override GridItemType GridItemType => GridItemType.Property;
+        public override bool IsValueEditable => true;
+        public override string PropertyLabel => "Test";
+        public override Type? PropertyType => typeof(int[]);
+        public override object? PropertyValue { get; set; }
+        public override bool ShouldRenderReadOnly => false;
+    }
+
+    private static PropertyGrid CreatePropertyGrid() => new();
+
+    [WinFormsFact]
+    public void ArrayElementGridEntry_ConstructorAndProperties()
+    {
+        using PropertyGrid grid = CreatePropertyGrid();
+        TestGridEntry parent = new(grid);
+        ArrayElementGridEntry entry = new(grid, parent, 2);
+
+        entry.Should().NotBeNull();
+        entry.PropertyLabel.Should().Be("[2]");
+        entry.GridItemType.Should().Be(GridItemType.ArrayValue);
+        entry.IsValueEditable.Should().BeTrue();
+        entry.PropertyType.Should().Be(typeof(int));
+        entry.ShouldRenderReadOnly.Should().BeFalse();
+        entry.PropertyValue.Should().Be(3);
+
+        entry.PropertyValue = 10;
+        entry.PropertyValue.Should().Be(10);
+    }
+}
