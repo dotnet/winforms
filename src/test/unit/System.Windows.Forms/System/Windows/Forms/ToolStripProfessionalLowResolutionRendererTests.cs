35307e0171cd92d1a3e679fc043501bf3e280625
Zheng Li (BEYONDSOFT CONSULTING INC) <v-zhgl@microsoft.com>
2025-07-09 17:02:02 +0800

    Add code coverage for ToolStripProfessionalLowResolutionRenderer

---
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/ToolStripProfessionalLowResolutionRendererTests.cs
@@ -0,0 +1,79 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Drawing;
+
+namespace System.Windows.Forms.Tests;
+
+public class ToolStripProfessionalLowResolutionRendererTests
+{
+    private readonly ToolStripProfessionalLowResolutionRenderer _toolStripProfessionalLowResolutionRenderer = new();
+
+    [Fact]
+    public void Ctor_Default_Success()
+    {
+        _toolStripProfessionalLowResolutionRenderer.Should().NotBeNull();
+        _toolStripProfessionalLowResolutionRenderer.Should().BeOfType<ToolStripProfessionalLowResolutionRenderer>();
+        _toolStripProfessionalLowResolutionRenderer.Should().BeAssignableTo<ToolStripProfessionalRenderer>();
+    }
+
+    [WinFormsFact]
+    public void OnRenderToolStripBackground_DoesNotThrow_ForNonDropDown()
+    {
+        using ToolStrip toolStrip = new();
+        using Bitmap bmp = new(10, 10);
+        using Graphics g = Graphics.FromImage(bmp);
+        ToolStripRenderEventArgs args = new(g, toolStrip);
+
+        Action action = () => _toolStripProfessionalLowResolutionRenderer.TestAccessor().Dynamic.OnRenderToolStripBackground(args);
+
+        action.Should().NotThrow();
+    }
+
+    [WinFormsFact]
+    public void OnRenderToolStripBackground_CallsBase_ForDropDown()
+    {
+        using ToolStripDropDown dropDown = new();
+        using Bitmap bmp = new(10, 10);
+        using Graphics g = Graphics.FromImage(bmp);
+        ToolStripRenderEventArgs args = new(g, dropDown);
+
+        Action action = () => _toolStripProfessionalLowResolutionRenderer.TestAccessor().Dynamic.OnRenderToolStripBackground(args);
+
+        action.Should().NotThrow();
+    }
+
+    [WinFormsTheory]
+    [InlineData(typeof(MenuStrip))]
+    [InlineData(typeof(StatusStrip))]
+    public void OnRenderToolStripBorder_DoesNotThrow_ForMenuOrStatusStrip(Type stripType)
+    {
+        using ToolStrip toolStrip = (ToolStrip)Activator.CreateInstance(stripType)!;
+        using Bitmap bmp = new(10, 10);
+        using Graphics g = Graphics.FromImage(bmp);
+        ToolStripRenderEventArgs args = new(g, toolStrip);
+
+        Action action = () => _toolStripProfessionalLowResolutionRenderer.TestAccessor().Dynamic.OnRenderToolStripBorder(args);
+
+        action.Should().NotThrow();
+    }
+
+    [WinFormsTheory]
+    [InlineData(typeof(ToolStripDropDown))]
+    [InlineData(typeof(ToolStrip))]
+    public void OnRenderToolStripBorder_DoesNotThrow_ForVariousToolStrips(Type toolStripType)
+    {
+        using ToolStrip toolStrip = (ToolStrip)Activator.CreateInstance(toolStripType)!;
+        using Bitmap bmp = new(10, 10);
+        using Graphics g = Graphics.FromImage(bmp);
+        ToolStripRenderEventArgs args = new(g, toolStrip);
+
+        Action action = () => _toolStripProfessionalLowResolutionRenderer.TestAccessor().Dynamic.OnRenderToolStripBorder(args);
+
+        action.Should().NotThrow();
+    }
+
+    [Fact]
+    public void RendererOverride_AlwaysReturnsNull() =>
+        _toolStripProfessionalLowResolutionRenderer.RendererOverride.Should().BeNull();
+}
