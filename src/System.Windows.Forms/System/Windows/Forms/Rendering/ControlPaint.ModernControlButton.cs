1adb76d2cdc941b4649d137018784a1e5c675a72
Klaus Löffelmann <klaus.loeffelmann@microsoft.com>
2025-07-21 17:44:07 -0700

    Introduce modern ControlPaint.ControlButtonRenderer (for UpDown controls, DarkMode, FileOpener etc.).

---
+++ src/System.Windows.Forms/System/Windows/Forms/Rendering/ControlPaint.ModernControlButton.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System.Windows.Forms;
+
+public static unsafe partial class ControlPaint
+{
+    [Flags]
+    internal enum ModernControlButton
+    {
+        Empty = 0x0,
+        Up = 0x1,
+        Down = 0x2,
+        UpDown = 0x3,
+        Right = 0x4,
+        Left = 0x8,
+        LeftRight = 0xC,
+        Ellipse = 0x10,
+        SingleBorder = 0x10000,
+        RoundedBorder = 0x20000
+    }
+}
