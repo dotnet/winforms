63bd6a6d3a50e3d79f21b33cbcd606135a8cde86
Zheng Li (BEYONDSOFT CONSULTING INC) <v-zhgl@microsoft.com>
2025-07-30 17:04:29 +0800

    Address FeedBacks

--- src/test/unit/System.Windows.Forms/System/Windows/Forms/RadioButtonBaseAdapterTests.cs
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/RadioButtonBaseAdapterTests.cs
@@ -34,28 +34,27 @@ public class RadioButtonBaseAdapterTests
 
         public new RadioButton Control => base.Control;
 
-        public void CallDrawCheckFlat(PaintEventArgs e, LayoutData layout, Color checkColor, Color checkBackground, Color checkBorder)
+        public void DrawFlatCheckMark(PaintEventArgs e, LayoutData layout, Color checkColor, Color checkBackground, Color checkBorder)
             => DrawCheckFlat(e, layout, checkColor, checkBackground, checkBorder);
 
-        public void CallDrawCheckBackgroundFlat(PaintEventArgs e, Rectangle bounds, Color borderColor, Color checkBackground)
+        public void DrawBackgroundFlatMark(PaintEventArgs e, Rectangle bounds, Color borderColor, Color checkBackground)
             => DrawCheckBackgroundFlat(e, bounds, borderColor, checkBackground);
 
-        public void CallDrawCheckBackground3DLite(PaintEventArgs e, Rectangle bounds, Color checkBackground, ColorData colors, bool disabledColors)
+        public void DrawBackground3DLiteMark(PaintEventArgs e, Rectangle bounds, Color checkBackground, ColorData colors, bool disabledColors)
             => DrawCheckBackground3DLite(e, bounds, checkBackground, colors, disabledColors);
 
-        public void CallDrawCheckOnly(PaintEventArgs e, LayoutData layout, Color checkColor, bool disabledColors)
+        public void DrawCheckOnlyMark(PaintEventArgs e, LayoutData layout, Color checkColor, bool disabledColors)
             => DrawCheckOnly(e, layout, checkColor, disabledColors);
 
-        public ButtonState CallGetState() => GetState();
+        public ButtonState GetStateMark() => GetState();
 
-        public void CallDrawCheckBox(PaintEventArgs e, LayoutData layout)
+        public void DrawCheckBoxMark(PaintEventArgs e, LayoutData layout)
             => DrawCheckBox(e, layout);
 
-        public void CallAdjustFocusRectangle(LayoutData layout)
+        public void AdjustFocusRectangleMark(LayoutData layout)
             => AdjustFocusRectangle(layout);
 
-        public LayoutOptions CallCommonLayout() => CommonLayout();
-
+        public LayoutOptions GetLayoutOptions() => CommonLayout();
         internal override void PaintOver(PaintEventArgs e, CheckState state)
         {
         }
@@ -87,15 +86,24 @@ public class RadioButtonBaseAdapterTests
         return (radioButton, radioButtonBaseAdapter);
     }
 
-    private static bool AnyPixelChanged(Bitmap bmp, Rectangle bounds)
+    /// <summary>
+    ///  Determines whether any pixel within the specified bounds of the bitmap differs from the reference pixel
+    ///  at the top-left corner of the bounds. Used to verify if drawing operations have modified the bitmap.
+    /// </summary>
+    /// <param name="bmp">The bitmap to inspect.</param>
+    /// <param name="bounds">The rectangle area within the bitmap to check for changes.</param>
+    /// <returns>
+    ///  <see langword="true"/> if any pixel in the bounds differs from the reference pixel; otherwise, <see langword="false"/>.
+    /// </returns>
+    private static bool HasPixelChanged(Bitmap bmp, Rectangle bounds)
     {
-        Color reference = bmp.GetPixel(bounds.X, bounds.Y);
+        Color referenceColor = bmp.GetPixel(bounds.X, bounds.Y);
 
         for (int x = bounds.X; x < bounds.Right; x++)
         {
             for (int y = bounds.Y; y < bounds.Bottom; y++)
             {
-                if (bmp.GetPixel(x, y) != reference)
+                if (bmp.GetPixel(x, y) != referenceColor)
                 {
                     return true;
                 }
@@ -114,9 +122,9 @@ public class RadioButtonBaseAdapterTests
         using PaintEventArgs e = new(g, new Rectangle(0, 0, 20, 20));
         LayoutData layout = new(new LayoutOptions());
 
-        radioButtonBaseAdapter.CallDrawCheckFlat(e, layout, Color.Red, Color.Blue, Color.Green);
+        radioButtonBaseAdapter.DrawFlatCheckMark(e, layout, Color.Red, Color.Blue, Color.Green);
 
-        bool anyPixelChanged = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChanged = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChanged.Should().BeTrue("DrawCheckFlat should modify the bitmap.");
     }
@@ -131,9 +139,9 @@ public class RadioButtonBaseAdapterTests
         Rectangle bounds = new(1, 1, 12, 12);
 
         radioButton.Enabled = true;
-        radioButtonBaseAdapter.CallDrawCheckBackgroundFlat(e, bounds, Color.Black, Color.White);
+        radioButtonBaseAdapter.DrawBackgroundFlatMark(e, bounds, Color.Black, Color.White);
 
-        bool anyPixelChangedEnabled = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedEnabled = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedEnabled.Should().BeTrue("drawing the background when enabled should modify the bitmap in the bounds area");
 
@@ -143,9 +151,9 @@ public class RadioButtonBaseAdapterTests
         }
 
         radioButton.Enabled = false;
-        radioButtonBaseAdapter.CallDrawCheckBackgroundFlat(e, bounds, Color.Black, Color.White);
+        radioButtonBaseAdapter.DrawBackgroundFlatMark(e, bounds, Color.Black, Color.White);
 
-        bool anyPixelChangedDisabled = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedDisabled = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedDisabled.Should().BeTrue("drawing the background when disabled should modify the bitmap in the bounds area");
     }
@@ -172,9 +180,9 @@ public class RadioButtonBaseAdapterTests
         };
 
         radioButton.Enabled = true;
-        radioButtonBaseAdapter.CallDrawCheckBackground3DLite(e, bounds, Color.White, colors, disabledColors: false);
+        radioButtonBaseAdapter.DrawBackground3DLiteMark(e, bounds, Color.White, colors, disabledColors: false);
 
-        bool anyPixelChangedEnabled = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedEnabled = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedEnabled.Should().BeTrue("drawing the 3D lite background when enabled should modify the bitmap in the bounds area");
 
@@ -184,9 +192,9 @@ public class RadioButtonBaseAdapterTests
         }
 
         radioButton.Enabled = false;
-        radioButtonBaseAdapter.CallDrawCheckBackground3DLite(e, bounds, Color.White, colors, disabledColors: true);
+        radioButtonBaseAdapter.DrawBackground3DLiteMark(e, bounds, Color.White, colors, disabledColors: true);
 
-        bool anyPixelChangedDisabled = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedDisabled = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedDisabled.Should().BeTrue("drawing the 3D lite background when disabled should modify the bitmap in the bounds area");
     }
@@ -204,24 +212,24 @@ public class RadioButtonBaseAdapterTests
         };
 
         radioButton.Checked = false;
-        radioButtonBaseAdapter.CallDrawCheckOnly(e, layoutData, Color.Black, disabledColors: false);
+        radioButtonBaseAdapter.DrawCheckOnlyMark(e, layoutData, Color.Black, disabledColors: false);
 
-        bool anyPixelChangedUnchecked = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedUnchecked = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedUnchecked.Should().BeFalse("DrawCheckOnly should not modify the bitmap when unchecked.");
 
         radioButton.Checked = true;
         radioButton.Enabled = true;
-        radioButtonBaseAdapter.CallDrawCheckOnly(e, layoutData, Color.Black, disabledColors: false);
+        radioButtonBaseAdapter.DrawCheckOnlyMark(e, layoutData, Color.Black, disabledColors: false);
 
-        bool anyPixelChangedChecked = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedChecked = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedChecked.Should().BeTrue("DrawCheckOnly should modify the bitmap when checked and enabled.");
 
         radioButton.Enabled = false;
-        radioButtonBaseAdapter.CallDrawCheckOnly(e, layoutData, Color.Black, disabledColors: true);
+        radioButtonBaseAdapter.DrawCheckOnlyMark(e, layoutData, Color.Black, disabledColors: true);
 
-        bool anyPixelChangedCheckedDisabled = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChangedCheckedDisabled = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChangedCheckedDisabled.Should().BeTrue("DrawCheckOnly should modify the bitmap when checked and disabled.");
     }
@@ -234,23 +242,23 @@ public class RadioButtonBaseAdapterTests
         radioButton.Checked = false;
         radioButton.Enabled = true;
         radioButton.InvokeOnMouseUp(new MouseEventArgs(MouseButtons.Left, 1, 0, 0, 0));
-        ButtonState state = radioButtonBaseAdapter.CallGetState();
+        ButtonState state = radioButtonBaseAdapter.GetStateMark();
 
         state.Should().Be(ButtonState.Normal);
 
         radioButton.Checked = true;
-        state = radioButtonBaseAdapter.CallGetState();
+        state = radioButtonBaseAdapter.GetStateMark();
 
         state.HasFlag(ButtonState.Checked).Should().BeTrue();
 
         radioButton.Enabled = false;
-        state = radioButtonBaseAdapter.CallGetState();
+        state = radioButtonBaseAdapter.GetStateMark();
 
         state.HasFlag(ButtonState.Inactive).Should().BeTrue();
 
         radioButton.Enabled = true;
         radioButton.InvokeOnMouseDown(new MouseEventArgs(MouseButtons.Left, 1, 0, 0, 0));
-        state = radioButtonBaseAdapter.CallGetState();
+        state = radioButtonBaseAdapter.GetStateMark();
 
         state.HasFlag(ButtonState.Pushed).Should().BeTrue();
     }
@@ -267,9 +275,9 @@ public class RadioButtonBaseAdapterTests
             CheckBounds = new Rectangle(2, 2, 12, 12)
         };
 
-        radioButtonBaseAdapter.CallDrawCheckBox(e, layoutData);
+        radioButtonBaseAdapter.DrawCheckBoxMark(e, layoutData);
 
-        bool anyPixelChanged = AnyPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+        bool anyPixelChanged = HasPixelChanged(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
 
         anyPixelChanged.Should().BeTrue("DrawCheckBox should modify the bitmap.");
     }
@@ -286,11 +294,11 @@ public class RadioButtonBaseAdapterTests
 
         radioButton.Text = string.Empty;
         radioButton.AutoSize = true;
-        radioButtonBaseAdapter.CallAdjustFocusRectangle(layoutData);
+        radioButtonBaseAdapter.AdjustFocusRectangleMark(layoutData);
         layoutData.Focus.Should().Be(layoutData.CheckBounds);
 
         radioButton.AutoSize = false;
-        radioButtonBaseAdapter.CallAdjustFocusRectangle(layoutData);
+        radioButtonBaseAdapter.AdjustFocusRectangleMark(layoutData);
         layoutData.Focus.Should().Be(layoutData.Field);
     }
 
@@ -299,7 +307,7 @@ public class RadioButtonBaseAdapterTests
     {
         (TestRadioButton radioButton, TestRadioButtonBaseAdapter radioButtonBaseAdapter) = CreateAdapter();
         radioButton.CheckAlign = ContentAlignment.BottomRight;
-        LayoutOptions layoutOptions = radioButtonBaseAdapter.CallCommonLayout();
+        LayoutOptions layoutOptions = radioButtonBaseAdapter.CommonLayout();
 
         layoutOptions.CheckAlign.Should().Be(ContentAlignment.BottomRight);
     }
