8e27080fad2264011f4b10ee3507662782180d27
Penal Buffalo <buffalo246@outlook.com>
2025-12-29 12:55:24 -0600

    Update KeyboardToolTipStateMachine.cs

--- src/System.Windows.Forms/System/Windows/Forms/ToolTip/KeyboardToolTipStateMachine.cs
+++ src/System.Windows.Forms/System/Windows/Forms/ToolTip/KeyboardToolTipStateMachine.cs
@@ -247,12 +247,17 @@ internal sealed partial class KeyboardToolTipStateMachine
         return wrapper;
     }
 
-    private void Transit(SmEvent @event, IKeyboardToolTip source)
+    private void Transit(SmEvent @event, IKeyboardToolTip? source)
     {
         bool fullFsmResetRequired = false;
         try
         {
-            ToolTip? toolTip = _toolToTip[source];
+            ToolTip? toolTip = null;
+            if (source != null)
+            {
+                toolTip = _toolToTip[source];
+            }
+
             if ((_currentTool is null || _currentTool.CanShowToolTipsNow()) && toolTip is not null)
             {
                 _currentState = Transition(source, toolTip, @event);
