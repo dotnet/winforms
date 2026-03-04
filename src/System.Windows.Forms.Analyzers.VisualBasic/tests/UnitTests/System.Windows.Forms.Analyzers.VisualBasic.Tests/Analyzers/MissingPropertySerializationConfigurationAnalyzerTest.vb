f943b14745d116b504490ad3bcad47ea4261b843
Shyam Gupta (DevDiv) <shgu@microsoft.com>
2026-03-04 11:26:51 -0800

    - Skipping Analyzer tests on x86 due to OOM exception. - Added '--ignore-exit-code 8' switch in PR builds to avoid build failure as xunit returns exit code 8 when the assembly has no tests to execute.

--- src/System.Windows.Forms.Analyzers.VisualBasic/tests/UnitTests/System.Windows.Forms.Analyzers.VisualBasic.Tests/Analyzers/MissingPropertySerializationConfigurationAnalyzerTest.vb
+++ src/System.Windows.Forms.Analyzers.VisualBasic/tests/UnitTests/System.Windows.Forms.Analyzers.VisualBasic.Tests/Analyzers/MissingPropertySerializationConfigurationAnalyzerTest.vb
@@ -6,6 +6,7 @@ Imports Microsoft.CodeAnalysis.VisualBasic.Testing
 Imports Xunit
 
 <ForceGC()>
+<SkipOnArchitecture(TestArchitectures.X86, "Analyzer tests hit OutOfMemoryException on x86 due to memory-mapped NuGet package extraction")>
 Public Class ControlPropertySerializationDiagnosticAnalyzerTest
 
     Private Const ProblematicCode As String =
