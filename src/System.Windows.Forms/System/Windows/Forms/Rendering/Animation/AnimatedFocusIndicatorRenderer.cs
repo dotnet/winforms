351432c4b295351bb936568e09523734e5a9c745
Simon Zhao (BEYONDSOFT CONSULTING INC) <v-weidzh@microsoft.com>
2026-08-24 13:51:32 +0800

    Increase the bottom height  of textbox when the control is focused.

--- src/System.Windows.Forms/System/Windows/Forms/Rendering/Animation/AnimatedFocusIndicatorRenderer.cs
+++ src/System.Windows.Forms/System/Windows/Forms/Rendering/Animation/AnimatedFocusIndicatorRenderer.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Drawing;
@@ -115,7 +115,7 @@ internal sealed class AnimatedFocusIndicatorRenderer : AnimatedControlRenderer
         focusPath.AddRoundedRectangle(bounds, new Size(cornerSize, cornerSize));
 
         Color color = GetCurrentColor(borderColor, focusColor);
-        using var focusPen = color.GetCachedPenScope(borderThickness);
+        using var focusPen = color.GetCachedPenScope(borderThickness + 1);
         graphics.DrawPath(focusPen, focusPath);
     }
 
