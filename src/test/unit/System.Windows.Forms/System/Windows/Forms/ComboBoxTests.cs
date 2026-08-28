a7ba88d57ecc8343aed7361915e9d4ac46a08f6a
Simon Zhao (BEYONDSOFT CONSULTING INC) <v-weidzh@microsoft.com>
2026-08-28 09:29:36 +0800

    Remove empty line

--- src/test/unit/System.Windows.Forms/System/Windows/Forms/ComboBoxTests.cs
+++ src/test/unit/System.Windows.Forms/System/Windows/Forms/ComboBoxTests.cs
@@ -885,7 +885,6 @@ public class ComboBoxTests
             control.ClientSize.Height - control.ModernFieldPadding.Vertical >= minimumTextHeight);
     }
 
-
     [WinFormsTheory]
     [InlineData(ComboBoxStyle.DropDown)]
     [InlineData(ComboBoxStyle.Simple)]
